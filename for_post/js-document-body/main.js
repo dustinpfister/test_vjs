d4bc718180e27d626f6d26124b175a063a6c18ec
dustin pfister <dustin.pfister@gmail.com>
2019-01-14 20:24:21 -0500

    working document body example

--- for_post/js-document-body/main.js
+++ for_post/js-document-body/main.js
@@ -1,1 +1,11 @@
-var body = document.body;
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d');
+
+// using document body to append a new canvas element
+document.body.appendChild(canvas);
+
+canvas.width = 320;
+canvas.height = 240;
+
+ctx.fillStyle='#000000';
+ctx.fillRect(0,0,canvas.width,canvas.height);

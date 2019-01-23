677434bd5c7f10ab4a0d506d64887bf816549a87
dustinpfister <dustin.pfister@gmail.com>
2019-01-22 20:26:28 -0500

    arguments object example

---
+++ for_post/js-javascript-arguments-object/main.js
@@ -0,0 +1,13 @@
+var func1 = function (a, b) {
+ 
+    if (arguments.length == 2) {
+        return a + b;
+    }
+ 
+    return a;
+ 
+};
+ 
+console.log(func1(40,2)); // 42
+ 
+console.log(func1(42)); // 42

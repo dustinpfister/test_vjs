1872924879d9618fae2d40439918ee0bfd391bb7
dustinpfister <dustin.pfister@gmail.com>
2019-01-25 11:29:27 -0500

    lit

---
+++ for_post/js-javascript-number/lit.js
@@ -0,0 +1,15 @@
+
+// decimal integer and floating points
+console.log( 42 ); // 42
+console.log(42.1234); // 42.1234
+
+// hex (base16) numbers 
+console.log( 0x2a); // 42
+
+// octal (base8) numbers 
+console.log(052); // 42
+console.log(0o52); // 42
+
+// exponents
+console.log(2e3); // 2000
+console.log(2e-1); // 0.2

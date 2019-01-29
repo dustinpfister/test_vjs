884c7c20ceb1e649e64666f219fb10945420f897
dustinpfister <dustin.pfister@gmail.com>
2019-01-29 14:28:13 -0500

    function statement

--- for_post/js-function-expression/hoisting.js
+++ for_post/js-function-expression/hoisting.js
@@ -1,5 +1,5 @@
-
-
+// a function expression where the bar variable
+// is getting hoisted but with the value of undefined
 try {
     bar(); // ERROR calling undefined
 
@@ -13,5 +13,14 @@ var bar = function () {
     return 'foo';
 
 };
-
 console.log(typeof bar); // function
+ 
+// with function statements this is not a problem
+ 
+console.log(foo()); // bar
+ 
+function foo() {
+    return 'bar';
+};
+ 
+console.log(foo()); // bar

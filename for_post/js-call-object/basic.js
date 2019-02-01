410704372aa408cb622907e466c1aa6e82125e42
dustinpfister <dustin.pfister@gmail.com>
2019-02-01 16:26:41 -0500

    basic example

--- for_post/js-call-object/basic.js
+++ for_post/js-call-object/basic.js
@@ -1,11 +1,15 @@
-
-let func = function (a,b) {
+let func = function (a, b) {
  
- let c = 5;
+    let c = 5;
  
- return a + b + c;
+    if (arguments.length >= 2) {
+ 
+        c += 5;
+ 
+    }
+ 
+    return a + b + c;
  
 };
  
-console.log(func(3,2)); // 10 
-
+console.log(func(3, 2)); // 15

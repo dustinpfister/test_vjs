094f802fb42fe5d640433ca09263e225f26135d6
dustinpfister <dustin.pfister@gmail.com>
2019-02-07 09:55:43 -0500

    bool.js

--- for_post/js-javascript-equals/bool.js
+++ for_post/js-javascript-equals/bool.js
@@ -1,10 +1,10 @@
-
 // two booleans
 console.log(false == false); // true
 console.log(!1 == false); // true
 console.log(!-42 == false); // true
 console.log(!0 == false); // false
 console.log(true == false); // false;
+ 
 // type conversion
 console.log(0 == false); // true
 console.log('' == false); // true

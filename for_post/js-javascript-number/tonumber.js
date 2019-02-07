0e36dafe1adea2eccbec9f583dbb7db6c7186dff
dustinpfister <dustin.pfister@gmail.com>
2019-02-07 14:09:14 -0500

    tonumber

--- for_post/js-javascript-number/tonumber.js
+++ for_post/js-javascript-number/tonumber.js
@@ -1,3 +1,6 @@
-let str = '42';
+let str = '42.2';
 
 console.log(typeof (str * 1)); // number
+console.log(typeof parseInt(str)); // number
+console.log(typeof parseFloat(str)); // number
+console.log(typeof Number(str)); // number

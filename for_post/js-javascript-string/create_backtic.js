8c7fae8b5d6bd65e80a84e00bc3eb985af345157
dustinpfister <dustin.pfister@gmail.com>
2019-02-08 18:18:04 -0500

    js-javascript-string folder

---
+++ for_post/js-javascript-string/create_backtic.js
@@ -0,0 +1,6 @@
+let getN = () => {
+    return 17;
+};
+let str = `n=${getN()}`;
+
+console.log(str); // 'n=17'
---
+++ for_post/js-javascript-string/create_basic.js
@@ -0,0 +1,5 @@
+let str = 'foobar';
+ 
+console.log(typeof str); // string
+console.log(str.constructor.name); // String
+console.log(str); // 'foobar'

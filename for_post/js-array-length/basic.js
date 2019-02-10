96d6e6a583033bc6838e1c883c246562fa173aa6
dustinpfister <dustin.pfister@gmail.com>
2019-02-10 14:41:29 -0500

    js array length

---
+++ for_post/js-array-length/basic.js
@@ -0,0 +1,11 @@
+var a = [1,2,3];
+console.log(a.length); // 3
+ 
+var a = Array.from({
+        0: 'a',
+        1: 'b',
+        2: 'c',
+        length: 3
+    });
+ 
+console.log(a.length); // 3
---
+++ for_post/js-array-length/negindex.js
@@ -0,0 +1,6 @@
+var a = ['a','b','c'];
+ 
+a[-1] = '!';
+ 
+console.log(a.length); // 3
+console.log(Object.keys(a).length); // 4

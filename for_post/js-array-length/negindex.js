7b6e417b50e0d162692375297e006d2c4ea8e438
dustinpfister <dustin.pfister@gmail.com>
2019-02-10 16:06:37 -0500

    update neg index

--- for_post/js-array-length/negindex.js
+++ for_post/js-array-length/negindex.js
@@ -1,6 +1,16 @@
-var a = ['a','b','c'];
- 
-a[-1] = '!';
+var a = Array.from({
+        0: 2,
+        1: 3,
+        2: 4,
+        length: 3
+    });
+a[-1] = 1;
  
 console.log(a.length); // 3
-console.log(Object.keys(a).length); // 4
+ 
+var b = ['a', 'b', 'c'];
+ 
+b[-1] = '!';
+
+console.log(b.length); // 3
+console.log(Object.keys(b).length); // 4

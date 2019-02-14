3bfca51bb7ecaadfdad995d4ad56ff26932f1780
dustinpfister <dustin.pfister@gmail.com>
2019-02-14 11:43:09 -0500

    js javascript date folder

--- for_post/js-touch-events/startendmove.js
+++ for_post/js-touch-events/startendmove.js
@@ -1,15 +1,15 @@
-
 // Get the canvas and 2d context
 var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
-
+ 
+// clear
 var cls = function () {
     // fill black
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
-
-// get first
+ 
+// get first pointer object
 var getFirst = function (e) {
     var bx = e.target.getBoundingClientRect();
     return {
@@ -17,7 +17,7 @@ var getFirst = function (e) {
         y: e.touches[0].clientY
     }
 };
-
+ 
 var touchStart = function (e) {
     var f = getFirst(e);
     e.preventDefault();
@@ -27,7 +27,7 @@ var touchStart = function (e) {
     ctx.arc(f.x, f.y, 15, 0, Math.PI * 2);
     ctx.stroke();
 };
-
+ 
 var touchMove = function (e) {
     var f = getFirst(e);
     e.preventDefault();
@@ -36,14 +36,14 @@ var touchMove = function (e) {
     ctx.arc(f.x, f.y, 15, 0, Math.PI * 2);
     ctx.stroke();
 };
-
+ 
 var touchEnd = function (e) {
     console.log(e.timeStamp);
 };
-
-// attach a touch start event for the canvas
+ 
+// attach a touch events
 canvas.addEventListener('touchstart', touchStart);
 canvas.addEventListener('touchend', touchEnd);
 canvas.addEventListener('touchmove', touchMove);
-
+ 
 cls();

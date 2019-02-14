008faca3ad300163b29febffbd73c04241271463
dustinpfister <dustin.pfister@gmail.com>
2019-02-14 12:14:34 -0500

    from number

--- for_post/js-javascript-date/basic.js
+++ for_post/js-javascript-date/basic.js
@@ -1,4 +1,4 @@
-let now = new Date(2019,1,14);
-console.log(typeof now); // 'object'
-console.log(now.constructor.name); // 'Date'
-console.log(now.toDateString()); // 'Thu Feb 14 2019'
+let d = new Date(2019,1,14);
+console.log(typeof d); // 'object'
+console.log(d.constructor.name); // 'Date'
+console.log(d.toDateString()); // 'Thu Feb 14 2019'
---
+++ for_post/js-javascript-date/fromnumber.js
@@ -0,0 +1,4 @@
+let z = new Date(0);
+console.log( z.toDateString() ); // Wed Dec 31 1969
+console.log( z.getTime()); // 0
+console.log( z.getTimezoneOffset()); // 300

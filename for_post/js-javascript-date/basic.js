1dc9a22c1fbb47c7bc9c154629efacfc9de72876
dustinpfister <dustin.pfister@gmail.com>
2019-02-14 11:50:28 -0500

    better basic example

--- for_post/js-javascript-date/basic.js
+++ for_post/js-javascript-date/basic.js
@@ -1,2 +1,4 @@
-let now = new Date();
-console.log(now.getFullYear());
+let now = new Date(2019,1,14);
+console.log(typeof now); // 'object'
+console.log(now.constructor.name); // 'Date'
+console.log(now.toDateString()); // 'Thu Feb 14 2019'

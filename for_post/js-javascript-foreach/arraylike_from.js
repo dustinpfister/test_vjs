3406a5b80c55e0852448b46d778922ed5fc63147
dustinpfister <dustin.pfister@gmail.com>
2019-02-16 15:09:53 -0500

    arraylike call

--- for_post/js-javascript-foreach/arraylike_from.js
+++ for_post/js-javascript-foreach/arraylike_from.js
@@ -1,11 +1,11 @@
-var obj = {0:1, 1:2, 2:3, length: 3};
-
+let obj = {0:1, 1:2, 2:3, length: 3};
+ 
 // Using Array.from can help change an array like
 // object into an array
-var arr = Array.from(obj);
+let arr = Array.from(obj);
 console.log(arr.constructor.name); // 'Array'
-
+ 
 // Now we have the methods
-var sum = 0;
+let sum = 0;
 arr.forEach((n)=>{ sum += n; });
 console.log(sum); // 6

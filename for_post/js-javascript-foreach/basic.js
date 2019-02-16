76ffd5b44f2b2bac0f3d5479ab4aafeb7989de1b
dustinpfister <dustin.pfister@gmail.com>
2019-02-16 12:24:17 -0500

    js-javascript-foreach

---
+++ for_post/js-javascript-foreach/basic.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3],
+sum = 0;
+arr.forEach((n) => {
+    sum += n;
+});
+console.log(sum); // 6
---
+++ for_post/js-javascript-foreach/basic_ecma5.js
@@ -0,0 +1,6 @@
+var arr = [1, 2, 3],
+sum = 0;
+arr.forEach(function(n){
+    sum += n;
+});
+console.log(sum); // 6

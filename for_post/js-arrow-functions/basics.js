a01fd0adee2476288923eca52520ce68c09e9232
dustinpfister <dustin.pfister@gmail.com>
2019-02-18 16:08:28 -0500

    basics

---
+++ for_post/js-arrow-functions/basics.js
@@ -0,0 +1,11 @@
+let foo = _ => 'bar';
+ 
+let bar = ()=> 'foobar';
+ 
+let pow = n => 'pow: '+ Math.pow(2,n);
+
+let est = (s,c) => {
+    return s / c * 6.5 
+};
+console.log(foo()); // 'bar'
+console.log(foo(4)); // 'pow: 16'

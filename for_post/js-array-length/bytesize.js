07d671f7362f71368f5b9674efdc692a55c2a493
dustinpfister <dustin.pfister@gmail.com>
2019-02-19 18:52:36 -0500

    byte length

--- for_post/js-array-length/bytesize.js
+++ for_post/js-array-length/bytesize.js
@@ -1,5 +1,13 @@
-var a = [1,2,25];
+var a = [255,25,257];
 
 var buff = Uint8Array.from(a);
 
-console.log(buff);
+console.log(buff); // [255,25,1]
+console.log(buff.length); // 3
+console.log(buff.byteLength); // 3
+
+var buff = Uint16Array.from(a);
+
+console.log(buff); // [255,25,257]
+console.log(buff.length); // 3
+console.log(buff.byteLength); // 6

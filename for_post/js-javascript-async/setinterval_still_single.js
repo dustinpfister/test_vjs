d6ef7d53e15b1da8e2a2c295bffe75ce04799440
dustinpfister <dustin.pfister@gmail.com>
2019-02-23 11:40:08 -0500

    setinterval comments

--- for_post/js-javascript-async/setinterval_still_single.js
+++ for_post/js-javascript-async/setinterval_still_single.js
@@ -1,3 +1,8 @@
+// set interval and set timeout might be async in nature
+// but by using them you are still working within the same
+// execution context. So it is still possible for one process
+// to be blocked by another if one is doing something heavy
+
 // doing something that will take a while
 var heavy = function () {
     var i = Math.pow(2, 30),
@@ -13,5 +18,10 @@ var notHeavy = function () {
     console.log('tick');
 }
  
-setInterval(heavy, 100);
+setInterval(heavy, 1000);
 setInterval(notHeavy, 100);
+
+// setting the delay to 100ms for notHeavy will not always result in
+// the method executing at that frequency. If another method that is doing
+// some heavy lifting of some kind is executing in the same event loop, the
+// not heavy method will be delayed.

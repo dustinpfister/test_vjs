93064e741eedb7e8d8da97fb48f2644993141e9b
dustinpfister <dustin.pfister@gmail.com>
2019-02-25 16:33:25 -0500

    types

---
+++ for_post/js-javascript-if/types.js
@@ -0,0 +1,23 @@
+var n = 42;
+
+// with brackets
+if (n === 42) {
+    console.log('the answer');
+}
+// > 'the answer'
+
+// without
+if (n >= 40)
+    console.log('the answer');
+// > 'the answer'
+
+if (n === '42') {
+    console.log('the answser is a string');
+} else {
+    if (n === 42) {
+        console.log('the answer is a number.')
+    } else {
+        console.log('no answer');
+    }
+}
+// > 'the answer is a number'

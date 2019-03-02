5646326c135d0d4d3b2b1acdb1a08bc991f3b050
dustinpfister <dustin.pfister@gmail.com>
2019-03-02 12:38:12 -0500

    basic2 looking good

--- for_post/js-javascript-return/basic2.js
+++ for_post/js-javascript-return/basic2.js
@@ -2,11 +2,13 @@ var figMoney = function (score, compare, rpm, imp, ctr) {
     ctr = ctr === undefined ? 0.5 : ctr;
     imp = imp === undefined ? 6.5 : imp;
     rpm = rpm === undefined ? 1.5 : rpm;
-    compare = compare === undefined ? 1 : rpm;
-    score = score === undefined ? 0 : rpm;
+    compare = compare === undefined ? 1 : compare;
+    score = score === undefined ? 0 : score;
 
     // lengthly expression
-    return score / compare * imp * ctr * rpm;
+    return '$' + (score / compare * imp * ctr * rpm).toFixed(2);
 };
 
-console.log( figMoney() ); // 0
+console.log( figMoney() ); // '$0.00'
+console.log( figMoney(77, 5, 2) ); // '$100.10'
+console.log( figMoney(7, 5, 1.4,6.5,0.12) ); // '$1.53'

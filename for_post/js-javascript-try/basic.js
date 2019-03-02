326d6973b80cdd54141a4d315ccc9baefd1cb134
dustinpfister <dustin.pfister@gmail.com>
2019-03-02 11:55:55 -0500

    added an object for the basic example

--- for_post/js-javascript-try/basic.js
+++ for_post/js-javascript-try/basic.js
@@ -1,7 +1,11 @@
-var str = '{\"n\":\"foo\"';
+var str = '{\"n\":\"27\"',
+obj = {
+    n: 42
+};
 
 try {
-    JSON.parse(str);
+    obj = JSON.parse(str);
 } catch (e) {
     console.log(e.message); // Unexpected end of JSON input
 }
+console.log(obj.n); // 42
--- for_post/js-javascript-try/fin.js
+++ for_post/js-javascript-try/fin.js
@@ -1,21 +1,12 @@
-
-
-
 try {
     try {
-
         throw new Error('My custom Error');
-
     }
     finally {
-
         console.log('finally');
     }
-
 } catch (e) {
-
     console.log(e.message);
-
 }
 
 // 'finally'

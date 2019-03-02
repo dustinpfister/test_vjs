160e561a7ddbeca04ff97dc5aa3591460b727e3d
dustinpfister <dustin.pfister@gmail.com>
2019-03-02 11:32:12 -0500

    fin

---
+++ for_post/js-javascript-try/fin.js
@@ -0,0 +1,22 @@
+
+
+
+try {
+    try {
+
+        throw new Error('My custom Error');
+
+    }
+    finally {
+
+        console.log('finally');
+    }
+
+} catch (e) {
+
+    console.log(e.message);
+
+}
+
+// 'finally'
+// 'My custom Error'

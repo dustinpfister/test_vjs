303d16db8761d22c110f4b10f292e261ce35e9ab
dustinpfister <dustin.pfister@gmail.com>
2019-03-05 16:37:02 -0500

    exp curve

---
+++ for_post/canvas-line/drawpoints.js
@@ -0,0 +1,14 @@
+// draw a polygon for the given context
+var drawPoints = function (ctx, points,close) {
+    var i = 2,
+    len = points.length;
+    ctx.beginPath();
+    ctx.moveTo(points[0], points[1]);
+    while (i < len) {
+        ctx.lineTo(points[i], points[i + 1]);
+        i += 2;
+    }
+    if(close){ctx.closePath();}
+    ctx.stroke();
+
+};

0eeca14606d175a164b0e4cced8662fe915f35c6
dustinpfister <dustin.pfister@gmail.com>
2019-03-12 19:08:43 -0400

    api-function

---
+++ for_post/js-javascript-module/api-function.js
@@ -0,0 +1,23 @@
+var mod = (function () {
+    // return a function
+    return function (point) {
+        return {
+            move: function (dx, dy) {
+                point.x += dx;
+                point.y += dy;
+            },
+            print: function () {
+                console.log('(' + point.x + ',' + point.y + ')')
+            }
+        }
+    };
+}
+    ());
+
+var pt = mod({
+        x: 42,
+        y: -12
+    });
+
+pt.move(3, 12);
+pt.print();

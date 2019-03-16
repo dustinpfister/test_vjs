e9f7a9657ace8c6a9790d56aea963aa177a72276
dustinpfister <dustin.pfister@gmail.com>
2019-03-16 12:34:51 -0400

    game1 example started

--- for_post/canvas-game/game1.js
+++ for_post/canvas-game/game1.js
@@ -1,3 +1,27 @@
 var canvas = document.getElementById('gamearea'),
 ctx = canvas.getContext('2d');
- 
+
+var ship = {
+    x: 5,
+    y: 12,
+    dx: 1,
+    dy: 1
+};
+
+var update = function () {
+
+    ship.x += ship.dx;
+    ship.y += ship.dy;
+
+};
+
+var draw = function () {};
+
+var loop = function () {
+
+    requestAnimationFrame(loop);
+
+    update();
+    draw();
+
+};

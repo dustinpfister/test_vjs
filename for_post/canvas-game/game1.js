554316ac5e0b91ccdd49f54f425874450e438cd1
dustinpfister <dustin.pfister@gmail.com>
2019-03-16 12:38:58 -0400

    drawing ship

--- for_post/canvas-game/game1.js
+++ for_post/canvas-game/game1.js
@@ -2,26 +2,30 @@ var canvas = document.getElementById('gamearea'),
 ctx = canvas.getContext('2d');
 
 var ship = {
-    x: 5,
-    y: 12,
-    dx: 1,
-    dy: 1
+    x: 160-16,
+    y: 120-16,
+    dx: 0,
+    dy: 0
 };
 
 var update = function () {
-
     ship.x += ship.dx;
     ship.y += ship.dy;
-
 };
 
-var draw = function () {};
+var draw = function () {
+    // black background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // draw ship
+    ctx.fillStyle='red';
+    ctx.fillRect(ship.x,ship.y,32,32);
+};
 
 var loop = function () {
-
     requestAnimationFrame(loop);
-
     update();
     draw();
-
 };
+
+loop();

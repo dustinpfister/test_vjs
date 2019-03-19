7d394ec2102cea1feab22033439c577a08434a80
dustinpfister <dustin.pfister@gmail.com>
2019-03-19 12:10:54 -0400

    rename box

--- for_post/canvas-game/game1.js
+++ for_post/canvas-game/game1.js
@@ -1,11 +1,14 @@
+// get the canvas and context
 var canvas = document.getElementById('gamearea'),
 ctx = canvas.getContext('2d');
-
+ 
+// set native size of the canvas
 canvas.width = 320;
 canvas.height = 240;
-
-// a simple state
-var ship = {
+ 
+// a simple state that is just a single object
+// that will be the moving box
+var bx = {
     x: 144,
     y: 104,
     a: 0,
@@ -20,17 +23,17 @@ var mod = function (x, m) {
 // an update loop for the state
 var update = function () {
 
-    var secs = (new Date() - ship.lastTick) / 1000;
-    ship.lastTick = new Date();
+    var secs = (new Date() - bx.lastTick) / 1000;
+    bx.lastTick = new Date();
 
-    var dx = Math.cos(ship.a) * ship.pps * secs;
-    var dy = Math.sin(ship.a) * ship.pps * secs;
+    var dx = Math.cos(bx.a) * bx.pps * secs;
+    var dy = Math.sin(bx.a) * bx.pps * secs;
 
-    ship.x += dx;
-    ship.y += dy;
+    bx.x += dx;
+    bx.y += dy;
 
-    ship.x = mod(ship.x, canvas.width - 32);
-    ship.y = mod(ship.y, canvas.height - 32);
+    bx.x = mod(bx.x, canvas.width - 32);
+    bx.y = mod(bx.y, canvas.height - 32);
 
 };
 
@@ -39,19 +42,19 @@ var draw = function () {
     // black background
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // draw ship
+    // draw bx
     ctx.fillStyle = 'red';
-    ctx.fillRect(ship.x, ship.y, 32, 32);
+    ctx.fillRect(bx.x, bx.y, 32, 32);
 };
 
 canvas.addEventListener('mousedown', function (e) {
 
-    var bx = e.target.getBoundingClientRect(),
-    x = e.clientX - bx.left,
-    y = e.clientY - bx.top;
+    var bb = e.target.getBoundingClientRect(),
+    x = e.clientX - bb.left,
+    y = e.clientY - bb.top;
 
-    // using Math.atan2 to set ship angle
-    ship.a = Math.atan2(y - canvas.width / 2, x - canvas.height / 2);
+    // using Math.atan2 to set bx angle
+    bx.a = Math.atan2(y - canvas.width / 2, x - canvas.height / 2);
 
 });
 

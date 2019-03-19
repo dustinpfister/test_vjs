a1ad087cbbfdc7ffa969e9155e628d6955bd83bd
dustinpfister <dustin.pfister@gmail.com>
2019-03-19 15:01:53 -0400

    drawing points

--- for_post/js-math-atan2/atan2-1.js
+++ for_post/js-math-atan2/atan2-1.js
@@ -4,13 +4,36 @@ ctx = canvas.getContext('2d');
 canvas.width = 320;
 canvas.height = 240;
 
+var state = {
+    toPoint: {
+        x: canvas.width / 2 + 50,
+        y: canvas.height / 2 + 50
+    },
+    fromPoint: {
+        x: canvas.width / 2,
+        y: canvas.height / 2
+    }
+};
+
 // UPADTE
 var update = function () {};
 
 // DRAW
+var drawPoint = function (point, style) {
+    style = style || 'red';
+    ctx.beginPath();
+    ctx.strokeStyle = style;
+    ctx.arc(point.x, point.y, 15, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.stroke();
+};
+
 var draw = function () {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawPoint(state.fromPoint, 'white');
+    drawPoint(state.toPoint, 'red');
 };
 
 // INPUT

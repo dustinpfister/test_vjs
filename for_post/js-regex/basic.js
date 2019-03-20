7e1b859ad5dd3d6478f99a8ee3e376a7a83d8efd
dustinpfister <dustin.pfister@gmail.com>
2019-03-20 19:54:40 -0400

    basic example

--- for_post/js-regex/basic.js
+++ for_post/js-regex/basic.js
@@ -1,5 +1,3 @@
 let data = 'regexp is **good** for pattern matching tasks',
-
 change = data.replace(/\*\*good\*\*/, 'great');
-
 console.log(change); // 'regexp is great for pattern matching tasks'

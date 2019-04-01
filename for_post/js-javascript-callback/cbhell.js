bb59cbae453dab484c980705b25c4d0d7a3434bd
dustinpfister <dustin.pfister@gmail.com>
2019-04-01 15:42:23 -0400

    cbhell example working

--- for_post/js-javascript-callback/cbhell.js
+++ for_post/js-javascript-callback/cbhell.js
@@ -3,12 +3,31 @@ path = require('path');
 
 let root = path.resolve('./');
 
+// read a root dir for files (using a callback)
 fs.readdir(root, function (e, files) {
 
     if (files) {
+
+        // for each file
         files.forEach(function (file) {
             let dir = path.join(root, file);
-            console.log(dir);
+
+            // get stats for a file (another callback)
+            fs.stat(dir, function (e, stats) {
+
+                if (stats.isFile() && path.extname(dir).toLowerCase() === '.js') {
+
+                    // read file (yet another callback)
+                    fs.readFile(dir, function (err, data) {
+
+                        console.log(data.toString());
+
+                    })
+
+                }
+
+            });
+
         });
     }
 

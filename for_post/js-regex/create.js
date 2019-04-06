b0df94febdc0636542ad2ea16c1d72b78ae9abd8
dustinpfister <dustin.pfister@gmail.com>
2019-04-06 15:44:45 -0400

    finished create example

--- for_post/js-regex/create.js
+++ for_post/js-regex/create.js
@@ -1,4 +1,11 @@
 // when using the RegEx constructor backslashes must be doubled up.
 let pat_datfile = new RegExp('dat_\\d+\\.json','gi');
 
-console.log('dat_20120822.json'.match(pat_datfile));
+console.log('filename: dat_20120822.json'.match(pat_datfile)[0]);
+// 'dat_20120822.json'
+
+// or the literal syntax can be used.
+let pat_datfile_lit = /dat_\d+.json/gi;
+
+console.log('-- dat_20120822.json -- dat_2013.json'.match(pat_datfile)[1]);
+// 'dat_2013.json'

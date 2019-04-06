9a7d40ddf7c4571428b3c2890ac3b8ba5542f6d1
dustinpfister <dustin.pfister@gmail.com>
2019-04-06 16:14:53 -0400

    js-string-match

---
+++ for_post/js-string-match/basic.js
@@ -0,0 +1,5 @@
+let patt = /dat_\d+.json/gi,
+str = 'Here is one file called dat_2017.json, and another called dat_2018.json',
+m = str.match(patt);
+console.log(m);
+[ 'dat_2017.json', 'dat_2018.json' ]

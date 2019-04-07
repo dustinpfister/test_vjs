b4a064ed081283306ef9139e73157459d8d9963c
dustinpfister <dustin.pfister@gmail.com>
2019-04-07 10:50:01 -0400

    basic

--- for_post/js-array-length/basic.js
+++ for_post/js-array-length/basic.js
@@ -1,6 +1,9 @@
 var a = [1,2,3];
 console.log(a.length); // 3
- 
+
+console.log( a[0]); // 1
+console.log( a[2]); // 3
+
 var a = Array.from({
         0: 'a',
         1: 'b',
--- for_post/js-array-length/basic_set.js
+++ for_post/js-array-length/basic_set.js
@@ -1,5 +1,3 @@
 let a = [1,2,3,4,5];
-
 a[10] = 10;
-
 console.log(a.length); // 11

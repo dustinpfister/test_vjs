04615d7204feecd44c1d4e237a37654fd747c332
dustinpfister <dustin.pfister@gmail.com>
2019-04-07 13:50:25 -0400

    delete

--- for_post/js-array-length/delete.js
+++ for_post/js-array-length/delete.js
@@ -1,8 +1,20 @@
+// delete
 let a = [1,2,3];
 
-console.log(a.length);
+console.log(a.length); // 3
 
 delete a[2];
 
 console.log(a[2]); // undefined
-console.log(a.length);
+console.log(a.length); // 3
+
+// set undefined
+let b = [1,2,3];
+
+console.log(b.length); // 3
+
+b[2] = undefined;
+
+console.log(b[2]); // undefined
+console.log(b.length); // 3
+

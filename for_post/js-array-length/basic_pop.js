058538fdc5f98b4f9f0c51f4bedaa149874fa037
dustinpfister <dustin.pfister@gmail.com>
2019-04-07 10:18:50 -0400

    basic pop

---
+++ for_post/js-array-length/basic_pop.js
@@ -0,0 +1,7 @@
+let a = [1,2,3,4,5],
+i = a.length;
+while (i--) {
+    a.pop();
+    console.log(a.length);
+    // 4 3 2 1 0
+}
--- for_post/js-array-length/basic_push.js
+++ for_post/js-array-length/basic_push.js
@@ -2,10 +2,8 @@ let a = [],
 count = 5,
 i = 0;
 while (i < count) {
-
     a.push(Math.pow(2, i));
     console.log(a.length);
     // 1 2 3 4 5
-
     i += 1;
 }

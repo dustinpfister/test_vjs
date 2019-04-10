466b0825cf06109e9904391cb1b2c565161ef5f6
dustinpfister <dustin.pfister@gmail.com>
2019-04-10 18:58:07 -0400

    indexof

---
+++ for_post/js-string-match/indexof.js
@@ -0,0 +1,6 @@
+let str= 'okay so here is a foo, and another foo, and foo foo';
+
+// returns the index of the first instance of the 
+// string 'foo', or -1 if it is not found
+console.log(str.indexOf('foo')); // 18
+console.log('bar'.indexOf('foo')); // -1

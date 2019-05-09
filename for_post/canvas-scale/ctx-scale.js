921bf082c7be7846f5b2b8c59f300116db912c7d
dustinpfister <dustin.pfister@gmail.com>
2019-05-09 19:49:05 -0400

    canvas scale change

--- for_post/canvas-scale/ctx-scale.js
+++ for_post/canvas-scale/ctx-scale.js
@@ -2,18 +2,26 @@ var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
 
 // sets the actual native size of the canvas
-canvas.width = 64;
-canvas.height = 48;
+canvas.width = 40;
+canvas.height = 40;
 
 // Scales the canvas via in-line css
-canvas.style.width = '640px';
-canvas.style.height = '480px';
+canvas.style.width = '100px';
+canvas.style.height = '100px';
+
+ctx.fillStyle='grey';
+ctx.fillRect(0,0,canvas.width,canvas.height)
+
+// red rect is 1/4 the size of the canvas
+ctx.strokeStyle = 'red';
+ctx.strokeRect(0, 0, 20, 20);
 
 // adds a scaling transformation
+ctx.save();
 ctx.scale(.5,.5);
 ctx.fillStyle = 'black';
-ctx.fillRect(5, 5, 16, 16);
+// black rect is 1/16 the size of the canvas
+ctx.fillRect(0, 0, 20, 20);
+console.log(canvas)
+ctx.restore();
 
-ctx.scale(2,2);
-ctx.strokeStyle = 'red';
-ctx.strokeRect(5, 5, 16, 16);

a547b74c5304bf6fb6a1d03106424f3381b7583c
dustinpfister <dustin.pfister@gmail.com>
2019-05-09 15:45:20 -0400

    on blur bubble update

--- for_post/js-onblur/bubble.js
+++ for_post/js-onblur/bubble.js
@@ -1,23 +1,11 @@
-var container = document.getElementById('container');
+var container = document.getElementById('container'),
+out = document.getElementById('out');
 
 // when focusout and focusin are attached to the container
 // of an input element they will work because of bubbling
 container.addEventListener('focusout', function (e) {
-    e.target.value = 'focus out';
+    out.innerText = 'Focused out from ' + e.target.id + ' in the container';
 });
 container.addEventListener('focusin', function (e) {
-    e.target.value = 'has focus';
-});
-
-// blur will not fire when attached to a div container
-// because the blur event does not bubble
-container.addEventListener('blur', function (e) {
-    console.log('will not fire on blur');
-});
-
-// blur will only work if it is attached to the input
-// element directly
-document.getElementById('foo')
-.addEventListener('blur', function (e) {
-    console.log('will fire on blur');
+    out.innerText = 'The ' + e.target.id + ' element is now focused in the container.';
 });

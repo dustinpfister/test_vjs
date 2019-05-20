5af20f3614e6d9a0feb4fa77aa33029a3505a600
dustinpfister <dustin.pfister@gmail.com>
2019-05-20 16:43:20 -0400

    better find example

--- for_post/js-javascript-break/nested_fine.js
+++ for_post/js-javascript-break/nested_fine.js
@@ -1,66 +1,34 @@
 
-let grid = {
-    w: 3,
-    h: 3,
-    cells: [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
-}
-
-let findGuy = (grid) =  > {
-	
-	let x = 0, y = 0;
-	
-	outer
-	
+// return a grid with flat cells array
+let flat = (grid) => {
+    let gridC = JSON.parse(JSON.stringify(grid)),
+    c = [];
+    gridC.cells.forEach((cells) => {
+        cells.forEach((cell) => {
+            c.push(cell);
+        });
+    });
+    gridC.cells = c;
+    return gridC;
 };
-/*
+
 let findGuy = (grid) => {
-
-let y = 0,
-x,
-c = 0,
-cell = {};
-
-outer: while (y < grid.h) {
-x = 0;
-while (x < grid.w) {
-cell = grid.cells[y * grid.w + x];
-if (cell.guy) {
-break outer;
-}
-c += 1;
-x += 1;
-}
-y += 1;
-}
-
-if (cell.hp > 0) {
-return {
-guy: cell,
-c: c
+    return flat(grid).cells.find((cell) => {
+        if (typeof cell === 'object') {
+            if (cell.type === 'guy') {
+                return true
+            }
+        }
+    });
 };
-}
-return {
-guy: false,
-c: c
-};
-
-};
- */
 
 console.log(findGuy({
-        h: 3,
         w: 3,
-        cells: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    })); // {guy: false, c:9}
-
-console.log(findGuy({
         h: 3,
-        w: 3,
-        cells: [0, 0, {
-                guy: true,
-                hp: 10
-            }, 0, 0, 0, 0, 0, 0]
-    })); // { guy: { guy: true, hp: 10 }, c: 2 }
+        cells: [
+            [0, 0, 0],
+            [0, {
+                    type: 'guy'
+                }, 0],
+            [0, 0, 0]]
+    }));

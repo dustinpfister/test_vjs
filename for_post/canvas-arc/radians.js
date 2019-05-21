3f2c858a3808fdc48b0808845e526211d1a57610
dustinpfister <dustin.pfister@gmail.com>
2019-05-21 15:50:22 -0400

    radians

---
+++ for_post/canvas-arc/radians.js
@@ -0,0 +1,9 @@
+let rad = Math.PI / 4,
+// radians to degrees
+deg = rad * (180 / Math.PI);
+console.log(deg); // 45
+
+deg = 90;
+// degrees to radians
+rad = deg / 180 * Math.PI;
+console.log(rad); // 1.57...

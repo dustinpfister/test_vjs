7ad67efc09076651cfdd513cef7095b376210713
dustinpfister <dustin.pfister@gmail.com>
2019-05-23 11:53:44 -0400

    on focus clean up

--- for_post/js-onfocus/onfocus.js
+++ for_post/js-onfocus/onfocus.js
@@ -1,12 +1,10 @@
 
+// a single focus event can be attached this way
 var onfocus = function (e) {
-
     console.log(e);
-
 };
 
+// attaching two or more will require the use of addEventListener
 document.getElementById('foo').addEventListener('focus', function () {
-
     console.log('foo');
-
 });

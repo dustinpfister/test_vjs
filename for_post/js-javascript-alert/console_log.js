54a739addc394cc11fe106cece18c82df0eb0313
dustinpfister <dustin.pfister@gmail.com>
2019-05-23 12:43:04 -0400

    console_log

---
+++ for_post/js-javascript-alert/console_log.js
@@ -0,0 +1,19 @@
+console.log('log a message');
+
+var a = (5 + 3) * 10,
+b = 20;
+
+// can log a var
+console.log(a); // 80
+
+// can log more than one thing in a single call
+console.log(a, b, 'foo'); // 80 20 'foo'
+
+// expressions
+console.log(a + b); // 100
+
+//and objects
+console.log({
+    x: 60,
+    y: 40
+});

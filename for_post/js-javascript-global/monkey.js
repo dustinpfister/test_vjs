9428785ad6b891231de466e6dec2ff8062e3d5d7
dustinpfister <dustin.pfister@gmail.com>
2019-06-11 20:37:37 -0400

    money patch

--- for_post/js-javascript-global/monkey.js
+++ for_post/js-javascript-global/monkey.js
@@ -1,6 +1,19 @@
+this.n = 42;
+
+try {
+    console.log(n);
+} catch (e) {
+    console.log(e.message);
+    // 'n is not defined'
+}
+
+// However monkey patching some objects
+// might not be such a great idea,
+// and may defeat the whole purpose in the process
 Object.prototype.n = 42;
 console.log(Object.n); // 42
 console.log(Math.n); // 42
-console.log({}.n); // 42
+console.log({}
+    .n); // 42
 
 console.log(n); // 42

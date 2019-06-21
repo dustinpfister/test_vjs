d3ba6f247b113ae5054927e6405a8c4efcecae06
dustinpfister <dustin.pfister@gmail.com>
2019-06-21 11:18:47 -0400

    placed basic.js into index.html

--- for_post/js-onblur/onblur.js
+++ for_post/js-onblur/onblur.js
@@ -6,22 +6,3 @@ foo.addEventListener('blur', function (e) {
 foo.addEventListener('focus', function (e) {
     e.target.value = 'has focus';
 });
-
-/*
-
-var forBlur = function (e) {
-    console.log(e.target.id + ' blur');
-};
-
-var forFocus = function (e) {
-    console.log(e.target.id + ' focus');
-};
-
-var foo = document.getElementById('foo');
-foo.addEventListener('blur', forBlur);
-foo.addEventListener('focus', forFocus);
-
-var bar = document.getElementById('bar');
-bar.addEventListener('blur', forBlur);
-bar.addEventListener('focus', forFocus);
-*/

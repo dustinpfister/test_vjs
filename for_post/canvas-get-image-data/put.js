2a23de3103b128a941edc2b55430599ce089dc5c
dustinpfister <dustin.pfister@gmail.com>
2019-06-23 11:37:34 -0400

    put

--- for_post/canvas-get-image-data/put.js
+++ for_post/canvas-get-image-data/put.js
@@ -6,11 +6,20 @@ ctx.fillRect(0, 0, 32, 32);
 ctx.strokeStyle = 'green';
 ctx.globalAlpha = 1;
 ctx.lineWidth = 1;
-ctx.strokeRect(1, 1, 3, 3);
+ctx.strokeRect(3, 3, 5, 5);
 
-var imgData = ctx.getImageData(1, 1, 3, 3);
+var imgData = ctx.getImageData(3, 3, 5, 5);
+//imgData.data[0] = 255;
+
+imgData.data.forEach(function (v, i,data) {
+    data[i] = v === 0 ? 0 : 255;
+});
+
+ctx.putImageData(imgData,3,3);
+
 var i = 0;
 while (i < imgData.data.length) {
+
     console.log(imgData.data.slice(i, i + 4));
     i += 4;
 }

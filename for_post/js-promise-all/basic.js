b9a1abf89742c9c6b2c0b80a20b7e57696a8b23e
dustinpfister <dustin.pfister@gmail.com>
2019-06-24 12:53:43 -0400

    basic example looking good

--- for_post/js-promise-all/basic.js
+++ for_post/js-promise-all/basic.js
@@ -1,12 +1,21 @@
 
+let util = require('util'),
+fs = require('fs');
+
 let readDir = (dir) => {
-    return require('util').promisify(require('fs').readdir)(dir);
+    return util.promisify(fs.readdir)(dir);
 };
 
-Promise.all([true, readDir('./')])
+Promise.all([
+        util.promisify(fs.stat)('./text.txt'),
+        util.promisify(fs.readFile)('./text.txt')
+    ])
 
 .then((a) => {
-
-    console.log('yeah');
-    console.log(a);
+    console.log(a[0].isFile()); // true
+    console.log(a[1].constructor.name); // Buffer
 })
+
+.catch ((e) => {
+    console.log(e.message);
+});

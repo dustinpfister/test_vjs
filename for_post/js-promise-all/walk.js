734c23596c71d481f623c00816cc70eb2a73b923
dustinpfister <dustin.pfister@gmail.com>
2019-06-24 20:42:10 -0400

    getting stats

--- for_post/js-promise-all/walk.js
+++ for_post/js-promise-all/walk.js
@@ -4,8 +4,21 @@ fs = require('fs');
 
 util.promisify(fs.readdir)('./')
 
-.then((files)=>{
-	
-	console.log(files);
-	
-});
+.then((files) => {
+
+    console.log(files);
+    return Promise.all(files.map((file) => {
+            return util.promisify(fs.stat)(file);
+        }));
+
+})
+
+.then((result) => {
+
+    console.log(result.map((stat) => {
+            return stat.isFile()
+        }))
+
+})
+
+.catch (() => {})

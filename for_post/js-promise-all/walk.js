e44d6c8bc9e1479fb21bba53f141ab20f5aa0dcc
dustinpfister <dustin.pfister@gmail.com>
2019-06-26 17:21:42 -0400

    reading all files

--- for_post/js-promise-all/walk.js
+++ for_post/js-promise-all/walk.js
@@ -15,7 +15,6 @@ let fileData = (path_file) => {
     });
 };
 let readFile = util.promisify(fs.readFile);
- 
 // lets do it
 let dir = path.resolve(process.argv[2] || process.cwd());
 readdir(dir)
@@ -25,8 +24,21 @@ readdir(dir)
             return fileData(path.join(dir, file));
         }))
 })
-.then((stats) => {
-
-    console.log(stats);
-
+.then((fileData) => {
+    // filter out folders
+    fileData = fileData.filter((data) => {
+            return data.isFile
+        });
+    // read all
+    return Promise.all(fileData.map((data) => {
+            return readFile(path.join(dir, data.fileName), 'utf8');
+        }));
+})
+.then((results) => {
+    results.forEach((data) => {
+        console.log(data);
+    });
+})
+.catch ((e) => {
+    console.log(e.message)
 });

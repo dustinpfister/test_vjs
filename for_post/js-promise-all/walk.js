cb436697ee7b07f4602641fca96bd4798f04258a
dustinpfister <dustin.pfister@gmail.com>
2019-06-26 17:13:04 -0400

    fileData method

--- for_post/js-promise-all/walk.js
+++ for_post/js-promise-all/walk.js
@@ -1,44 +1,32 @@
 let util = require('util'),
 path = require('path'),
 fs = require('fs');
-
+// functions that return a promise
 let readdir = util.promisify(fs.readdir);
 let stat = util.promisify(fs.stat);
+let fileData = (path_file) => {
+    return stat(path_file).then((stats) => {
+        return {
+            stats: stats,
+            isFile: stats.isFile(),
+            fileName: path.basename(path_file),
+            path_file: path_file
+        }
+    });
+};
 let readFile = util.promisify(fs.readFile);
-
+ 
+// lets do it
 let dir = path.resolve(process.argv[2] || process.cwd());
 readdir(dir)
 .then((files) => {
     return Promise.all(files.map((file) => {
-            return stat(path.join(dir, file));
+            //return stat(path.join(dir, file));
+            return fileData(path.join(dir, file));
         }))
 })
-.then((stats)=>{
-	
-	console.log(stats);
-	
-})
+.then((stats) => {
 
-/*
-//util.promisify(fs.readdir)('./');
-//util.
+    console.log(stats);
 
-.then((files) => {
-
-console.log(files);
-return Promise.all(files.map((file) => {
-return util.promisify(fs.stat)(file);
-}));
-
-})
-
-.then((result) => {
-
-console.log(result.map((stat) => {
-return stat.isFile()
-}))
-
-})
-
-.catch (() => {})
-*/
+});

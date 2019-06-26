aa87327965b7f6893e2f123ca463e3f776625d3f
dustinpfister <dustin.pfister@gmail.com>
2019-06-26 17:06:56 -0400

    walk stats

--- for_post/js-promise-all/walk.js
+++ for_post/js-promise-all/walk.js
@@ -1,24 +1,44 @@
-
 let util = require('util'),
+path = require('path'),
 fs = require('fs');
 
-util.promisify(fs.readdir)('./')
+let readdir = util.promisify(fs.readdir);
+let stat = util.promisify(fs.stat);
+let readFile = util.promisify(fs.readFile);
+
+let dir = path.resolve(process.argv[2] || process.cwd());
+readdir(dir)
+.then((files) => {
+    return Promise.all(files.map((file) => {
+            return stat(path.join(dir, file));
+        }))
+})
+.then((stats)=>{
+	
+	console.log(stats);
+	
+})
+
+/*
+//util.promisify(fs.readdir)('./');
+//util.
 
 .then((files) => {
 
-    console.log(files);
-    return Promise.all(files.map((file) => {
-            return util.promisify(fs.stat)(file);
-        }));
+console.log(files);
+return Promise.all(files.map((file) => {
+return util.promisify(fs.stat)(file);
+}));
 
 })
 
 .then((result) => {
 
-    console.log(result.map((stat) => {
-            return stat.isFile()
-        }))
+console.log(result.map((stat) => {
+return stat.isFile()
+}))
 
 })
 
 .catch (() => {})
+*/

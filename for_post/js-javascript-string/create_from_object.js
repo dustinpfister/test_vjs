bdbaab51403c13c6640ca610ae80baa9db924f5b
dustinpfister <dustin.pfister@gmail.com>
2019-06-25 21:18:26 -0400

    create from object

--- for_post/js-javascript-string/create_from_object.js
+++ for_post/js-javascript-string/create_from_object.js
@@ -9,7 +9,7 @@ let str = Object.keys(obj).map((key) => {
         return obj[key];
     }).join('');
 console.log(str);
-// Hello, This is Dustin. Have a nice day
+// 'Hello, This is Dustin. Have a nice day'
 
 // for in
 let str2 = '';
@@ -17,4 +17,7 @@ for (let prop in obj) {
     str2 += obj[prop];
 }
 console.log(str2);
-// Hello, This is Dustin. Have a nice day
+// 'Hello, This is Dustin. Have a nice day'
+
+console.log(JSON.stringify(obj));
+// {"intro":"Hello, ","mess":"This is Dustin. ","end":"Have a nice day"}

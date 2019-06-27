cafde3ba8cfdf06b9f48388f1856ecd0b6772321
dustinpfister <dustin.pfister@gmail.com>
2019-06-27 18:31:27 -0400

    api example working okay

--- for_post/js-call-apply-and-bind/api.js
+++ for_post/js-call-apply-and-bind/api.js
@@ -1,14 +1,21 @@
-
-
 var setFrame = function (frame, opt) {
     opt = opt || {};
-    frame = frame === undefined ? 0;
-    opt.maxFrame = opt.maxFrame === undefined ? 50;
+    opt.frame = frame === undefined ? 0 : frame;
+    opt.maxFrame = opt.maxFrame === undefined ? 50 : opt.maxFrame;
+    opt.forFrame = opt.forFrame || function () {
+        console.log(this);
+    };
     var per = opt.frame / opt.maxFrame;
     opt.forFrame.call({
         frame: opt.frame,
         maxFrame: opt.maxFrame,
         per: per,
-        bias: 1 - Math.abs(0.5 - per) / 0.5;
+        bias: 1 - Math.abs(0.5 - per) / 0.5
     });
 };
+
+setFrame(10, {
+    forFrame: function () {
+        console.log(this.per); // 0.2
+    }
+})

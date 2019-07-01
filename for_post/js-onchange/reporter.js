2b49fea0a0dea778a982c23b0f3ccbe6168818a5
dustinpfister <dustin.pfister@gmail.com>
2019-07-01 17:44:03 -0400

    show and hide canvas

--- for_post/js-onchange/reporter.js
+++ for_post/js-onchange/reporter.js
@@ -3,10 +3,24 @@ var Reporter = function (opt) {
     opt = opt || {};
 
     var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
     container = document.createElement('div'),
     appendTo = opt.appendTo || document.body;
 
+    canvas.width = 320;
+    canvas.height = 240;
+    canvas.style.display = 'none';
+
+    var draw = function (ctx, canvas) {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    };
+
     var input_x = document.createElement('input');
+
+    container.appendChild(input_x);
+    container.appendChild(canvas);
+    container.style.width = canvas.width + 'px';
 
     // append
     appendTo.appendChild(container);
@@ -22,14 +36,18 @@ var Reporter = function (opt) {
 
         console.log('focus');
         console.log(e);
+        canvas.style.display = 'block';
 
     });
 
-    input_x.addEventListener('blur', function () {
+    input_x.addEventListener('blur', function (e) {
 
         console.log('blur');
         console.log(e);
+        canvas.style.display = 'none';
 
     });
+
+    draw(ctx, canvas);
 
 };

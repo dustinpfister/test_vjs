9363f9e97fbdbeacd4866064542707010f27d9fd
dustinpfister <dustin.pfister@gmail.com>
2019-07-01 18:03:10 -0400

    reporter looks good

--- for_post/js-onchange/reporter.js
+++ for_post/js-onchange/reporter.js
@@ -1,20 +1,13 @@
 var Reporter = function (opt) {
+    // option defaults
     opt = opt || {};
     opt.a = opt.a || 45;
-
-    var canvas = document.createElement('canvas'),
-    ctx = canvas.getContext('2d'),
-    container = document.createElement('div'),
-    appendTo = opt.appendTo || document.body;
-    canvas.width = 320;
-    canvas.height = 240;
-    canvas.style.display = 'none';
-
-    // draw method for the canvas
-    var draw = function (ctx, canvas) {
+    opt.data = opt.data || {
+        lineLength: 100
+    };
+    opt.draw = opt.draw || function (ctx, canvas) {
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-
         ctx.strokeStyle = 'white';
         var cx = canvas.width / 2,
         cy = canvas.height / 2,
@@ -22,24 +15,29 @@ var Reporter = function (opt) {
         ctx.beginPath();
         ctx.moveTo(cx, cy);
         ctx.lineTo(
-            Math.cos(r) * 75 + cx,
-            Math.sin(r) * 75 + cy);
+            Math.cos(r) * this.lineLength + cx,
+            Math.sin(r) * this.lineLength + cy);
         ctx.stroke();
     };
-
+    // set up canvas and input element
+    var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    container = document.createElement('div'),
+    appendTo = opt.appendTo || document.body;
+    canvas.width = 320;
+    canvas.height = 240;
+    canvas.style.display = 'none';
     var input_a = document.createElement('input');
     input_a.value = opt.a;
     container.appendChild(input_a);
     container.appendChild(canvas);
     container.style.width = canvas.width + 'px';
-
-    // append
     appendTo.appendChild(container);
 
     // ON CHANGE, focus, and blur of input element
     input_a.addEventListener('change', function (e) {
         opt.a = e.target.value;
-        draw(ctx, canvas);
+        opt.draw.call(opt.data, ctx, canvas);
     });
     input_a.addEventListener('focus', function (e) {
         canvas.style.display = 'block';
@@ -49,5 +47,5 @@ var Reporter = function (opt) {
     });
 
     // first draw
-    draw(ctx, canvas);
+    opt.draw.call(opt.data, ctx, canvas);
 };

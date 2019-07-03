67935c20793a1ff6b93b8fe85fd0f003874fea01
dustinpfister <dustin.pfister@gmail.com>
2019-07-03 15:38:36 -0400

    draw line from center to point

--- for_post/js-onchange/ataner.js
+++ for_post/js-onchange/ataner.js
@@ -7,12 +7,35 @@ var aTaner = function (opt) {
         lineLength: 100
     };
     opt.draw = opt.draw || function (ctx, canvas) {
+        // fill
+        ctx.fillStyle = 'black';
+        ctx.lineWidth = 3;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // stroke point that can change
         ctx.beginPath();
         ctx.strokeStyle = 'green';
-        ctx.lineWidth = 3;
+
         ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
         ctx.stroke();
+
+        // center
+        ctx.beginPath();
+        ctx.strokeStyle = 'blue';
+        ctx.arc(this.cx, this.cy, 10, 0, Math.PI * 2);
+        ctx.stroke();
+
+        // draw line
+        ctx.beginPath();
+        ctx.moveTo(opt.cx, opt.cy);
+        ctx.lineTo(
+            Math.cos(opt.a) * 100 + opt.cx,
+            Math.sin(opt.a) * 100 + opt.cy);
+        ctx.stroke();
+    };
+
+    var figureAngle = function () {
+        opt.a = Math.atan2(opt.y - opt.cy, opt.x - opt.cx)
     };
 
     // set up canvas and input element
@@ -51,6 +74,7 @@ var aTaner = function (opt) {
         var el = e.target,
         axis = el.id.split('_')[1];
         opt[axis] = e.target.value;
+        figureAngle();
         opt.draw.call(opt, ctx, canvas);
     };
     input_y.addEventListener('change', onChange);
@@ -66,9 +90,11 @@ var aTaner = function (opt) {
         var box = e.target.getBoundingClientRect();
         input_x.value = opt.x = e.clientX - box.left;
         input_y.value = opt.y = e.clientY - box.top;
+        figureAngle();
         opt.draw.call(opt, ctx, canvas);
     });
 
     // first draw
+    figureAngle();
     opt.draw.call(opt, ctx, canvas);
 };

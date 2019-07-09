a4dc98ca9db4d5399d4320c7c58acd621503428b
dustinpfister <dustin.pfister@gmail.com>
2019-07-09 15:29:06 -0400

    number_function example looks good

--- for_post/js-javascript-number/number_function.js
+++ for_post/js-javascript-number/number_function.js
@@ -2,6 +2,24 @@ let n = Number('17');
 console.log(typeof n); // 'number'
 console.log(n); // 17
 
+// String with a space results in NaN
 n = Number('17 6');
 console.log(typeof n); // 'number'
 console.log(n); // NaN
+
+// Booleans
+n = Number(true);
+console.log(typeof n); // 'number'
+console.log(n); // 1
+
+// Objects with valueOf methods
+let obj = {
+    x: 10,
+    y: 42,
+    valueOf: function () {
+        return this.x + this.y;
+    }
+}
+n = Number(obj);
+console.log(n); // 52
+console.log(parseInt(obj)); // NaN

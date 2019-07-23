8cb6e43aec988adbd62b5fa8dc402b922b7d296e
dustinpfister <dustin.pfister@gmail.com>
2019-07-23 16:29:41 -0400

    false-neg update

--- for_post/js-javascript-undefined/false-neg.js
+++ for_post/js-javascript-undefined/false-neg.js
@@ -1,7 +1,16 @@
 let r;
+let myVar;
 try {
     r = typeof myVar === undefined;
 } catch (e) {
     console.log(e.toString());
 }
 console.log(r); // false
+
+r = 0;
+try {
+    r = Number(myVar === undefined);
+} catch (e) {
+    r = -1;
+}
+console.log(r); // -1

7b47f8be6943425c2ca917d97b233eeb0e7b549a
dustinpfister <dustin.pfister@gmail.com>
2019-08-01 19:45:25 -0400

    bool-values

---
+++ for_post/js-javascript-undefined/bool-values.js
@@ -0,0 +1,6 @@
+console.log(!!undefined); // false
+console.log(!!null); // false
+console.log(!!0); // false
+console.log(!!''); // false
+console.log(!!NaN); // false
+console.log(!!false); // false

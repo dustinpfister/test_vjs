74abdd7bd8cfc44fb5a4039c24362ba7ad818442
dustinpfister <dustin.pfister@gmail.com>
2019-08-07 16:20:10 -0400

    weight example is looking good

--- for_post/js-javascript-arguments-object/weight.js
+++ for_post/js-javascript-arguments-object/weight.js
@@ -1,21 +1,16 @@
-let weight = function(text, keyword){
-
+let weight = function (text, keyword) {
     let w = 0,
     i = 2,
     len = arguments.length;
     while (i < len) {
-
         w += arguments[i](text, keyword);
-
-        //console.log(arguments[i]);
-
         i += 1;
     }
-
     return w;
 
 };
 
+// 100 point for each total match
 let totalMatch = (text, keyword) => {
     let w = 0,
     m = text.match(new RegExp(keyword, 'g'));
@@ -25,11 +20,30 @@ let totalMatch = (text, keyword) => {
     return w;
 };
 
-let text = 'This is some text about fuzzy cats in action';
+// 25 points for each word
+let keywords = (text, keyword) => {
+    let w = 0,
+    kwArr = keyword.split(' ')
+        text.split(' ').forEach((tw) => {
+            kwArr.forEach((kw) => {
+                w += kw === tw ? 25 : 0;
+            });
+        });
+    return w;
+};
+
+// 1 point for each word
+let wordCount = (text, keyword) => {
+    return text.split(' ').length;
+};
+
+let text = 'this is some text about fuzzy cats in action becuase cats are cool';
 
 let w1 = weight(
         text,
         'fuzzy cats',
-        totalMatch);
+        totalMatch,
+        keywords,
+        wordCount);
 
 console.log(w1);

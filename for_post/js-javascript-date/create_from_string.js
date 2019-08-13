5a79ab1cc7df50f3141ecea32c94a20a756178a1
dustinpfister <dustin.pfister@gmail.com>
2019-08-13 17:11:31 -0400

    create_from_string javaScript date demo

---
+++ for_post/js-javascript-date/create_from_string.js
@@ -0,0 +1,7 @@
+// date created with the number 2019
+let d1 = new Date(2019),
+// date created with the string '2019'
+d2 = new Date('2019');
+
+console.log(d1.toDateString()); // Wed Dec 31 1969
+console.log(d2.toDateString()); // Mon Dec 31 2018

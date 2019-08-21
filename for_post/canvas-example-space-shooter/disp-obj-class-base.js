426dbdf2492e1e8e374c0e7f99ac6b3375cfbeab
dustinpfister <dustin.pfister@gmail.com>
2019-08-21 17:59:54 -0400

    base draw methd

--- for_post/canvas-example-space-shooter/disp-obj-class-base.js
+++ for_post/canvas-example-space-shooter/disp-obj-class-base.js
@@ -8,6 +8,8 @@ disp.BaseObj = function (opt) {
 
     this.x = opt.x === undefined ? 0;
     this.y = opt.y === undefined ? 0;
+    this.w = opt.w === undefined ? 16;
+    this.h = opt.h === undefined ? 16;
     this.heading = opt.heading === undefined ? 0;
     this.pps = opt.pps === undefined ? 0;
 
@@ -18,4 +20,18 @@ disp.BaseObj.prototype.update = function (t) {
     t = t === undefined ? 0 : t;
     disp.moveObj(this, t);
     disp.applyBounds(this, canvas);
-}
+};
+
+// Base draw to a canvas method
+disp.BaseObj.prototype.draw = function (ctx) {
+
+    var hw = this.w / 2,
+    hh = this.h / 2;
+
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.heading);
+    ctx.strokeRect(-hw, -hh, w, h);
+    ctx.restore();
+
+};

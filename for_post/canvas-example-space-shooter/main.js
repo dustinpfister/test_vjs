68e30dbcd119d5f04681ee0e891fcc0b484e47cd
dustinpfister <dustin.pfister@gmail.com>
2019-08-21 15:04:16 -0400

    allpy bounds worked out

--- for_post/canvas-example-space-shooter/main.js
+++ for_post/canvas-example-space-shooter/main.js
@@ -1,44 +1,68 @@
 var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
 
+var lt = new Date();
+
+// apply bounds
 var applyBounds = function (pt, canvas) {
     var w = pt.w || 16;
-    if (pt.x < -pt.w) {
-        pt.x = canvas.width - pt.w - Math.abs(pt.x) % canvas.width;
+    if (pt.x < -w) {
+        pt.x = canvas.width + w - Math.abs(pt.x) % (canvas.width + w);
     }
-    if (pt.x > canvas.width + pt.w) {
-        px.x = pt.x % canvas.width;
+    if (pt.x > canvas.width + w) {
+        pt.x = pt.x % canvas.width;
     }
 };
 
+// ship
 var ship = {
-    x: 0,
-    y: 0,
-    heading: 0,
-    pps: 32,
+    x: 160,
+    y: 120,
+    heading: Math.PI,
+    pps: 128,
     update: function (t) {
         t = t === undefined ? 0 : t;
         t = t / 1000;
         var delta = this.pps * t;
         this.x += Math.cos(this.heading) * delta;
-        this.y += Math.cos(this.heading) * delta;
+        this.y += Math.sin(this.heading) * delta;
+        applyBounds(this, canvas);
     }
 };
 
-var lt = new Date();
-
 var update = function () {
-
     var now = new Date(),
     t = now - lt;
-
+    ship.update(t);
+    lt = now;
 };
 
-var draw = function () {};
+var draw = function () {
+
+    ctx.fillStyle = 'black';
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.save();
+    ctx.translate(ship.x, ship.y);
+    ctx.rotate(ship.heading);
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(8, 16);
+    ctx.lineTo(-8, 16);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+
+};
 
 var loop = function () {
 
     requestAnimationFrame(loop);
+    update();
+    draw();
 
 };
 

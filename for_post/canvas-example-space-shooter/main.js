b1715bfb1dbb1a301d2ef5e9fc29366fda998c38
dustinpfister <dustin.pfister@gmail.com>
2019-08-23 09:58:25 -0400

    started new state machine in main

--- for_post/canvas-example-space-shooter/main.js
+++ for_post/canvas-example-space-shooter/main.js
@@ -1,16 +1,6 @@
 var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
-var lt = new Date();
-var enemies = [];
 
-// player ship
-var ship = new Ship({
-        canvas: canvas,
-        x: 16,
-        y: 120,
-        pps: 32,
-        heading: Math.PI / 180 * 0
-    });
 // make enemies
 var makeEnemies = function (count, canvas) {
     var e,
@@ -31,6 +21,7 @@ var makeEnemies = function (count, canvas) {
     };
     return enemies;
 };
+
 // purge
 var purgeEnemies = function (enemies) {
     var i = enemies.length;
@@ -41,6 +32,57 @@ var purgeEnemies = function (enemies) {
         }
     }
 };
+
+var States = (function () {
+
+    var lt = new Date();
+    //enemies = [],
+    //ship;
+
+    return {
+
+        disp: {},
+
+        current: 'init',
+
+        init: function () {
+
+            // player ship
+            this.disp.ship = new Ship({
+                    canvas: canvas,
+                    x: 16,
+                    y: 120,
+                    pps: 32,
+                    heading: Math.PI / 180 * 0
+                });
+
+            this.disp.enemies = makeEnemies(3, canvas);
+
+            this.current = 'game';
+
+        },
+
+        game: function () {
+
+            var now = new Date(),
+            t = now - lt,
+            ship = this.disp.ship,
+            enemies = this.disp.enemies;
+
+            ship.update(t, enemies);
+            enemies.forEach(function (enemy) {
+                enemy.update(t, [ship]);
+            });
+            purgeEnemies(enemies);
+            lt = now;
+
+        }
+
+    };
+
+}
+    ());
+
 // Main Update
 var update = function () {
     var now = new Date(),
@@ -60,19 +102,20 @@ var draw = function () {
     // clear
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     // draw player
-    ship.draw(ctx, 'blue', 'blue');
+    States.disp.ship.draw(ctx, 'blue', 'blue');
     // draw enemies
-    enemies.forEach(function (enemy) {
+    States.disp.enemies.forEach(function (enemy) {
         enemy.draw(ctx, 'red', 'red');
     });
 };
 // Main APP loop
 var loop = function () {
     requestAnimationFrame(loop);
-    update();
+    //update();
+    States[States.current]();
     draw();
 };
-enemies = makeEnemies(3, canvas);
+//enemies = makeEnemies(3, canvas);
 
 loop();
 
@@ -94,7 +137,7 @@ var moveShip = function (e) {
     }
     cx = canvas.width / 2,
     cy = canvas.height / 2;
-    ship.heading = Math.PI + Math.atan2(cy - y, cx - x);
+    States.disp.ship.heading = Math.PI + Math.atan2(cy - y, cx - x);
 };
 
 // EVENTS

d19a5f671acec6a4b92ca080576247707308718a
dustinpfister <dustin.pfister@gmail.com>
2019-08-23 10:05:38 -0400

    making canvas part of States

--- for_post/canvas-example-space-shooter/main.js
+++ for_post/canvas-example-space-shooter/main.js
@@ -1,52 +1,53 @@
-var canvas = document.getElementById('the-canvas'),
-ctx = canvas.getContext('2d');
-
-// make enemies
-var makeEnemies = function (count, canvas) {
-    var e,
-    enemies = [];
-    count = count || 1;
-    while (count--) {
-        e = new Ship({
-                canvas: canvas,
-                x: Math.floor(canvas.width * Math.random()),
-                y: Math.floor(canvas.height * Math.random()),
-                pps: 16,
-                shotPPS: 64,
-                shotLife: 5000,
-                heading: Math.PI * 2 * Math.random(),
-                shotDelay: 3000
-            });
-        enemies.push(e);
-    };
-    return enemies;
-};
-
-// purge
-var purgeEnemies = function (enemies) {
-    var i = enemies.length;
-    while (i--) {
-        var enemy = enemies[i];
-        if (enemy.HP === 0) {
-            enemies.splice(i, 1);
-        }
-    }
-};
-
 var States = (function () {
 
-    var lt = new Date();
-    //enemies = [],
-    //ship;
+    var lt = new Date(),
+    canvas = document.getElementById('the-canvas'),
+    ctx = canvas.getContext('2d');
 
+    // HELPERS
+    // make enemies
+    var makeEnemies = function (count, canvas) {
+        var e,
+        enemies = [];
+        count = count || 1;
+        while (count--) {
+            e = new Ship({
+                    canvas: canvas,
+                    x: Math.floor(canvas.width * Math.random()),
+                    y: Math.floor(canvas.height * Math.random()),
+                    pps: 16,
+                    shotPPS: 64,
+                    shotLife: 5000,
+                    heading: Math.PI * 2 * Math.random(),
+                    shotDelay: 3000
+                });
+            enemies.push(e);
+        };
+        return enemies;
+    };
+
+    // purge
+    var purgeEnemies = function (enemies) {
+        var i = enemies.length;
+        while (i--) {
+            var enemy = enemies[i];
+            if (enemy.HP === 0) {
+                enemies.splice(i, 1);
+            }
+        }
+    };
+
+    // PUBLIC API
     return {
 
-        disp: {},
+        canvas: canvas,
+        ctx: ctx,
+        disp: {}, // display Objects to be used with the renderer
 
-        current: 'init',
+        current: 'init', // current state
 
+        // Initialize the Game State
         init: function () {
-
             // player ship
             this.disp.ship = new Ship({
                     canvas: canvas,
@@ -55,27 +56,22 @@ var States = (function () {
                     pps: 32,
                     heading: Math.PI / 180 * 0
                 });
-
             this.disp.enemies = makeEnemies(3, canvas);
-
             this.current = 'game';
-
         },
 
+        // Main Game State
         game: function () {
-
             var now = new Date(),
             t = now - lt,
             ship = this.disp.ship,
             enemies = this.disp.enemies;
-
             ship.update(t, enemies);
             enemies.forEach(function (enemy) {
                 enemy.update(t, [ship]);
             });
             purgeEnemies(enemies);
             lt = now;
-
         }
 
     };
@@ -83,17 +79,9 @@ var States = (function () {
 }
     ());
 
-// Main Update
-var update = function () {
-    var now = new Date(),
-    t = now - lt;
-    ship.update(t, enemies);
-    enemies.forEach(function (enemy) {
-        enemy.update(t, [ship]);
-    });
-    purgeEnemies(enemies);
-    lt = now;
-};
+var canvas = States.canvas,
+ctx = States.ctx;
+
 // Main Draw
 var draw = function () {
     ctx.fillStyle = 'black';
@@ -115,8 +103,6 @@ var loop = function () {
     States[States.current]();
     draw();
 };
-//enemies = makeEnemies(3, canvas);
-
 loop();
 
 // move ship handler that will work with mouse

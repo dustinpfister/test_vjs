309cf8f0dcdfee8bfad23a3891ef43b76fa461e3
dustin pfister <dustin.pfister@gmail.com>
2019-08-26 10:01:46 -0400

    printGrid method working okay

--- for_post/js-path-find/node_basic.js
+++ for_post/js-path-find/node_basic.js
@@ -1,5 +1,24 @@
-let Grid = require('./grid.js');
+let Grid = require('./grid.js'),
+os = require('os');
 
 let g = new Grid();
 
-console.log(g.nodes);
+let printGrid = (grid)=> {
+  
+    grid.nodes.forEach((row) => {
+       
+        row.forEach((node) => {
+            
+            process.stdout.write(node.i + ':');
+            
+        });
+        
+        process.stdout.write(os.EOL);
+        
+    });
+    
+};
+
+printGrid(g);
+
+//console.log(g.nodes);

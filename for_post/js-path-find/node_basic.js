e795da2793ae95fa78be2f4e696f2f90be4edd99
dustinpfister <dustin.pfister@gmail.com>
2019-08-27 12:19:35 -0400

    using node_grid with node_basic

--- for_post/js-path-find/node_basic.js
+++ for_post/js-path-find/node_basic.js
@@ -1,24 +1,16 @@
-let Grid = require('./grid.js'),
+let Grid = require('./node_grid.js').Grid,
 os = require('os');
 
 let g = new Grid();
 
 let printGrid = (grid)=> {
-    
     var tiles = ['.', 'S', 'E', '#'];
-  
     grid.nodes.forEach((row) => {
-       
         row.forEach((node) => {
-            
             process.stdout.write(tiles[node.ti]);
-            
         });
-        
         process.stdout.write(os.EOL);
-        
     });
-    
 };
 
 printGrid(g);

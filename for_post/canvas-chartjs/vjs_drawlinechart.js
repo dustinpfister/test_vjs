322db131b21287c1ee5e9999ea3b5cdd3a46a177
dustinpfister <dustin.pfister@gmail.com>
2019-09-08 19:52:59 -0400

    draw line chart function working okay

--- for_post/canvas-chartjs/vjs_drawlinechart.js
+++ for_post/canvas-chartjs/vjs_drawlinechart.js
@@ -1,27 +1,28 @@
 
-/*{
-clicks: [120,30,24,250,300]
-}
-
- */
-
 var drawLineChart = (function () {
-
-    return function (canvas, data) {
-
-        var ctx = canvas.getContext();
-
-        Object.keys(data).forEach(function (setName) {
-
-            var values = data[setName];
-            values.forEach(function (n) {
-				
-				
-			});
-
+    // set scale helper
+    var setScale = function (canvas, values) {
+        var highest = Math.max.apply(null, values);
+        return values.map(function (val) {
+            return val / highest * canvas.height;
         });
-
     };
-
+    // return the draw function
+    return function (canvas, data) {
+        var ctx = canvas.getContext('2d');
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        Object.keys(data).forEach(function (setName) {
+            var values = setScale(canvas, data[setName]);
+            ctx.beginPath();
+            ctx.strokeStyle = 'white';
+            values.forEach(function (n, i) {
+                var x = canvas.width / (values.length - 1) * i,
+                y = canvas.height - n;
+                ctx.lineTo(x, y);
+            });
+            ctx.stroke();
+        });
+    };
 }
     ());

1b865493c2b05e6f3a97bd005af09f30e3b4dffe
dustinpfister <dustin.pfister@gmail.com>
2019-09-08 19:35:22 -0400

    drawlinechart file for canvas-chartjs

---
+++ for_post/canvas-chartjs/vjs_drawlinechart.js
@@ -0,0 +1,27 @@
+
+/*{
+clicks: [120,30,24,250,300]
+}
+
+ */
+
+var drawLineChart = (function () {
+
+    return function (canvas, data) {
+
+        var ctx = canvas.getContext();
+
+        Object.keys(data).forEach(function (setName) {
+
+            var values = data[setName];
+            values.forEach(function (n) {
+				
+				
+			});
+
+        });
+
+    };
+
+}
+    ());

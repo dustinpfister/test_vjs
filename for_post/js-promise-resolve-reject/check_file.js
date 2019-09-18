19759f245e4997eff0d0c312ef960feedc9a3a16
dustinpfister <dustin.pfister@gmail.com>
2019-09-18 12:39:03 -0400

    check file example looks good

--- for_post/js-promise-resolve-reject/check_file.js
+++ for_post/js-promise-resolve-reject/check_file.js
@@ -1,11 +1,14 @@
 let fs = require('fs'),
+path = require('path'),
 promisify = require('util').promisify;
 
 let stat = promisify(fs.stat),
 readFile = promisify(fs.readFile);
 
+let thePath = path.resolve(process.argv[2] || process.cwd());
+
 // get stats of path
-stat(process.argv[2] || process.cwd())
+stat(thePath)
 
 // just check if dir
 .then((stats) => {
@@ -18,11 +21,14 @@ stat(process.argv[2] || process.cwd())
     return stats;
 })
 
-.then((stats)=>{
-	
-	console.log('okay');
-	console.log(stats);
-	
+// read file
+.then((stats) => {
+    return readFile(thePath)
+})
+
+// log the data
+.then((data) => {
+    console.log(data.toString());
 })
 
 .catch((e) => {

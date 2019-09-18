f11d7909ff8cfa548d4c72962adfa5d049ed6876
dustinpfister <dustin.pfister@gmail.com>
2019-09-18 12:11:27 -0400

    basic promise example

--- for_post/js-promise-resolve-reject/basic_promise_example.js
+++ for_post/js-promise-resolve-reject/basic_promise_example.js
@@ -13,6 +13,8 @@ let defaultTest = () => {
     return false;
 }
 
+// so I have a method that returns a promise that can resolve
+// or reject depending on the outcome of what it does
 let delayTest = (delay, theTest) => {
     delay = delay === undefined ? 1000 : delay;
     theTest = theTest === undefined ? defaultTest : theTest;
@@ -26,14 +28,13 @@ let delayTest = (delay, theTest) => {
     });
 };
 
+// I can then use the promise example
+// and have methods that will fire if the promise resolves
+// or rejects
 delayTest()
 .then((mess) => {
-
     console.log(mess)
-
 })
 .catch((e) => {
-
     console.log(e);
-
-})
+});

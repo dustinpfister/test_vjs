2da3f3854b6029ef414db47be3ad5d7fbb62350b
dustinpfister <dustin.pfister@gmail.com>
2019-09-20 16:19:23 -0400

    atan2 two points method returns degrees

--- for_post/js-math-atan2/atan2-two-points-method.js
+++ for_post/js-math-atan2/atan2-two-points-method.js
@@ -6,11 +6,11 @@ var p1 = {
 p2 = {
     x: 75,
     y: 100
-
 };
 
+// a findAngle method that takes four arguments and returns and angle in degrees
 var findAngle = function (x1, y1, x2, y2) {
-    return Math.atan2(y1 - y2, x1 - x2) + Math.PI;
+    return ( Math.atan2(y1 - y2, x1 - x2) + Math.PI )  / Math.PI * 180;
 };
-
-console.log(findAngle(p1.x, p1.y, p2.x, p2.y) / Math.PI * 180); // 63.43
+var a = findAngle(p1.x, p1.y, p2.x, p2.y);
+console.log(a); // 63.43

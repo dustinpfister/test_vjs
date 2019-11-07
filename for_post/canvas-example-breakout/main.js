10295980a013e220e2c83b6018ed3c9774c7f6ae
dustinpfister <dustin.pfister@gmail.com>
2019-11-07 17:11:58 -0500

    canvas-example-grid

--- for_post/canvas-example-breakout/main.js
+++ for_post/canvas-example-breakout/main.js
@@ -1,4 +1,11 @@
-
+var Grid = function (opt) {
+    opt = opt || {};
+    this.xOffset = opt.xOffset || 5;
+    this.yOffset = opt.yOffset || 5;
+    this.cellSize = opt.cellSize || 32;
+    this.cellWidth = opt.cellWidth || 7;
+    this.cellHeight = opt.cellHeight || 6;
+};
 
 (function () {
 
@@ -14,49 +21,38 @@
     canvas.width = 320;
     canvas.height = 240;
 
-    var state = {
-        canvas: canvas,
-        ctx: ctx,
-        currentState: 'title'
+    var drawGridLines = function (ctx, grid) {
+
+        var x,
+        y,
+        cx = 0,
+        cy = 0;
+        while (cy < grid.cellHeight + 1) {
+            y = cy * grid.cellSize + grid.yOffset + 0.5;
+            x = grid.xOffset + 0.5;
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            x = grid.xOffset + grid.cellSize * grid.cellWidth + 0.5;
+            ctx.lineTo(x, y);
+            ctx.stroke();
+            cy += 1;
+        }
+        while (cx < grid.cellWidth + 1) {
+            y = grid.yOffset + 0.5;
+            x = cx * grid.cellSize + grid.xOffset + 0.5;
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            y = grid.yOffset + grid.cellSize * grid.cellHeight + 0.5;
+            ctx.lineTo(x, y);
+            ctx.stroke();
+            cx += 1;
+        }
+
     };
 
-    // Draw Module
-    var draw = (function () {
+    var grid = new Grid();
 
-        // main draw method
-        var api = function (state) {
-            // clear screen
-            api.cls(state);
-            // draw current state
-            api.state[state.currentState](state);
-        };
-
-        // 'clear screen' draw method
-        api.cls = function (state) {
-            var ctx = state.ctx,
-            canvas = state.canvas;
-            ctx.fillStyle = 'black';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-        };
-
-        // Draw methods for each state
-        api.state = {};
-        // title
-        api.state.title = function (state) {
-            var ctx = state.ctx,
-            canvas = state.canvas;
-            ctx.fillStyle = 'white';
-            ctx.font = '30px arial';
-            ctx.textAlign = 'center';
-            ctx.fillText('BREAKOUT', canvas.width / 2, canvas.height / 3);
-        };
-
-        return api;
-
-    }
-        ());
-
-    draw(state);
+    drawGridLines(ctx, grid);
 
 }
     ());

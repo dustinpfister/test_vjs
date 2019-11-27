7cffd693fe1ae5f12aae31c8026dfb6aacf83c88
dustinpfister <dustin.pfister@gmail.com>
2019-11-26 20:26:47 -0500

    drawing

--- for_post/canvas-example-pop-the-lock/main.js
+++ for_post/canvas-example-pop-the-lock/main.js
@@ -1,11 +1,10 @@
-
+// STATE
 var ptl = {
-
     sec_current: 0,
     sec_target: 50,
     sec_total: 100,
     sec_margin: 2,
-    tick_dir: -1,
+    tick_dir: 1,
     tick_rate: 100,
     tick_last: new Date(),
     tick: function () {
@@ -22,11 +21,30 @@ var ptl = {
     }
 };
 
-setTimeout(function () {
+// DRAW
+var drawPTL = function (ptl, ctx, canvas) {
 
-    ptl.tick();
+    // background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-}, 1000);
+    // draw base circle
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, Math.PI * 2);
+    ctx.stroke();
+
+    // draw current
+    ctx.strokeStyle = 'blue';
+    ctx.beginPath();
+    var r = ptl.sec_current / ptl.sec_total * Math.PI * 2,
+    x = Math.cos(r) * 100 + canvas.width / 2,
+    y = Math.sin(r) * 100 + canvas.height / 2;
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.stroke();
+
+};
 
 // SETUP CANVAS
 (function () {
@@ -38,6 +56,13 @@ setTimeout(function () {
     // set width and height
     canvas.width = 320;
     canvas.height = 240;
+
+    var loop = function () {
+        requestAnimationFrame(loop);
+        ptl.tick();
+        drawPTL(ptl, ctx, canvas);
+    };
+    loop();
 
 }
     ());

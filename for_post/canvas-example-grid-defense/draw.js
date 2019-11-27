a549833128736b3f2518895efb4b06ad129cb670
dustinpfister <dustin.pfister@gmail.com>
2019-11-27 08:39:55 -0500

    canvas-example-grid-defence folder

---
+++ for_post/canvas-example-grid-defense/draw.js
@@ -0,0 +1,17 @@
+// draw Cell Lines
+var draw = {};
+
+draw.gridCellLines = function (grid, ctx, style) {
+    var ci = 0,
+    cell,
+    cLen = grid.cells.length;
+    ctx.strokeStyle = style || 'red';
+    while (ci < cLen) {
+        cell = grid.cells[ci];
+        ctx.strokeRect(
+            cell.x * grid.cellSize + grid.xOffset + 0.5,
+            cell.y * grid.cellSize + grid.yOffset + 0.5,
+            grid.cellSize, grid.cellSize)
+        ci += 1;
+    }
+};
---
+++ for_post/canvas-example-grid-defense/main.js
@@ -0,0 +1,28 @@
+// SETUP CANVAS
+(function () {
+    // create and append canvas element, and get 2d context
+    var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    container = document.getElementById('gamearea') || document.body;
+    container.appendChild(canvas);
+    // set width and height
+    canvas.width = 320;
+    canvas.height = 240;
+
+    // creating a grid instance
+    var grid = new Grid({
+            xOffset: 15,
+            yOffset: 25,
+            cellSize: 32,
+            cellWidth: 9
+        });
+
+    // fill black
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // draw grid lines
+    draw.gridCellLines(grid, ctx, 'white');
+
+}
+    ());

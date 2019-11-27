9c51a9ad7d924056ef558ce9df00941e4da39381
dustinpfister <dustin.pfister@gmail.com>
2019-11-26 20:35:14 -0500

    warpSec

--- for_post/canvas-example-pop-the-lock/main.js
+++ for_post/canvas-example-pop-the-lock/main.js
@@ -5,18 +5,23 @@ var ptl = {
     sec_total: 100,
     sec_margin: 2,
     tick_dir: 1,
-    tick_rate: 100,
+    tick_rate: 40,
     tick_last: new Date(),
+    inrange: false,
+    wrapSec: function (sec) {
+        if (sec > this.sec_total) {
+            sec %= this.sec_total;
+        }
+        if(sec < 0) {
+            sec = this.sec_total - (this.sec_total + Math.abs(sec)) % this.sec_total;
+        }
+        return sec;
+    },
     tick: function () {
         var time = new Date() - this.tick_last,
         ticks = time / this.tick_rate;
         this.sec_current += ticks * this.tick_dir;
-        if (this.sec_current > this.sec_total) {
-            this.sec_current %= this.sec_total;
-        }
-        if (this.sec_current < 0) {
-            this.sec_current = this.sec_total - (this.sec_total + Math.abs(this.sec_current)) % this.sec_total;
-        }
+        this.sec_current = this.wrapSec(this.sec_current);
         this.tick_last = new Date();
     }
 };

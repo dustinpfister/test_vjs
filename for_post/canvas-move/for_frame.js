7dda293555d272725b64178a9c1a0ca8e9e43885
dustinpfister <dustin.pfister@gmail.com>
2019-11-29 11:29:05 -0500

    for_frame example working okay

--- for_post/canvas-move/for_frame.js
+++ for_post/canvas-move/for_frame.js
@@ -1,18 +1,20 @@
-var boxMovement = function (frame, maxFrame, canvas) {
-
+var pointMovement = function (frame, maxFrame, canvas) {
     let sx = -32,
-    mx = canvas.width + 32;
-
+    mx = canvas.width + 64;
+    return {
+        x: sx + mx * (frame / maxFrame),
+        y: canvas.height / 2 - 16,
+        r: 16
+    };
 };
 
 // draw a state
-var draw = function (bx, ctx, canvas) {
-    canvas = st.canvas;
+var draw = function (pt, ctx, canvas) {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'white';
     ctx.beginPath();
-    ctx.arc(bx.x, bx.y, 16, 0, Math.PI * 2);
+    ctx.arc(pt.x, pt.y, pt.r, 0, Math.PI * 2);
     ctx.fill();
 };
 
@@ -20,9 +22,14 @@ var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
 canvas.width = 320;
 canvas.height = 240;
+
 // app loop
+var pt, frame = 0,
+maxFrame = 50;
 var loop = function () {
     requestAnimationFrame(loop);
-    draw(state, ctx, canvas);
+    draw(pointMovement(47, maxFrame, canvas), ctx, canvas);
+    frame += 1;
+    frame %= maxFrame;
 };
 loop();

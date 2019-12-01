ae83f0cf35235e7434cc10f785e51c4380b24a10
dustinpfister <dustin.pfister@gmail.com>
2019-12-01 18:25:55 -0500

    closure_pps example

---
+++ for_post/js-javascript-return/closure_pps.js
@@ -0,0 +1,55 @@
+var pps = function (obj) {
+    obj = obj || {};
+    obj.pps = obj.pps === undefined ? 32 : obj.pps;
+    obj.x = obj.x === undefined ? 0 : obj.x;
+    obj.y = obj.y === undefined ? 0 : obj.y;
+    obj.r = obj.r === undefined ? 0 : obj.r;
+    var lastTime = new Date();
+    // Main API Method
+    var api = function () {
+        var now = new Date(),
+        t = now - lastTime,
+        sec = t / 1000;
+        obj.x += Math.cos(obj.r) * obj.pps * sec;
+        obj.y += Math.sin(obj.r) * obj.pps * sec;
+        lastTime = now;
+        return obj;
+    };
+    // single static method
+    api.set = function (opt) {
+        opt = opt || {};
+        obj.x = opt.x === undefined ? obj.x : opt.x;
+        obj.y = opt.y === undefined ? obj.y : opt.y;
+        obj.r = opt.r === undefined ? obj.r : opt.r;
+        return obj;
+    };
+    // return the public API
+    return api;
+};
+
+var boxState = pps({
+        x: 0,
+        y: 50,
+        r: 0,
+        pps: 100
+    }), bx;
+
+var loop = function () {
+
+    setTimeout(loop, 100);
+    bx = boxState();
+
+    if (bx.x >= 500) {
+
+        boxState.set({
+            x: 0,
+            y: 50,
+            r: Math.PI * 1.9 + Math.random() * (Math.PI * 0.2)
+        });
+
+    }
+
+    console.log(bx.x.toFixed(2), bx.y.toFixed(2));
+
+};
+loop();

23735930520ee685ff30e165156f4175bf222fd3
dustinpfister <dustin.pfister@gmail.com>
2019-12-03 14:23:46 -0500

    basic_objects

---
+++ for_post/js-array-sort/basic_objects.js
@@ -0,0 +1,19 @@
+var arr = [
+    {hp: 0}, 
+    {hp: 57}, 
+    {hp: 50},
+    {hp: 0},
+    {hp: 7}
+];
+
+arr.sort(function (a, b) {
+    if (a.hp > b.hp) {
+        return -1;
+    }
+    if (a.hp < b.hp) {
+        return 1;
+    }
+    return 0;
+});
+
+console.log(arr.map((e) => e.hp).join(':'));

65651b50902c3e004d5fe246e76a7fb7a1beaef4
dustinpfister <dustin.pfister@gmail.com>
2019-12-10 21:20:27 -0500

    img_state to index

--- for_post/js-math-pow/img_state.js
+++ for_post/js-math-pow/img_state.js
@@ -1,5 +1,4 @@
-
-var imgFromIndex = function (index) {
+var IMGStringFromIndex = function (index) {
     var size = 7 * 7,
     maxIndex = Math.pow(2, size) - 1,
     num,
@@ -15,4 +14,19 @@ var imgFromIndex = function (index) {
     return String(baseStr + num).slice(size * -1).split('').reverse().join('');
 };
 
-console.log(imgFromIndex(Math.pow(2, 7 * 7)));
+var indexFromIMGString = function (string) {
+    var index = 0;
+    string.split('').forEach(function (pix, i) {
+        index += pix == 1 ? Math.pow(2, i): 0;
+    });
+    return index;
+
+}
+
+var str = IMGStringFromIndex( 123456789012345 );
+
+console.log(str);
+
+var index = indexFromIMGString(str);
+
+console.log(index);

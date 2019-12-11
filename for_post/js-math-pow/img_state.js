86bb6f5576b6771b81f091a57b1a1d87faa9a79e
dustinpfister <dustin.pfister@gmail.com>
2019-12-11 13:00:06 -0500

    parseInt example

--- for_post/js-math-pow/img_state.js
+++ for_post/js-math-pow/img_state.js
@@ -1,31 +1,42 @@
-var IMGStringFromIndex = function (index) {
-    var size = 7 * 7,
-    maxIndex = Math.pow(2, size) - 1,
+var IMGStringFromIndex = function (index, colorDepth, size) {
+
+    index = index || 0;
+    size = size || 7 * 7;
+    colorDepth = colorDepth || 2;
+
+    var maxIndex = Math.pow(colorDepth, size) - 1,
     num,
     baseStr;
+
     if (index > maxIndex) {
         index = maxIndex;
     }
     if (index < 0) {
         index = 0;
     }
-    num = index.toString(2);
+    num = index.toString(colorDepth);
     baseStr = new Array(size).fill('0').join('');
     return String(baseStr + num).slice(size * -1).split('').reverse().join('');
 };
 
-var indexFromIMGString = function (string) {
+// Basic indexFromString using parseInt
+var indexFromString = function (string, colorDepth) {
+    return parseInt(str.split('').reverse().join(''), colorDepth);
+};
+// complex indexFromString using Math.pow
+var indexFromString2 = function (string, colorDepth) {
+    colorDepth = colorDepth || 2;
     var index = 0;
     string.split('').forEach(function (pix, i) {
-        index += pix == 1 ? Math.pow(2, i): 0;
+        index += Math.pow(colorDepth, i) * Number(pix);
     });
     return index;
 }
 
-var str = IMGStringFromIndex( Math.pow(2, 7 * 7 ) - 1 );
+var str = IMGStringFromIndex(Math.pow(2, 7 * 7) - 1);
 console.log(str);
 // 1111111111111111111111111111111111111111111111111
 
-var index = indexFromIMGString(str);
-console.log(index);
-// 562949953421311
+str = '023331';
+console.log( indexFromString(str, 4) );
+console.log( indexFromString2(str, 4) );

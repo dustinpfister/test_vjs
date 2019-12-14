eab2f5e087b704332b277090f44cd5f9a96045ed
dustinpfister <dustin.pfister@gmail.com>
2019-12-14 13:00:33 -0500

    canvas-example-clock-idle-game

---
+++ for_post/canvas-example-clock-idle-game/main.js
@@ -0,0 +1,37 @@
+
+var getClock = function (now, start) {
+    var clock = {};
+    clock.now = now || new Date(2000, 3, 6, 10, 5);
+    clock.start = start || new Date(1983, 3, 6, 10, 5);
+    clock.t = clock.now - clock.start;
+    clock.timeText = Number(clock.t / 1000).toFixed(1);
+    return clock;
+};
+
+var drawClockText = function (clock, canvas, ctx, x, y) {
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.font = '70px courier';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+    ctx.fillText(clock.timeText, x + 0.5, y + 0.5);
+    ctx.strokeText(clock.timeText, x + 0.5, y + 0.5);
+};
+
+// create and append canvas element, and get 2d context
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d'),
+container = document.getElementById('gamearea') || document.body;
+container.appendChild(canvas);
+// set width and height
+canvas.width = 640;
+canvas.height = 240;
+
+var loop = function () {
+    requestAnimationFrame(loop);
+    var clock = getClock(new Date());
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawClockText(clock, canvas, ctx, canvas.width / 2, canvas.height / 2);
+};
+loop();

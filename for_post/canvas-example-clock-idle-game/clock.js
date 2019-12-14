10118472e8235f0dcf6df24f76e1284ba95df387
dustinpfister <dustin.pfister@gmail.com>
2019-12-14 13:36:13 -0500

    get basic values

--- for_post/canvas-example-clock-idle-game/clock.js
+++ for_post/canvas-example-clock-idle-game/clock.js
@@ -1,14 +1,24 @@
 
 var clock = {};
 
+clock.getBasicValues = function (t) {
+    t = t === undefined ? 0 : t;
+    t = t < 0 ? 0 : t;
+    var sec = t / 1000;
+    return {
+        t: t,
+        sec: sec,
+        days: sec / 86400,
+    }
+};
+
 clock.get = function (now, start) {
     var c = {};
     c.now = now || new Date(2000, 3, 6, 10, 5);
     c.start = start || new Date(1983, 3, 6, 10, 5);
-    // get t value of the given dates
-    c.t = c.now - c.start;
-    c.sec = c.t / 1000;
-    c.days = c.sec / 86400;
+
+    // get basic values
+    c = Object.assign(c, clock.getBasicValues(c.now - c.start));
 
     // percent done is between zero and 2.5 billion seconds
     c.per = c.sec / (2.5 * Math.pow(10, 9));

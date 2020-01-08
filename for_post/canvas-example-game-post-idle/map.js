5b409530cb76657e14d736b9037a1e553f85f8e2
dustinpfister <dustin.pfister@gmail.com>
2020-01-08 10:49:27 -0500

    type index

--- for_post/canvas-example-game-post-idle/map.js
+++ for_post/canvas-example-game-post-idle/map.js
@@ -1,5 +1,16 @@
 var g = {};
 
+// CREATE A GRID OBJECT
+
+// make and return a new grid object by just passing width and height values
+g.createGridObject = function (w, h) {
+    var grid = {};
+    grid.width = w || 64;
+    grid.height = h || 16;
+    grid.cells = [];
+    return g.createClearCellGrid(grid);
+};
+
 // create a new grid object with blank cells by passing a given grid Object
 g.createClearCellGrid = function (grid) {
     var g = {};
@@ -16,18 +27,10 @@ g.createClearCellGrid = function (grid) {
         g.cells.push({
             i: i,
             x: i % grid.width,
-            y: Math.floor(i / grid.width)
+            y: Math.floor(i / grid.width),
+            type: 0 // type index (0 == blank , 1 == plant)
         });
         i += 1;
     }
     return g;
-};
-
-// make and return a new grid object by just passing width and height values
-g.createGridObject = function (w, h) {
-    var grid = {};
-    grid.width = w || 64;
-    grid.height = h || 16;
-    grid.cells = [];
-    return g.createClearCellGrid(grid);
 };

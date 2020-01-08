3cfbfe2a2aef8565b4f31950cd80e57c66286459
dustinpfister <dustin.pfister@gmail.com>
2020-01-08 12:11:34 -0500

    started events

--- for_post/canvas-example-game-post-idle/main.js
+++ for_post/canvas-example-game-post-idle/main.js
@@ -1,27 +1,50 @@
-// create and append canvas element, and get 2d context
+// CANVAS
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
 container = document.getElementById('gamearea') || document.body;
 container.appendChild(canvas);
-
-// set width and height
 canvas.width = 320;
 canvas.height = 120;
 
+// CREATE GRID
 var grid = g.createGridObject(4, 4);
 grid.xOffset = canvas.width / 2 - grid.width * grid.cellSize / 2;
 grid.yOffset = 0;
 
-var loop = function () {
+var mousedown = false,
+gridDelta = {
+    x: 0,
+    y: 0
+};
 
+// MAIN APP LOOP
+var loop = function () {
     requestAnimationFrame(loop);
+
+    grid.xOffset += gridDelta.x;
 
     // fill black
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     // draw map
     drawMap(grid, ctx, canvas);
-
 };
-
 loop();
+
+// EVENTS
+canvas.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+    mousedown = true;
+});
+canvas.addEventListener('mouseup', function (e) {
+    e.preventDefault();
+    mousedown = false;
+    gridDelta.x = 0;
+    gridDelta.y = 0;
+});
+canvas.addEventListener('mousemove', function (e) {
+    e.preventDefault();
+    if (mousedown) {
+        gridDelta.x = 1;
+    }
+});

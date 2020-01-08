4578f8b96594b6618dda0b277abcc193922b977f
dustinpfister <dustin.pfister@gmail.com>
2020-01-08 11:54:17 -0500

    parse grid properties

--- for_post/canvas-example-game-post-idle/map.js
+++ for_post/canvas-example-game-post-idle/map.js
@@ -2,42 +2,44 @@ var g = {};
 
 // CREATE A GRID OBJECT
 
+// parse grid properties
+g.parseGridProps = function (grid) {
+    var a = {};
+    a.width = grid.width || 64;
+    a.height = grid.height || 16;
+    a.cellSize = grid.cellSize || 32;
+    a.xOffset = grid.xOffset === undefined ? 0 : grid.xOffset;
+    a.yOffset = grid.yOffset === undefined ? 0 : grid.yOffset;
+    a.cells = [];
+    return a;
+};
+
 // make and return a new grid object by just passing width and height values
 g.createGridObject = function (w, h) {
-    var grid = {};
-    grid.width = w || 64;
-    grid.height = h || 16;
-    grid.cellSize = 32;
-    g.xOffset = grid.xOffset === undefined ? 0 : grid.xOffset;
-    g.yOffset = grid.yOffset === undefined ? 0 : grid.yOffset;
-    grid.cells = [];
-    return g.createClearCellGrid(grid);
+    var a = g.parseGridProps({
+            width: w,
+            height: h
+        });
+    return g.createClearCellGrid(a);
 };
 
 // create a new grid object with blank cells by passing a given grid Object
 g.createClearCellGrid = function (grid) {
-    var g = {};
-    g.width = grid.width || 64;
-    g.height = grid.height || 16;
-    g.cellSize = grid.cellSize || 32;
-    g.xOffset = grid.xOffset === undefined ? 0 : grid.xOffset;
-    g.yOffset = grid.yOffset === undefined ? 0 : grid.yOffset;
-    g.cells = [];
-
+   var a = g.parseGridProps(grid);
     // create clean cells
     var i = 0,
     x,
     y,
-    len = g.width * g.height;
+    len = a.width * a.height;
     while (i < len) {
-        g.cells.push({
+        a.cells.push({
             i: i,
-            x: i % grid.width,
-            y: Math.floor(i / grid.width),
+            x: i % a.width,
+            y: Math.floor(i / a.width),
             type: 0, // type index (0 = sand , 1-5 = grass, 6-10 = wood),
             worth: 0
         });
         i += 1;
     }
-    return g;
+    return a;
 };

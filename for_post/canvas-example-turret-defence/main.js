346d0c03c1c2fd722ec75c0bd2d42f803326702f
dustinpfister <dustin.pfister@gmail.com>
2020-01-10 17:32:01 -0500

    canvas-example-turret-defence

---
+++ for_post/canvas-example-turret-defence/main.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('the-canvas'),
+ctx = canvas.getContext('2d');
+canvas.width = 320;
+canvas.height = 240;
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);

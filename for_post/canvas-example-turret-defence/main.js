e24403748855a89f17cd5a563ec8040ca0748d98
dustinpfister <dustin.pfister@gmail.com>
2020-01-10 17:53:31 -0500

    better turret drawing

--- for_post/canvas-example-turret-defence/main.js
+++ for_post/canvas-example-turret-defence/main.js
@@ -3,6 +3,7 @@ ctx = canvas.getContext('2d');
 canvas.width = 320;
 canvas.height = 240;
 
+// the turret object
 var turret = {
     cx: canvas.width / 2,
     cy: canvas.height / 2,
@@ -11,31 +12,35 @@ var turret = {
     lt: new Date() // last time turret was updated
 };
 
+// update turret method
 var updateTurret = function (turret) {
-
     var now = new Date(),
     t = (now - turret.lt) / 1000;
-
     turret.heading += turret.rps * t;
     turret.heading %= Math.PI * 2;
-
     turret.lt = now;
-
 };
 
+// draw the turret
 var drawTurret = function (turret, ctx, canvas) {
     ctx.save();
     ctx.translate(turret.cx, turret.cy);
     ctx.rotate(turret.heading);
     ctx.fillStyle = 'red';
-    ctx.fillRect(-16, -16, 32, 32);
+    ctx.fillRect(-8, -8, 16, 16);
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(16, 0);
+    ctx.stroke();
     ctx.restore();
 };
 
+// main app loop
 var loop = function () {
     requestAnimationFrame(loop);
     updateTurret(turret);
-
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     drawTurret(turret, ctx, canvas);

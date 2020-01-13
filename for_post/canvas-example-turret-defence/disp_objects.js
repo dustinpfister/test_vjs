44ea4253849807dce1118b8d5f9f6ef72e1c630f
dustinpfister <dustin.pfister@gmail.com>
2020-01-13 09:53:05 -0500

    life span ajust

--- for_post/canvas-example-turret-defence/disp_objects.js
+++ for_post/canvas-example-turret-defence/disp_objects.js
@@ -9,7 +9,7 @@ disp.createTurretObject = function () {
         cx: canvas.width / 2,
         cy: canvas.height / 2,
         heading: 0,
-        rps: 1, // radians per second
+        rps: 0, // radians per second
         lt: new Date(), // last time turret was updated
         shots: [],
         shotsMax: 13,
@@ -21,37 +21,54 @@ disp.createTurretObject = function () {
 
 // update turret shots
 disp.updateTurretShots = function (turret, secs) {
-    // spawn new shots
-    turret.shotTime += secs;
-    var newShots = Math.floor(turret.shotTime / turret.shotDelay);
-    if (newShots >= 1) {
-        turret.shotTime -= newShots * turret.shotDelay;
-        if (turret.shots.length < turret.shotsMax) {
-            turret.shots.push({
-                sx: turret.cx,
-                sy: turret.cy,
-                x: turret.cx,
-                y: turret.cy,
-                heading: turret.heading,
-                pps: 64,
-                lifeSpan: 4,
-                shotTime: new Date()
-            });
+
+    if (turret.paused) {
+
+        turret.shots.forEach(function (shot) {
+            if (!(shot.sx === shot.x && shot.sy === shot.y)) {
+                shot.lifeSpanAjust = (new Date() - shot.shotTime) / 1000 * -1;
+                shot.sx = shot.x;
+                shot.sy = shot.y;
+            }
+            shot.shotTime = new Date();
+        });
+
+    } else {
+        // spawn new shots
+        turret.shotTime += secs;
+        var newShots = Math.floor(turret.shotTime / turret.shotDelay);
+        if (newShots >= 1) {
+            turret.shotTime -= newShots * turret.shotDelay;
+            if (turret.shots.length < turret.shotsMax) {
+                turret.shots.push({
+                    sx: turret.cx,
+                    sy: turret.cy,
+                    x: turret.cx,
+                    y: turret.cy,
+                    heading: turret.heading,
+                    pps: 64,
+                    lifeSpan: 1,
+                    lifeSpanAjust: 0,
+                    shotTime: new Date()
+                });
+            }
         }
-    }
-    // update active shots
-    var i = turret.shots.length,
-    now = new Date(),
-    shot,
-    t;
-    while (i--) {
-        shot = turret.shots[i];
-        t = (now - shot.shotTime) / 1000;
-        shot.x = shot.sx + Math.cos(shot.heading) * t * shot.pps;
-        shot.y = shot.sy + Math.sin(shot.heading) * t * shot.pps;
-        if (t >= shot.lifeSpan) {
-            turret.shots.splice(i, 1);
+        // update active shots
+        var i = turret.shots.length,
+        now = new Date(),
+        shot,
+        t;
+        while (i--) {
+            shot = turret.shots[i];
+            t = (now - shot.shotTime) / 1000;
+            shot.x = shot.sx + Math.cos(shot.heading) * t * shot.pps;
+            shot.y = shot.sy + Math.sin(shot.heading) * t * shot.pps;
+            // purge old shots
+            if (t >= shot.lifeSpan + shot.lifeSpanAjust) {
+                turret.shots.splice(i, 1);
+            }
         }
+
     }
 };
 
@@ -65,7 +82,7 @@ disp.updateTurret = function (turret) {
     } else {
         turret.heading += turret.rps * secs;
         turret.heading %= Math.PI * 2;
-        disp.updateTurretShots(turret, secs);
         turret.lt = now;
     }
+    disp.updateTurretShots(turret, secs);
 };

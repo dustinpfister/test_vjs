0a93711c2ad71fa36c9e0a05358b8e58a207da12
dustinpfister <dustin.pfister@gmail.com>
2020-01-16 16:54:56 -0500

    bird pos

--- for_post/canvas-example-game-flappy-collector/main.js
+++ for_post/canvas-example-game-flappy-collector/main.js
@@ -16,7 +16,7 @@ var bird = {
     y: 0,
     size: 32,
     pps: 64,
-    gameOver: false,
+    //gameOver: false,
     lt: new Date()
 };
 
@@ -31,18 +31,16 @@ var updateBirdPosition = function (bird, canvas) {
     var now = new Date(),
     secs = (now - bird.lt) / 1000;
     bird.y += bird.pps * secs;
-    if (bird.y >= canvas.height + bird.size) {
-        bird.y = canvas.height + bird.size;
-        bird.gameOver = true;
+    if (bird.y >= canvas.height - bird.size) {
+        bird.y = canvas.height - bird.size;
+        //bird.gameOver = true;
     }
     bird.lt = now;
 };
 
 // INPUT
 canvas.addEventListener('click', function () {
-
     flap(bird);
-
 });
 
 // DRAW
@@ -58,7 +56,8 @@ var drawBird = function (bird, ctx) {
 var drawInfo = function (bird, ctx) {
     ctx.fillStyle = 'white';
     ctx.textBaseline = 'top';
-    ctx.fillText('game over: ' + bird.gameOver, 10, 10);
+    ctx.fillText('bird pos: ( ' + Math.floor(bird.x) + ',' + Math.floor(bird.y) + ')', 10, 10);
+    //ctx.fillText('game over: ' + bird.gameOver, 10, 10);
 };
 
 // Main APP Loop

c306c6e6b06d45c46c8421910b38d8f786c68e0f
dustinpfister <dustin.pfister@gmail.com>
2020-01-16 16:49:47 -0500

    flap method

--- for_post/canvas-example-game-flappy-collector/main.js
+++ for_post/canvas-example-game-flappy-collector/main.js
@@ -9,6 +9,8 @@ canvas.height = 240;
 
 ctx.translate(0.5, 0.5);
 
+// STATE
+
 var bird = {
     x: 32,
     y: 0,
@@ -18,52 +20,55 @@ var bird = {
     lt: new Date()
 };
 
-var updateBirdPosition = function (bird, canvas) {
+var flap = function (bird) {
+    bird.y -= 32;
+    if (bird.y < 0) {
+        bird.y = 0;
+    }
+};
 
+var updateBirdPosition = function (bird, canvas) {
     var now = new Date(),
     secs = (now - bird.lt) / 1000;
-
     bird.y += bird.pps * secs;
-
     if (bird.y >= canvas.height + bird.size) {
-
         bird.y = canvas.height + bird.size;
         bird.gameOver = true;
-
     }
-
     bird.lt = now;
-
 };
+
+// INPUT
+canvas.addEventListener('click', function () {
+
+    flap(bird);
+
+});
+
+// DRAW
 
 var drawBackground = function (ctx) {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
-
 var drawBird = function (bird, ctx) {
     ctx.fillStyle = 'green';
     ctx.fillRect(bird.x, bird.y, 32, 32);
 };
-
 var drawInfo = function (bird, ctx) {
-
     ctx.fillStyle = 'white';
     ctx.textBaseline = 'top';
     ctx.fillText('game over: ' + bird.gameOver, 10, 10);
-
 };
 
+// Main APP Loop
+
 var loop = function () {
-
     requestAnimationFrame(loop);
-
     drawBackground(ctx);
     drawBird(bird, ctx);
     drawInfo(bird, ctx);
-
     updateBirdPosition(bird, canvas);
-
 };
 
 loop();

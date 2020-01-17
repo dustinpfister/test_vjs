1a94da40e2fc57adfffbd17ca61bc2b2035eb687
dustinpfister <dustin.pfister@gmail.com>
2020-01-17 12:32:16 -0500

    main.js clean up

--- for_post/canvas-example-game-flappy-collector/main.js
+++ for_post/canvas-example-game-flappy-collector/main.js
@@ -1,16 +1,13 @@
-
-
+// create a canvas
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
 container = document.getElementById('gamearea') || document.body;
 container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
-
 ctx.translate(0.5, 0.5);
 
-
-
+// Create new Game
 var bird = game.newBird();
 
 // INPUT
@@ -18,19 +15,14 @@ canvas.addEventListener('click', function () {
     game.flap(bird);
 });
 
-
 // Main APP Loop
 var loop = function () {
-
     requestAnimationFrame(loop);
-
     drawBackground(ctx);
     drawBerries(bird, ctx);
     drawBird(bird, ctx);
     drawInfo(bird, ctx);
-
     game.update(bird, canvas);
-
 };
 
 loop();

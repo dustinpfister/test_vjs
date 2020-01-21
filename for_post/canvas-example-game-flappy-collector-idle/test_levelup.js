b787b283db352ff89d61fea2978ddbf06fcf7b41
dustinpfister <dustin.pfister@gmail.com>
2020-01-21 17:18:28 -0500

    levelup test working good

--- for_post/canvas-example-game-flappy-collector-idle/test_levelup.js
+++ for_post/canvas-example-game-flappy-collector-idle/test_levelup.js
@@ -1,10 +1,18 @@
+
+var berryNextLevelSet = function (bird) {
+    var e = Math.floor(Math.log(bird.berriesCollected) / Math.log(2));
+    bird.berriesNextLevel = e >= 6 ? Math.pow(2, bird.berryLevel + 5) : 32;
+};
+
 var berryLevelCheck = function (bird) {
     var e = Math.floor(Math.log(bird.berriesCollected) / Math.log(2));
-    console.log(e);
+    berryNextLevelSet(bird);
     bird.berryLevel = e >= 6 ? e - 4 : 1;
+    berryNextLevelSet(bird);
 };
 
 var bird = {
+    berryLevel:1,
     berriesCollected: 128
 };
 

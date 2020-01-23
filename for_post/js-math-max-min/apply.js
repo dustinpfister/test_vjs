f34b3047996fb8573118ba5a799b3f5c3b949f39
dustinpfister <dustin.pfister@gmail.com>
2020-01-22 20:55:39 -0500

    js-math-max-min demo folder

---
+++ for_post/js-math-max-min/apply.js
@@ -0,0 +1,5 @@
+
+var nums = [7, -4, 0, 8, 12, -2];
+
+console.log( Math.min.apply(null, nums) ); // -4
+console.log( Math.max.apply(null, nums) ); // 12
---
+++ for_post/js-math-max-min/basic.js
@@ -0,0 +1,8 @@
+
+var x = 3,
+y = -7,
+z = 0;
+
+var min = Math.min(x, y, z);
+
+console.log(min); // -7

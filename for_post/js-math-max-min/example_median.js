5250df170b7972eaee156c2ea6b7fc48f14486fb
dustinpfister <dustin.pfister@gmail.com>
2020-01-25 10:46:29 -0500

    sum and mean

--- for_post/js-math-max-min/example_median.js
+++ for_post/js-math-max-min/example_median.js
@@ -6,12 +6,27 @@ var getMedian = function (nums) {
     return nums.length % 2 ? nums[half] : (nums[half - 1] + nums[half]) / 2;
 };
 
+var getSum = function (nums) {
+    var i = nums.length,
+    sum = 0;
+    while (i--) {
+        sum += nums[i];
+    }
+    return sum;
+};
+
+var getMean = function (nums) {
+    return getSum(nums) / nums.length;
+};
+
 var getAverages = function (nums) {
     var avgs = {};
     avgs.min = Math.min.apply(null, nums);
     avgs.max = Math.max.apply(null, nums);
     avgs.range = avgs.max - avgs.min;
     avgs.median = getMedian(nums);
+    avgs.sum = getSum(nums);
+    avgs.mean = getMean(nums);
     return avgs;
 };
 
@@ -19,6 +34,8 @@ var nums = [1, 2, 4, 7];
 
 var avgs = getAverages(nums);
 
-console.log( avgs.median ); // 3
-console.log( avgs.min, avgs.max); // 1 7
-console.log( avgs.range ); // 6
+console.log(avgs.median); // 3
+console.log(avgs.min, avgs.max); // 1 7
+console.log(avgs.range); // 6
+console.log(avgs.sum); // 14
+console.log(avgs.mean); // 3.5

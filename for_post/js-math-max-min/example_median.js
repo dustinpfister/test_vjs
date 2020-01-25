6e23fb7c66c732255229853fa04576ad6a23a3e3
dustinpfister <dustin.pfister@gmail.com>
2020-01-25 10:41:17 -0500

    median function working great

--- for_post/js-math-max-min/example_median.js
+++ for_post/js-math-max-min/example_median.js
@@ -1,13 +1,24 @@
+var getMedian = function (nums) {
+    var half = Math.floor(nums.length / 2);
+    nums.sort(function (a, b) {
+        return a - b;
+    });
+    return nums.length % 2 ? nums[half] : (nums[half - 1] + nums[half]) / 2;
+};
+
 var getAverages = function (nums) {
-
     var avgs = {};
-
     avgs.min = Math.min.apply(null, nums);
     avgs.max = Math.max.apply(null, nums);
-
-    avgs.range = agvs.max - avgs.min;
-    avgs.sorted = nums.sort();
-
+    avgs.range = avgs.max - avgs.min;
+    avgs.median = getMedian(nums);
     return avgs;
-
 };
+
+var nums = [1, 2, 4, 7];
+
+var avgs = getAverages(nums);
+
+console.log( avgs.median ); // 3
+console.log( avgs.min, avgs.max); // 1 7
+console.log( avgs.range ); // 6

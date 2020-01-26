583322deb9fe06364b6f7dd10c2343eb5ca7491d
dustinpfister <dustin.pfister@gmail.com>
2020-01-26 15:58:47 -0500

    set delta

--- for_post/canvas-example-pointer-movement/main.js
+++ for_post/canvas-example-pointer-movement/main.js
@@ -9,10 +9,6 @@ canvas.width = 320;
 canvas.height = 240;
 ctx.translate(0.5, 0.5);
 
-// fill black
-ctx.fillStyle = 'black';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
 // get canvas relative point
 var getCanvasRelative = function (e) {
     var canvas = e.target,
@@ -26,57 +22,112 @@ var getCanvasRelative = function (e) {
     };
 };
 
+var distance = function (x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+};
+
+var drawBackground = function (pm, ctx, canvas) {
+
+    // fill black
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+}
+
+// draw a navigation circle when moving the map
+var drawNavCircle = function (pm, ctx, canvas) {
+    var cx = canvas.width / 2,
+    cy = canvas.height / 2,
+    x,
+    y,
+    min = Math.min(cx, cy),
+    per = 0,
+    a = pm.angle;
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 3;
+    // draw circle
+    ctx.beginPath();
+    ctx.arc(cx, cy, min / 2, 0, Math.PI * 2);
+    ctx.stroke();
+    // draw direction line
+    x = Math.cos(a) * min + cx;
+    y = Math.sin(a) * min + cy;
+    ctx.beginPath();
+    ctx.moveTo(cx, cy);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    // draw delta circle
+    per = pm.delta / 3;
+    x = Math.cos(a) * min * per + cx;
+    y = Math.sin(a) * min * per + cy;
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.stroke();
+};
+
 // Pointer Movement State
 var pm = {
-    move: true,
+    move: false,
     angle: 0,
     dist: 0,
     delta: 0,
-    startPoint: {
+    sp: { // start point
         x: -1,
         y: -1
     },
-    currentPoint: {
+    cp: { // current point
         x: -1,
         y: -1
     }
 };
 
-// draw a navigation circle when moving the map
-var drawNavCircle = function (pm, ctx, canvas) {
+// update the pm based on startPoint, and currentPoint
+var updatePM = function (pm) {
+
     if (pm.move) {
-        var cx = canvas.width / 2,
-        cy = canvas.height / 2,
-        x,
-        y,
-        min = Math.min(cx, cy),
-        per = 0,
-        a = pm.angle;
-        ctx.strokeStyle = 'white';
-        ctx.lineWidth = 3;
-        // draw circle
-        ctx.beginPath();
-        ctx.arc(cx, cy, min / 2, 0, Math.PI * 2);
-        ctx.stroke();
-        // draw direction line
-        x = Math.cos(a) * min + cx;
-        y = Math.sin(a) * min + cy;
-        ctx.beginPath();
-        ctx.moveTo(cx, cy);
-        ctx.lineTo(x, y);
-        ctx.stroke();
-        // draw delta circle
-        per = pm.delta / 3;
-        x = Math.cos(a) * min * per + cx;
-        y = Math.sin(a) * min * per + cy;
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
-        ctx.stroke();
+        pm.dist = distance(pm.sp.x, pm.sp.y, pm.cp.x, pm.cp.y);
+        var per = pm.dist / 64;
+        per = per > 1 ? 1 : per;
+        per = per < 0 ? 0 : per;
+        pm.delta = per * 3;
     }
+
 };
 
 canvas.addEventListener('mousedown', function (e) {
     var pos = getCanvasRelative(e);
+    pm.move = true;
+    pm.sp = {
+        x: pos.x,
+        y: pos.y
+    };
+});
+canvas.addEventListener('mousemove', function (e) {
+    var pos = getCanvasRelative(e);
+    pm.cp = {
+        x: pos.x,
+        y: pos.y
+    };
+});
+canvas.addEventListener('mouseup', function (e) {
+    var pos = getCanvasRelative(e);
+    pm.move = false;
+    pm.sp = {
+        x: -1,
+        y: -1
+    };
+    pm.cp = {
+        x: -1,
+        y: -1
+    };
 });
 
-drawNavCircle(pm, ctx, canvas);
+var loop = function () {
+
+    requestAnimationFrame(loop);
+    updatePM(pm);
+    drawBackground(pm, ctx, canvas);
+    drawNavCircle(pm, ctx, canvas);
+
+};
+loop();

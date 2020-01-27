5456d1d5c7411360f4d0a178ee336914f81a3cf6
dustinpfister <dustin.pfister@gmail.com>
2020-01-27 08:03:09 -0500

    added a distnace method

--- for_post/canvas-example-turret-defense/todo.md
+++ for_post/canvas-example-turret-defense/todo.md
@@ -1,3 +1,6 @@
 ## canvas-example-turret-defense todo list
 
-* set turret RPS based on enemies array
+* (done) set turret RPS based on enemies array
+* (done) angle distance used when setting RPS
+* (done) distance method for utils
+* Shots hit and kill enemies
--- for_post/canvas-example-turret-defense/utils.js
+++ for_post/canvas-example-turret-defense/utils.js
@@ -13,6 +13,11 @@ u.normalizeHalf = function (n, scale) {
     return u.mod(n + h, c) - h;
 };
 
+// distance
+u.distance = function (x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+};
+
 // the angular distance between two angles
 u.angleDistance = function (a, b, scale) {
     var m = scale || u.defaultAngleScale,

614db04672dfc00e4171ed0e7dec18e3471145fa
dustinpfister <dustin.pfister@gmail.com>
2020-01-28 10:24:23 -0500

    started stateMachine global

--- for_post/canvas-example-state-machine/main.js
+++ for_post/canvas-example-state-machine/main.js
@@ -8,3 +8,53 @@ canvas.height = 240;
 // fill black
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+var stateMachine = (function () {
+
+    // create a new state machine
+    return function () {
+
+        // states
+        var states = {
+            currentState: 'game'
+        };
+
+        // main loop
+        var loop = function () {
+
+            requestAnimationFrame(loop);
+
+        };
+
+        return {
+
+            load: function (stateObj) {
+                // just reference the object for now as long as
+                // that works okay
+                states[stateObj.name || 'game'] = stateObj;
+            },
+            start: function () {
+                loop();
+            }
+        }
+
+    };
+
+}
+    ());
+
+sm.load({
+
+    name: 'game',
+
+    init: function () {},
+
+    every: {
+        tick: function (game, sm) {},
+        userPointer: {
+            start: function (game, pt, sm) {},
+            move: function (game, pt, sm) {},
+            end: function (game, pt, sm) {}
+        }
+    }
+});

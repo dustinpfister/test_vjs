6284ab51d861fcb478ff94906a6a048a321627d5
dustinpfister <dustin.pfister@gmail.com>
2020-01-28 19:36:51 -0500

    moved events to state machine

--- for_post/canvas-example-game-map-idle/main.js
+++ for_post/canvas-example-game-map-idle/main.js
@@ -52,6 +52,46 @@ var states = {
                     y: pos.y
                 };
 
+            },
+            move: function (pos, grid, e) {
+
+                var deltas = g.getPointerMovementDeltas(grid, canvas, pos.x, pos.y);
+
+                grid.moveDistance = 0;
+                if (grid.mapMoveStartPoint.x != -1 && grid.mapMoveStartPoint.y != -1) {
+                    grid.moveDistance = u.distance(pos.x, pos.y, grid.mapMoveStartPoint.x, grid.mapMoveStartPoint.y);
+                }
+
+                if (grid.moveDistance >= 32) {
+                    grid.mapMoveMode = true;
+                    grid.mapMoveDeltas.x = deltas.x;
+                    grid.mapMoveDeltas.y = deltas.y;
+                } else {
+                    grid.mapMoveMode = false;
+                }
+
+            },
+            end: function (pos, grid, e) {
+
+                if (!grid.mapMoveMode) {
+                    var cell = g.getCellFromCanvasPoint(grid, pos.x, pos.y);
+                    if (cell.i === grid.selectedCellIndex) {
+                        grid.selectedCellIndex = -1;
+                    } else {
+                        if (cell.i >= 0) {
+                            grid.selectedCellIndex = cell.i;
+                        }
+                    }
+                }
+
+                grid.mapMoveMode = false;
+                grid.mapMoveDeltas.x = 0;
+                grid.mapMoveDeltas.y = 0;
+                grid.mapMoveStartPoint = {
+                    x: -1,
+                    y: -1
+                };
+
             }
         }
     },
@@ -99,8 +139,28 @@ canvas.addEventListener('mousedown', function (e) {
 });
 
 canvas.addEventListener('mouseup', function (e) {
+
+    var pos = u.getCanvasRelative(e);
+    var stateObj = states[states.currentState];
+    if (stateObj.pointer) {
+        var handler = stateObj.pointer.end;
+        if (handler) {
+            handler(pos, states.grid, e);
+        }
+    }
+
     //g.userCanvasActionEnd(states.grid, e, pxRatio);
 });
 canvas.addEventListener('mousemove', function (e) {
+
+    var pos = u.getCanvasRelative(e);
+    var stateObj = states[states.currentState];
+    if (stateObj.pointer) {
+        var handler = stateObj.pointer.move;
+        if (handler) {
+            handler(pos, states.grid, e);
+        }
+    }
+
     //g.userCanvasActionMove(states.grid, e, pxRatio);
 });

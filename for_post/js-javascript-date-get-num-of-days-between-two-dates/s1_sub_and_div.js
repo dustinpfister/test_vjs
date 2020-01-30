e610e068007cbbc804a17d40527012a7c266e591
dustinpfister <dustin.pfister@gmail.com>
2020-01-30 11:21:23 -0500

    s1 working okay for what it is

--- for_post/js-javascript-date-get-num-of-days-between-two-dates/s1_sub_and_div.js
+++ for_post/js-javascript-date-get-num-of-days-between-two-dates/s1_sub_and_div.js
@@ -1,10 +1,10 @@
 
 var getDayDiff1 = function (d1, d2) {
-
     return ((d1 - d2) / 1000 / 60 / 60 / 24);
-
 };
 
-var days = getDayDiff1(new Date(), new Date(2017, 1, 2));
+var d1 = new Date(2020, 0, 30),
+d2 = new Date(2017, 1, 2);
 
-console.log(days);
+var days = getDayDiff1(d1, d2);
+console.log(days); // 1092

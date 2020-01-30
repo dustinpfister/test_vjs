5165f821e36894c9857659cab2adfa9c84006eeb
dustinpfister <dustin.pfister@gmail.com>
2020-01-30 11:48:10 -0500

    s2 is working as exspected

--- for_post/js-javascript-date-get-num-of-days-between-two-dates/s2_loop.js
+++ for_post/js-javascript-date-get-num-of-days-between-two-dates/s2_loop.js
@@ -5,10 +5,10 @@ var getDaysInMonth = function (y, m) {
 };
 
 var getDayDiff2 = function (d1, d2) {
-
     var y = d2.getFullYear(),
     m = d2.getMonth(),
-    d = d2.getDate();
+    d = d2.getDate(),
+    days = getDaysInMonth(y, m);
     console.log(y, m, getDaysInMonth(y, m));
     do {
         m = Number(m) + 1;
@@ -16,13 +16,14 @@ var getDayDiff2 = function (d1, d2) {
             m = 0;
             y = Number(y) + 1;
         }
-        console.log(y, m, getDaysInMonth(y, m));
+        //console.log(y, m, getDaysInMonth(y, m));
+        days += getDaysInMonth(y, m);
     } while ((y + '.' + m != d1.getFullYear() + '.' + d1.getMonth()));
-
+    return days - d2.getDate() - (getDaysInMonth(d1.getFullYear(), d1.getMonth()) - d1.getDate());
 };
 
 var d1 = new Date(2020, 0, 30),
 d2 = new Date(2017, 1, 2);
 
 var days = getDayDiff2(d1, d2);
-//console.log(days); // 1092?
+console.log(days); // 1092

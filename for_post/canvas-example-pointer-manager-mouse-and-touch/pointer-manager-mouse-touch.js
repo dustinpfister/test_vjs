6c560d8000e6e0cfc161869e2680b6950fea9422
dustinpfister <dustin.pfister@gmail.com>
2020-01-29 20:00:34 -0500

    get pointer helper

--- for_post/canvas-example-pointer-manager-mouse-and-touch/pointer-manager-mouse-touch.js
+++ for_post/canvas-example-pointer-manager-mouse-and-touch/pointer-manager-mouse-touch.js
@@ -1,4 +1,5 @@
 var PMMT = (function () {
+
     // get canvas relative point
     var getCanvasRelative = function (e) {
         var canvas = e.target,
@@ -12,27 +13,41 @@ var PMMT = (function () {
         };
     };
 
+    // out of canvas
+    var outOfCanvas = function (sm, pos) {
+        return pos.x < 0 || pos.x >= sm.canvas.width || pos.y < 0 || pos.y >= sm.canvas.height;
+    };
+
+    // get a pointer object if the state has one else return false
+    var getPointer = function (sm) {
+        var stateObj = sm[sm.currentState] || {};
+        if (stateObj.pointer) {
+            return stateObj.pointer;
+        }
+        return false;
+    };
+
     // attach pointer events
     var attachPointerEvent = function (sm, domType, smType) {
         // attach a hander of the given domType to the canvas
         sm.canvas.addEventListener(domType, function (e) {
             // get position and state
             var pos = getCanvasRelative(e),
-            stateObj = sm[sm.currentState] || {},
+            pointer = getPointer(sm),
             hander,
             endHander;
             // prevent default
             e.preventDefault();
             // if we have a point object
-            if (stateObj.pointer) {
-                handler = stateObj.pointer[smType];
+            if (pointer) {
+                handler = pointer[smType];
                 // if we have a hander
                 if (handler) {
                     // do not fire handler if we go out of bounds
                     // but trigger and end for the current state if
                     // if is there
-                    if (pos.x < 0 || pos.x >= sm.canvas.width || pos.y < 0 || pos.y >= sm.canvas.height) {
-                        endHandler = stateObj.pointer.end;
+                    if (outOfCanvas(sm, pos)) {
+                        endHandler = pointer.end;
                         if (endHandler) {
                             endHandler(pos, sm, e);
                         }

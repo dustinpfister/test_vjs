559e2176be624353b3701b5089e74bfa10743aef
dustinpfister <dustin.pfister@gmail.com>
2020-01-29 19:30:57 -0500

    model is now part of the sm

--- for_post/canvas-example-pointer-manager-mouse-and-touch/main.js
+++ for_post/canvas-example-pointer-manager-mouse-and-touch/main.js
@@ -7,44 +7,6 @@ canvas.width = 320;
 canvas.height = 240;
 ctx.translate(0.5, 0.5);
 
-var model = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
-};
-
-var sm = {
-    currentState: 'demo',
-    model: model,
-    demo: {
-        tick: function (model, sm) {
-            ctx.fillStyle = 'black';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            ctx.strokeStyle = 'white';
-            ctx.beginPath();
-            ctx.arc(model.x, model.y, 5, 0, Math.PI * 2);
-            ctx.stroke();
-        },
-        pointer: {
-            start: function (pos, sm, e) {
-                sm.model.down = true;
-            },
-            move: function (pos, sm, e) {
-                var m = sm.model;
-                if (m.down) {
-                    m.x = pos.x;
-                    m.y = pos.y;
-                }
-            },
-            end: function () {
-                var m = sm.model;
-                m.down = false;
-                m.x = canvas.width / 2,
-                m.y = canvas.height / 2
-            }
-        }
-    }
-};
-
 // get canvas relative point
 var getCanvasRelative = function (e) {
     var canvas = e.target,
@@ -91,6 +53,42 @@ var attachPointerEvent = function (states, canvas, domType, smType) {
     });
 };
 
+var sm = {
+    currentState: 'demo',
+    model: {
+        x: canvas.width / 2,
+        y: canvas.height / 2
+    },
+    demo: {
+        tick: function (model, sm) {
+            ctx.fillStyle = 'black';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.strokeStyle = 'white';
+            ctx.beginPath();
+            ctx.arc(model.x, model.y, 5, 0, Math.PI * 2);
+            ctx.stroke();
+        },
+        pointer: {
+            start: function (pos, sm, e) {
+                sm.model.down = true;
+            },
+            move: function (pos, sm, e) {
+                var m = sm.model;
+                if (m.down) {
+                    m.x = pos.x;
+                    m.y = pos.y;
+                }
+            },
+            end: function () {
+                var m = sm.model;
+                m.down = false;
+                m.x = canvas.width / 2,
+                m.y = canvas.height / 2
+            }
+        }
+    }
+};
+
 // mouse events
 attachPointerEvent(sm, canvas, 'mousedown', 'start');
 attachPointerEvent(sm, canvas, 'mousemove', 'move');
@@ -104,7 +102,7 @@ attachPointerEvent(sm, canvas, 'touchcancel', 'end');
 
 var loop = function () {
     requestAnimationFrame(loop);
-    sm[sm.currentState].tick(model, sm);
+    sm[sm.currentState].tick(sm.model, sm);
 
 };
 loop();

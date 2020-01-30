5c01c8b82f53142a9d58c09dbb6faf5dbc418274
dustinpfister <dustin.pfister@gmail.com>
2020-01-30 11:39:23 -0500

    looping months

---
+++ for_post/js-javascript-date-get-num-of-days-between-two-dates/s2_loop.js
@@ -0,0 +1,23 @@
+
+var getDayDiff2 = function (d1, d2) {
+
+    var y = d2.getFullYear(),
+    m = d2.getMonth(),
+    d = d2.getDate();
+    console.log(y, m);
+    do {
+        m = Number(m) + 1;
+        if (m >= 12) {
+            m = 0;
+            y = Number(y) + 1;
+        }
+        console.log(y, m);
+    } while ((y + '.' + m != d1.getFullYear() + '.' + d1.getMonth()));
+
+};
+
+var d1 = new Date(2020, 0, 30),
+d2 = new Date(2017, 1, 2);
+
+var days = getDayDiff2(d1, d2);
+//console.log(days); // 1092?

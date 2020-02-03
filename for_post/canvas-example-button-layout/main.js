b6664473f6a7d6d73636d42512203f747128cc11
dustinpfister <dustin.pfister@gmail.com>
2020-02-03 18:35:59 -0500

    steping counter

--- for_post/canvas-example-button-layout/main.js
+++ for_post/canvas-example-button-layout/main.js
@@ -5,31 +5,28 @@ container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
 
+var count = 0;
+
 // create button layout
-var buttons = []
-
-// push manual gather button
-buttons.push({
-    x: 16,
-    y: 100,
-    w: 64,
-    h: 32,
-    label: 'Gather',
-    onAction: function (pos, opt, e) {
-        console.log('foo');
-    }
-});
-
 var blObj = u.mkButtonLayout({
-        attachTo: canvas,
-        buttons: buttons
-    });
+    attachTo: canvas,
+    buttons: [{
+            x: 16,
+            y: 100,
+            w: 64,
+            h: 32,
+            label: 'Step 0',
+            onAction: function (pos, opt, e) {
+                count += 1;
+                console.log(count);
+            }
+        }
+    ]
+});
 
 var loop = function () {
     requestAnimationFrame(loop);
-
     draw.background(ctx, canvas);
     draw.buttonLayout(ctx, blObj);
-
 };
 loop();

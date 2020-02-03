66cc4a0bd40be645e22dedcb4e0104eb8077c8fe
dustinpfister <dustin.pfister@gmail.com>
2020-02-02 19:06:00 -0500

    applyed upgrade effect draft to game.js

--- for_post/canvas-example-basic-idle-game/game.js
+++ for_post/canvas-example-basic-idle-game/game.js
@@ -1,7 +1,61 @@
 var game = (function () {
 
-    // create and return a new game save state
-    var createNewState = function () {
+    // UPGRADES
+    var upgradeData = [{
+            dispName: 'Manual Gather',
+            cost: {
+                base: 10,
+                pow: 1.09,
+                inc: 5
+            },
+            effect: function (state, level) {
+                state.gatherRate.manual = 1 + level + Math.floor(Math.pow(1.05, level) - 1);
+            }
+        }
+    ];
+
+    // make a new upgradeState object from an upgradeData object
+    var makeUS = function (ud) {
+        return {
+            dispName: ud.dispName,
+            level: 0,
+            cost: Object.assign({
+                current: ud.cost
+            }, ud.cost)
+        };
+    };
+
+    // get the breakdown for base, inc, and pow that sets current cost
+    var getUSCostBreakdown = function (us) {
+        return {
+            base: us.cost.base,
+            inc: us.cost.inc * us.level,
+            pow: Math.floor(Math.pow(us.cost.pow, us.level))
+        };
+    };
+
+    // set the given upgrade state to the given level
+    var setUSCurrentCost = function (us, level) {
+        var bd;
+        level = level || 0;
+        us.level = level;
+        var bd = getUSCostBreakdown(us);
+        us.cost.current = bd.base + bd.inc + bd.pow;
+    };
+
+    // apply the effect of an upgrade
+    var applyUSEffectToState = function (us, state, ud) {
+        ud.effect(state, us.level);
+    };
+
+    // set the upgrade level
+    var setUpgradeLevel = function (us, state, ud, level) {
+        setUSCurrentCost(us, level);
+        applyUSEffectToState(us, state, ud);
+    };
+
+    // create and return a new game save state with the given upgradeData
+    var createNewState = function (upgradeData) {
         return {
             money: 0,
             tickRate: 3000,
@@ -9,7 +63,11 @@ var game = (function () {
             gatherRate: {
                 manual: 1,
                 auto: 0
-            }
+            },
+            // just set zero for each upgrade
+            US: upgradeData.map(function (ud) {
+                return 0;
+            })
         };
     };
 
@@ -17,7 +75,7 @@ var game = (function () {
 
         // return the state object to use
         getState: function () {
-            return createNewState();
+            return createNewState(upgradeData);
         },
 
         // a manual gather action has happened to the given state

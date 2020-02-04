3f09af08d35f8cf9bf1f13709c819b77c5bfa714
dustinpfister <dustin.pfister@gmail.com>
2020-02-03 19:41:36 -0500

    func_n_static

--- for_post/js-iife/func_n_static.js
+++ for_post/js-iife/func_n_static.js
@@ -1,23 +1,39 @@
-var bx = (function () {
+var BX = (function () {
 
-    // making the api a function
+    // a private method
+    var getCenterPoint = function (bx) {
+        return {
+            x: bx.x + bx.w / 2,
+            y: bx.y + bx.h / 2
+        };
+    };
+
+    // a public main api a function
     var api = function (opt) {
         opt = opt || {};
         opt.x = opt.x === undefined ? 0 : opt.x;
         opt.y = opt.y === undefined ? 0 : opt.y;
         opt.w = opt.w === undefined ? 32 : opt.w;
         opt.h = opt.h === undefined ? 32 : opt.h;
+        return opt;
     };
 
+    // a public static method
     api.distance = function (bx1, bx2) {
-        var x1 = bx1.x + bx1.w / 2,
-        y1 = bx1.y + bx1.h / 2,
-        x2 = bx2.x + bx2.w / 2,
-        y2 = bx2.y + bx2.h / 2;
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        var pos1 = getCenterPoint(bx1),
+        pos2 = getCenterPoint(bx2);
+        return Math.sqrt(Math.pow(pos1.x - pos2.x, 2) + Math.pow(pos1.y - pos2.y, 2));
     };
 
     return api;
 
 }
     ());
+
+bx1 = BX(),
+bx2 = BX({
+        x: 32,
+        y: 32
+    });
+
+console.log(BX.distance(bx1, bx2).toFixed(2)); // 45.25

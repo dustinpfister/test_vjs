d99a11340e19c25b8b544e48fc9c607eefe607a3
dustinpfister <dustin.pfister@gmail.com>
2020-02-03 19:34:33 -0500

    static distance method

--- for_post/js-iife/func_n_static.js
+++ for_post/js-iife/func_n_static.js
@@ -9,6 +9,14 @@ var bx = (function () {
         opt.h = opt.h === undefined ? 32 : opt.h;
     };
 
+    api.distance = function (bx1, bx2) {
+        var x1 = bx1.x + bx1.w / 2,
+        y1 = bx1.y + bx1.h / 2,
+        x2 = bx2.x + bx2.w / 2,
+        y2 = bx2.y + bx2.h / 2;
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    };
+
     return api;
 
 }

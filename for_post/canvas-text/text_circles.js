44bb70922b3c5c9f9ee8f293c5ae8b4e77256316
dustinpfister <dustin.pfister@gmail.com>
2020-02-06 08:21:56 -0500

    setting radius of circle based on text width

--- for_post/canvas-text/text_circles.js
+++ for_post/canvas-text/text_circles.js
@@ -4,16 +4,25 @@ var tc = {};
 tc.createTextCircleObject = function (opt) {
     opt = opt || {};
     var tcObj = {};
+    tcObj.ctx = opt.ctx || document.createElement('canvas').getContext('2d');
     tcObj.text = opt.text || 'text circle';
     tcObj.fontSize = opt.fontSize || 10;
     tcObj.fontFamily = opt.fontFamily || 'arial';
     tcObj.x = opt.x === undefined ? 0 : opt.x;
     tcObj.y = opt.y === undefined ? 0 : opt.y;
-    tcObj.r = opt.r === undefined ? 25 : opt.r;
+    //tcObj.r = opt.r === undefined ? 25 : opt.r;
     tcObj.h = opt.h === undefined ? 0 : opt.h;
 
     tcObj.textStyles = ['red', 'black'];
     tcObj.circleStyles = ['white', 'black'];
+
+    tcObj.ctx.save();
+    tcObj.ctx.font = tcObj.fontSize + 'px ' + tcObj.fontFamily;
+    tcObj.m = tcObj.ctx.measureText(tcObj.text);
+    console.log(tcObj.ctx.font, tcObj.m.width);
+    tcObj.r = Math.ceil(tcObj.m.width / 2);
+    tcObj.ctx.restore();
+
     return tcObj;
 };
 
@@ -25,6 +34,15 @@ tc.draw = function (ctx, tcOpj) {
 
     ctx.translate(tcObj.x, tcObj.y);
     ctx.rotate(tcObj.h);
+
+    // draw circle
+    styles = tcObj.circleStyles || [];
+    ctx.fillStyle = styles[0] || 'red';
+    ctx.strokeStyle = styles[1] || 'black';
+    ctx.beginPath();
+    ctx.arc(0, 0, tcObj.r, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.stroke();
 
     // draw text
     ctx.font = tcObj.fontSize + 'px ' + tcObj.fontFamily;

89147656fd0c44960531ae04c948d40004545799
dustinpfister <dustin.pfister@gmail.com>
2020-02-10 16:08:39 -0500

    mk_html_table

--- for_post/js-javascript-example-multiplication-table/mtable.js
+++ for_post/js-javascript-example-multiplication-table/mtable.js
@@ -19,6 +19,10 @@ var mTable = (function () {
             cells.push(cell);
             i += 1;
         }
+        cells.w = w;
+        cells.h = h;
+        cells.wOffset = wOffset;
+        cells.hOffset = hOffset;
         return cells;
     };
 
@@ -26,8 +30,22 @@ var mTable = (function () {
     var api = function (w, h, forN, wOffset, hOffset) {
         w = w === undefined ? 10 : w;
         h = h === undefined ? 10 : h;
+        wOffset = wOffset === undefined ? 1 : wOffset;
+        hOffset = hOffset === undefined ? 1 : hOffset;
         forN = forN === undefined ? forN_default : forN;
         return mkCells(w, h, forN, wOffset, hOffset);
+    };
+
+    api.mk_html_div = function (cells, cellSize) {
+        cellSize = cellSize || 32;
+        var html = '<div style=\"position:absolute;\">';
+        html += cells.map(function (cell) {
+            var x = (cell.x - cells.wOffset) * cellSize,
+            y = (cell.y - cells.hOffset) * cellSize;
+            console.log(cell);
+            return '<div style=\"position:absolute;left:' + x + 'px;top:' + y + 'px;\">' + cell.n + '<\/div>';
+        }).join();
+        return html + '<\/div>';
     };
 
     return api; ;
@@ -37,4 +55,6 @@ var mTable = (function () {
 
 var table = mTable(5, 5);
 
-console.log(table);
+//console.log(table);
+
+console.log(mTable.mk_html_div(table, 32));

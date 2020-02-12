e7c761244fc37213f9a8079fd868fa09338548cd
dustinpfister <dustin.pfister@gmail.com>
2020-02-11 19:46:05 -0500

    basic_filter example

---
+++ for_post/js-javascript-foreach/basic_filter.js
@@ -0,0 +1,7 @@
+let arr = [7, 'foo', 13, 'bar', false, -15, null, NaN, 120, 20];
+
+arr = arr.filter((el) => {
+        return typeof el === 'number' && String(el) != 'NaN' && el >= 0 && el <= 100;
+    });
+
+console.log(arr);
--- for_post/js-javascript-foreach/basic_reduce.js
+++ for_post/js-javascript-foreach/basic_reduce.js
@@ -1,3 +1,5 @@
 let arr = [1, 2, 3],
-sum = arr.reduce((s,r)=>{return s+r;});
+sum = arr.reduce((s, r) => {
+        return s + r;
+    });
 console.log(sum); // 6

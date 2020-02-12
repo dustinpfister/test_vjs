1e5eb7d9502b7942637cec429700edb2ec7ee438
dustinpfister <dustin.pfister@gmail.com>
2020-02-12 09:13:56 -0500

    setting font size

--- for_post/canvas-text/example_wiggle_text/text_wiggle.js
+++ for_post/canvas-text/example_wiggle_text/text_wiggle.js
@@ -6,7 +6,7 @@ var wiggleText = (function () {
         halfWidth = obj.str.length * deltaWidth / 2;
         return obj.str.split('').map(function (ch, i) {
             return {
-                x: i * deltaWidth - halfWidth,
+                x: i * deltaWidth - halfWidth + obj.fontSize / 2,
                 y: obj.fontSize / 2 * -1,
                 ch: ch
             };
@@ -35,6 +35,8 @@ var wiggleText = (function () {
             ctx.translate(obj.cx, obj.cy);
             ctx.fillStyle = '#00ffff';
             ctx.font = obj.fontSize + 'px courier';
+            ctx.textBaseline = 'top';
+            ctx.textAlign = 'center';
             obj.chars.forEach(function (c) {
                 ctx.fillText(c.ch, c.x, c.y);
             });

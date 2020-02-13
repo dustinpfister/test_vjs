763c347c4b36e869d25719c58b0eb5d5f8798c4b
dustinpfister <dustin.pfister@gmail.com>
2020-02-12 19:02:07 -0500

    setting defaults in main create1 public method

--- for_post/canvas-example-star/star.js
+++ for_post/canvas-example-star/star.js
@@ -1,8 +1,6 @@
 var starMod = (function () {
 
     var getPoint = function (radian, radius, ox, oy) {
-        ox = ox === undefined ? 0 : ox;
-        oy = oy === undefined ? 0 : oy;
         return {
             x: Math.cos(radian) * radius + ox,
             y: Math.sin(radian) * radius + oy
@@ -12,17 +10,22 @@ var starMod = (function () {
     return {
 
         // create start method 1
-        create1: function (pointCount, outerR, innerR, radianAjust) {
+        create1: function (pointCount, outerR, innerR, radianAjust, ox, oy) {
+
             var i = 0,
             pt,
             r,
             rd = Math.PI * 2 / pointCount,
             points = [];
+
             radianAjust = radianAjust || 0;
+            ox = ox === undefined ? 0 : ox;
+            oy = oy === undefined ? 0 : oy;
+
             while (i < pointCount) {
-                pt = getPoint(rd * i + radianAjust, outerR);
+                pt = getPoint(rd * i + radianAjust, outerR, ox, oy);
                 points.push(pt.x, pt.y);
-                pt = getPoint(rd * i + rd / 2 + radianAjust, innerR);
+                pt = getPoint(rd * i + rd / 2 + radianAjust, innerR, ox, oy);
                 points.push(pt.x, pt.y);
                 i += 1;
             }

789d4ae2440187d47984b800810be22ca56d97c6
dustinpfister <dustin.pfister@gmail.com>
2020-02-14 11:45:01 -0500

    node example working

--- for_post/js-javascript-example-multiplication-table/node/test.js
+++ for_post/js-javascript-example-multiplication-table/node/test.js
@@ -1,5 +1,26 @@
-var mTable = require('./mtable_node');
+var mTable = require('./mtable.js');
 
-var t = mTable();
+var pad = function (n) {
+    return String('0000' + n).slice(-4);
+};
 
-console.log(t);
+var renderTable = function (table) {
+    var i = 0,
+    line = '',
+    len = table.length,
+    cell;
+    while (i < len) {
+
+        cell = table[i];
+        line += pad(cell.n) + '|';
+        if (cell.x === table.w) {
+            console.log(line);
+            line = '';
+        }
+        i += 1;
+    }
+};
+
+var t = mTable(20,100);
+
+renderTable(t);

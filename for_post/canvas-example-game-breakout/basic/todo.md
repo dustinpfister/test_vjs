f161a8b5d6cb66cbedcd5af169ebe0cf61b8455a
dustinpfister <dustin.pfister@gmail.com>
2020-02-16 11:52:05 -0500

    added mod and normalizeAngle util methods

--- for_post/canvas-example-game-breakout/basic/todo.md
+++ for_post/canvas-example-game-breakout/basic/todo.md
@@ -1,5 +1,7 @@
 # todo list for canvas-example-game-breakout  - basic example
-* git hit detection working for paddle
-* get ball bounds and deflection working
+* (done) git hit detection working for paddle
+* (done) get ball bounds and deflection working
+* util method that will return a new angle that is the inversion of the given angle
+
 * git hit detection working for blocks
 * re spawn blocks when all are gone
--- for_post/canvas-example-game-breakout/basic/utils.js
+++ for_post/canvas-example-game-breakout/basic/utils.js
@@ -1,5 +1,9 @@
 var util = {};
 
+util.mod = function mod(x, m) {
+    return (x % m + m) % m;
+};
+
 util.boundingBox = function (x1, y1, w1, h1, x2, y2, w2, h2) {
     return !(
         (y1 + h1) < (y2) ||
@@ -8,3 +12,7 @@ util.boundingBox = function (x1, y1, w1, h1, x2, y2, w2, h2) {
         x1 > (x2 + w2));
 };
 
+// normalize angle method
+util.normalizeAngle: function (a, scale) {
+    return util.mod(a, scale || Math.PI * 2);
+}

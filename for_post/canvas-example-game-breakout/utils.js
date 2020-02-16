58359b18c36f4c9d29d02ed94cf5c376c02717fa
dustinpfister <dustin.pfister@gmail.com>
2020-02-16 10:13:49 -0500

    added a utils lib for breakout

---
+++ for_post/canvas-example-game-breakout/utils.js
@@ -0,0 +1,9 @@
+var util = {};
+
+util.boundingBox = function (a,b) {
+    return !(
+        (a.y + a.h) < (b.y) ||
+        a.y > (b.y + b.h) ||
+        (a.x + a.w) < b.x ||
+        a.x > (b.x + b.w));
+};

4fc4396ec59453ab722fce54f88736a6fcccce17
dustinpfister <dustin.pfister@gmail.com>
2020-02-17 10:39:55 -0500

    fire shot method worked out

--- for_post/canvas-example-game-cannon-shoot/main.js
+++ for_post/canvas-example-game-cannon-shoot/main.js
@@ -18,15 +18,21 @@ var createNewState = function () {
             heading: 0
         },
         cannon: {
-            angle: 0,
-            power: 10
+            heading: 0,
+            power: 1
         }
     };
 };
 
+var fireShot = function (state) {
+    var sh = state.shot,
+    ca = state.cannon;
+    sh.pps = 32 + Math.floor(64 * ca.power);
+    sh.heading = ca.heading;
+    state.mode = 'fired';
+}
+
 var state = createNewState();
-
-
 
 // MAIN APP LOOP
 var loop = function () {

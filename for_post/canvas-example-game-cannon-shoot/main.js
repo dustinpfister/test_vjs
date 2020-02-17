459cac2f4f17373d26ae75d9e4f7f84e603038c5
dustinpfister <dustin.pfister@gmail.com>
2020-02-17 10:25:44 -0500

    started state object design

--- for_post/canvas-example-game-cannon-shoot/main.js
+++ for_post/canvas-example-game-cannon-shoot/main.js
@@ -8,6 +8,26 @@ canvas.width = 320;
 canvas.height = 240;
 ctx.translate(0.5, 0.5);
 
+var createNewState = function () {
+    return {
+        mode: 'aim', // 'aim', 'fired, and 'over' modes
+        shot: {
+            x: 0,
+            y: 0,
+            pps: 64,
+            heading: 0
+        },
+        cannon: {
+            angle: 0,
+            power: 10
+        }
+    };
+};
+
+var state = createNewState();
+
+
+
 // MAIN APP LOOP
 var loop = function () {
     requestAnimationFrame(loop);

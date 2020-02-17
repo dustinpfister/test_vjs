47a414cdebcb83c029ab4d8045a849730fa2cccb
dustinpfister <dustin.pfister@gmail.com>
2020-02-17 09:47:24 -0500

    canvas-example-game-cannon-shoot

---
+++ for_post/canvas-example-game-cannon-shoot/main.js
@@ -0,0 +1,17 @@
+
+// CANVAS
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d'),
+container = document.getElementById('gamearea') || document.body;
+container.appendChild(canvas);
+canvas.width = 320;
+canvas.height = 240;
+ctx.translate(0.5, 0.5);
+
+// MAIN APP LOOP
+var loop = function () {
+    requestAnimationFrame(loop);
+
+    draw.background(ctx, canvas);
+};
+loop();

82c1a710be041112fceb701fdd0579ad447e4a2b
dustinpfister <dustin.pfister@gmail.com>
2020-02-19 14:31:51 -0500

    basic idea

--- for_post/canvas-example-fractal/s1_basic/basic.js
+++ for_post/canvas-example-fractal/s1_basic/basic.js
@@ -7,20 +7,28 @@ var opt = {
         var bxArr = api.ani.bxArr = [];
         var i = 0,
         per,
-        bxCount = 5,
+        bxCount = 10,
         maxSize = canvas.width;
         while (i < bxCount) {
+            // figure out the percent for the current box
             per = api.per + 1 / bxCount * i;
             per %= 1;
+            // create and push the box
             bx = {};
             bx.w = maxSize * per;
             bx.h = maxSize * per;
             bx.x = canvas.width / 2 - (bx.w / 2);
             bx.y = canvas.height / 2 - (bx.h / 2);
+            bx.per = bx.w / maxSize;
             bxArr.push(bx);
             i += 1;
         }
-
+        bxArr.sort(function (a, b) {
+            if (a.per > b.per) {
+                return 1;
+            }
+            return -1;
+        });
     }
 };
 
--- for_post/canvas-example-fractal/s1_basic/draw.js
+++ for_post/canvas-example-fractal/s1_basic/draw.js
@@ -1,10 +1,9 @@
 // DRAW
 var draw = {};
 draw.bx = function (ctx, bx) {
-    ctx.fillStyle = 'red';
     ctx.strokeStyle = 'white';
-    ctx.globalAlpha = 0.5;
-    ctx.lineWidth = 3;
+    ctx.globalAlpha = 0.05 + bx.per * 0.95;
+    ctx.lineWidth = 6;
     ctx.beginPath();
     ctx.rect(bx.x, bx.y, bx.w, bx.h);
     ctx.stroke();
@@ -12,10 +11,12 @@ draw.bx = function (ctx, bx) {
 draw.bxArr = function (ctx, ani) {
     var i = 0,
     len = ani.bxArr.length;
+    ctx.save();
     while (i < len) {
         draw.bx(ctx, ani.bxArr[i]);
         i += 1;
     }
+    ctx.restore();
 };
 draw.back = function (ctx, canvas) {
     ctx.fillStyle = 'black';

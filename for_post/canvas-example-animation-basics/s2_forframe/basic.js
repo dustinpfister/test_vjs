2e9d5ce25d18d518658f1d4fca56fa8770567564
dustinpfister <dustin.pfister@gmail.com>
2020-02-19 08:50:33 -0500

    basic example of ff_basic looking good

--- for_post/canvas-example-animation-basics/s2_forframe/basic.js
+++ for_post/canvas-example-animation-basics/s2_forframe/basic.js
@@ -1,6 +1,6 @@
-
-
-var drawBx = function (ctx, bx) {
+// DRAW
+var draw = {};
+draw.bx = function (ctx, bx) {
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'white';
     ctx.beginPath();
@@ -8,31 +8,29 @@ var drawBx = function (ctx, bx) {
     ctx.fill();
     ctx.stroke();
 };
+draw.back = function (ctx, canvas) {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
 
 var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
 ctx.translate(0.5, 0.5);
 
-var boxAni = FF({
-        forFrame: function (api, f, mf) {
-            var bx = api.ani.bx = {
-                w: 32,
-                h: 32
-            };
-            bx.x = (canvas.width - 32) * api.per;
-            bx.y = canvas.height / 2 - 16;
-        }
-    });
-
-var bx = boxAni(-5, 50).bx;
-console.log(bx);
-ctx.fillStyle = 'black';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-drawBx(ctx, bx);
-
-/*
-var loop = function () {
-requestAnimationFrame(loop);
+var opt = {
+    forFrame: function (api, f, mf) {
+        var bx = api.ani.bx = {
+            w: 32,
+            h: 32
+        };
+        bx.x = (canvas.width - 32) * api.per;
+        bx.y = canvas.height / 2 - 16 + canvas.height / 4 * api.bias;
+    }
 };
-loop();
-*/
+
+// create an animation method
+var ani = FF(opt);
+
+var bx = ani(25, 50).bx;
+draw.back(ctx, canvas)
+draw.bx(ctx, bx);

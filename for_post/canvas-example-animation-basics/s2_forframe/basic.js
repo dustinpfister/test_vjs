582fa254381421055e50042f50bb7ab48a04ef22
dustinpfister <dustin.pfister@gmail.com>
2020-02-19 08:39:23 -0500

    wraping frame index

--- for_post/canvas-example-animation-basics/s2_forframe/basic.js
+++ for_post/canvas-example-animation-basics/s2_forframe/basic.js
@@ -1,27 +1,4 @@
-var FF = function (opt) {
-    var api = {};
-    opt = opt || {};
 
-    api.ani = {};
-    api.forFrame = opt.forFrame || function () {};
-    var forFrame = function (frameIndex, maxFrame) {
-        api.frameIndex = frameIndex;
-        api.maxFrame = maxFrame;
-        api.per = frameIndex / maxFrame;
-        api.forFrame.call(api, api, frameIndex, maxFrame);
-        // return just the ani object
-        return api.ani;
-    };
-    // public method used to set by frameIndex
-    // over max Frames
-    return function (frame, maxFrame) {
-        frame = frame === undefined ? 0 : frame;
-        maxFrame = maxFrame === undefined ? 50 : maxFrame;
-        frame = frame > maxFrame ? frame % maxFrame : frame;
-        forFrame(frame, maxFrame);
-        return api.ani;
-    };
-};
 
 var drawBx = function (ctx, bx) {
     ctx.fillStyle = 'red';
@@ -47,7 +24,7 @@ var boxAni = FF({
         }
     });
 
-var bx = boxAni(-75, 50).bx;
+var bx = boxAni(-5, 50).bx;
 console.log(bx);
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
--- for_post/canvas-example-animation-basics/s2_forframe/ff_basic.js
+++ for_post/canvas-example-animation-basics/s2_forframe/ff_basic.js
@@ -14,10 +14,15 @@ var FF = function (opt) {
     // public method used to set by frameIndex
     // over max Frames
     return function (frame, maxFrame) {
+        // defaults if undefined for frame index and max frame
         frame = frame === undefined ? 0 : frame;
         maxFrame = maxFrame === undefined ? 50 : maxFrame;
+        // wrap frame index
         frame = frame > maxFrame ? frame % maxFrame : frame;
+        frame = frame < 0 ? maxFrame - Math.abs(frame) % maxFrame : frame;
+        // call forFrame with parsed frame and maxFrame
         forFrame(frame, maxFrame);
+        // return just the animation object
         return api.ani;
     };
 };

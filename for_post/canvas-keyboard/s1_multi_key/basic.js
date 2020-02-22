4e6496e0c6aaf88b3b5e79a120856495d5649744
dustinpfister <dustin.pfister@gmail.com>
2020-02-22 10:14:50 -0500

    basic idea of multi key is working

--- for_post/canvas-keyboard/s1_multi_key/basic.js
+++ for_post/canvas-keyboard/s1_multi_key/basic.js
@@ -1,33 +1,22 @@
 // Draw
-var draw = {};
-draw.back = function () {
+var draw = function (ctx, canvas, state) {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
-draw.dispObj = function (ctx, dispObj) {
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = 'white';
 };
 
 var canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
 
-var game = {
-    keys: [],
-    dispObj: {
-        x: 0,
-        y: 0
-    }
+var state = {
+    keys: {}
 };
 
-game.keyHandler = function (e) {
-    game.keys[e.keyCode] = e.type === 'keyup' ? true : false
+var keyHandler = function (e) {
+    state.keys[e.key] = e.type === 'keyup' ? false : true;
+	console.log(state.keys);
+    draw(ctx, canvas, state);
 };
 
-var keyDown = function (e) {
-    game.keys[e.keyCode] = true;
-};
-var keyUp = function (e) {
-    game.keys[e.keyCode] = true;
-};
-window.addEventListener('keydown', game.keyHandler);
-window.addEventListener('keyup', game.keyHandler);
+window.addEventListener('keydown', keyHandler);
+window.addEventListener('keyup', keyHandler);

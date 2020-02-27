d95cbfd82364f49976c614a44928705dc188212c
dustinpfister <dustin.pfister@gmail.com>
2020-02-27 11:09:30 -0500

    js-javascript-example-tax-brackets folder started

---
+++ for_post/js-javascript-example-tax-brackets/s1_bracket_module/tax_brackets.js
@@ -0,0 +1,21 @@
+var tax = (function () {
+
+    var tableData = '10:9700,12:39475';
+
+    var createBracketObject = function (rate, lower, upper) {
+        return {
+            rate: rate,
+            lower: lower,
+            upper: upper
+            amount: 0
+        };
+    };
+
+    var createBracketsTable: function (tableData) {};
+
+    var api = {};
+
+    return api; ;
+
+}
+    ());

4486ce1f1d65cbf17e23809093be00c6ed771893
dustinpfister <dustin.pfister@gmail.com>
2020-03-10 17:26:23 -0400

    started a get disc path method

--- for_post/canvas-example-clay-shoot/game.js
+++ for_post/canvas-example-clay-shoot/game.js
@@ -1,7 +1,6 @@
 var canvas = document.createElement('canvas'),
-ctx = canvas.getContext('2d'),
-container = document.getElementById('gamearea') || document.body;
-container.appendChild(canvas);
+ctx = canvas.getContext('2d');
+document.body.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
 ctx.translate(0.5, 0.5);
@@ -15,3 +14,60 @@ var getCanvasRelative = function (e) {
         bx: bx
     };
 };
+
+var drawPoints = function (ctx, points, close) {
+    var i = 2,
+    len = points.length;
+    ctx.beginPath();
+    ctx.moveTo(points[0], points[1]);
+    while (i < len) {
+        ctx.lineTo(points[i], points[i + 1]);
+        i += 2;
+    }
+    if (close) {
+        ctx.closePath();
+    }
+    ctx.stroke();
+
+};
+
+var getDiscPath = function (opt) {
+    opt = opt || {};
+    opt.canvas = opt.canvas === undefined ? {
+        width: 320,
+        height: 240
+    }
+     : opt.canvas;
+    opt.leftSize = opt.sr === undefined ? true : false;
+    opt.angleMax = 20;
+    opt.angleMin = 70;
+    opt.anglePer = opt.anglePer === undefined ? 1 : opt.anglePer;
+    opt.pointCount = 50;
+    var sx = opt.leftSize ? 0 : opt.canvas.width,
+    sy = opt.canvas.height,
+    i = 0,
+    x,
+    y,
+    points = [],
+    angleDelta = (opt.angleMax - opt.angleMin) * opt.anglePer;
+    while (i < opt.pointCount) {
+        var a = (opt.angleMin + angleDelta / opt.pointCount * i),
+        mr = Math.PI / 180 * opt.angleMin,
+        sr = opt.leftSize ? Math.PI * 2 - mr : Math.PI + mr,
+        d = opt.leftSize ? 1 : -1,
+        r = sr + a / 90 * (Math.PI * 2) * d;
+        x = Math.cos(r) * canvas.width,
+        y = Math.sin(r) * canvas.height,
+        points.push(x, y);
+        i += 1;
+    }
+
+    return points;
+};
+
+var p = getDiscPath();
+console.log(p);
+ctx.fillStyke = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = 'red';
+drawPoints(ctx, p, false);

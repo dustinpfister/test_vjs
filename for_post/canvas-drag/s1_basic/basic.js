5029a5a1d9d7cf35a8823fa4c5ff49eae9d51547
dustinpfister <dustin.pfister@gmail.com>
2020-03-10 19:04:29 -0400

    pointer methods

--- for_post/canvas-drag/s1_basic/basic.js
+++ for_post/canvas-drag/s1_basic/basic.js
@@ -1,10 +1,4 @@
-var canvas = document.getElementById('mycanvas'),
-ctx = canvas.getContext('2d');
-document.body.appendChild(canvas);
-canvas.width = 320;
-canvas.height = 240;
-ctx.translate(0.5, 0.5);
-
+// get canvas relative and distance methods
 var getCanvasRelative = function (e) {
     var canvas = e.target,
     bx = canvas.getBoundingClientRect();
@@ -14,11 +8,11 @@ var getCanvasRelative = function (e) {
         bx: bx
     };
 };
-
 var distance = function (x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
 
+// draw
 var draw = function (ctx, canvas, state) {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -28,6 +22,40 @@ var draw = function (ctx, canvas, state) {
     ctx.fill();
 };
 
+// Event handlers
+var pointerDown = function (state) {
+    return function (e) {
+        var pos = getCanvasRelative(e);
+        if (distance(pos.x, pos.y, state.x, state.y) <= state.r) {
+            state.down = true;
+        }
+    };
+};
+var pointerMove = function (state) {
+    return function (e) {
+        var pos = getCanvasRelative(e);
+        if (state.down) {
+            state.x = pos.x;
+            state.y = pos.y;
+            draw(ctx, canvas, state);
+        }
+    };
+};
+var pointerUp = function (state) {
+    return function (e) {
+        state.down = false;
+    };
+};
+
+// set up canvas
+var canvas = document.getElementById('mycanvas'),
+ctx = canvas.getContext('2d');
+document.body.appendChild(canvas);
+canvas.width = 320;
+canvas.height = 240;
+ctx.translate(0.5, 0.5);
+
+// the state object
 var state = {
     x: canvas.width / 2,
     y: canvas.height / 2,
@@ -35,26 +63,13 @@ var state = {
     down: false
 };
 
-canvas.addEventListener('mousedown', function (e) {
+// attach for mouse and touch
+canvas.addEventListener('mousedown', pointerDown(state));
+canvas.addEventListener('mousemove', pointerMove(state));
+canvas.addEventListener('mouseup', pointerUp(state));
+canvas.addEventListener('touchstart', pointerDown(state));
+canvas.addEventListener('touchmove', pointerMove(state));
+canvas.addEventListener('touchend', pointerUp(state));
 
-    var pos = getCanvasRelative(e);
-    if (distance(pos.x, pos.y, state.x, state.y) <= state.r) {
-        state.down = true;
-    }
-
-});
-
-canvas.addEventListener('mousemove', function (e) {
-    var pos = getCanvasRelative(e);
-    if (state.down) {
-        state.x = pos.x;
-        state.y = pos.y;
-        draw(ctx, canvas, state);
-    }
-});
-
-canvas.addEventListener('mouseup', function (e) {
-    state.down = false;
-});
-
+// starting draw
 draw(ctx, canvas, state);

fe7b4e707829be8cef08f867c20df0d82d001603
dustinpfister <dustin.pfister@gmail.com>
2020-03-10 11:28:51 -0400

    canvas-line section folders

--- for_post/canvas-example-clay-shoot/game.js
+++ for_post/canvas-example-clay-shoot/game.js
@@ -4,5 +4,14 @@ container = document.getElementById('gamearea') || document.body;
 container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
-
 ctx.translate(0.5, 0.5);
+
+var getCanvasRelative = function (e) {
+    var canvas = e.target,
+    bx = canvas.getBoundingClientRect();
+    return {
+        x: (e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - bx.left,
+        y: (e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - bx.top,
+        bx: bx
+    };
+};

209fa2b45cf0f824bb4dd8c2ca7f6e0a0e3e7eaa
dustinpfister <dustin.pfister@gmail.com>
2020-03-13 06:14:55 -0400

    get_dir.js

---
+++ for_post/canvas-angle-shortest-direction/s1_get_dir_method/git_dir.js
@@ -0,0 +1,26 @@
+
+var getDir = (function () {
+    // modulo
+    var mod = function (x, m) {
+        return (x % m + m) % m;
+    };
+    // normalize an angle to half radians
+    var normalizeHalf = function (n) {
+        var c = Math.PI * 2,
+        h = c / 2;
+        return mod(n + h, c) - h;
+    };
+    // public get dir method
+    return function (a, b) {
+        var z = a - b;
+        if (a === b) {
+            return 0;
+        }
+        if (normalizeHalf(z) < 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    };
+}
+    ());

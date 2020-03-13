0a33a3d46fed6d49787d475a1e70ee7a6bb23e59
dustinpfister <dustin.pfister@gmail.com>
2020-03-13 09:30:35 -0400

    array values

--- for_post/canvas-angle-shortest-direction/s2_array_example/array.js
+++ for_post/canvas-angle-shortest-direction/s2_array_example/array.js
@@ -14,11 +14,23 @@ var findDir = (function () {
             return -1;
         }
     };
-}());
+}
+    ());
 
+// works with arrays
 var arr = [0, 1, 2, 3, 4],
 len = arr.length;
-
 console.log(findDir(len, 1, 4)); // -1
 console.log(findDir(len, 4, 0)); // 1
 console.log(findDir(len, 2, 2)); // 0
+
+// works with an object of index/degree/number values
+var state = {
+    iMax: 32,
+    iCurrent: 16,
+    iTarget: 3
+};
+console.log(findDir(state.iMax, state.iCurrent, state.iTarget)); // -1
+
+// index/degree/number values that can be converted to radians
+console.log( Math.PI * 2 / state.iMax * state.iCurrent ); // 3.14159...

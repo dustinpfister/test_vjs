a6ff82c1c330591e68c689ae222c1f8b2bccd087
dustinpfister <dustin.pfister@gmail.com>
2020-03-13 09:23:23 -0400

    findDir method

--- for_post/canvas-angle-shortest-direction/s2_array_example/array.js
+++ for_post/canvas-angle-shortest-direction/s2_array_example/array.js
@@ -1,23 +1,24 @@
-var mod = function (x, m) {
-    return (x % m + m) % m;
-};
+var findDir = (function () {
+    var mod = function (x, m) {
+        return (x % m + m) % m;
+    };
+    return function (indexMax, indexCurrent, indexTarget) {
+        var z = indexCurrent - indexTarget,
+        h = indexMax / 2;
+        if (indexCurrent === indexTarget) {
+            return 0;
+        }
+        if (mod(z + h, indexMax) - h < 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    };
+}());
 
-var findDir = function (arr, indexCurrent, indexTarget) {
-    var z = indexCurrent - indexTarget,
-    c = arr.length,
-    h = c / 2;
-    if (indexCurrent === indexTarget) {
-        return 0;
-    }
-    if (mod(z + h, c) - h < 0) {
-        return 1;
-    } else {
-        return -1;
-    }
-};
+var arr = [0, 1, 2, 3, 4],
+len = arr.length;
 
-var arr = [0, 1, 2, 3, 4];
-
-console.log(findDir(arr, 1, 4)); // -1
-console.log(findDir(arr, 4, 0)); // 1
-console.log(findDir(arr, 2, 2)); // 0
+console.log(findDir(len, 1, 4)); // -1
+console.log(findDir(len, 4, 0)); // 1
+console.log(findDir(len, 2, 2)); // 0

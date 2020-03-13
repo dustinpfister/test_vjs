69f6747761945a88d4d7519db907125edd27e3b6
dustinpfister <dustin.pfister@gmail.com>
2020-03-13 09:20:04 -0400

    made find dir method simpler

--- for_post/canvas-angle-shortest-direction/s2_array_example/array.js
+++ for_post/canvas-angle-shortest-direction/s2_array_example/array.js
@@ -2,18 +2,14 @@ var mod = function (x, m) {
     return (x % m + m) % m;
 };
 
-var normalizeHalf = function (arr, index) {
-    var c = arr.length,
-    h = c / 2;
-    return mod(index + h, c) - h;
-};
-
 var findDir = function (arr, indexCurrent, indexTarget) {
-    var z = indexCurrent - indexTarget;
+    var z = indexCurrent - indexTarget,
+    c = arr.length,
+    h = c / 2;
     if (indexCurrent === indexTarget) {
         return 0;
     }
-    if (normalizeHalf(arr, z) < 0) {
+    if (mod(z + h, c) - h < 0) {
         return 1;
     } else {
         return -1;
@@ -22,6 +18,6 @@ var findDir = function (arr, indexCurrent, indexTarget) {
 
 var arr = [0, 1, 2, 3, 4];
 
-console.log( findDir(arr, 1, 4) ); // -1
-console.log( findDir(arr, 4, 0) ); // 1
-console.log( findDir(arr, 2, 2) ); // 0
+console.log(findDir(arr, 1, 4)); // -1
+console.log(findDir(arr, 4, 0)); // 1
+console.log(findDir(arr, 2, 2)); // 0

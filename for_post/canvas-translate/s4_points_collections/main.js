1cd1c4b9d4687dcc4f4976bc914db7fcb7260d3b
dustinpfister <dustin.pfister@gmail.com>
2020-03-17 18:57:46 -0400

    disp object format working

--- for_post/canvas-translate/s4_points_collections/main.js
+++ for_post/canvas-translate/s4_points_collections/main.js
@@ -4,6 +4,25 @@ ctx.translate(0.5, 0.5);
 canvas.width = 320;
 canvas.height = 240;
 
+var dispObjects = [{
+        x: 64,
+        y: 64,
+        r: Math.PI / 180 * 10,
+        points: [-32, -32, 32, -32, 32, 32]
+    }
+];
 
 draw.back(ctx, canvas);
-draw.points(ctx, [10,10, 100, 10, 100,100], true, 'white', 'red', 3);
+
+var i = 0,
+disp,
+len = dispObjects.length;
+while (i < len) {
+    disp = dispObjects[i];
+    ctx.save();
+    ctx.translate(disp.x, disp.y);
+    ctx.rotate(disp.r);
+    draw.points(ctx, disp.points, true, 'white', 'red', 3);
+    ctx.restore();
+    i += 1;
+}

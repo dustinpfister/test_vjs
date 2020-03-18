8e4f1d6abd9d265bda7de67010ecf72596fa4cbf
dustinpfister <dustin.pfister@gmail.com>
2020-03-18 12:03:57 -0400

    going with a Particle class

--- for_post/canvas-example-particles-binary/particles.js
+++ for_post/canvas-example-particles-binary/particles.js
@@ -1,25 +1,40 @@
 
 var paricles = (function () {
 
-    var DEFAULT_POOL_SIZE = 20;
+    var DEFAULT_POOL_SIZE = 20,
+    PARTICLE_MIN_RADIUS = 8;
 
+    /*
     var createPartcile = function () {
-        return {
-            x: -1,
-            y: -1,
-            heading: 0,
-            bits: '00', // [0,0] inactive, [1,0] // blue, [0,1] red, [1,1] // explode
-            pps: 32, // pixels per second
-            life: 3000 // life left in milliseconds when in explode mode
-        };
+    return {
+    x: -1,
+    y: -1,
+    heading: 0,
+    bits: '00', // [0,0] inactive, [1,0] // blue, [0,1] red, [1,1] // explode
+    pps: 32, // pixels per second
+    life: 3000 // life left in milliseconds when in explode mode
     };
+    };
+     */
+
+    var Particle = function () {
+        this.x = -1;
+        this.y = -1;
+        this.heading = 0;
+        this.bits = '00'; // [0,0] inactive, [1,0] // blue, [0,1] red, [1,1] // explode
+        this.pps = 32; // pixels per second
+        this.life = 3000; // life left in milliseconds when in explode mode
+    };
+
+    Particle.prototype.radius = PARTICLE_MIN_RADIUS;
 
     var createPool = function () {
         var len = DEFAULT_POOL_SIZE,
         i = len,
         pool = [];
         while (i--) {
-            pool.push(createPartcile());
+            //pool.push(createPartcile());
+            pool.push(new Particle());
         }
         return pool;
     };

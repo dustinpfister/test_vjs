b2bdee1581141248d4fb8469be68699516f537ae
dustinpfister <dustin.pfister@gmail.com>
2020-03-18 13:49:36 -0400

    base alpha effect

--- for_post/canvas-example-particles-binary/draw.js
+++ for_post/canvas-example-particles-binary/draw.js
@@ -12,17 +12,19 @@ var draw = (function () {
         pool: function (state) {
 
             var i = state.pool.length;
+            ctx.globalAlpha = 0.8;
             while (i--) {
                 var part = state.pool[i];
                 if (part.bits != '00') {
                     var color = part.bits === '01' ? 'blue' : 'red';
-                    color = part.bits === '11' ? 'white' : color;
+                    color = part.bits === '11' ? 'purple' : color;
                     ctx.beginPath();
                     ctx.fillStyle = color;
                     ctx.arc(part.x, part.y, part.radius, 0, Math.PI * 2);
                     ctx.fill();
                 }
             }
+            ctx.globalAlpha = 1;
 
         },
 

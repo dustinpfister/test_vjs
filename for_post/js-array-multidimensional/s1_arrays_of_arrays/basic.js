ceedb9039988aab5d402e262a90ae691419eed0a
dustinpfister <dustin.pfister@gmail.com>
2020-03-31 13:00:19 -0400

    js-array-multidimensional

---
+++ for_post/js-array-multidimensional/s1_arrays_of_arrays/basic.js
@@ -0,0 +1,13 @@
+// a 2d array of arrays can be created
+// in a literal way like this
+var grid = [
+    ['0', '1', '2', '3'],
+    ['4', '5', '6', '7'],
+    ['8', '9', 'a', 'b'],
+    ['c', 'd', 'e', 'f']
+];
+
+// elements can then be accessed like this
+var cell = grid[3][2];
+
+console.log(cell); // 'e'

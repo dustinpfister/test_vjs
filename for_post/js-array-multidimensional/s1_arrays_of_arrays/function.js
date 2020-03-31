5d50557a9f0aa247e0d5c47590ea195a8006dcf6
dustinpfister <dustin.pfister@gmail.com>
2020-03-31 13:11:21 -0400

    function example looking good

--- for_post/js-array-multidimensional/s1_arrays_of_arrays/function.js
+++ for_post/js-array-multidimensional/s1_arrays_of_arrays/function.js
@@ -1,15 +1,22 @@
-
-var createGrid = function () {
+// A function of some kind like this can be used
+// to create the gird
+var createGrid = function (w, h, forCell) {
     var grid = [],
     y = 0,
     row,
     i = 0,
     x;
+    w = w === undefined ? 3 : w;
+    h = h === undefined ? 3 : h;
+    forCell = forCell === undefined ? function (i) {
+        return i;
+    }
+     : forCell;
     while (y < 4) {
         x = 0;
         row = [];
         while (x < 4) {
-            row.push(i.toString(16));
+            row.push(forCell(i));
             i += 1;
             x += 1;
         }
@@ -19,9 +26,10 @@ var createGrid = function () {
     return grid;
 };
 
-var grid = createGrid();
-
-// elements can then be accessed like this
+// I can then create the same grid like this
+var grid = createGrid(4, 4, function (i) {
+        return i.toString(16);
+    });
 var cell = grid[3][2];
 
 console.log(cell); // 'e'

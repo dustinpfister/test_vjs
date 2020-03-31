b5f0a566d50b270804e64e4fdbfe06a7233ebf8a
dustinpfister <dustin.pfister@gmail.com>
2020-03-31 13:27:50 -0400

    literal example

--- for_post/js-array-multidimensional/s2_linear_array/literal.js
+++ for_post/js-array-multidimensional/s2_linear_array/literal.js
@@ -1,9 +1,10 @@
-var grid = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+var grid = {
+    w: 4,
+    h: 4,
+    cells: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'],
+    get: function (x, y) {
+        return this.cells[y * this.w + x];
+    }
+};
 
-var w = 4,
-x = 2,
-y = 3,
-i = y * w + x,
-cell = grid[i];
-
-console.log(cell); // 'e'
+console.log(grid.get(2, 3)); // 'e'

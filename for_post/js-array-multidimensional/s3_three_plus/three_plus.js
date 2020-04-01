cb9caf427f4b991a1ece31709fed3c779521d050
dustinpfister <dustin.pfister@gmail.com>
2020-04-01 10:43:33 -0400

    stuck

--- for_post/js-array-multidimensional/s3_three_plus/three_plus.js
+++ for_post/js-array-multidimensional/s3_three_plus/three_plus.js
@@ -33,21 +33,24 @@ var threePlus = function (dims, forCell) {
     pos = {};
     while (i < len) {
 
-        Object.keys(dims).forEach(function (d, di) {
-            var val = dims[d];
+        Object.keys(dims).forEach(function (dimName, di) {
+            var val = dims[dimName];
+            // product of all dims from 0 to current
+            var p = getDimProduct(dims, 0, di);
+            // product of dims from less one from current to current
+            var pdi = di - 1 < 0 ? 0 : di - 1;
+            var p2 = getDimProduct(dims, pdi, di);
+            var p3 = getDimProduct(dims, pdi, di - 1);
 
-            var p = getDimProduct({
-                    x: 2,
-                    y: 2,
-                    z: 2
-                }, 0, di);
-            var p2 = getDimProduct({
-                    x: 2,
-                    y: 2,
-                    z: 2
-                }, di - 1 < 0 ? 0 : di - 1, di);
+            //var s1 = (i % p2 + Math.floor(i / p)) % val;
 
-            pos[d] = (i % p2 + Math.floor(i / p)) % val;
+            var a = i % p2,
+            b = Math.floor(i / p),
+            c = a + b,
+            s1 = c % val;
+
+            pos[dimName] = s1;
+
         });
 
         var cell = {};
@@ -63,25 +66,35 @@ var threePlus = function (dims, forCell) {
 
 };
 
-var three = threePlus();
+var print = function (threePlus) {
+    return threePlus.map(function (a) {
+        return Object.values(a).join('')
+    }).sort().join('\n');
+}
+
+var three = threePlus({
+        x: 2,
+        y: 2,
+        z: 2,
+        //a: 2,
+        //b: 2
+    });
 
 console.log(three);
 
-/*
-var p = getDimProduct({
-x: 2,
-y: 2,
-z: 2
-}, 0, 2);
+// output
+//  [ { i: 0, x: 0, y: 0, z: 0 },
+//    { i: 1, x: 1, y: 1, z: 1 },
+//    { i: 2, x: 1, y: 0, z: 0 },
+//    { i: 3, x: 0, y: 1, z: 1 },
+//    { i: 4, x: 0, y: 1, z: 0 },
+//    { i: 5, x: 1, y: 0, z: 1 } ]
 
-console.log(p);
- */
 
-/*
-[ { i: 0, x: 0, y: 0, z: 0 },
-{ i: 1, x: 1, y: 0, z: 0 },
-{ i: 2, x: 0, y: 1, z: 0 },
-{ i: 3, x: 1, y: 1, z: 0 },
-{ i: 4, x: 0, y: 0, z: 1 },
-{ i: 5, x: 1, y: 1, z: 1 } ]
-*/
+// Desired output
+//  [ { i: 0, x: 0, y: 0, z: 0 },
+//    { i: 1, x: 1, y: 0, z: 0 },
+//    { i: 2, x: 0, y: 1, z: 0 },
+//    { i: 3, x: 1, y: 1, z: 0 },
+//    { i: 4, x: 0, y: 0, z: 1 },
+//    { i: 5, x: 1, y: 1, z: 1 } ]

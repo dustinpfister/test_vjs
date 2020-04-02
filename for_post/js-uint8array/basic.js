eae207cf3e31ce48bd0059dbfddabaa18a948baa
dustinpfister <dustin.pfister@gmail.com>
2020-04-02 12:58:22 -0400

    js-uint8array

---
+++ for_post/js-uint8array/basic.js
@@ -0,0 +1,7 @@
+var arr = new Uint8Array(3);
+
+arr[0] = 128;
+arr[1] = -1;
+arr[2] = 199.75;
+
+console.log(arr);
---
+++ for_post/js-uint8array/clamped.js
@@ -0,0 +1,8 @@
+var clamped = new Uint8ClampedArray(3),
+notClamped = new Uint8Array(3);
+
+clamped[0] = 258;
+notClamped[0] = 258;
+
+console.log(clamped[0]); // 255
+console.log(notClamped[0]); // 2

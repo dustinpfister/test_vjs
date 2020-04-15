c584ddbe215875d59c67463f12acd4708935bd03
dustinpfister <dustin.pfister@gmail.com>
2020-04-15 10:54:51 -0400

    started s2 folder for canvas-hit-region

---
+++ for_post/canvas-hit-region/s2_box_module/box.js
@@ -0,0 +1,39 @@
+
+var Box = (function () {
+
+    var clone = function (bx) {
+        return JSON.parse(JSON.stringify(bx));
+    };
+
+    var api = {};
+
+    api.create = function (opt) {
+        opt = opt || {};
+        return {
+            x: opt.x === undefined ? 0 : opt.x,
+            y: opt.y === undefined ? 0 : opt.y,
+            w: opt.w === undefined ? 32 : opt.w,
+            h: opt.h === undefined ? 32 : opt.h
+        };
+    };
+
+    api.boundingBox = function (bx1, bx2) {
+        return !((bx1.y + bx1.h) < bx2.y ||
+            bx1.y > (bx2.y + bx2.h) ||
+            (bx1.x + bx1.w) < bx2.x ||
+            bx1.x > (bx2.x + bx2.w));
+    };
+
+    api.moveByHeading = function (bx, heading, delta) {
+        heading = heading === undefined ? 0 : heading;
+        delta = delta === undefined ? 1 : delta;
+        var nbx = clone(bx);
+        nbx.x = nbx.x + Math.cos(heading) * delta;
+        nbx.y = nbx.y + Math.sin(heading) * delta;
+        return nbx;
+    };
+
+    return api;
+
+}
+    ());
---
+++ for_post/canvas-hit-region/s2_box_module/draw.js
@@ -0,0 +1,16 @@
+var draw = {};
+
+draw.back = function (ctx, canvas) {
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+// draw a box
+draw.box = function (ctx, bx, fill, stroke) {
+    ctx.fillStyle = fill || '#ffffff';
+    ctx.strokeStyle = stroke || '#000000';
+    ctx.beginPath();
+    ctx.rect(bx.x, bx.y, bx.w, bx.h);
+    ctx.fill();
+    ctx.stroke();
+};

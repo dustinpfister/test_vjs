ccfcfbb42bd24d478193a25f6551a98a887ed653
dustinpfister <dustin.pfister@gmail.com>
2020-04-21 18:16:09 -0400

    dist example looking good

--- for_post/js-math-random/s4_distribution/dist.js
+++ for_post/js-math-random/s4_distribution/dist.js
@@ -1,32 +1,28 @@
-var distMethod = function (count, w, h, method) {
-    count = count === undefined ? 100 : count;
-    w = w === undefined ? 320 : w;
-    h = h === undefined ? 240 : h;
-    method = method || function (i) {
+var dist = (function () {
+    var api = function (count, w, h, method) {
+        count = count === undefined ? 100 : count;
+        w = w === undefined ? 320 : w;
+        h = h === undefined ? 240 : h;
+        method = method || api.methodOne;
+        var points = [],
+        i = count;
+        while (i--) {
+            points.push(method(i, w, h));
+        }
+        return points;
+    };
+    api.methodOne = function (i, w, h) {
         return {
             x: Math.floor(Math.random() * w),
             y: Math.floor(Math.random() * h)
         };
     };
-    var points = [],
-    i = count;
-    while (i--) {
-        points.push(method(i));
-    }
-    return points;
-};
-
-var distMethodTwo = function (count, w, h) {
-    count = count === undefined ? 100 : count;
-    w = w === undefined ? 320 : w;
-    h = h === undefined ? 240 : h;
-    var points = [],
-    i = count;
-    while (i--) {
-        points.push({
-            x: Math.floor(Math.random() * w),
-            y: Math.floor(Math.random() * h)
-        });
-    }
-    return points;
-};
+    api.methodTwo = function (i, w, h) {
+        return {
+            x: Math.pow(2, Math.log(w) / Math.log(2) * Math.random()),
+            y: Math.pow(2, Math.log(h) / Math.log(2) * Math.random())
+        };
+    };
+    return api;
+}
+    ());

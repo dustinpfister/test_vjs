d46003e1e9b28e3947edfd955bf5936642a2341b
dustinpfister <dustin.pfister@gmail.com>
2020-04-21 10:02:09 -0400

    new basic example

--- for_post/js-math-log/s1_basics/basic.js
+++ for_post/js-math-log/s1_basics/basic.js
@@ -1,9 +1,12 @@
 
-
+// the value of number (a)
 var a = 1000,
-p = Math.log(a),
+// The math log of (a) returns the power (p) 
+// that will result in (a) when used with Math.pow where
+// Math.E is the base
+p = Math.log(a), 
 b = Math.pow( Math.E, p);
 
-console.log(a);
-console.log(p);
-console.log(b);
+console.log(a); // 1000
+console.log(p); // 6.907755278982137
+console.log(b); // 999.9999999999994

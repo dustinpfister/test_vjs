04a04bdee369feefb81648010d417d84dd33ca1c
dustinpfister <dustin.pfister@gmail.com>
2020-04-21 17:35:13 -0400

    s3_round

---
+++ for_post/js-math-random/s3_round.js
@@ -0,0 +1,17 @@
+
+console.log( Math.round(0.01) ); // 0
+console.log( Math.ceil(0.01) ); // 1
+console.log( Math.floor(0.01) ); // 0
+
+console.log( Math.round(0.5) ); // 1
+console.log( Math.ceil(0.5) ); // 1
+console.log( Math.floor(0.5) ); // 0
+
+console.log( Math.round(0.99) ); // 1
+console.log( Math.ceil(0.99) ); // 1
+console.log( Math.floor(0.99) ); // 0
+
+console.log('******');
+console.log( Math.round(0.25 * 6) ); // 0 - 6
+console.log( Math.ceil(Math.random() * 6) ); // 1 - 6
+console.log( Math.floor(Math.random() * 6) ); // 0 - 5

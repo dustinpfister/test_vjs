9eadc9512c2bac57a8a4d040f01e9ead13ca2cc6
dustinpfister <dustin.pfister@gmail.com>
2020-04-21 09:57:56 -0400

    new s1_basics basic folder

--- for_post/js-math-log/old/basic_first.js
+++ for_post/js-math-log/old/basic_first.js
@@ -1,7 +1,8 @@
 // this is the first basic example I had for the js-math-log post
 console.log(Math.pow(Math.E,Math.log(1000))); // 999.99...
 
-// not sure where is was goinf with this
+// working out something a litter different here
+// but be sure to look at what is in the new s*_* folders
 var l = Math.log(1000),
 e = Math.E,
 n = Math.pow(e, l);
--- for_post/js-math-log/s1_basics/basic.js
+++ for_post/js-math-log/s1_basics/basic.js
@@ -1,7 +1,9 @@
-var l = Math.log(1000),
-e = Math.E,
-n = Math.pow(e, l);
 
-console.log(l);
-console.log(e);
-console.log(n);
+
+var a = 1000,
+p = Math.log(a),
+b = Math.pow( Math.E, p);
+
+console.log(a);
+console.log(p);
+console.log(b);

0a511e174606a453095c21d5e0554e101b0b20e3
dustinpfister <dustin.pfister@gmail.com>
2020-04-21 18:05:16 -0400

    distMethod

--- for_post/js-math-random/s4_distribution/dist.js
+++ for_post/js-math-random/s4_distribution/dist.js
@@ -1,4 +1,22 @@
-var distMethodOne = function (count, w, h) {
+var distMethod = function (count, w, h, method) {
+    count = count === undefined ? 100 : count;
+    w = w === undefined ? 320 : w;
+    h = h === undefined ? 240 : h;
+    method = method || function (i) {
+        return {
+            x: Math.floor(Math.random() * w),
+            y: Math.floor(Math.random() * h)
+        };
+    };
+    var points = [],
+    i = count;
+    while (i--) {
+        points.push(method(i));
+    }
+    return points;
+};
+
+var distMethodTwo = function (count, w, h) {
     count = count === undefined ? 100 : count;
     w = w === undefined ? 320 : w;
     h = h === undefined ? 240 : h;

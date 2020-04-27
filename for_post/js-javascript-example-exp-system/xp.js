66d1802f6c512f4ed1d36a4896889b7bc2e96657
dustinpfister <dustin.pfister@gmail.com>
2020-04-27 17:46:24 -0400

    deltaNext

--- for_post/js-javascript-example-exp-system/xp.js
+++ for_post/js-javascript-example-exp-system/xp.js
@@ -1,17 +1,27 @@
 var XP = (function () {
+    var DEFAULTS = {
+        level: 1,
+        xp: 0,
+        cap: 30,
+        deltaNext: 50
+    };
     // set level with given xp
-    var set = function (xp) {
-        return (1 + Math.sqrt(1 + 8 * xp / 50)) / 2;
+    var set = function (xp, deltaNext) {
+        return (1 + Math.sqrt(1 + 8 * xp / deltaNext)) / 2;
     };
     // get exp to the given level with given current_level and xp
-    var XPto = function (level) {
-        return ((Math.pow(level, 2) - level) * 50) / 2;
+    var getXPtoLevel = function (level, deltaNext) {
+        return ((Math.pow(level, 2) - level) * deltaNext) / 2;
     };
-    var parseByXP = function (xp, cap) {
-        var l = set(xp);
+    var parseByXP = function (xp, cap, deltaNext) {
+        xp = xp === undefined ? DEFAULTS.xp : xp;
+        cap = cap === undefined ? DEFAULTS.cap : cap;
+        deltaNext = deltaNext === undefined ? DEFAULTS.deltaNext : deltaNext;
+
+        var l = set(xp, deltaNext);
         l = l > cap ? cap : l;
         var level = Math.floor(l),
-        forNext = XPto(level + 1);
+        forNext = getXPtoLevel(level + 1, deltaNext);
         return {
             level: level,
             levelFrac: l,
@@ -21,8 +31,12 @@ var XP = (function () {
         };
     };
     return {
-        parseByLevel: function (l, cap) {
-            return parseByXP(XPto(l, cap));
+        parseByLevel: function (l, cap, deltaNext) {
+            l = l === undefined ? DEFAULTS.level : l;
+            deltaNext = deltaNext === undefined ? DEFAULTS.deltaNext : deltaNext;
+            var xp = getXPtoLevel(l, deltaNext);
+            console.log(xp);
+            return parseByXP(xp, cap, deltaNext);
         },
         parseByXP: parseByXP
     };

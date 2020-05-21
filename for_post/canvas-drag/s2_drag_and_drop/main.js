b0ac1e16ed2108ecbe4b47e3b14c4da89cdf9d61
dustinpfister <dustin.pfister@gmail.com>
2020-05-21 09:28:37 -0400

    draging circles

--- for_post/canvas-drag/s2_drag_and_drop/main.js
+++ for_post/canvas-drag/s2_drag_and_drop/main.js
@@ -8,7 +8,39 @@ ctx.translate(0.5, 0.5);
 
 var game = gameMod();
 
-console.log( gameMod.get(game, 34, 34) );
-
 draw.back(ctx, canvas);
 draw.circles(ctx, game);
+
+var grab = false;
+// Event handlers
+var pointerDown = function (game) {
+    return function (e) {
+        var pos = utils.getCanvasRelative(e),
+        cir = gameMod.get(game, pos.x, pos.y);
+        grab = cir ? cir : false;
+    };
+};
+var pointerMove = function (game) {
+    return function (e) {
+        var pos = utils.getCanvasRelative(e);
+        if (grab) {
+            grab.x = pos.x;
+            grab.y = pos.y;
+            draw.back(ctx, canvas);
+            draw.circles(ctx, game);
+        }
+    };
+};
+var pointerUp = function (game) {
+    return function (e) {
+        grab = false;
+    };
+};
+
+// attach for mouse and touch
+canvas.addEventListener('mousedown', pointerDown(game));
+canvas.addEventListener('mousemove', pointerMove(game));
+canvas.addEventListener('mouseup', pointerUp(game));
+canvas.addEventListener('touchstart', pointerDown(game));
+canvas.addEventListener('touchmove', pointerMove(game));
+canvas.addEventListener('touchend', pointerUp(game));

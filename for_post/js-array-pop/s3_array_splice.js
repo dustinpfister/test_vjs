e75edaf6152671ab04c18992abb76a0c444f7a7a
dustinpfister <dustin.pfister@gmail.com>
2020-05-30 13:29:29 -0400

    s3 example

--- for_post/js-array-pop/s3_array_splice.js
+++ for_post/js-array-pop/s3_array_splice.js
@@ -3,3 +3,8 @@ var arr = [1,2,3,4];
 console.log( arr.splice(arr.length-1, 1)[0] ); // 4
 console.log( arr.splice(0, 1)[0] ); // 1
 console.log( arr ); // [2,3]
+
+// splice can also be used to inject new elements
+arr.splice(1,0, 2.1, 2.2);
+
+console.log(arr); // [2, 2.1, 2.2, 2.3, 3]

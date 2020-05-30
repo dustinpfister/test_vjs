6683eb9020244b7f9330a494e068bc8c182b5d22
dustinpfister <dustin.pfister@gmail.com>
2020-05-30 13:19:54 -0400

    array-pop folder

---
+++ for_post/js-array-pop/s1_array_pop.js
@@ -0,0 +1,6 @@
+var arr = [1,2,3,4];
+
+var n = arr.pop();
+
+console.log(n); // 4
+console.log(arr); // [1,2,3]
---
+++ for_post/js-array-pop/s2_array_shift.js
@@ -0,0 +1,6 @@
+var arr = [1,2,3,4];
+
+var n = arr.shift();
+
+console.log(n); // 1
+console.log(arr); // [2,3,4]

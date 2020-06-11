b5690c02ce887189a5b92b54a2b18d659ac62d96
dustinpfister <dustin.pfister@gmail.com>
2020-06-11 11:02:30 -0400

    array-of folder

---
+++ for_post/js-array-of/array_of.js
@@ -0,0 +1,13 @@
+let arr = Array.of(10),
+arr2 = new Array(10),
+arr3 = [10];
+
+// first element
+console.log(arr[0]); // 10
+console.log(arr2[0]); // undefined
+console.log(arr3[0]); // 10
+
+// lengths
+console.log(arr.length); // 1
+console.log(arr2.length); // 10
+console.log(arr3.length); // 1

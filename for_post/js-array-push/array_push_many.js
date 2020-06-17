7aad88ebdcc091579a3476fd45f02e1fd23c73d2
dustinpfister <dustin.pfister@gmail.com>
2020-06-17 17:02:00 -0400

    push many example updated

--- for_post/js-array-push/array_push_many.js
+++ for_post/js-array-push/array_push_many.js
@@ -2,3 +2,8 @@ var arr = [16, 32];
 arr.push(64, 128, 256)
 
 console.log(arr.join('-')); // '16-32-64-128-256'
+
+var b = [1, 2, 3];
+[].push.apply(b, [4, 5, 6]);
+
+console.log(b.join('-')); // '1-2-3-4-5-6'

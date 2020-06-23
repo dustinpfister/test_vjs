ae779ce678f35dc2ba5621f41b58ed4f99f6c1a7
dustinpfister <dustin.pfister@gmail.com>
2020-06-22 20:04:20 -0400

    math-pow nthroot example

--- for_post/js-nth-root/math-pow.js
+++ for_post/js-nth-root/math-pow.js
@@ -1,7 +1,13 @@
+var nthRoot = function (n, degree) {
+    return Math.pow(n, 1 / degree);
+};
 
-var num = 25,
-degree = 3;
+console.log(Math.sqrt(25)); // 5
+console.log(nthRoot(25, 2)); // 5
 
-var x = Math.pow(num, 1 / degree); // 5
-
-console.log(Math.sqrt(num)); // 5
+var n = nthRoot(25, 3);
+console.log(n); // 2.924017738212866
+console.log(n * n * n); // 24.999999999999996
+n = nthRoot(25, 4);
+console.log(n); // 2.23606797749979
+console.log(n * n * n * n); // 25.000000000000007

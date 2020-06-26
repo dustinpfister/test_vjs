3a75ecb419f52176fda89393a8f96cc433f1ff70
dustinpfister <dustin.pfister@gmail.com>
2020-06-26 13:38:11 -0400

    s1 example working

--- prototypes/js-javascript-example-fizzbuzz/s1_many_console_logs.js
+++ prototypes/js-javascript-example-fizzbuzz/s1_many_console_logs.js
@@ -1,12 +1,15 @@
 for (var i = 1; i <= 100; i++) {
     var output = '';
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && (i % 5 != 0)) {
         console.log('Fizz');
     }
-    if (i % 5 == 0) {
+    if (i % 3 != 0 && (i % 5 == 0)) {
         console.log('Buzz');
     }
-    if ((i % 3 != 0) && (i % 5!= 0)) {
+    if (i % 3 == 0 && (i % 5 == 0)) {
+        console.log('FizzBuzz');
+    }
+    if ((i % 3 != 0) && (i % 5 != 0)) {
         console.log(i);
     }
 }

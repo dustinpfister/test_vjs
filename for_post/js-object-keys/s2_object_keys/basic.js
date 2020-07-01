4eeaaf02ed5825a35baa686ea44c77e0e1067765
dustinpfister <dustin.pfister@gmail.com>
2020-07-01 08:07:07 -0400

    js object keys folder

---
+++ for_post/js-object-keys/s2_object_keys/basic.js
@@ -0,0 +1,13 @@
+var obj = {
+    x: 0,
+    y: 35,
+    attack: 12,
+    hp : {
+      current: 47,
+      max: 50
+    }
+};
+ 
+var keys = Object.keys(obj);
+ 
+console.log(keys); // ["x", "y", "attack", "hp"]

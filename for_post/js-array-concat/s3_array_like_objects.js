9ca60e1b62fa12db3f812a58da5656425e55a6b9
dustinpfister <dustin.pfister@gmail.com>
2020-07-13 13:02:27 -0400

    s4_array_like

--- for_post/js-array-concat/s3_array_like_objects.js
+++ for_post/js-array-concat/s3_array_like_objects.js
@@ -5,4 +5,10 @@ var obj = {
     length: 3
 };
 
-Array.prototype.concat.call(obj, 4, 5, [6, 7]);
+var a = Array.prototype.concat.call(obj, 4, 5, [6, 7]),
+b = Array.from(obj).concat(4, 5, [6, 7]);
+
+console.log(a);
+// [ { '0': 1, '1': 2, '2': 3, length: 3 }, 4, 5, 6, 7 ]
+console.log(b);
+// [ 1, 2, 3, 4, 5, 6, 7 ]

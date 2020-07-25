1413d0f546f2e413597fa9f43e0161a937c7310c
dustinpfister <dustin.pfister@gmail.com>
2020-07-25 10:02:41 -0400

    js-array folder

---
+++ for_post/js-array/s2_creating/constr.js
@@ -0,0 +1,11 @@
+// use the new keyword an call the constructor
+let a = new Array();
+a.push(1,2,3,4);
+ 
+console.log(a); // [ 1, 2, 3, 4 ]
+
+// An argument can be given to set a starting length
+let b = new Array(10);
+ 
+console.log(b.length); // 10
+console.log(b); // [ <10 empty items> ]

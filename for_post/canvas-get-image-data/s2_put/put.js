8a09ae565818d540c5abd0660baa5c42eec841d4
dustinpfister <dustin.pfister@gmail.com>
2020-08-02 09:42:32 -0400

    section folders

---
+++ for_post/canvas-get-image-data/s2_put/put.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById('the-canvas'),
+ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(0, 0, 32, 32);
+ctx.strokeStyle = 'green';
+ctx.globalAlpha = 1;
+ctx.lineWidth = 1;
+ctx.strokeRect(3, 3, 5, 5);
+var imgData = ctx.getImageData(3, 3, 5, 5);
+imgData.data.forEach(function (v, i,data) {
+    data[i] = v === 0 ? 0 : 255;
+});
+ctx.putImageData(imgData,3,3);
+var i = 0;
+while (i < imgData.data.length) {
+    console.log(imgData.data.slice(i, i + 4));
+    i += 4;
+}

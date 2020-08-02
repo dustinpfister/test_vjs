b566529023092d065b17aacdc3a86da1360ce030
dustinpfister <dustin.pfister@gmail.com>
2020-08-02 10:10:27 -0400

    better put example

--- for_post/canvas-get-image-data/s2_put/put2.js
+++ for_post/canvas-get-image-data/s2_put/put2.js
@@ -8,3 +8,17 @@ ctx.fillRect(0, 0, 32, 32);
 var data = new Uint8ClampedArray([ 255,255,0,255]);
 var img = new ImageData( data,1,1)
 ctx.putImageData( img, 1,1);
+
+
+
+/*
+var i = 0, px,x,y,
+html =
+while (i < imgData.data.length) {
+    var px = imgData.data.slice(i, i + 4),
+    x = i % imgData.width,
+    y = Math.floor(i / imgData.width);
+    console.log(px.join(','));
+    i += 4;
+}
+*/

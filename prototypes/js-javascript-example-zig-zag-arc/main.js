e96a1f590f470044044a6151fa149050e13284ce
dustinpfister <dustin.pfister@gmail.com>
2020-08-10 12:12:26 -0400

    we have our methods

--- prototypes/js-javascript-example-zig-zag-arc/main.js
+++ prototypes/js-javascript-example-zig-zag-arc/main.js
@@ -3,4 +3,12 @@ var linPerToArcPer = function (linPer) {
     return 1 - Math.abs(Math.cos(Math.PI * linPer));
 };
 
-console.log( linPerToArcPer(1) );
+var linPerToZigZagPer = function (linPer, waveCount) {
+    waveCount = waveCount === undefined ? 1 : waveCount;
+    var radian = Math.PI * waveCount * linPer
+        return 1 - Math.abs(Math.cos(radian));
+};
+
+console.log(linPerToArcPer(0.5));
+
+console.log(linPerToZigZagPer(0.125, 4));

352d4db8f2529ad4de4cbe1ff12678ae06232d9d
dustinpfister <dustin.pfister@gmail.com>
2020-08-10 13:35:59 -0400

    borders

--- prototypes/js-javascript-example-zig-zag-arc/lib/draw.js
+++ prototypes/js-javascript-example-zig-zag-arc/lib/draw.js
@@ -8,13 +8,18 @@ draw.back = function (ctx, canvas) {
 draw.pool = function (ctx, game) {
     var pool = game.pool;
     ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 3;
     pool.forEach(function (disp) {
         var bias = utils.linPerToBiasPer(disp.i / disp.iMax);
         if (disp.active) {
             ctx.save();
             ctx.translate(disp.x, disp.y);
             ctx.globalAlpha = 0.25 + 0.75 * bias;
-            ctx.fillRect(disp.w / 2 * -1, disp.h / 2 * -1, disp.w, disp.h);
+            ctx.beginPath();
+            ctx.rect(disp.w / 2 * -1, disp.h / 2 * -1, disp.w, disp.h);
+            ctx.fill();
+            ctx.stroke();
             ctx.restore();
         }
     });

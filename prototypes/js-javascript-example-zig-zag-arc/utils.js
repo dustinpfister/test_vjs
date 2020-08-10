f3ae67ca5eb0657a2620043b51ee5f070f44a636
dustinpfister <dustin.pfister@gmail.com>
2020-08-10 12:16:33 -0400

    utils.js

--- prototypes/js-javascript-example-zig-zag-arc/utils.js
+++ prototypes/js-javascript-example-zig-zag-arc/utils.js
@@ -1,14 +1,11 @@
+var utils = {};
 
-
-var linPerToZigZagPer = function (linPer, waveCount) {
+utils.linPerToZigZagPer = function (linPer, waveCount) {
     waveCount = waveCount === undefined ? 1 : waveCount;
     var radian = Math.PI * waveCount * linPer
         return 1- Math.abs(Math.cos(radian));
 };
 
-var linPerToArcPer = function (linPer) {
+utils.linPerToArcPer = function (linPer) {
     return linPerToZigZagPer(linPer, 1);
 };
-
-console.log(linPerToArcPer(0));
-console.log(linPerToZigZagPer(0, 4));

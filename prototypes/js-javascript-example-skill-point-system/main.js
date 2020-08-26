ce6f6384e75effa07ee7ed8c330f3af2a32630cf
dustinpfister <dustin.pfister@gmail.com>
2020-08-26 10:01:33 -0400

    using canvas

--- prototypes/js-javascript-example-skill-point-system/main.js
+++ prototypes/js-javascript-example-skill-point-system/main.js
@@ -1,6 +1,28 @@
+// create and append canvas element, and get 2d context
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d'),
+container = document.getElementById('canvas-app') || document.body;
+container.appendChild(canvas);
+// set width and height
+canvas.width = 320;
+canvas.height = 240;
 
-var levelObj = XP.parseByLevel(7, 100, 10000);
+var state = {
+    levelObj: {},
+    level: 1,
+    levelCap: 100,
+    deltaNext: 10000,
+    DPS: 0,
+    skillPoints: 0,
+    skillOptions: {}
+};
 
-var DPS = XP.applySkillPoints(levelObj, 0, {});
+state.levelObj = XP.parseByLevel(state.level, state.levelCap, state.deltaNext);
 
-console.log( Number(DPS) );
+state.DPS = XP.applySkillPoints(state.levelObj, state.skillPoints, state.skillOptions);
+
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+console.log(Number(state.DPS));
+

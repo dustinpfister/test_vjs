f223bb5980700c571d99ee2beebc9e85d7b0a851
dustinpfister <dustin.pfister@gmail.com>
2020-08-26 12:42:40 -0400

    valueMax

--- prototypes/js-javascript-example-skill-point-system/main.js
+++ prototypes/js-javascript-example-skill-point-system/main.js
@@ -7,7 +7,7 @@ container.appendChild(canvas);
 canvas.width = 320;
 canvas.height = 240;
 
-console.log( Number(XP.parseByLevel(7)) );
+console.log(Number(XP.parseByLevel(7)));
 
 var state = {
     levelObj: {},
@@ -17,7 +17,8 @@ var state = {
     DPS: 0,
     skillPoints: 0,
     skillOptions: {},
-    values: []// values array for the graph
+    values: [], // values array for the graph
+    valueMax: 0
 };
 
 var createValues = function (state) {
@@ -28,12 +29,14 @@ var createValues = function (state) {
         state.values.push(Number(DPS));
         state.level += 1;
         //state.skillPoints += Math.floor(Math.pow(5, state.level));
-         state.skillPoints += Math.floor(Math.pow(1.847, state.level)); // 1000sp over 100 levels
+        state.skillPoints += Math.floor(Math.pow(1.847, state.level)); // 1000sp over 100 levels
         //state.skillPoints += 1;
     }
+    state.valueMax = Math.max.apply(null, state.values);
 };
 
 createValues(state);
 
+console.log(state.valueMax);
+
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-console.log(state.values);
--- prototypes/js-javascript-example-skill-point-system/todo.md
+++ prototypes/js-javascript-example-skill-point-system/todo.md
@@ -8,7 +8,7 @@
 * (done) The XP.applySkillPoints method takes a levelObj, skillPoint value, and a options argument as arguments
 * (done) The XP.applySkillPoints method returns an object with a valueOf method
 * (done) The valueOf method is used to return the current primitive value of what is betting set by XP.applySkillPoints
-* add a valueOf method for levelObjects that will return the level value
-* The options argument is used to tweak the effects of skillPoints and level
+* (done) add a valueOf method for levelObjects that will return the level value
 
+* The options argument is used to tweak the effects of skillPoints and level
 * display a line chart between level 1 and cap for levelObj used for XP.applySkillPoints

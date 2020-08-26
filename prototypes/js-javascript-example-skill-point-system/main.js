921941a22a87d632e47441a0a3a19c4d3a5b2ae3
dustinpfister <dustin.pfister@gmail.com>
2020-08-26 13:02:00 -0400

    draw.valuesChart method

--- prototypes/js-javascript-example-skill-point-system/main.js
+++ prototypes/js-javascript-example-skill-point-system/main.js
@@ -1,3 +1,35 @@
+var draw = {};
+
+draw.back = function (ctx, canvas) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
+draw.valuesChart = function (ctx, canvas, state) {
+    var h = canvas.height,
+    w = canvas.width,
+    i = 0,
+    per,
+    n,
+    x,
+    y,
+    len = state.values.length;
+    ctx.beginPath();
+    while (i < len) {
+        n = state.values[i];
+        per = n <= 0 ? 0 : n / state.valueMax;
+        x = w / len * i;
+        y = h - per * h;
+        if (i === 0) {
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+        }
+        console.log(x, y);
+        i += 1;
+    }
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+};
+
 // create and append canvas element, and get 2d context
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
@@ -6,8 +38,6 @@ container.appendChild(canvas);
 // set width and height
 canvas.width = 320;
 canvas.height = 240;
-
-console.log(Number(XP.parseByLevel(7)));
 
 var state = {
     levelObj: {},
@@ -29,14 +59,14 @@ var createValues = function (state) {
         state.values.push(Number(DPS));
         state.level += 1;
         //state.skillPoints += Math.floor(Math.pow(5, state.level));
-        state.skillPoints += Math.floor(Math.pow(1.847, state.level)); // 1000sp over 100 levels
-        //state.skillPoints += 1;
+        //state.skillPoints += Math.floor(Math.pow(1.847, state.level)); // 1000sp over 100 levels
+        state.skillPoints += 1;
     }
     state.valueMax = Math.max.apply(null, state.values);
 };
 
+console.log();
+
 createValues(state);
-
-console.log(state.valueMax);
-
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+draw.back(ctx, canvas);
+draw.valuesChart(ctx, canvas, state);

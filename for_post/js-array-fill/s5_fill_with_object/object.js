0bb5a897a0a419f4279f9f34a9f5868e81024ea8
dustinpfister <dustin.pfister@gmail.com>
2020-08-28 08:01:34 -0400

    clean up

--- for_post/js-array-fill/s5_fill_with_object/object.js
+++ for_post/js-array-fill/s5_fill_with_object/object.js
@@ -1,16 +1,18 @@
-var obj = {
-    x: 4,
-    y: 7
-};
-
 var clone = function (obj) {
     return JSON.parse(JSON.stringify(obj));
 };
 
-var arr = Array.apply(null, {
+var fillWithClonedObject = function (obj) {
+    return Array.apply(null, {
         length: 3
     }).map(function () {
         return clone(obj);
+    });
+};
+
+var arr = fillWithClonedObject({
+        x: 4,
+        y: 7
     });
 
 arr[1].x = 0;

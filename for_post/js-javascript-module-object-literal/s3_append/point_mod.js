8a30669ab3c90edcc511f153b24cb15ec8132ea9
dustinpfister <dustin.pfister@gmail.com>
2020-08-28 11:48:17 -0400

    s3_append folder

---
+++ for_post/js-javascript-module-object-literal/s3_append/point_mod.js
@@ -0,0 +1,26 @@
+// starting out with an object literal, and some hard coded defaults
+var pointMod = {
+    xDeflaut: 0,
+    yDefault: 0
+};
+// no state object in the module, but a method to create
+pointMod.create = function (x, y) {
+    return {
+        x: x === undefined ? pointMod.xDefault : x,
+        y: y === undefined ? pointMod.yDefault : y
+    };
+};
+// move a point
+pointMod.move = function (point, dx, dy) {
+    // create a new Point
+    var newPoint = this.create(point.x, point.y);
+    // mutate the new point, and not the source point
+    newPoint.x += dx;
+    newPoint.y += dy;
+    // return the newPoint without mutating the given source point
+    return newPoint;
+};
+// distance
+pointMod.distance = function (pointA, pointB) {
+    return Math.sqrt(Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2));
+};

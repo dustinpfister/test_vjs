398d9c7624b0abc5caa8c5d425d3983ea1a0e6bf
dustinpfister <dustin.pfister@gmail.com>
2020-08-28 12:07:25 -0400

    s4 folder

--- for_post/js-javascript-module-object-literal/s4_breaking_from_pattern/point_mod.js
+++ for_post/js-javascript-module-object-literal/s4_breaking_from_pattern/point_mod.js
@@ -1,19 +1,17 @@
-// starting out with an object literal, and some hard coded defaults
-var pointMod = {
-    xDefault: 0,
-    yDefault: 0
-};
-// no state object in the module, but a method to create
-pointMod.create = function (x, y) {
+// starting out with an function expression which is also a kind of object in javaScript
+var pointMod = function (x, y) {
     return {
         x: x === undefined ? pointMod.xDefault : x,
         y: y === undefined ? pointMod.yDefault : y
     };
 };
+// so we can append static methods to it
+pointMod.xDefault = 0;
+pointMod.yDefault = 0;
 // move a point
 pointMod.move = function (point, dx, dy) {
     // create a new Point
-    var newPoint = this.create(point.x, point.y);
+    var newPoint = pointMod(point.x, point.y);
     // mutate the new point, and not the source point
     newPoint.x += dx;
     newPoint.y += dy;
@@ -24,9 +22,7 @@ pointMod.move = function (point, dx, dy) {
 pointMod.distance = function (pointA, pointB) {
     return Math.sqrt(Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2));
 };
-
-var origin = pointMod.create(),
-a = pointMod.move(origin, 10, 5),
-b = pointMod.move(origin, 25, 17),
-d = pointMod.distance(a, b);
-console.log(d.toFixed(2)); // '19.21'
+// so we can not create points like this
+var origin = pointMod();
+// and my methods still work fine
+console.log(pointMod.move(origin,5,10)); // { x: 5, y: 10 }

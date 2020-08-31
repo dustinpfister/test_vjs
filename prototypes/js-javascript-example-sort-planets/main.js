747cbaac5c7b0b3d77f3868f0cee20826b333edb
dustinpfister <dustin.pfister@gmail.com>
2020-08-31 16:52:26 -0400

    clean up

--- prototypes/js-javascript-example-sort-planets/main.js
+++ prototypes/js-javascript-example-sort-planets/main.js
@@ -1,38 +1,3 @@
-var getCanvasRelative = function (e) {
-    var canvas = e.target,
-    bx = canvas.getBoundingClientRect();
-    return {
-        x: (e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - bx.left,
-        y: (e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - bx.top,
-        bx: bx
-    };
-};
-
-var draw = {};
-
-draw.back = function (ctx, canvas) {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
-
-draw.targets = function (ctx, targets) {
-    var i = targets.length,
-    target,
-    pl;
-    while (i--) {
-        target = targets[i];
-        pl = target.pl;
-        ctx.fillStyle = 'white';
-        ctx.beginPath();
-        ctx.arc(pl.x, pl.y, pl.r, 0, Math.PI * 2);
-        ctx.fill();
-        ctx.fillStyle = 'blue';
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
-        ctx.fillText(i, pl.x, pl.y);
-    }
-};
-
 var container = document.getElementById('canvas-app'),
 canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d');
@@ -50,9 +15,19 @@ var update = function (planets, x, y) {
     draw.targets(ctx, targets);
 };
 
-update(planets, canvas.width / 2, canvas.height / 2);
+var getCanvasRelative = function (e) {
+    var canvas = e.target,
+    bx = canvas.getBoundingClientRect();
+    return {
+        x: (e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - bx.left,
+        y: (e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - bx.top,
+        bx: bx
+    };
+};
 
-canvas.addEventListener('mousedown', function (e) {
+canvas.addEventListener('click', function (e) {
     var pos = getCanvasRelative(e);
     update(planets, pos.x, pos.y);
 });
+
+update(planets, canvas.width / 2, canvas.height / 2);

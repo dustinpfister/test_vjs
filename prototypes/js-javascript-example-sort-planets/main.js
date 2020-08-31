c6e3944418ea7e60d7967681012ee9bea3b03e8a
dustinpfister <dustin.pfister@gmail.com>
2020-08-31 16:35:11 -0400

    get targets function

--- prototypes/js-javascript-example-sort-planets/main.js
+++ prototypes/js-javascript-example-sort-planets/main.js
@@ -1,4 +1,6 @@
-
+var distance = function (x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+};
 var createPlanets = function (opt) {
     opt = opt || {};
     opt.canvas = opt.canvas || {
@@ -10,7 +12,7 @@ var createPlanets = function (opt) {
     y,
     a,
     d,
-    dMax = Math.min.call(null, opt.canvas.width / 2, opt.canvas.height/ 2),
+    dMax = Math.min.call(null, opt.canvas.width / 2, opt.canvas.height / 2),
     planets = [];
     while (i < opt.count) {
         d = dMax / opt.count * i;
@@ -21,11 +23,38 @@ var createPlanets = function (opt) {
             x: x,
             y: y,
             r: 8,
-            minerals: 0
+            minerals: 50 + Math.round(50 * Math.random())
         });
         i += 1;
     }
     return planets;
+};
+
+var getTargets = function (planets, x, y) {
+    var i = planets.length,
+    pl,
+    d,
+    targets = [];
+    while (i--) {
+        pl = planets[i];
+        d = distance(x, y, pl.x, pl.y);
+        targets.push({
+            pl: pl,
+            i: i,
+            d: d
+        });
+    }
+    // sort targets by distance
+    targets.sort(function (a, b) {
+        if (a.d > b.d) {
+            return 1;
+        }
+        if (a.d < b.d) {
+            return -1;
+        }
+        return 0;
+    });
+    return targets;
 };
 
 var draw = {};
@@ -57,7 +86,7 @@ var planets = createPlanets({
         canvas: canvas
     });
 
-console.log(planets);
+console.log(getTargets(planets, 0, 0));
 
 draw.back(ctx, canvas);
 draw.planets(ctx, planets);

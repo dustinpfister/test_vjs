9addf452eb4583a50ad3b20dbf82cc59f2cb6cd0
dustinpfister <dustin.pfister@gmail.com>
2020-09-02 11:58:52 -0400

    better find angle method

--- for_post/js-math-atan2/s2_two_points/atan2-two-points-method.js
+++ for_post/js-math-atan2/s2_two_points/atan2-two-points-method.js
@@ -5,12 +5,16 @@ var p1 = {
 },
 p2 = {
     x: 75,
-    y: 100
+    y: 50
 };
 
 // a findAngle method that takes four arguments and returns and angle in degrees
-var findAngle = function (x1, y1, x2, y2) {
-    return ( Math.atan2(y1 - y2, x1 - x2) + Math.PI )  / Math.PI * 180;
+var findAngle = function (x1, y1, x2, y2, scale) {
+    scale = scale === undefined ? 360 : scale;
+    var radian = Math.atan2(y1 - y2, x1 - x2) + Math.PI;
+    return (radian / (Math.PI * 2) * scale ) % scale;
 };
-var a = findAngle(p1.x, p1.y, p2.x, p2.y);
-console.log(a); // 63.43
+
+var a = findAngle(p1.x, p1.y, p2.x, p2.y),
+b = findAngle(p2.x, p2.y, p1.x, p1.y);
+console.log(a, b); // 0 180

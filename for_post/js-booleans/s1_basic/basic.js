a5083bbd6dd107e75222fed1d7d1c5c4512083b4
dustinpfister <dustin.pfister@gmail.com>
2020-09-03 11:24:48 -0400

    js-booleans folder

---
+++ for_post/js-booleans/s1_basic/basic.js
@@ -0,0 +1,14 @@
+var firstRun = true;
+var loop = function () {
+ 
+    setTimeout(loop, 1000);
+ 
+    if (firstRun) {
+        console.log('first run!');
+        firstRun = false;
+    }
+ 
+    console.log('tick');
+ 
+}
+loop();

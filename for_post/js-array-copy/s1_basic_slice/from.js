f7f528b0999f72faaa980cf1f85550fe7db08fa1
dustinpfister <dustin.pfister@gmail.com>
2020-09-03 17:17:09 -0400

    from example now has shallow code

--- for_post/js-array-copy/s1_basic_slice/from.js
+++ for_post/js-array-copy/s1_basic_slice/from.js
@@ -1,6 +1,15 @@
+// Array.from will work fine if making a shallow clone
+var a = [1,2,3,4],
+b = Array.from(a);
+
+a[0] = 'a';
+console.log(a);
+// [ 'a', 2, 3, 4 ]
+console.log(b);
+// [ 1, 2, 3, 4 ]
 
 // Array.from is only good for making a shallow copy
-var a = [{x:40},{x:50},{x:60}],
+a = [{x:40},{x:50},{x:60}];
 b = Array.from(a);
 a[0].x = 0;
 console.log(a);

f1364d07e0072e18130593187feb7bec0fe43bbb
dustinpfister <dustin.pfister@gmail.com>
2020-09-10 09:35:33 -0400

    s1_basics folder for js-parseint

--- for_post/js-parseint/basic.js
+++
@@ -1,4 +0,0 @@
-console.log( parseInt('42') ); // 42
-console.log( parseInt('  6' * '7') ); // 42
-console.log( parseInt(42) ); // 42
-console.log( parseInt(42.1234) ); // 42
---
+++ for_post/js-parseint/s1_basics/frac.js
@@ -0,0 +1,2 @@
+console.log( parseInt('42.1234') ); // 42
+console.log( parseInt('42.9876') ); // 42

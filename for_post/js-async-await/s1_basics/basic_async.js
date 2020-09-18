e2a28eeb62c752937470542e5c2ec426c21e81e8
dustinpfister <dustin.pfister@gmail.com>
2020-09-18 12:52:10 -0400

    good basic_async example

--- for_post/js-async-await/s1_basics/basic_async.js
+++ for_post/js-async-await/s1_basics/basic_async.js
@@ -1,13 +1,15 @@
-
-let foo = (delay) => {
-    return async() => {
-        await setTimeout(function () {
-            return 1;
-        }, delay);
+let foo = async (delay) => {
+    let st = new Date(),
+    now = st,
+    t = 0;
+    while(t < delay){
+        now = new Date();
+        t = now - st;
     }
+    return 1;
 };
 
-let bar = function () {
+let bar = async ()=> {
     console.log('start');
     await foo(3000);
     console.log('end');

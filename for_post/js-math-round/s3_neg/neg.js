849f3271e44717bfab4848c4f0b88d67b8be5da9
dustinpfister <dustin.pfister@gmail.com>
2020-09-26 13:36:35 -0400

    new s3 section on negative numbers

---
+++ for_post/js-math-round/s3_neg/neg.js
@@ -0,0 +1,7 @@
+console.log( Math.round(1.4) ); // 1
+console.log( Math.round(1.5) ); // 2
+console.log( Math.round(1.6) ); // 2
+
+console.log( Math.round(-1.4) ); // -1
+console.log( Math.round(-1.5) ); // -1
+console.log( Math.round(-1.6) ); // -2

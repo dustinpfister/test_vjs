6a7edab55e56ceb3e2b48f8c4d043903e8b28727
dustinpfister <dustin.pfister@gmail.com>
2020-10-03 12:26:32 -0400

    filter object keys

--- prototypes/js-array-filter/s2_filter_obj_keys/object_keys.js
+++ prototypes/js-array-filter/s2_filter_obj_keys/object_keys.js
@@ -1,17 +1,29 @@
+var filterObjectKeys = function (obj, func, thisValue) {
+    func = func || function () {
+        return true;
+    };
+    thisValue = thisValue || obj;
+    var b = {};
+    Object.keys(obj).filter(function (key, i) {
+        return func.call(thisValue, key, obj[key], i, obj);
+    }).forEach(function (key) {
+        b[key] = obj[key];
+    });
+    return b;
+};
+
 var obj = {
     a: 1,
     b: '1',
     c: 2,
     d: 3
-},
-b = {};
+};
 
-Object.keys(obj).filter(function (key) {
-    var el = obj[key];
-    return typeof el === 'number';
-}).forEach(function (key) {
-    b[key] = obj[key];
-});
+var b = filterObjectKeys(obj, function (key, val, i, obj) {
+        return typeof val === 'number';
+    });
 
 console.log(obj);
+// { a: 1, b: '1', c: 2, d: 3 }
 console.log(b);
+//{ a: 1, c: 2, d: 3 }

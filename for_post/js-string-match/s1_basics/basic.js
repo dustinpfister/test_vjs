68dfd4fba36b445a53fc1b3861c6d77440eea497
dustinpfister <dustin.pfister@gmail.com>
2020-10-04 09:33:22 -0400

    no_match example

--- for_post/js-string-match/s1_basics/basic.js
+++ for_post/js-string-match/s1_basics/basic.js
@@ -1,11 +1,9 @@
 let str = 'foo bar baz bar 42 zoo bar';
 
 // String.indexOf
-console.log( str.indexOf('bar') );
-// 4
+console.log( str.indexOf('bar') ); // 4
 
 // String.match
 var match = str.match('bar');
 console.log( match.index ); // 4
 console.log( match[0] ); // 'bar'
-//[ 'bar', index: 4, input: 'foo bar baz bar 42 zoo bar' ]
---
+++ for_post/js-string-match/s1_basics/no_match.js
@@ -0,0 +1,5 @@
+let str = 'foo bar baz bar 42 zoo bar';
+// String.indexOf
+console.log( str.indexOf('nope') ); // -1
+// String.match
+console.log(str.match('nope')); // null

6031711396b85499b691573261781741ce3b6b42
dustinpfister <dustin.pfister@gmail.com>
2020-10-08 11:10:57 -0400

    call example

--- for_post/js-function/s5_this/call.js
+++ for_post/js-function/s5_this/call.js
@@ -5,4 +5,11 @@ var pt = {
 pt.distance = function (x, y) {
     return Math.sqrt(Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
 };
-console.log( pt.distance(10, 5).toFixed(2) ); // 11.18
+
+var pt2 = {
+    x: -10,
+    y: -10
+}
+
+console.log(pt.distance(0, 0).toFixed(2)); // 0.00
+console.log(pt.distance.call(pt2, 0, 0).toFixed(2)); // 14.14

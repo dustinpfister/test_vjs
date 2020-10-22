035c67ff52c8ad22bb22fb6e542a25aaba240082
dustinpfister <dustin.pfister@gmail.com>
2020-10-22 12:32:54 -0400

    comment

--- for_post/js-regex/s2_assertions/word_bound.js
+++ for_post/js-regex/s2_assertions/word_bound.js
@@ -1,4 +1,5 @@
 let text = 'thats excellent that we have some example text';
+// pattern with a \b world bounder assertion
 let patt = /\be\S+/ig;
 console.log( text.replace(patt, 'match') );
 // 'thats match that we have some match text'

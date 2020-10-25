063f41584885bd6aafb0593f58f9376b26f5f96c
dustinpfister <dustin.pfister@gmail.com>
2020-10-25 08:46:34 -0400

    better index example

--- for_post/js-javascript-foreach/s1_basics/index.js
+++ for_post/js-javascript-foreach/s1_basics/index.js
@@ -1,6 +1,26 @@
+
+let values = [100, 20, 50, 75],
+min = Math.min.apply(null, values),
+max = Math.max.apply(null, values),
+points = [];
+
+values.forEach((num, i, val) => {
+    points.push({
+        x: 320 / val.length * i,
+        y: (num - min) / (max - min) * 240
+    });
+});
+
+console.log(points);
+
+
+
+/*
 let a = [2, 5, 10],
 b = [];
 a.forEach((n, i) => {
-    b.push( Math.pow(n, i) );
+b.push( Math.pow(n, i) );
 });
 console.log(b); // [1, 5, 100]
+)
+*/

131c41ef9255acdde4781a56f0fe3257ad80f02e
dustinpfister <dustin.pfister@gmail.com>
2020-10-26 16:58:14 -0400

    module like

--- for_post/js-javascript-example-fizzbuzz/s4_array.js
+++ for_post/js-javascript-example-fizzbuzz/s4_array.js
@@ -1,18 +1,19 @@
 // fizzer
-var fizzer = function (i, m, mess) {
+var fizz = {};
+fizz.fizzer = function (i, m, mess) {
     if (i % m === 0) {
         return mess;
     }
     return '';
 }
 // create array
-var createFizzBuzzArray = function (len) {
+fizz.createFizzBuzzArray = function (len) {
     len = len || 100;
     var arr = [];
     for (var i = 1; i <= len; i++) {
         var output = '';
-        output += fizzer(i, 3, 'Fizz');
-        output += fizzer(i, 5, 'Buzz');
+        output += fizz.fizzer(i, 3, 'Fizz');
+        output += fizz.fizzer(i, 5, 'Buzz');
         if (output === '') {
             output = i;
         }
@@ -24,7 +25,7 @@ var createFizzBuzzArray = function (len) {
     return arr;
 };
 
-var arr = createFizzBuzzArray(100);
+var arr = fizz.createFizzBuzzArray(100);
 var html = arr.map(function (result) {
     return '<p>' + result.i + ' : ' + result.output + '<\/p>';
 }).join('');

e5f1d83178eba5e3e437b840d6a86cce4aaf0003
dustinpfister <dustin.pfister@gmail.com>
2020-10-27 17:55:39 -0400

    s1 example js-javascript-numbers

---
+++ for_post/js-javascript-number/s1_basics/lit.js
@@ -0,0 +1,9 @@
+// just logging a number literal
+console.log(42); // 42
+
+// NaN is also a number literal
+console.log(typeof NaN); // 'number'
+
+// there are some others
+console.log(typeof Infinity); // 'number'
+console.log(typeof -Infinity); // 'number'

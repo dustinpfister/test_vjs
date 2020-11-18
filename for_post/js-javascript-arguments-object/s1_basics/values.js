87dc0e18ac6befa8fbe2319bad27c9b786189482
dustinpfister <dustin.pfister@gmail.com>
2020-11-18 10:22:26 -0500

    values example

---
+++ for_post/js-javascript-arguments-object/s1_basics/values.js
@@ -0,0 +1,16 @@
+let sum = function (a, b) {
+    let sum = 0,
+    i = 0,
+    len = arguments.length;
+    if (len >= 1) {
+        while (i < len) {
+            sum += arguments[i];
+            i += 1;
+        }
+    }
+    return sum;
+};
+
+console.log(sum()); // 0
+console.log(sum(5)); // 5
+console.log(sum(5, 10, 7)); // 22

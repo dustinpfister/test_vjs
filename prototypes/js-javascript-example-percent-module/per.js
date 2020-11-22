c65aa66f1897db1cd16ce138f668ac3f85b2e231
dustinpfister <dustin.pfister@gmail.com>
2020-11-22 14:56:40 -0500

    bias percent method

--- prototypes/js-javascript-example-percent-module/per.js
+++ prototypes/js-javascript-example-percent-module/per.js
@@ -1,13 +1,28 @@
 
-var per = (function () {
-    var per = function(n, d){
+var Percent = (function () {
+
+    // main api function
+    var api = function(n, d, methodKey, args){
+        return api[methodKey].apply(null, [n,d].concat(args));
+    };
+
+    // basic percent function
+    api.basePer = function(n, d){
         return n / d;
     };
-    var api = {
-        per : per
+
+    // 'bias' percent function
+    api.bias = function(n, d){
+        var per = api.basePer(n, d);
+        return 1 - Math.abs(per - 0.5) / 0.5;
     };
+
     return api;
 }
     ());
 
-console.log( per.per(1, 5) ); // 0.2
+var per = Percent(1, 5, 'basePer', []),
+bias = Percent(3, 5, 'bias', []);
+
+console.log( per ); // 0.2
+console.log( bias ); // 0.2

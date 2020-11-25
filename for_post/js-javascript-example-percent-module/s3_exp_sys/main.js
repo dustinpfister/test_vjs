214f2abbf875756029e6a12beb0ac282d088297d
dustinpfister <dustin.pfister@gmail.com>
2020-11-25 18:55:03 -0500

    createTable method

--- for_post/js-javascript-example-percent-module/s3_exp_sys/main.js
+++ for_post/js-javascript-example-percent-module/s3_exp_sys/main.js
@@ -2,7 +2,7 @@
 
 //console.log(levelObj);
 
-var xpCap = 1000,
+var xpCap = 10000,
 levelCap = 20;
 /*
 var parseByXp = function(xp){
@@ -22,8 +22,15 @@ console.log( parseByXp(23.102118) );
 // can not figure out parse by level by expression so I might need a table
 // parseByLevel(1)
 
-var level = 0;
-while(level < levelCap){
-    console.log( level + 1 ,level, Percent.log3(level, levelCap, 10, 0, 1) * xpCap );
-    level += 1;
-}
+var createtable = function(){
+    var level = 0,
+    table = [];
+    while(level < levelCap){
+        table.push(Percent.log3(level, levelCap, 20, 0, 1) * xpCap);
+        level += 1;
+    }
+    return table;
+};
+
+var table = createtable();
+console.log(table);

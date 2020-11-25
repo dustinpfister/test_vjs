eb9a96b48ab6426cd95407afb11516b4fd595796
dustinpfister <dustin.pfister@gmail.com>
2020-11-25 18:49:15 -0500

    table solution started

--- for_post/js-javascript-example-percent-module/s3_exp_sys/main.js
+++ for_post/js-javascript-example-percent-module/s3_exp_sys/main.js
@@ -1,9 +1,10 @@
 //var levelObj = XP.parseByXP(10, 100);
 
 //console.log(levelObj);
-var xpCap = 1000,
-levelCap = 100;
 
+var xpCap = 1000,
+levelCap = 20;
+/*
 var parseByXp = function(xp){
     xp = xp === undefined ? 0 : xp; //23.102118,
     var l = Percent.log3(xp, xpCap, 10, 0, 1) * levelCap
@@ -14,8 +15,15 @@ var parseByXp = function(xp){
     };
 };
 
+
 console.log( parseByXp(23.102118) );
+*/
 
-// can not figure out parse by level
-
+// can not figure out parse by level by expression so I might need a table
 // parseByLevel(1)
+
+var level = 0;
+while(level < levelCap){
+    console.log( level + 1 ,level, Percent.log3(level, levelCap, 10, 0, 1) * xpCap );
+    level += 1;
+}

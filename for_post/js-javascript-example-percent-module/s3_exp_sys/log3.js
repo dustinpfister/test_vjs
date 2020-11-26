6b813fe382ac17e5a007ab500d1212a30e92dedc
dustinpfister <dustin.pfister@gmail.com>
2020-11-26 12:53:28 -0500

    log3 now working so far

--- for_post/js-javascript-example-percent-module/s3_exp_sys/log3.js
+++ for_post/js-javascript-example-percent-module/s3_exp_sys/log3.js
@@ -20,12 +20,18 @@ var log3 = (function () {
     };
     // PUBLIC API
     return {
-        getPer : function(n, d){
-            return Math.log(1 + basic.getPer(n, d)) / Math.log(2);
+        getPer : function(n, d, a){
+            a = a === undefined ? 12 : a;
+            //return Math.log(1 + basic.getPer(n, d)) / Math.log(2);
+            var per = basic.getPer(n, d)
+            return clamp(Math.log(1 + per) / Math.log(a - (a - 2) * per))
 
         },
-        getN : function(per, d){
-            return Math.round((Math.pow(2, 1 + per) / 2 - 1) * d);
+        getN : function(per, d, a){
+            a = a === undefined ? 12 : a;
+            //return Math.round((Math.pow(2, 1 + per) / 2 - 1) * d);
+            // not working
+            return return Math.round((Math.pow(2, 1 + per) / Math.log(a - (a - 2) * per) - 1) * d);
         }
     };
 }
@@ -34,12 +40,14 @@ var log3 = (function () {
 // test - the result of getN should equal the original value of n
 var testForN = function(n, d){
     var per = log3.getPer(n, d),
-    n2 = log3.getN(per, d);
-    return n === n2;
+    n2 = log3.getN(per, d),
+    result = n === n2;
+    console.log(n, n2, n === n2);
+    return result;
 };
 
 var n = 0,
-d = 1000,
+d = 10,
 result,
 pass = true;
 while(n <= d){
@@ -47,7 +55,7 @@ while(n <= d){
     if(!result){
         pass = false;
     }
-    console.log( n ,'n == n2 : ' + result ); 
+    //console.log( n ,'n == n2 : ' + result ); 
     n += 1
 }
 console.log('passed? :' + pass);

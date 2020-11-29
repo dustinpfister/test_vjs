4950eb6ac199c96b542d335f2ea7a8db14bd73fd
dustinpfister <dustin.pfister@gmail.com>
2020-11-29 09:49:48 -0500

    patt-length example looking good

--- for_post/js-regex-exec/s2_get_all/patt-length.js
+++ for_post/js-regex-exec/s2_get_all/patt-length.js
@@ -19,9 +19,24 @@ var getIndexValues = function (str, regex) {
     return matchArray;
 };
 
-var str = 'so Then this is a only a tEst of String Things',
-patt = /[A-Z]/g; // pattern to match for Capital letters
+var str = 'If a=400, a2=10, and b=a*a2 then b=4000',
+patt = /\S+=\d+/g;
 
-// using getIndexValues gives me an array of all pattern matches
 var matches = getIndexValues(str, patt);
 console.log(matches);
+/*
+[{
+        text: 'a=400',
+        index: 3,
+        endIndex: 8
+    }, {
+        text: 'a2=10',
+        index: 10,
+        endIndex: 15
+    }, {
+        text: 'b=4000',
+        index: 33,
+        endIndex: 39
+    }
+]
+*/

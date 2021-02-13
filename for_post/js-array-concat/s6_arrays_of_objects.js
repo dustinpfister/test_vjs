3feb0ec4e35e4bc398086e5408d2883ab45e2a4d
dustinpfister <dustin.pfister@gmail.com>
2021-02-13 10:26:30 -0500

    s6 example looking good

--- for_post/js-array-concat/s6_arrays_of_objects.js
+++ for_post/js-array-concat/s6_arrays_of_objects.js
@@ -4,6 +4,17 @@ var b = [{x:20, y: 40},{x:8, y: 89},{x:63, y: 4}];
 
 var c = a.concat(b);
 
-console.log(a);
-console.log(b);
-console.log(c)
+// Array.concat does not mutate in place and
+// returns a new array
+console.log(a.length, b.length, c.length);
+// 3 3 6
+
+// however the objects will still be references to the same
+// objects in memory. Mutating the value of a property in an 
+// object in one of the arrays will effect the same object in
+// other arrays
+
+c[0].x=99;
+c[0].y=99;
+console.log(a[0], c[0]);
+// { x: 99, y: 99 } { x: 99, y: 99 }

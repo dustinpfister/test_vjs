6c77b645d83991335b365d7f4377f5b2caa7fd3a
dustinpfister <dustin.pfister@gmail.com>
2021-03-29 15:14:57 -0400

    log once node example

--- prototypes/js-javascript-example-log-once/example_node.js
+++ prototypes/js-javascript-example-log-once/example_node.js
@@ -1,4 +1,8 @@
 let path = require('path'),
 utils = require( path.join(__dirname, 'lib/utils.js') );
 
-console.log(utils.isBrowser());
+var i = 10,
+logOnce = utils.createLogOnce();
+while(i--){
+    logOnce('i=' + i);
+}

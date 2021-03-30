5c79d91c00ed280413c893fcc7263681818bf640
dustinpfister <dustin.pfister@gmail.com>
2021-03-30 11:35:05 -0400

    s3 folder for js-javascript-closure

---
+++ for_post/js-javascript-closure/s3_call_once/call_once.js
@@ -0,0 +1,22 @@
+// call a function just one time
+let callOnce = (func) => {
+    let calls = 1;
+    return function () {
+        if (calls > 0) {
+            func.apply(null, arguments);
+            calls--;
+        }
+    };
+};
+
+// create a method that will just log a message once
+let createLogOnce = () => {
+    return callOnce((mess) => {
+        console.log(mess);
+    });
+};
+
+let trap = createLogOnce();
+
+trap('okay'); // 'okay' logged to the console
+trap('nope'); // (nothing)

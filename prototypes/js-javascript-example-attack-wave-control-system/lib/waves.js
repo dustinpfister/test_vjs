f87c6f1036d1211a6d29cb317201efe0eefb94dc
dustinpfister <dustin.pfister@gmail.com>
2021-03-31 11:56:12 -0400

    waveButtons local in waves.create

--- prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
+++ prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
@@ -11,13 +11,20 @@ var waves = (function () {
 
     api.create = function (opt) {
         opt = opt || {};
-        return {
-            waveCount: opt.waveCount || 30, // total number of waves
-            waveButtons: poolMod.create({
+        var waveButtons = poolMod.create({
                 count: 10, // max number of buttons on the canvas
                 spawn: spawn,
-                update: update
-            })
+                update: update,
+                x: 0,
+                y: opt.startY || 0,
+                data: {
+                    waveCount: opt.waveCount || 30, // total number of waves
+                }
+            });
+        return {
+            x: opt.x || 0, // the upper left position of the wave bar
+            y: opt.y || 0,
+            waveButtons: waveButtons
         };
     };
 

30f6569945c1ccc5623fe433573b28bbec63f1b1
dustinpfister <dustin.pfister@gmail.com>
2021-03-31 16:13:37 -0400

    spawning enemies

--- for_post/js-javascript-example-attack-wave-control-system/lib/game.js
+++ for_post/js-javascript-example-attack-wave-control-system/lib/game.js
@@ -6,12 +6,17 @@ var gameMod = (function () {
     var unitSpawn = function (obj, pool, sm, opt) {
         obj.heading = Math.PI * 0.5;
         obj.x = sm.canvas.width / 2;
-        obj.lifespan = 5;
+        obj.y = 0;
+        obj.lifespan = Infinity;
     };
 
     // on unit update
     var unitUpdate = function (obj, pool, sm, secs) {
         obj.pps = UNIT_PPS;
+        poolMod.moveByPPS(obj, secs);
+        if (obj.y >= sm.canvas.height - 50) {
+            obj.lifespan = 0;
+        }
     };
 
     var onWaveStart = function (waveObj, sm) {
@@ -39,6 +44,10 @@ var gameMod = (function () {
 
         // update wave buttons
         waveMod.update(sm, secs);
+
+        //
+
+        poolMod.update(sm.game.unitPool, secs, sm);
 
     };
 

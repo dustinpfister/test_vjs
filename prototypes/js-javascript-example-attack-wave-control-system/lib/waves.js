81f6550dfc46a60877f7d63b006e78d924b587f8
dustinpfister <dustin.pfister@gmail.com>
2021-03-31 12:18:50 -0400

    wave bar is moving

--- prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
+++ prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
@@ -2,40 +2,51 @@ var waveMod = (function () {
 
     var api = {};
 
-    var spawn = function (obj, pool, state, opt) {
-
+    // spawn an update methods
+    var spawn = function (obj, pool, sm, opt) {
+        obj.heading = Math.PI * 1.5;
+        obj.pps = 4;
+        obj.h = 128;
         obj.lifespan = Infinity;
     };
 
-    var update = function (obj, pool, state, secs) {};
+    var update = function (obj, pool, sm, secs) {
 
+        poolMod.moveByPPS(obj, secs);
+
+    };
+
+    // create a waveButtons pool
     api.create = function (opt) {
         opt = opt || {};
         opt.startY = opt.startY || 0;
         var pool = poolMod.create({
-                count: 10, // max number of buttons on the canvas
+                count: 5, // max number of buttons on the canvas
                 spawn: spawn,
                 update: update,
                 data: {
                     waveCount: opt.waveCount || 30, // total number of waves
                 }
             });
-
         // set all to active
         poolMod.setActiveStateForAll(pool, true);
-
         pool.objects.map(function (obj, i) {
-
+            spawn(obj, pool, {}, {});
             obj.x = opt.x || 0;
-            obj.y = opt.startY + obj.h * i
+            obj.y = opt.startY + obj.h * i;
 
         });
-
         return {
             x: opt.x || 0, // the upper left position of the wave bar
             y: opt.y || 0,
             pool: pool
         };
+    };
+
+    api.update = function (sm, secs) {
+
+        poolMod.update(sm.waveButtons.pool, secs, sm);
+
     };
 
     return api;
--- prototypes/js-javascript-example-attack-wave-control-system/main.js
+++ prototypes/js-javascript-example-attack-wave-control-system/main.js
@@ -8,8 +8,21 @@ var sm = {
     ctx: canvasObj.ctx,
     waveButtons: waveMod.create({
         startY: 64
-    })
+    }),
+    lt: new Date()
 };
 
-draw.background(sm.ctx, sm.canvas, 'blue');
-draw.waveButtons(sm.ctx, sm.waveButtons.pool)
+var loop = function () {
+    var now = new Date(),
+    secs = (now - sm.lt) / 1000;
+    requestAnimationFrame(loop);
+
+    // update wave buttons
+    waveMod.update(sm, secs);
+
+    draw.background(sm.ctx, sm.canvas, 'blue');
+    draw.waveButtons(sm.ctx, sm.waveButtons.pool);
+
+    sm.lt = now;
+};
+loop();

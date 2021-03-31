42a520ed89ffee91410ebebde111bd65ef7fa1e0
dustinpfister <dustin.pfister@gmail.com>
2021-03-31 11:49:46 -0400

    waveCount

--- prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
+++ prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
@@ -7,14 +7,14 @@ var waves = (function () {
         obj.lifespan = Infinity;
     };
 
-    var update = function (obj, pool, state, secs) {
-
-    };
+    var update = function (obj, pool, state, secs) {};
 
     api.create = function (opt) {
         opt = opt || {};
         return {
+            waveCount: opt.waveCount || 30, // total number of waves
             waveButtons: poolMod.create({
+                count: 10, // max number of buttons on the canvas
                 spawn: spawn,
                 update: update
             })

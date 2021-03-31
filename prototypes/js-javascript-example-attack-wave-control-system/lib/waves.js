84ab02d2d74638a1400c684e5b8cd18bf7093a89
dustinpfister <dustin.pfister@gmail.com>
2021-03-31 11:47:37 -0400

    object pool methods in wave.js

--- prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
+++ prototypes/js-javascript-example-attack-wave-control-system/lib/waves.js
@@ -1,11 +1,23 @@
 var waves = (function () {
 
-
     var api = {};
+
+    var spawn = function (obj, pool, state, opt) {
+
+        obj.lifespan = Infinity;
+    };
+
+    var update = function (obj, pool, state, secs) {
+
+    };
 
     api.create = function (opt) {
         opt = opt || {};
         return {
+            waveButtons: poolMod.create({
+                spawn: spawn,
+                update: update
+            })
         };
     };
 

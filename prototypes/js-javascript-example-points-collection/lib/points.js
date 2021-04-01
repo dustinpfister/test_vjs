a1b176b78692dcab46e8757b0e7fb8bc0edad6f7
dustinpfister <dustin.pfister@gmail.com>
2021-04-01 11:03:37 -0400

    points-collection proto folder

---
+++ prototypes/js-javascript-example-points-collection/lib/points.js
@@ -0,0 +1,24 @@
+var points = [0, 0, 100, 0, 100, 100];
+
+var points = [[0, 0, 100, 0, 100, 100, 100, 'close', 'stroke', 'fill']];
+
+var points = {
+    close: true,
+    stroke: 'black',
+    fill: 'white',
+    points: [0, 0, 100, 0, 100, 100]
+};
+
+var points = [{
+        close: true,
+        stroke: 'black',
+        fill: 'white',
+        points: [0, 0, 100, 0, 100, 100]
+    }, {
+        close: true,
+        stroke: 'black',
+        fill: false,
+        points: [45, 20, 75, 30, 35, 10]
+    },
+	[]
+]

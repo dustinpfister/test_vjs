5577d52fc7650d268321e4717288d143fbbf5e21
dustinpfister <dustin.pfister@gmail.com>
2021-04-06 13:35:50 -0400

    map system simple proto started

---
+++ prototypes/js-javascript-example-map-system-simple/lib/draw.js
@@ -0,0 +1,36 @@
+var draw = {};
+
+draw.back = function (ctx, canvas) {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+draw.turret = function (ctx, state) {
+    var turret = state.turret;
+    ctx.save();
+    ctx.translate(turret.x, turret.y);
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(turret.w * 0.5 * -1, turret.h * 0.5 * -1, turret.w, turret.h);
+    ctx.rotate(turret.facing);
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'red';
+    ctx.fillRect(turret.w * 0.3 * -1, turret.h * 0.3 * -1, turret.w * 0.6, turret.h * 0.6);
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(turret.w, 0);
+    ctx.stroke();
+    ctx.restore();
+};
+
+draw.shots = function (ctx, state) {
+    var shots = state.shots;
+    shots.forEach(function (shot) {
+        if (shot.active) {
+            ctx.save()
+            ctx.fillStyle = 'white';
+            ctx.translate(shot.x, shot.y);
+            ctx.fillRect(-3, -3, 6, 6);
+            ctx.restore();
+        }
+    });
+};
---
+++ prototypes/js-javascript-example-map-system-simple/main.js
@@ -0,0 +1,40 @@
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d');
+document.getElementById('canvas-app').appendChild(canvas);
+canvas.width = 640;
+canvas.height = 480;
+
+var state = stateMod.create({
+        canvas: canvas
+    });
+
+var lt = new Date();
+var loop = function () {
+    var now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+    stateMod.updateTurretFacing(state, secs);
+    stateMod.updateShots(state, secs);
+    draw.back(ctx, canvas);
+    draw.turret(ctx, state);
+    draw.shots(ctx, state);
+    lt = now;
+};
+loop();
+
+var pointerDown = function (e) {
+    var pos = utils.getCanvasRelative(e);
+    stateMod.updateTurretTarget(state, pos.x, pos.y);
+    state.down = true;
+};
+var pointerMove = function (e) {
+    var pos = utils.getCanvasRelative(e);
+    stateMod.updateTurretTarget(state, pos.x, pos.y);
+};
+var pointerUp = function (e) {
+    state.down = false;
+};
+
+canvas.addEventListener('mousedown', pointerDown);
+canvas.addEventListener('mousemove', pointerMove);
+canvas.addEventListener('mouseup', pointerUp);

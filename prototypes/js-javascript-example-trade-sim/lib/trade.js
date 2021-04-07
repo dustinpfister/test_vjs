21814f443903e4822a95ed5658d67c0527bd29a3
dustinpfister <dustin.pfister@gmail.com>
2021-04-07 13:39:54 -0400

    ITEM data

---
+++ prototypes/js-javascript-example-trade-sim/lib/trade.js
@@ -0,0 +1,45 @@
+var trade = (function () {
+
+    var ITEMS = {};
+
+    ITEMS.raspberry = {
+        desc : 'Raspberry',
+        valueRange: [1, 10]
+    };
+
+    ITEMS.apple = {
+        desc : 'Apple',
+        valueRange: [3, 18]
+    };
+
+    ITEMS.kiwi = {
+        desc : 'kiwi',
+        valueRange: [1, 3.50]
+    };
+
+    var api = {};
+
+    // create a player item object that will
+    // contain data for the players collection
+    // of items
+    var createPlayerItemObject = function(opt){
+        opt = opt || {};
+        return {
+            desc: opt.desc || '',
+            count: opt.count || 0,
+            avgCost: opt.avgCost || 0
+        };
+    };
+
+    api.create = function () {
+
+        return {
+            items_player: []
+        };
+
+    };
+
+    return api;
+
+}
+    ());

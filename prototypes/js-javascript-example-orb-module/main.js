0ea52d1880fc784557eea271c5942f97b48a2547
dustinpfister <dustin.pfister@gmail.com>
2021-04-09 16:16:08 -0400

    things look good now

--- prototypes/js-javascript-example-orb-module/main.js
+++ prototypes/js-javascript-example-orb-module/main.js
@@ -5,29 +5,33 @@ var printOrbData = function(orb){
     console.log('');
 };
 
-// pure
+// pure ( [1,0,0,0], [0,1,0,0] )
 printOrbData( orbMod.create({points:[1,0,0,0]}) );
 printOrbData( orbMod.create({points:[0,7,0,0]}) );
 printOrbData( orbMod.create({points:[0,0,8,0]}) );
 printOrbData( orbMod.create({points:[0,0,0,3]}) );
 
-
-// dule
+// dule ( [1,0,0,1], [0,1,1,0])
 printOrbData( orbMod.create({points:[0,7,0,7]}) );
 printOrbData( orbMod.create({points:[7,0,0,7]}) );
 printOrbData( orbMod.create({points:[0,7,7,0]}) );
 printOrbData( orbMod.create({points:[7,0,7,0]}) );
 
-
-// triple
+// triple ( [1,1,1,0], [1,0,1,1] )
 printOrbData( orbMod.create({points:[3,3,0,3]}) );
 printOrbData( orbMod.create({points:[3,3,3,0]}) );
 
-// quad
+// quad ( [1,1,1,1] )
 printOrbData( orbMod.create({points:[8,8,8,8]}) );
 
+// composite ( [1,1,0,2], [4,0,1,4] )
+printOrbData( orbMod.create({points:[2,2,0,4]}) );
+printOrbData( orbMod.create({points:[0,2,1,3]}) );
+printOrbData( orbMod.create({points:[8,0,2,8]}) );
 
+// from orbs test
+var a = orbMod.create({points:[1,0,0,0]}),
+b = orbMod.create({points:[1,0,2,0]});
+var orb = orbMod.fromOrbs([a, b]);
+printOrbData(orb);
 
-
-
-//var d = orbMod.fromOrbs([a, b]);

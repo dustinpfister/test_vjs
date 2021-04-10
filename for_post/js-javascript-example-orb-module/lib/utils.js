6c0d0b3541019ed67d007271e8c63ad299480f05
dustinpfister <dustin.pfister@gmail.com>
2021-04-10 09:38:58 -0400

    started a lib folder

---
+++ for_post/js-javascript-example-orb-module/lib/utils.js
@@ -0,0 +1,38 @@
+var utils = {};
+
+utils.GCD = function (a, b) {
+    if (!b) {
+        return a;
+    }
+    return GCD(b, a % b);
+};
+
+utils.GCDFromArray = function (points) {
+    var ai = 0,
+    d,
+    gd = 1,
+    bi;
+    while (ai < points.length) {
+        if (points[ai] < 1) {
+            ai += 1;
+            continue;
+        }
+        bi = 0;
+        while (bi < points.length) {
+            if (bi === ai || points[bi] < 1) {
+                bi += 1;
+                continue;
+            }
+            d = utils.GDC(points[ai], points[bi]);
+            if (points[ai] === points[bi]) {
+                d = 1;
+            }
+            if (d > gd) {
+                gd = d;
+            }
+            bi += 1;
+        }
+        ai += 1;
+    }
+    return gd;
+};

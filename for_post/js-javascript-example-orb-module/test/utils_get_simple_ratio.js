34f30f4a9ca8982f920ea9cb15b79afb8364e23b
dustinpfister <dustin.pfister@gmail.com>
2021-04-11 10:39:19 -0400

    more examples for getSimpleRatio

--- for_post/js-javascript-example-orb-module/test/utils_get_simple_ratio.js
+++ for_post/js-javascript-example-orb-module/test/utils_get_simple_ratio.js
@@ -6,8 +6,24 @@ testMod.runTest({
     name_method: 'getSimpleRatio',
     tests: [
         {
-            args: [ [5, 10] ],
-            exspect : [1, 2]
+            args: [ [1,0,0,0] ],
+            exspect : [1,0,0,0]
+        },
+        {
+            args: [ [0,3,3,3] ],
+            exspect : [0,1,1,1]
+        },
+        {
+            args: [ [7,7,7,7] ],
+            exspect : [1,1,1,1]
+        },
+        {
+            args: [ [2,2,5,0] ],
+            exspect : [2,2,5,0]
+        },
+        {
+            args: [ [14,0,7,0] ],
+            exspect : [2,0,1,0]
         }
     ]
 });

f08fd5dd2db47885e35038cabf18dc986edf4df0
dustinpfister <dustin.pfister@gmail.com>
2021-04-11 13:33:55 -0400

    started a find type test

---
+++ for_post/js-javascript-example-orb-module/test/orb_find_type.js
@@ -0,0 +1,30 @@
+let path = require('path'),
+testMod = require( path.resolve(__dirname, './testmod.js') );
+
+testMod.runTest({
+    name_mod: 'orb_node',
+    name_method: 'createFromPoints',
+    testFunction : function(result, exspect, testObj, opt) {
+        return exspect(result);
+    },
+    tests: [
+        {
+            args: [[1,0,0,0]],
+            exspect : function(orb){
+                return orb.type === 'pure';
+            }
+        },
+        {
+            args: [[0,2,2,8]],
+            exspect : function(orb){
+                return orb.type === 'composite';
+            }
+        },
+        {
+            args: [[0,7,14,1]],
+            exspect : function(orb){
+                return orb.type === 'composite';
+            }
+        }
+    ]
+});

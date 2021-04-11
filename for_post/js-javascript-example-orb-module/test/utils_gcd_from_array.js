056679ab67bacd0cd776a6e828759bc8352fc935
dustinpfister <dustin.pfister@gmail.com>
2021-04-11 10:26:53 -0400

    more tests for utils_gcd_from_array

--- for_post/js-javascript-example-orb-module/test/utils_gcd_from_array.js
+++ for_post/js-javascript-example-orb-module/test/utils_gcd_from_array.js
@@ -16,6 +16,14 @@ testMod.runTest({
         {
             args: [ [8, 2, 16, 2] ],
             exspect : 2
+        },
+        {
+            args: [ [0, 2, 0, 2] ],
+            exspect : 2
+        },
+        {
+            args: [ [15, 420, 5, 80] ],
+            exspect : 5
         }
     ]
 });

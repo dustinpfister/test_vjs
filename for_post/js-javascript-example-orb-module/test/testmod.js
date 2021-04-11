d5b60b2425ba67442cebf10ca1bbff0fab26a914
dustinpfister <dustin.pfister@gmail.com>
2021-04-11 12:49:50 -0400

    great built in log method

--- for_post/js-javascript-example-orb-module/test/testmod.js
+++ for_post/js-javascript-example-orb-module/test/testmod.js
@@ -2,10 +2,14 @@ let path = require('path')
 
 // PRIVATE helpers and values
 
+let EOL = '\n';
+
 let colors = {
    black: '\u001b[30m',
    red: '\u001b[31m',
    green: '\u001b[32m',
+   orange: '\u001b[33m',
+   cyan: '\u001b[36m',
    white: '\u001b[37m',
    reset: '\u001b[39m'
 };
@@ -13,10 +17,19 @@ let colors = {
 // build in log method
 let log = (mess, type, bool) => {
     let out = mess;
-    if(type === 'result.pass' && typeof bool === 'boolean'){
-        out = (bool === true ? colors.green : colors.red) + out + colors.reset;
+    if(type === 'info'){
+        out = colors.cyan + out + colors.reset + EOL;
     }
-    console.log(out);
+    if(type === 'result'){
+        out = '    ' + colors.orange + out + colors.reset + EOL;;
+    }
+    if(type === 'result.pass' && typeof bool === 'boolean'){
+        out = '    ' + (bool === true ? colors.green : colors.red) + out + colors.reset + EOL;;
+    }
+    if(type === 'space'){
+        out = EOL;
+    }
+    process.stdout.write(out);
 };
 
 // parse options
@@ -56,6 +69,7 @@ api.runTest = (opt) => {
     let mod = require( path.resolve(opt.dir_lib, opt.name_mod + '.js') ),
     method = mod[opt.name_method],
     log = opt.log;
+    log('', 'space');
     log('module: ' + opt.name_mod, 'info');
     log('method: ' + opt.name_method, 'info');
     opt.tests.forEach((testObj) => {
@@ -66,6 +80,7 @@ api.runTest = (opt) => {
         log('exspect | result: ' + testObj.exspect + ' | ' + testResult, 'result');
         log('pass: ' + pass, 'result.pass', pass );
     });
+    log('', 'space');
 };
 
 // export api
--- for_post/js-javascript-example-orb-module/test/utils_gcd.js
+++ for_post/js-javascript-example-orb-module/test/utils_gcd.js
@@ -7,13 +7,13 @@ testMod.runTest({
     testFunction : function(result, exspect, testObj, opt) {
         return exspect === result;
     },
-    log: function(mess, type, pass){
-        if(type === 'result.pass'){
-            console.log((pass ? '\u001b[32m' : '\u001b[31m') + mess + '\u001b[39m');
-        }else{
-            console.log(mess);
-        }
-    },
+    //log: function(mess, type, pass){
+    //    if(type === 'result.pass'){
+    //        console.log((pass ? '\u001b[32m' : '\u001b[31m') + mess + '\u001b[39m');
+    //    }else{
+    //        console.log(mess);
+    //    }
+    //},
     tests: [
         {
             args: [5, 10],

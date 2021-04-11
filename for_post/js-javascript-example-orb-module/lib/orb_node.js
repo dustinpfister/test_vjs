245afdbc63eef2dafd927adad8fe91b8cd675018
dustinpfister <dustin.pfister@gmail.com>
2021-04-11 13:32:09 -0400

    orb_node.js and createFromPoints test

--- for_post/js-javascript-example-orb-module/lib/orb_node.js
+++ for_post/js-javascript-example-orb-module/lib/orb_node.js
@@ -1,5 +1,6 @@
 var orbMod = (function (global) {
 
+    // LOAD UTILS if NODE
     var isBrowser = (function(global){
         return function () {
             try {
@@ -9,93 +10,15 @@ var orbMod = (function (global) {
             }
         };
     }(global));
-
     if(!isBrowser()){
+        // utils should be in same folder as orb_node
         var path = require('path');
         var utils = require(path.join(__dirname, 'utils.js'));
     }
 
+    // PUBLIC API
     var api = {};
 
-    // set orb values based on a given points array
-/*
-    var setByPoints = function (orb, points) {
-        orb.points = Array.from(points);
-        // find the simple ratio
-        orb.ratio = utils.getSimpleRatio(orb.points);
-        // find type
-        findType(orb);
-        return orb;
-    };
-*/
-
-/*
-    var findType = function (orb) {
-        var oneCT = 0,
-        nonOne = false,
-        oneTypes = ['pure', 'dual', 'triple', 'quad'];
-        // find count of 1's in the ratio
-        orb.ratio.forEach(function (pt) {
-            if (pt === 1) {
-                oneCT += 1;
-            } else {
-                if (pt != 0) {
-                    nonOne = true;
-                }
-            }
-        });
-        // default to a type based on count of ones in ratio
-        orb.type = oneTypes[oneCT - 1];
-        // if any value that is not 1 is in the ratio then default to composite
-        if (nonOne) {
-            orb.type = 'composite';
-        }
-    };
-*/
-
-    // create and return an Orb Object
-/*
-    api.create = function(opt){
-        var orb = {};
-        opt = opt || {};
-        opt.points = opt.points || null;
-        opt.ratio = opt.ratio || null;
-        opt.level = opt.level || null;
-        // if points i opt, set by points
-        if (opt.points) {
-            setByPoints(orb, opt.points);
-        }
-        // if just calling new Orb()
-        if (!opt.points && !opt.ratio && !opt.orbs) {
-            setByPoints(orb, [1, 0, 0, 0]);
-        }
-        return orb;
-    };
-*/
-
-/*
-    // create an orb from a collection of orbs
-    api.fromOrbs = function (orbCollection) {
-        var points = [0, 0, 0, 0],
-        tab = function (a) {
-            a.points.forEach(function (pt, i) {
-                points[i] += pt;
-            });
-        };
-        // if Array of Orbs (combine, new from)
-        if (orbCollection.constructor.name === 'Array') {
-            orbCollection.forEach(function (a) {
-                tab(a);
-            });
-            var orb = setByPoints(api.create(), points);
-            return orb;
-        } else {
-            // assume just single orb is given
-            // then just set by the given orbs points (clone orb)
-            return setByPoints(api.create(), orbCollection.points);
-        }
-    };
-*/
 
     api.createFromPoints = function(points){
         points = points || [1,0,0,0];
@@ -105,15 +28,11 @@ var orbMod = (function (global) {
         return orb
     };
 
-
-
-    // is browser?
-
+    // EXPORT/RETURN PUBLIC API
     // if nodejs, export utils
     if (!isBrowser()) {
          module.exports = api;
     }
-
     return api;
 
 }
--- for_post/js-javascript-example-orb-module/test/orb_create_points.js
+++ for_post/js-javascript-example-orb-module/test/orb_create_points.js
@@ -11,7 +11,19 @@ testMod.runTest({
         {
             args: [[1,0,0,0]],
             exspect : function(orb){
-                return orb.points.join('') === '1000';
+                return orb.points.join('') === '1000' && orb.ratio.join('') === '1000';
+            }
+        },
+        {
+            args: [[0,2,2,8]],
+            exspect : function(orb){
+                return orb.points.join('') === '0228' && orb.ratio.join('') === '0114';
+            }
+        },
+        {
+            args: [[0,7,14,1]],
+            exspect : function(orb){
+                return orb.points.join('') === '07141' && orb.ratio.join('') === '07141';
             }
         }
     ]

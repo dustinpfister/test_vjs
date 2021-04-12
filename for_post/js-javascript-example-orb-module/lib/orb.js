0025b584de840b4d5470f623cb1b799d41010914
dustinpfister <dustin.pfister@gmail.com>
2021-04-12 09:46:42 -0400

    orb_old

--- for_post/js-javascript-example-orb-module/lib/orb.js
+++ for_post/js-javascript-example-orb-module/lib/orb.js
@@ -1,80 +1,86 @@
 var orbMod = (function (global) {
 
+    // LOAD UTILS if NODE
+    var isBrowser = (function(global){
+        return function () {
+            try {
+                return global === window;
+            } catch (e) {
+                return false;
+            }
+        };
+    }(global));
+    if(!isBrowser()){
+        // ratio should be in same folder as orb_node
+        var path = require('path');
+        var ratio = require(path.join(__dirname, 'ratio.js'));
+    }
 
+    // PUBLIC API
     var api = {};
 
-    // set orb values based on a given points array
-    var setByPoints = function (orb, points) {
-        orb.points = Array.from(points);
-        // find the simple ratio
-        orb.ratio = utils.getSimpleRatio(orb.points);
-        // find type
-        findType(orb);
-        return orb;
-    };
-
-    var findType = function (orb) {
-        var oneCT = 0,
-        nonOne = false,
-        oneTypes = ['pure', 'dual', 'triple', 'quad'];
-        // find count of 1's in the ratio
-        orb.ratio.forEach(function (pt) {
-            if (pt === 1) {
-                oneCT += 1;
-            } else {
-                if (pt != 0) {
-                    nonOne = true;
-                }
-            }
-        });
-        // default to a type based on count of ones in ratio
-        orb.type = oneTypes[oneCT - 1];
-        // if any value that is not 1 is in the ratio then default to composite
-        if (nonOne) {
-            orb.type = 'composite';
+    // fire the type of the orb
+    var findType = function(orb){
+        var type = 'composite',
+        binArr = ratio.isBinaryArray(orb.ratio),
+        elCount = ratio.countNonZero(orb.ratio);
+        if(binArr){
+           type = ['pure', 'dual', 'tripple', 'quad'][elCount - 1];
         }
+        return type;
     };
 
-    // create and return an Orb Object
-    api.create = function(opt){
+    // create from points
+    api.createFromPoints = function(points){
+        points = points || [1,0,0,0];
         var orb = {};
-        opt = opt || {};
-        opt.points = opt.points || null;
-        opt.ratio = opt.ratio || null;
-        opt.level = opt.level || null;
-        // if points i opt, set by points
-        if (opt.points) {
-            setByPoints(orb, opt.points);
-        }
-        // if just calling new Orb()
-        if (!opt.points && !opt.ratio && !opt.orbs) {
-            setByPoints(orb, [1, 0, 0, 0]);
-        }
-        return orb;
+        orb.points = points;
+        orb.ratio = ratio.getSimpleRatio(orb.points);
+        orb.type = findType(orb);
+
+        // LEVEL, and INCREMENTAL
+        // The level of the orb is the power of the simple ratio to the power of 2
+        // the ratio.getLevel method should use if the points array is given along with the 
+        // base set to 2, the same method should also work to get the 
+        //  incremental by just setting base to 1
+        orb.level = Math.floor(ratio.getLevel(orb.points, 2)) + 1;
+        orb.incremental = ratio.getLevel(orb.points, 1);
+
+        return orb
     };
 
-    // create an orb from a collection of orbs
-    api.fromOrbs = function (orbCollection) {
-        var points = [0, 0, 0, 0],
-        tab = function (a) {
-            a.points.forEach(function (pt, i) {
-                points[i] += pt;
-            });
-        };
-        // if Array of Orbs (combine, new from)
-        if (orbCollection.constructor.name === 'Array') {
-            orbCollection.forEach(function (a) {
-                tab(a);
-            });
-            var orb = setByPoints(api.create(), points);
-            return orb;
-        } else {
-            // assume just single orb is given
-            // then just set by the given orbs points (clone orb)
-            return setByPoints(api.create(), orbCollection.points);
-        }
+    // create from ratio helper
+    api.createFromRatio = function(r, n, base){
+        n = n === undefined ? 1: n;
+        base = base === undefined ? 1: base;
+        return api.createFromPoints(ratio.getRaisedRatio(r, n, base));
     };
 
+    // create from a ratio and 1 relative level
+    api.createFromLevel = function(r, level){
+        var simp = ratio.getSimpleRatio(r),
+        return createFromRatio(r, level === undefined ? 1 : level, 2);
+    };
+
+    // create from a collection of orbs made before hand
+    api.createFromOrbs = function(orbCollection){
+        // just add up the points
+        var points = orbCollection.map(function(orb){
+            return orb.points;
+        }).reduce(function(acc, points){
+            return acc.map(function(el, i){
+                return el + points[i];
+            });
+        });
+        // and create a new orb with the sum of the points
+        return api.createFromPoints(points);
+    };
+
+    // EXPORT/RETURN PUBLIC API
+    // if nodejs, export ratio
+    if (!isBrowser()) {
+         module.exports = api;
+    }
     return api;
 
 }

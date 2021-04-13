4c59ccf9c9cdbef3715547b577688a7643778f49
dustinpfister <dustin.pfister@gmail.com>
2021-04-13 13:18:19 -0400

    createing an orbPool object in main.js

--- prototypes/js-javascript-example-orb-pool-module/lib/orbpool.js
+++ prototypes/js-javascript-example-orb-pool-module/lib/orbpool.js
@@ -3,8 +3,25 @@ var orbPoolMod = (function (global) {
     // PUBLIC API
     var api = {};
 
-    api.create = function(opt){
-        var orbPool = {};
+    // create and return an 'orbPool' object
+    api.create = function (opt) {
+        opt = opt || {};
+        var orbPool = poolMod.create({
+                count: opt.count || 20,
+                maxSecs: opt.maxSecs || 0.25,
+                spawn: function (obj, pool, sm, orbOptions) {
+                    obj.data = {};
+                    obj.data.orb = orbMod.createFromPoints(orbOptions.points || [1, 0, 0, 0]);
+                    obj.x = 32;
+                    obj.y = 32;
+                    obj.w = 32;
+                    obj.h = 32;
+                },
+                // update the button
+                update: function (obj, pool, sm, secs) {
+                    obj.lifespan = Infinity;
+                }
+            });
         return orbPool;
     };
 
@@ -12,4 +29,3 @@ var orbPoolMod = (function (global) {
 
 }
     (this));
-

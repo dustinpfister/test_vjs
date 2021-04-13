8a02db2459b67b670c36f58235b9fa78267c6bc7
dustinpfister <dustin.pfister@gmail.com>
2021-04-13 13:06:54 -0400

    orbpool.js started

---
+++ prototypes/js-javascript-example-orb-pool-module/lib/orbpool.js
@@ -0,0 +1,15 @@
+var orbPoolMod = (function (global) {
+
+    // PUBLIC API
+    var api = {};
+
+    api.create = function(opt){
+        var orbPool = {};
+        return orbPool;
+    };
+
+    return api;
+
+}
+    (this));
+

5cf3ecabc65c3487ff64bb11c941156c9bf1a2dd
dustinpfister <dustin.pfister@gmail.com>
2021-04-15 15:51:03 -0400

    main app loop

--- prototypes/js-javascript-example-basic-game-egg-eater/main.js
+++ prototypes/js-javascript-example-basic-game-egg-eater/main.js
@@ -8,20 +8,24 @@ ctx = canvasObj.ctx;
 
 var game = gameMod.create({canvas:canvas});
 
-var lt = new Date();
+var lt = new Date(),
+rate = 20;
 
 var loop = function(){
     var now = new Date(),
     secs = (now - lt) / 1000;
 
-    lt = now;
-
     requestAnimationFrame(loop);
+
+    if(secs > 1 / rate){
+
+        lt = now;
+
 game.pool.disp[0].active = true;
 
-    draw.back(ctx, canvas);
-    draw.disp(ctx, canvas, game.guy);
-    draw.pool(ctx, canvas, game.pool);
-
+        draw.back(ctx, canvas);
+        draw.disp(ctx, canvas, game.guy);
+        draw.pool(ctx, canvas, game.pool);
+    }
 };
 loop();

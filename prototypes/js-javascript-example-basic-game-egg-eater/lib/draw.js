7481fa337a4665321aa205b1d8a8bbf3c6a8bed1
dustinpfister <dustin.pfister@gmail.com>
2021-04-15 15:32:44 -0400

    drawing display object

--- prototypes/js-javascript-example-basic-game-egg-eater/lib/draw.js
+++ prototypes/js-javascript-example-basic-game-egg-eater/lib/draw.js
@@ -1,9 +1,15 @@
 (function(api){
 
+    // draw background
     api.back = function(ctx, canvas){
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     };
 
+    // draw display object
+    api.disp = function(ctx, canvas, disp){
+        ctx.fillStyle = disp.fill || 'white';
+        ctx.fillRect(disp.x, disp.y, disp.w, disp.h);
+    };
 
 }(this['draw'] = {}));
--- prototypes/js-javascript-example-basic-game-egg-eater/main.js
+++ prototypes/js-javascript-example-basic-game-egg-eater/main.js
@@ -1,10 +1,21 @@
 
-var canvasObj = utils.createCanvas(),
+var canvasObj = utils.createCanvas({
+   width: 640,
+   height: 480
+}),
 canvas = canvasObj.canvas,
 ctx = canvasObj.ctx;
 
 
 
-console.log(dispMod.createDisp());
+console.log();
+
+var obj = dispMod.createDisp({
+   x: 32,
+   w: 96,
+   h: 96,
+   y: canvas.height - 96 - 32
+});
 
 draw.back(ctx, canvas);
+draw.disp(ctx, canvas, obj);

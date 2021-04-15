c99f0813320c39c2f16a06353dbb86c3b83922fb
dustinpfister <dustin.pfister@gmail.com>
2021-04-15 15:24:26 -0400

    create disp object started

--- prototypes/js-javascript-example-basic-game-egg-eater/lib/disp.js
+++ prototypes/js-javascript-example-basic-game-egg-eater/lib/disp.js
@@ -1,7 +1,16 @@
 (function(api){
 
     api.createDisp = function(opt){
-        var obj = {};
+        var obj = {
+            x: opt.x === undefined ? 0 : opt.x,
+            y: opt.y === undefined ? 0 : opt.y,
+            w: opt.w || 32,
+            h: opt.h || 32
+        };
+        obj.hw = obj.w / 2;
+        obj.hh = obj.h / 2;
+        obj.cx = obj.x + obj.hw;
+        obj.cy = obj.y + obj.hh;
         return obj;
     };
 

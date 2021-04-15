9a0e8332fc11cc98883d577fc6f239abed65fdd9
dustinpfister <dustin.pfister@gmail.com>
2021-04-15 15:13:21 -0400

    added bounding box

--- prototypes/js-javascript-example-basic-game-egg-eater/lib/utils.js
+++ prototypes/js-javascript-example-basic-game-egg-eater/lib/utils.js
@@ -1,5 +1,13 @@
 var utils = {};
 
+utils.boundingBox = function (a, b) {
+    return !(
+        (a.y + a.h) < b.y ||
+        a.y > (b.y + b.h) ||
+        (a.x + a.w) < b.x ||
+        a.x > (b.x + b.w));
+};
+
 // create a canvas
 utils.createCanvas = function(opt){
     opt = opt || {};

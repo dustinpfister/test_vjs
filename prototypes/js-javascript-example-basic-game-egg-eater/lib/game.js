7f77af6831cf3fbea8d574fec066fd9ebe20e96f
dustinpfister <dustin.pfister@gmail.com>
2021-04-15 15:39:13 -0400

    game.js started

--- prototypes/js-javascript-example-basic-game-egg-eater/lib/game.js
+++ prototypes/js-javascript-example-basic-game-egg-eater/lib/game.js
@@ -2,6 +2,16 @@
 
     // draw background
     api.create = function(opt){
+       opt = opt || {};
+       opt.canvas = opt.canvas || {width: 320, height: 240};
+       var game = {
+          guy : dispMod.createDisp({
+              x: 32,
+              w: 96,
+              h: 96,
+              y: canvas.height - 96 - 32
+          })
+       };
 
     };
 

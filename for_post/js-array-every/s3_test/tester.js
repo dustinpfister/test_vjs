efdadadf96759f91d35ab740fb70cc8dc133964b
dustinpfister <dustin.pfister@gmail.com>
2021-07-12 14:53:06 -0400

    test all method

--- for_post/js-array-every/s3_test/tester.js
+++ for_post/js-array-every/s3_test/tester.js
@@ -1,13 +1,18 @@
-
-let tester = (array, tester) => {
-
-    let pass = true,
+let testAll = (array, tester, every) => {
+    let pass = false,
     i = 0;
+    every = every === undefined ? true : every;
     while (i < len) {
         var el = array[i];
         var result = tester(el, i, array);
-        if (!result) {
+        // every
+        if (!result && every) {
             pass = false;
+            break;
+        }
+        // not every (some)
+        if (result && !every) {
+            pass = true;
             break;
         }
         i += 1;

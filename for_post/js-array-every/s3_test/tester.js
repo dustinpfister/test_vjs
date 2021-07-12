c2d0bd65936def827b91050385e7038ffcd870b2
dustinpfister <dustin.pfister@gmail.com>
2021-07-12 15:03:10 -0400

    tester function looks good

--- for_post/js-array-every/s3_test/tester.js
+++ for_post/js-array-every/s3_test/tester.js
@@ -1,21 +1,40 @@
 let testAll = (array, tester, every) => {
     let pass = false,
-    i = 0;
+    i = 0,
+    len = array.length;
     every = every === undefined ? true : every;
     while (i < len) {
         var el = array[i];
         var result = tester(el, i, array);
-        // every
-        if (!result && every) {
-            pass = false;
-            break;
-        }
-        // not every (some)
-        if (result && !every) {
+        // if every
+        if (every) {
             pass = true;
-            break;
+            if (!result) {
+                pass = false;
+                break;
+            }
+        }
+        // if not every (some)
+        if (!every) {
+            pass = false; // default pass to false
+            if (result) {
+                pass = true;
+                break;
+            }
         }
         i += 1;
     }
     return pass;
 };
+
+let isNum = (el, i, array) => {
+    return typeof el === 'number';
+};
+
+let arr1 = [1, 2, 'c'];
+console.log(testAll(arr1, isNum, true)); // false
+console.log(testAll(arr1, isNum, false)); // true
+
+let arr2 = [1, 2, 3];
+console.log(testAll(arr2, isNum, true)); // true
+console.log(testAll(arr2, isNum, false)); // true

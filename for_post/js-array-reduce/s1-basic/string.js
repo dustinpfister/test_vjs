c92d3474008ed47842c5daafdee662280f70f637
dustinpfister <dustin.pfister@gmail.com>
2021-07-13 13:20:46 -0400

    string example looks good

--- for_post/js-array-reduce/s1-basic/string.js
+++ for_post/js-array-reduce/s1-basic/string.js
@@ -1,10 +1,12 @@
 let strs = ['foo', 'man', 'chew'];
 
+// so reduce can be used to join an array of strings
 let reducer = (acc, str, i, arr) => {
     let term = i === arr.length - 1 ? '' : '-';
     return acc + str + term;
 };
-
 let s = strs.reduce(reducer, '');
+console.log( s ); // 'foo-man-chew'
 
-console.log(s);
+// however there is the array join method that can be used
+console.log( strs.join('-') ); // 'foo-man-chew'

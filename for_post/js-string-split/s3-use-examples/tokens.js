6c3b8cb1c7b13d2cb52bb1fcbe0b1b8f0b2a2f7c
dustinpfister <dustin.pfister@gmail.com>
2021-07-14 15:05:16 -0400

    using replace with string split example

--- for_post/js-string-split/s3-use-examples/tokens.js
+++ for_post/js-string-split/s3-use-examples/tokens.js
@@ -1,11 +1,13 @@
 
 var tokens = function (string) {
-    return string.toLowerCase().split(' ');
+    var patt_unwanted = /[,\?!]/g;
+    return string.toLowerCase().replace(patt_unwanted, '').split(' ');
 };
 
-var text = 'This is some text',
+var text = 'Okay so then, this is some text! What do you think?',
 arr = tokens(text),
 wc = arr.length;
 
-console.log(arr); // [ 'this', 'is', 'some', 'text' ]
-console.log(wc); // 4
+console.log(arr); 
+// ['okay', 'so', 'then', 'this', 'is', 'some', 'text', 'what', 'do', 'you', 'think']
+console.log(wc); // 11

27f656ce4fd20a275d9e6386ca67807de76133cd
dustinpfister <dustin.pfister@gmail.com>
2021-07-14 14:04:13 -0400

    clean up

--- for_post/js-string-split/s1-basics/sep-regex.js
+++ for_post/js-string-split/s1-basics/sep-regex.js
@@ -1,6 +1,4 @@
 var str = 'This is some text 123 more text 7 also numbers 1 in here';
-
 var arr = str.split(/\d+/);
-
 console.log(arr);
 // [ 'This is some text ', ' more text ', ' also numbers ', ' in here' ]

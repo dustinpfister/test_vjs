9240b5416e13fdf06466bc2e3dde243fab4968de
dustinpfister <dustin.pfister@gmail.com>
2021-07-14 16:02:57 -0400

    good code examples

--- for_post/js-string-split/s3-regex/to-lines.js
+++ for_post/js-string-split/s3-regex/to-lines.js
@@ -1,6 +1,6 @@
 
 var createLinesArray = function (text) {
-    return text.split(/\n|[\r\n]/);
+    return text.split(/\n|\r\n/);
 };
 
 // Linux / Mac
@@ -8,5 +8,12 @@ var createLinesArray = function (text) {
 var text1 = 'This is a line\n' +
 'and then this is anoter. \n' +
 'The end.';
-
 console.log(createLinesArray(text1));
+// [ 'This is a line', 'and then this is anoter. ', 'The end.' ]
+
+
+var text2 = 'In Windows\r\n' +
+'There is another pattern,\r\n' +
+'used.';
+console.log(createLinesArray(text2));
+// [ 'In Windows', 'There is another pattern,', 'used.' ]

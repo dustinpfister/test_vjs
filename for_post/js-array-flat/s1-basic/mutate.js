00ead03d67ea18772b365da5e9a67e02c7306087
dustinpfister <dustin.pfister@gmail.com>
2021-07-15 15:20:13 -0400

    mutate example

---
+++ for_post/js-array-flat/s1-basic/mutate.js
@@ -0,0 +1,26 @@
+// polly fill for old versions of node
+Array.prototype.flat = function (depth) {
+    var arr = this;
+    depth = depth === undefined ? 1 : depth;
+    var flattenLevel = function (arr, level) {
+        var reducer = function (acc, val) {
+            if (typeof val === 'object') {
+                if (val.constructor.name === 'Array') {
+                    var nextLevel = level + 1;
+                    if (nextLevel <= depth) {
+                        return acc.concat(flattenLevel(val, nextLevel));
+                    }
+                    return acc.concat([val]);
+                }
+            }
+            return acc.concat(val);
+        };
+        return arr.reduce(reducer, []);
+    };
+    return flattenLevel(arr, 0);
+};
+
+var arr = [ 1,[2,3] ];
+var flat = arr.flat();
+console.log( arr ); // [ 1, [2, 3] ]
+console.log( flat); // [ 1, 2, 3 ]

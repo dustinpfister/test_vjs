76ac4a36142095673fcde9742cffc436dac78581
dustinpfister <dustin.pfister@gmail.com>
2021-07-15 13:04:57 -0400

    recursive calling

--- for_post/js-array-flat/s2-alt-vjs/reduce-concat.js
+++ for_post/js-array-flat/s2-alt-vjs/reduce-concat.js
@@ -1,5 +1,11 @@
 var flatten = function (arr) {
     var reducer = function (acc, val) {
+        if (typeof val === 'object') {
+            if (val.constructor.name === 'Array') {
+                console.log(val)
+                return acc.concat(flatten(val));
+            }
+        }
         return acc.concat(val);
     };
     return arr.reduce(reducer, []);
@@ -8,4 +14,4 @@ var flatten = function (arr) {
 let nums = [[1, 2, 3, [4, 5]], 6, 7];
 let flat = flatten(nums);
 console.log(flat);
-// [ 1, 2, 3, [4, 5], 4, 5 ]
+// [ 1, 2, 3, 4, 5, 6, 7 ]

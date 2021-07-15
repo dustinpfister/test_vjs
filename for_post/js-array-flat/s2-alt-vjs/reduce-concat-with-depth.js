64cca1493cd35c17aa8196258b603ba1410ecfcb
dustinpfister <dustin.pfister@gmail.com>
2021-07-15 13:38:31 -0400

    seems to work with 0, 1, and 2 depths

--- for_post/js-array-flat/s2-alt-vjs/reduce-concat-with-depth.js
+++ for_post/js-array-flat/s2-alt-vjs/reduce-concat-with-depth.js
@@ -1,18 +1,20 @@
 
 var flatten = function (arr, depth) {
-    var level = 1;
+    var level = 0;
     depth = depth === undefined ? 1 : depth;
     var flattenLevel = function (arr) {
         var reducer = function (acc, val) {
             if (typeof val === 'object') {
-                level += 1;
                 if (val.constructor.name === 'Array') {
+                    level += 1;
+                    //console.log(level, val);
                     if (level <= depth) {
                         return acc.concat(flattenLevel(val));
+                    } else {
+                        return acc.concat([val]);
                     }
                 }
             }
-            level += 1;
             return acc.concat(val);
         };
         return arr.reduce(reducer, []);
@@ -22,10 +24,12 @@ var flatten = function (arr, depth) {
 };
 
 let nums = [[1, 2, 3, [4, 5]], 6, 7];
+// [ [ 1, 2, 3, [ 4, 5 ] ], 6, 7 ]
 
+console.log(flatten(nums, 0));
 
-console.log( flatten(nums, 1) );
+console.log(flatten(nums, 1));
 // [ 1, 2, 3, [ 4, 5 ], 6, 7 ]
 
-console.log( flatten(nums, 2) );
+console.log(flatten(nums, 2));
 // [ 1, 2, 3, 4, 5, 6, 7 ]

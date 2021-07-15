f362fcf0263e6afcbb10450e7fe9b1a9f2377a94
dustinpfister <dustin.pfister@gmail.com>
2021-07-15 13:00:24 -0400

    reduce concat example only goes one level deep so far

--- for_post/js-array-flat/s2-alt-vjs/reduce-concat.js
+++ for_post/js-array-flat/s2-alt-vjs/reduce-concat.js
@@ -5,7 +5,7 @@ var flatten = function (arr) {
     return arr.reduce(reducer, []);
 };
 
-let nums = [[1, 2, 3], 4, 5];
+let nums = [[1, 2, 3, [4, 5]], 6, 7];
 let flat = flatten(nums);
 console.log(flat);
-// [ 1, 2, 3, 4, 5 ]
+// [ 1, 2, 3, [4, 5], 4, 5 ]

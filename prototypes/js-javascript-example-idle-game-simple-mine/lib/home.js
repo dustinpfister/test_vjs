3d6f582052115f11b1dcb15322645982fa414c23
dustinpfister <dustin.pfister@gmail.com>
2021-07-16 11:25:01 -0400

    new ore collection helper started

--- prototypes/js-javascript-example-idle-game-simple-mine/lib/home.js
+++ prototypes/js-javascript-example-idle-game-simple-mine/lib/home.js
@@ -1,9 +1,30 @@
 (function (api) {
 
-    api.create = function (opt) {
+    var ORE_DATA_DEFAULT = [
+        // ore 0 - copper
+        {
+            name: 'copper',
+            baseValue: 1
+        }
+    ];
 
+    var newOreCollection = function (home) {
+        var oreCollection = [];
+        console.log(home);
+        home.OREDATA.forEach(function (oreProps) {
+            oreCollection.push({
+                name: oreProps.name,
+                amount: 0
+            });
+        });
+        return oreCollection;
+    };
+
+    api.create = function (opt) {
+        opt = opt || {};
         var home = {};
-        home.oreCollection = [];
+        home.OREDATA = opt.OREDATA || ORE_DATA_DEFAULT;
+        home.oreCollection = opt.oreCollection || newOreCollection(home);
         return home;
 
     };

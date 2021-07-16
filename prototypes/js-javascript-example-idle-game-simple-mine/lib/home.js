07ed8c45d3b54361c5ea7fb277dc8439497ccc81
dustinpfister <dustin.pfister@gmail.com>
2021-07-16 11:14:37 -0400

    home.js started

---
+++ prototypes/js-javascript-example-idle-game-simple-mine/lib/home.js
@@ -0,0 +1,12 @@
+(function (api) {
+
+    api.create = function (opt) {
+
+        var home = {};
+
+        return home;
+
+    };
+
+}
+    (this['homeMod'] = {}));
--- prototypes/js-javascript-example-idle-game-simple-mine/main.js
+++ prototypes/js-javascript-example-idle-game-simple-mine/main.js
@@ -1,2 +1,4 @@
 
-console.log('hello');
+var home = homeMod.create();
+
+console.log(home);

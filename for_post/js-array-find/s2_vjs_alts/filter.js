e2e0ec74ef3665809cdbbc575404292869be5e1d
dustinpfister <dustin.pfister@gmail.com>
2021-07-19 10:55:24 -0400

    commnets

--- for_post/js-array-find/s2_vjs_alts/filter.js
+++ for_post/js-array-find/s2_vjs_alts/filter.js
@@ -12,3 +12,6 @@ console.log(b); // 3
 var c = a.filter(cb);
 // the first index will hen be the same result
 console.log(c[0]); // 3
+// however I can also get the full collection of elements that meet
+// the conditions of the expresion used in the callback
+console.log(c); // [3, 4, 5]
--- for_post/js-array-find/todo.md
+++ for_post/js-array-find/todo.md
@@ -6,4 +6,4 @@
 * start an s2 section that will contain alternative examples
   * s2 example using filter
   * s2 example using sort and filter
-* s3 section on lodash find
+* s3 section on use case examples

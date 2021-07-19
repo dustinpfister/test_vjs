4a89f77b6a292f6d0a6989b9881ece68a892cec6
dustinpfister <dustin.pfister@gmail.com>
2021-07-19 10:53:22 -0400

    s2 folder started

---
+++ for_post/js-array-find/s2_vjs_alts/filter.js
@@ -0,0 +1,14 @@
+// and array of numbers
+var a = [1, 2, 3, 4, 5, 6, 7];
+
+var cb = function (n) {
+    return n > 2 && n < 6;
+};
+// find will just give the first result moving from left to right
+var b = a.find(cb);
+console.log(b); // 3
+
+// so filter can be used also
+var c = a.filter(cb);
+// the first index will hen be the same result
+console.log(c[0]); // 3

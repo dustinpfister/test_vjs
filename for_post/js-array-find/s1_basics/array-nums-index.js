05710f0e70db31c33f341cc0ed6dbc9882ba20d2
dustinpfister <dustin.pfister@gmail.com>
2021-07-19 10:49:06 -0400

    array of nums index example looks good

--- for_post/js-array-find/s1_basics/array-nums-index.js
+++ for_post/js-array-find/s1_basics/array-nums-index.js
@@ -1,10 +1,14 @@
 // and array of numbers
 var a = [1, 2, 3, 4, 0, 0, -4, -2, 0, 2, 4, 6, 8];
 
-var b = [];
-var c = a.find(function (n, i, arr) {
+var result = {
+    source: null,
+    el: []
+};
+var b = a.find(function (n, i, arr) {
+        result.source = arr;
         if (i >= 5) {
-            b.push({
+            result.el.push({
                 index: i,
                 n: n
             });
@@ -12,5 +16,19 @@ var c = a.find(function (n, i, arr) {
         }
         return false;
     });
-console.log(b);
-console.log(c); // 4
+console.log(result);
+/*
+{
+    source: [1, 2, 3, 4, 0, 0, -4, -2, 0, 2, 4, 6, 8],
+    el: [
+        { index: 5, n: 0},
+        { index: 6, n: -4},
+        { index: 7, n: -2},
+        { index: 8, n: 0},
+        { index: 9, n: 2},
+        { index: 10, n: 4}
+    ]
+}
+
+*/
+console.log(b); // 4

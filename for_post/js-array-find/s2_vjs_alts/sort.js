fb39d1b2704a1f140b01cdcc57dff9aa487049b0
dustinpfister <dustin.pfister@gmail.com>
2021-07-19 11:08:17 -0400

    commnets

--- for_post/js-array-find/s2_vjs_alts/sort.js
+++ for_post/js-array-find/s2_vjs_alts/sort.js
@@ -14,5 +14,8 @@ var a = [3, 3, 0, 12, 0, -7, 37, 2];
 // if I want to find the greatest number
 var b = a.sort(sorter);
 console.log(b[0]); // 37
+// sort will also mutate the array in place and make each elements
+// be in order based on the conditions in the sorter function
+console.log(a); // [ 37, 12, 3, 3, 2, 0, 0, -7 ]
 
 

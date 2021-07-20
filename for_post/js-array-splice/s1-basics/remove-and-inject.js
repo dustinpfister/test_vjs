b915313aa743c9575f5cdb063e4a3cb73c61c4a0
dustinpfister <dustin.pfister@gmail.com>
2021-07-20 12:32:27 -0400

    remove and inject method looks good

--- for_post/js-array-splice/s1-basics/remove-and-inject.js
+++ for_post/js-array-splice/s1-basics/remove-and-inject.js
@@ -1,3 +1,15 @@
 var a = [1, 2, 4, 5];
 a.splice(1, 2, 'c', 'd');
 console.log(a); // [ 1, 'c', 'd', 5 ]
+
+// having an array as an element to inject will result in 
+// the array being injected as an element
+a.splice(2, 1, ['e, f, g'] );
+console.log(a);
+// [ 1, 'c', [ 'e, f, g' ], 5 ]
+
+// The function apply method and array concat method can be used
+// to inject an array of elements as single elements in the array
+[].splice.apply(a, [2, 1].concat(['e', 'f', 'g']));
+console.log(a);
+// [ 1, 'c', 'e', 'f', 'g', 5 ]

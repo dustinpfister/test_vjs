946cded0852012b6db933d97270f4c00a687da7c
dustinpfister <dustin.pfister@gmail.com>
2021-07-21 11:21:58 -0400

    nums example done

--- prototypes/js-array-find-index/s1-basic/nums.js
+++ prototypes/js-array-find-index/s1-basic/nums.js
@@ -1,8 +1,7 @@
-(function (api) {
-
-    api.create = function () {
-        return xpObj;
-    };
-
-}
-    (this['XPWP'] = {}));
+var a = [null, 'foo', 42, 'bar', false, 11];
+// find index of first number from left
+var index = a.findIndex(function (el) {
+        return typeof el === 'number';
+    });
+console.log(index); // 2
+console.log(a[index]); // 42

657e2bfa8d823973d4eae4dcbd6c3d7e1932eb05
dustinpfister <dustin.pfister@gmail.com>
2021-07-21 15:05:48 -0400

    have a basic idea of waypoints working

--- prototypes/js-javascript-example-exp-system-waypoints/lib/xp-waypoints.js
+++ prototypes/js-javascript-example-exp-system-waypoints/lib/xp-waypoints.js
@@ -7,7 +7,7 @@
         opt.parseBy = opt.parseBy || 'xp';
         opt.wayPoints = opt.wayPoints || [
             {levelCap: 5, expPowDelta: 1},
-            {levelCap: 10, expPowDelta: 0.25}
+            {levelCap: 10, expPowDelta: 1.125}
         ];
         var xpObj = {
             xp: 0,
@@ -18,10 +18,26 @@
         return xpObj;
     };
 
+    // get the top level cap base on wayPoints array
     var getCap = function(xpObj){
         return Math.max.apply(null, xpObj.wayPoints.map(function(wp){
             return wp.levelCap;
         }));
+    };
+
+    // get the p value used to find xp values for each level
+    var getPValue = function(xpObj, i){
+        var p = 0, //xpObj.wayPoints[i].expPowDelta,
+        wpIndex = 0,
+        levelCap = 0,
+        len = xpObj.wayPoints.length;
+        while(wpIndex < len){
+            if(i <= xpObj.wayPoints[wpIndex].levelCap){
+                return xpObj.wayPoints[wpIndex].expPowDelta;
+            }
+            wpIndex += 1;
+        }
+        return 0;
     };
 
     // create and return a level table for the given xpObj
@@ -29,10 +45,12 @@
         var cap = getCap(xpObj),
         table = [],
         i = 0,
-        p = xpObj.wayPoints[0].expPowDelta;
+        p;
         while(i < cap){
+            p = getPValue(xpObj, i);
             table.push({
                 i: i,
+                p: p,
                 xpNeeded: Math.pow(xpObj.expBase, p * i)
             });
             i += 1;
--- prototypes/js-javascript-example-exp-system-waypoints/todo.md
+++ prototypes/js-javascript-example-exp-system-waypoints/todo.md
@@ -3,4 +3,5 @@
 ## () - 0.0.0 - create method, and basic proof of concept
 * (done) start an xp-waypoints.js module
 * (done) I will want a create method of the xp-waypoints module that will create and return a standard object
-* 
+* (done) have a create table method
+* have a get p value helper

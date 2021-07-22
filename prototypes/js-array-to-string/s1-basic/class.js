dc2ea00d0cc9c24707bf6537895cd5a033e0a28f
dustinpfister <dustin.pfister@gmail.com>
2021-07-22 09:37:42 -0400

    comments

--- prototypes/js-array-to-string/s1-basic/class.js
+++ prototypes/js-array-to-string/s1-basic/class.js
@@ -1,13 +1,16 @@
+// a constructor function to create a class of an object
 var Stack = function (a) {
     this.a = a || [];
 };
-
+// the to string method is used to define what a string value should be for
+// this class of an object
 Stack.prototype.toString = function () {
     return this.a.map(function (obj) {
         return '{x:' + obj.x + ',y:' + obj.y + '}';
     }).join(',');
 };
-
+// value of is used to define what a number value should be for this
+// class of object
 Stack.prototype.valueOf = function () {
     return this.a.reduce(function (acc, el) {
         if (typeof el === 'number') {

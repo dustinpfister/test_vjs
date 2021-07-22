507571d7cb0c799a30739bf29ebeaab1f536e590
dustinpfister <dustin.pfister@gmail.com>
2021-07-22 14:24:24 -0400

    commnets

--- prototypes/js-function-inverse/s1-basic-examples/basic-5x-minus-7.js
+++ prototypes/js-function-inverse/s1-basic-examples/basic-5x-minus-7.js
@@ -1,8 +1,23 @@
 // have x, return y
-var func1 = function(x){
+var getY = function(x){
     return 5 * x - 7;
 };
-// have y, return x
-var func2 = function(y){
+// have y, return y
+var getX = function(y){
    return ( y + 7 ) / 5;
 };
+
+// The functions can be used to get
+// whatever the unknown is for a given known
+
+// have y to get x
+var x = getX(42);
+var y = getY(x);
+console.log(x); // 9.8
+console.log(y); // 42
+
+// have x to get y
+var y = getY(9.8);
+var x = getX(y);
+console.log(x); // 9.8
+console.log(y); // 42

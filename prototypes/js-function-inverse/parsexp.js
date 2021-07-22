fea20c46a9befb1603ef22cd33f0550f041102a3
dustinpfister <dustin.pfister@gmail.com>
2021-07-22 13:55:45 -0400

    simple parse by xp and level example working

--- prototypes/js-function-inverse/parsexp.js
+++ prototypes/js-function-inverse/parsexp.js
@@ -1,9 +1,16 @@
 // I have the level, I want xp
 
-var parseByLevel = function(level){
-    return 1000 + 50 * level + Math.pow(2, level);
+var parseByLevel = function (level) {
+    //return 1000 + 50 * level + Math.pow(2, level);
+    return Math.pow(2, level);
+};
+
+var parseByXp = function (xp) {
+    return Math.log(xp) / Math.log(2);
 };
 
 var xp = parseByLevel(10);
-
 console.log(xp);
+
+var level = parseByXp(xp);
+console.log(level);

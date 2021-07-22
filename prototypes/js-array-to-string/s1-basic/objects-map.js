5788fdedac81ee9d3fa29296c35a23d0dd1487f9
dustinpfister <dustin.pfister@gmail.com>
2021-07-22 09:22:28 -0400

    comments for objects-map

--- prototypes/js-array-to-string/s1-basic/objects-map.js
+++ prototypes/js-array-to-string/s1-basic/objects-map.js
@@ -3,9 +3,10 @@ var a = [
     {x: 0, y: 0},
     {x: 12, y: 35}
 ];
-
+// creating a new array b from a
 var b = a.map(function(obj){
     return '{x:'+ obj.x + ',y:' + obj.y + '}';
 });
+// and then calling toString off of b
 console.log( b.toString() );
 // {x:42,y:12},{x:0,y:0},{x:12,y:35}

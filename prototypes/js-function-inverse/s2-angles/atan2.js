0f18277c50af85f0a3e1af592202a03fb89997eb
dustinpfister <dustin.pfister@gmail.com>
2021-07-23 10:44:09 -0400

    atan2 example

--- prototypes/js-function-inverse/s2-angles/atan2.js
+++ prototypes/js-function-inverse/s2-angles/atan2.js
@@ -1,17 +1,22 @@
-// get a when normalized x, and y are known
-var getA = function (x, y) {
+// get d when normalized x, and y are known
+var getD = function (x, y) {
     var n = Math.atan2(y, x),
     radian = n < 0 ? Math.PI + (Math.PI - Math.abs(n)) : n;
     return radian / (Math.PI * 2) * 360;
 };
-/*
-// get x, and y, when a is known
-var getY = function (x, base) {
-return Math.log(x) / Math.log(base);
-};
- */
 
-console.log(getA(10, 0));
-console.log(getA(0, 10));
-console.log(getA(-10, 0));
-console.log(getA(10, -10));
+// get a normalized position when d is known
+var getPos = function (d) {
+    var radian = Math.PI / 180 * d;
+    return {
+        x: Math.cos(radian),
+        y: Math.sin(radian)
+    };
+};
+
+var d = getD(0.9396926207859084, 0.3420201433256687);
+console.log(d); // 20
+
+var pos = getPos(20);
+console.log(pos);
+// { x: 0.9396926207859084, y: 0.3420201433256687 }

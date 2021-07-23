f06d657c6827a1076a0546ee3f7e2a3fed3e9b57
dustinpfister <dustin.pfister@gmail.com>
2021-07-23 11:05:38 -0400

    todo

--- prototypes/js-function-inverse/s2-angles/dist-angle.js
+++ prototypes/js-function-inverse/s2-angles/dist-angle.js
@@ -1,7 +1,4 @@
-// get degree and radian function
-//var getDegree = function (radian) {
-//    return radian / (Math.PI * 2) * 360;
-//};
+// get radian, get degree, and a distance function
 var getRadian = function (degree) {
     return degree / 360 * (Math.PI * 2);
 };
@@ -10,11 +7,9 @@ var getD = function (x, y) {
     radian = n < 0 ? Math.PI + (Math.PI - Math.abs(n)) : n;
     return radian / (Math.PI * 2) * 360;
 };
-// A distance function
 var distance = function (x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
-
 // get position when distance and degree are known
 var getPosition = function (dist, degree) {
     var radian = getRadian(degree);
@@ -23,7 +18,6 @@ var getPosition = function (dist, degree) {
         y: Math.sin(radian) * dist
     }
 };
-
 // get distance and degree when position is known
 var getDistAngle = function (x, y) {
     var dist = distance(0, 0, x, y);
@@ -32,12 +26,10 @@ var getDistAngle = function (x, y) {
         degree: getD(x, y)
     }
 };
-
-
+// looking good
 var pos = getPosition(9, 20);
 console.log(pos);
 // { x: 8.457233587073176, y: 3.0781812899310186 }
-
 var da = getDistAngle(pos.x, pos.y);
 console.log(da);
 // { dist: 9, degree: 20 }

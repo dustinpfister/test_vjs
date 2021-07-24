3f53e9695f66d45db641cb9f597a2ca79670d414
dustinpfister <dustin.pfister@gmail.com>
2021-07-24 15:19:44 -0400

    sm object started

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/main.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/main.js
@@ -1,10 +1,16 @@
 (function () {
 
-    var canvasObj = canvasMod.createCanvas({
-        container: document.getElementById('canvas-app')
-    });
-	
-	console.log(canvasObj);
+    var sm = {};
+
+    // canvas object
+    sm.canvasObj = canvasMod.createCanvas({
+            container: document.getElementById('canvas-app')
+        });
+
+    // game object
+    sm.game = {};
+
+    console.log(sm);
 
     //var orb = orbMod.createFromLevel([1, 0, 2, 0], 3);
     //console.log(orb);

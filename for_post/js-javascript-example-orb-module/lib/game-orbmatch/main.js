c3707399d9c63778296bc34cbd8108773603b33b
dustinpfister <dustin.pfister@gmail.com>
2021-07-24 15:35:05 -0400

    draw info method

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/main.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/main.js
@@ -7,6 +7,14 @@
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     };
+    // draw info of a single given orb
+    draw.orbInfo = function (sm, ctx, canvas, orb) {
+        ctx.fillStyle = 'white';
+        ctx.textBaseline = 'top';
+        ctx.fillText('level: ' + orb.level, 10, 20);
+        ctx.fillText('points: ' + orb.points.join(','), 10, 30);
+        ctx.fillText('ratio: ' + orb.ratio.join(','), 10, 40);
+    };
 
     var sm = {};
 
@@ -28,7 +36,7 @@
     game.update = function () {};
 
     draw.background(sm, sm.canvasObj.ctx, sm.canvasObj.canvas);
-
+    draw.orbInfo(sm, sm.canvasObj.ctx, sm.canvasObj.canvas, sm.game.player.orbs[0]);
     console.log(sm);
 }
     ());

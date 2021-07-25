c78ad6fd4fe6e06e9d5a5d81746d1a11dd68d2b0
dustinpfister <dustin.pfister@gmail.com>
2021-07-25 12:16:23 -0400

    game.js started

---
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/game.js
@@ -0,0 +1,9 @@
+(function (api) {
+    // create and return a new game object
+    api.create = function (opt) {
+        opt = opt || {};
+        var game = {};
+        return game;
+    };
+}
+    (this['GameMod'] = {}));

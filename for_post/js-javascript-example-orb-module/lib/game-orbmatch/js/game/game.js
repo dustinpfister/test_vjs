91111811e99097e8811c7e401e2083f99808d0b1
dustinpfister <dustin.pfister@gmail.com>
2021-07-25 12:59:53 -0400

    create player object helper

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/game/game.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/game/game.js
@@ -1,13 +1,21 @@
 (function (api) {
+
+    // create a player/ai object
+    var createPlayerObject = function (opt) {
+        opt = opt || {};
+        var player = {};
+        player.orbCollection = OrbCollection.create();
+        player.slots = [];
+        return player;
+    };
+
     // create and return a new game object
     api.create = function (opt) {
         opt = opt || {};
         var game = {};
 
         // the start of a player object
-        game.player = {};
-        game.player.orbCollection = OrbCollection.create();
-        game.player.slots = [];
+        game.player = createPlayerObject();
 
         return game;
     };

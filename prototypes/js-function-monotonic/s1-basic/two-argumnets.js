22489f44ca814c2c789044622ba199f425220630
dustinpfister <dustin.pfister@gmail.com>
2021-07-27 12:55:38 -0400

    better two argument example

--- prototypes/js-function-monotonic/s1-basic/two-argumnets.js
+++ prototypes/js-function-monotonic/s1-basic/two-argumnets.js
@@ -1,16 +1,19 @@
 var threeArguments = function (x, min, max) {
-    return min + (1 - 1 / (1 + x)) * max;
+    return min + (max - min) * (1 - (1 / (1 + x)));
 };
 var monotonic = function (x) {
-    var a = x % 3,
-    b = Math.floor(x / 3) / 3 * 5,
+    var a = x % 4,
+    b = Math.floor(x / 4) * 5,
     c = b + 5;
+
+    console.log(a, b, c);
+
     return threeArguments(a, b, c);
 
 };
 
 var x = 0,
-len = 9,
+len = 12,
 results = [];
 while (x < len) {
     results.push({
@@ -19,4 +22,7 @@ while (x < len) {
     });
     x += 1;
 }
-console.log(JSON.stringify(results));
+console.log(results.map((obj) => {
+        return obj.y
+    }));
+//console.log(JSON.stringify(results));

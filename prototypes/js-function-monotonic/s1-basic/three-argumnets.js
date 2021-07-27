f414f02558afda0949985a44f0a90d30d75fe086
dustinpfister <dustin.pfister@gmail.com>
2021-07-27 13:01:40 -0400

    commnets

--- prototypes/js-function-monotonic/s1-basic/three-argumnets.js
+++ prototypes/js-function-monotonic/s1-basic/three-argumnets.js
@@ -1,13 +1,16 @@
+// a function that uses three arguments, that can be used
+// in a monotonic way, or not, depending on the argument domain
 var threeArguments = function (x, min, max) {
     return min + (max - min) * (1 - (1 / (1 + x)));
 };
+// monotonic function that makes use of the three arguments function
+// and uses it in a way that is strictly increasing, by making sure that
+// proper values are used for the min and max arguments that will result in the
+// return value only going up for all values of x 
 var monotonic = function (x) {
     var a = x % 4,
     b = Math.floor(x / 4) * 5,
     c = b + 5;
-
-    console.log(a, b, c);
-
     return threeArguments(a, b, c);
 
 };
--- prototypes/js-function-monotonic/todo.md
+++ prototypes/js-function-monotonic/todo.md
@@ -2,5 +2,5 @@
 
 ## () - first set of examples
 * (done) start an s1 section
-  * (done)s1-basic function that uses Math.pow
+  * (done) s1-basic function that uses Math.pow
   * (done) s1-three-arguments function that makes use of two or more arguments

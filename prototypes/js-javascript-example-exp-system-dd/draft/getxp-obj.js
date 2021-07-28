25b77e69356064d54929971629a1b82d2f7b7965
dustinpfister <dustin.pfister@gmail.com>
2021-07-28 10:43:13 -0400

    getxp-obj script looks good

--- prototypes/js-javascript-example-exp-system-dd/draft/getxp-obj.js
+++ prototypes/js-javascript-example-exp-system-dd/draft/getxp-obj.js
@@ -1,13 +1,3 @@
-
-// get xp function when level is known
-var getXP1 = function(level){
-    return level * ( level - 1 ) * 500;
-};
-
-var getXP2 = function(level){
-    return 500 * Math.pow(level, 2) - 500 * level;
-};
-
 var getObj = function(level){
     var a = Math.pow(level, 2),
     b = 500 * a,
@@ -28,8 +18,6 @@ table = [];
 while(level <= levelCap){
     table.push({
         level: level,
-        xp1: getXP1(level),
-        xp2: getXP2(level),
         obj: getObj(level)
     });
     level += 1;

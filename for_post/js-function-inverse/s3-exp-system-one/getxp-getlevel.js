f6588b456a494906fa525705a2fbcf7b8bb3890f
dustinpfister <dustin.pfister@gmail.com>
2021-07-28 12:35:01 -0400

    comments

--- for_post/js-function-inverse/s3-exp-system-one/getxp-getlevel.js
+++ for_post/js-function-inverse/s3-exp-system-one/getxp-getlevel.js
@@ -20,3 +20,15 @@ while (level <= cap) {
     level += 1;
 }
 console.log(results);
+/*
+[ { level: 1, getXPResult: 0, getLevelResult: 1 },
+  { level: 2, getXPResult: 100, getLevelResult: 2 },
+  { level: 3, getXPResult: 300, getLevelResult: 3 },
+  { level: 4, getXPResult: 600, getLevelResult: 4 },
+  { level: 5, getXPResult: 1000, getLevelResult: 5 },
+  { level: 6, getXPResult: 1500, getLevelResult: 6 },
+  { level: 7, getXPResult: 2100, getLevelResult: 7 },
+  { level: 8, getXPResult: 2800, getLevelResult: 8 },
+  { level: 9, getXPResult: 3600, getLevelResult: 9 },
+  { level: 10, getXPResult: 4500, getLevelResult: 10 } ]
+*/

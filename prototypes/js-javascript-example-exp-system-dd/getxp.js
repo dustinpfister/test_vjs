31dca3365ab2f4f33b3626b9ca6300ea42b31d00
dustinpfister <dustin.pfister@gmail.com>
2021-07-28 09:39:54 -0400

    getxp table

--- prototypes/js-javascript-example-exp-system-dd/getxp.js
+++ prototypes/js-javascript-example-exp-system-dd/getxp.js
@@ -1,4 +1,19 @@
 
+// get xp function when level is known
 var getXP = function(level){
     return level * ( level - 1 ) * 500;
 };
+
+// create table
+var level = 1,
+levelCap = 10,
+table = [];
+while(level <= levelCap){
+    table.push({
+        level: level,
+        xp: getXP(level)
+    });
+    level += 1;
+}
+
+console.log(table);

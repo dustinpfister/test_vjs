810b8c42b8e22e6e6d105059cc66bdd9e856d3a9
dustinpfister <dustin.pfister@gmail.com>
2021-07-28 18:31:18 -0400

    func1 clean up

--- prototypes/js-function-diminishing-returns/s1-basics/func1.js
+++ prototypes/js-function-diminishing-returns/s1-basics/func1.js
@@ -2,16 +2,8 @@ var dimReturn = function (n) {
     return 1 - 1 / (n + 1);
 };
 
-var getAttackValue = function (baseValue, skillBonus, skillPoints) {
-    return parseFloat((baseValue + skillBonus * dimReturn(skillPoints)).toFixed(2));
-};
-
-
-
-/*
-console.log(dimReturn(0)); // 0
-console.log(dimReturn(0.5));
-console.log(dimReturn(5));
-console.log(dimReturn(50));
-console.log(dimReturn(500));
-*/
+console.log(dimReturn(0));   // 0
+console.log(dimReturn(0.5)); // 0.33333333333333337
+console.log(dimReturn(5));   // 0.8333333333333334
+console.log(dimReturn(50));  // 0.9803921568627451
+console.log(dimReturn(500)); // 0.998003992015968

18117b80830a1c51b48cddbcb828a851184a0876
dustinpfister <dustin.pfister@gmail.com>
2021-07-29 14:28:45 -0400

    comments

--- for_post/js-function-many-to-one/s1-basics/dsin.js
+++ for_post/js-function-many-to-one/s1-basics/dsin.js
@@ -1,7 +1,7 @@
 var dSin = function(d){
     return Math.sin(Math.PI / 180 * d);
 };
-// two values for the indepandant variable d
-// will result in the same depedant value being retruned
+// two values for the independent variable d
+// will result in the same dependent value being returned
 console.log( dSin(90) );  // 1
 console.log( dSin(450) ); // 1

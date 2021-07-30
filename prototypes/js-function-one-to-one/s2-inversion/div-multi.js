84f25e99499de19c6a8735c9ccdda6aacf3f90b0
dustinpfister <dustin.pfister@gmail.com>
2021-07-30 19:11:15 -0400

    div-multi example

--- prototypes/js-function-one-to-one/s2-inversion/div-multi.js
+++ prototypes/js-function-one-to-one/s2-inversion/div-multi.js
@@ -1,7 +1,14 @@
+// get y if x is known
 var getY = function(x){
     return x * 5;
 };
-
+// get x if y is known
 var getX = function(y){
     return y / 5;
 };
+
+var a = [1,2,3,4,5].map(getY);
+console.log(a);
+
+var b = a.map(getX);
+console.log(b);

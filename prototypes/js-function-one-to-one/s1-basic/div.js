5f745ec8f6a8caf56686976606795c68cccf25d7
dustinpfister <dustin.pfister@gmail.com>
2021-07-30 19:15:17 -0400

    comments

--- prototypes/js-function-one-to-one/s1-basic/div.js
+++ prototypes/js-function-one-to-one/s1-basic/div.js
@@ -3,8 +3,9 @@ var getY = function(x){
 };
 
 // say I have a domain like this
-var domain = [1,2,3,4,5,6];
+var domain = [1, 2, 3, 4, 5, 6];
 // I can use array map to create a codomain
 // so that I now have a y value for every value of x
 var coDomain = domain.map(getY);
 console.log(coDomain);
+// [ 5, 10, 15, 20, 25, 30 ]

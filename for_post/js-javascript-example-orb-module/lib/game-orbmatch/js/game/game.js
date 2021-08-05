7d713500ca00d208b94e723201c9d97d3a3b99aa
dustinpfister <dustin.pfister@gmail.com>
2021-08-05 14:31:11 -0400

    faction prop

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/game/game.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/game/game.js
@@ -15,7 +15,7 @@
 
         // position slots
         playerObj.slots.orbs.forEach(function (orb, i) {
-            orb.y = 240 + 60;
+            orb.y = 240 + 60 * (opt.faction === 'player' ? 1: -1);
             orb.x = 32 + (640 - 32) / 4 * i;
         });
 
@@ -27,9 +27,9 @@
         opt = opt || {};
         var game = {};
         // the start of a player object
-        game.player = createPlayerObject();
+        game.player = createPlayerObject({faction:'player'});
         // start the ai object
-        game.ai = createPlayerObject();
+        game.ai = createPlayerObject({faction:'ai'});
         return game;
     };
 }

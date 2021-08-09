18030fced6e1066262fcc072c05362747469a926
dustinpfister <dustin.pfister@gmail.com>
2021-08-09 14:05:14 -0400

    current state prop for game object

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/game/game.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/game/game.js
@@ -14,20 +14,21 @@
                 points: [0, 0, 0, 0],
                 level: 0
             });
-
         // position slots
         playerObj.slots.orbs.forEach(function (orb, i) {
             orb.y = 240 + 60 * (playerObj.faction === 'player' ? 1 : -1);
             orb.x = 32 + (640 - 32) / 4 * i;
         });
-
         return playerObj;
     };
 
     // create and return a new game object
     api.create = function (opt) {
         opt = opt || {};
-        var game = {};
+        var game = {
+            turnNumber: 0,
+            currentState: 'playerTurn'
+        };
         // the start of a player object
         game.player = createPlayerObject({
                 faction: 'player'

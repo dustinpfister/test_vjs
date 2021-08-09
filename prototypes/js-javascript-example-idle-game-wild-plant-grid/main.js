64e0a022c63463033c8255b8e8d9d96eed61f2d9
dustinpfister <dustin.pfister@gmail.com>
2021-08-09 11:40:18 -0400

    using create canvas in main.js

--- prototypes/js-javascript-example-idle-game-wild-plant-grid/main.js
+++ prototypes/js-javascript-example-idle-game-wild-plant-grid/main.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+
+var sm = {
+    canvasObj: utils.createCanvas()
+};
+
+var draw = {};
+
+draw.background = function (sm) {
+    var ctx = sm.canvasObj.ctx,
+    canvas = sm.canvasObj.canvas;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+};
+
+draw.background(sm);

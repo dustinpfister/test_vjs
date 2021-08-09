9e2efa19dc3960a4291812a16f4ade2dad5fa69b
dustinpfister <dustin.pfister@gmail.com>
2021-08-09 11:44:23 -0400

    game state started

--- prototypes/js-javascript-example-idle-game-wild-plant-grid/main.js
+++ prototypes/js-javascript-example-idle-game-wild-plant-grid/main.js
@@ -1,15 +1,26 @@
 
-var sm = {
-    canvasObj: utils.createCanvas()
-};
 
 var draw = {};
 
-draw.background = function (sm) {
-    var ctx = sm.canvasObj.ctx,
-    canvas = sm.canvasObj.canvas;
+draw.background = function (sm, ctx, canvas) {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
 
-draw.background(sm);
+
+var sm = {
+    canvasObj: utils.createCanvas(),
+    game: {},
+    states: {}
+};
+
+// game state
+sm.states.game = {
+    update: function () {},
+    draw: function (sm, ctx, canvas) {
+        draw.background(sm, ctx, canvas);
+    }
+};
+
+sm.states['game'].draw(sm, sm.canvasObj.ctx, sm.canvasObj.canvas);
+

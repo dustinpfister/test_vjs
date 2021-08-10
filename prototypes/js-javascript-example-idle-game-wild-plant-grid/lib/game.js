41e393c1e35ee141b5c5719b9c64363c5ce69880
dustinpfister <dustin.pfister@gmail.com>
2021-08-10 12:24:45 -0400

    cell color prop in data object

--- prototypes/js-javascript-example-idle-game-wild-plant-grid/lib/game.js
+++ prototypes/js-javascript-example-idle-game-wild-plant-grid/lib/game.js
@@ -2,7 +2,14 @@
 
     api.create = function () {
         var game = {};
-        game.grid = gridMod.create({xOffset: 32, yOffset: 32});
+        game.grid = gridMod.create({
+                xOffset: 32,
+                yOffset: 32
+            });
+        // set some data for cells
+        game.grid.cells.forEach(function (cell) {
+            cell.data.fillStyle = 'lime';
+        });
         return game;
     };
 

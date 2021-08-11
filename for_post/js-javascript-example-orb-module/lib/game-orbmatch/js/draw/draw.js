a0d8661ecb67e87faf0d8ba1b3d5a15d0e3e6fcc
dustinpfister <dustin.pfister@gmail.com>
2021-08-11 13:17:33 -0400

    started an object in draw.js for draw methods that will differ from one game state to another

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/draw/draw.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/draw/draw.js
@@ -38,12 +38,21 @@ draw.orbCollection = function (sm, ctx, canvas, pouch) {
         }
     });
 };
+
+draw.forGameState = {
+    playerTurnOrbMenu : function(sm, ctx, canvas){
+        draw.orbCollection(sm, ctx, canvas, sm.game.player.pouch);
+    }
+};
+
 // draw the current game state
 draw.gameState = function(sm, ctx, canvas){
     draw.background(sm, ctx, canvas);
     draw.slotAreas(sm, ctx, canvas);
     draw.orbCollection(sm, ctx, canvas, sm.game.player.slots);
-    draw.orbCollection(sm, ctx, canvas, sm.game.player.pouch);
+
+    // for current game state
+    draw.forGameState[sm.game.currentState](sm, ctx, canvas);
 };
 // draw info of a single given orb
 draw.orbInfo = function (sm, ctx, canvas, orb) {

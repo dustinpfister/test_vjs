eb7b25fddf3d4004dccbba4a4a8b816c2b337248
dustinpfister <dustin.pfister@gmail.com>
2021-08-11 11:59:21 -0400

    PLANTS const started

--- prototypes/js-javascript-example-idle-game-wild-plant-grid/lib/grid-plants.js
+++ prototypes/js-javascript-example-idle-game-wild-plant-grid/lib/grid-plants.js
@@ -1,14 +1,30 @@
 (function (api) {
 
+    var PLANTS = [
+        // 0 - none
+        {
+            desc: 'none',
+            minFert: 0
+        },
+        // 1 - grass
+        {
+            desc: 'Grass',
+            minFert: 1
+        }
+    ]
+
     api.create = function () {
         var grid = gridMod.create({
                 xOffset: 32,
                 yOffset: 32
             });
+        // start with one fertPoint
+        grid.fertPoints = 1;
         // set some data for cells
         grid.cells.forEach(function (cell) {
             // fill style
             cell.data.fillStyle = 'lime';
+            cell.data.plantIndex = 0;
         });
         return grid;
     };

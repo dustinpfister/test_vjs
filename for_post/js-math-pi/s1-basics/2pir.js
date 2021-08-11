95ea0b53214bc6fe9fee5fbad9d65544f97b7b53
dustinpfister <dustin.pfister@gmail.com>
2021-08-11 10:16:57 -0400

    area example

--- for_post/js-math-pi/s1-basics/2pir.js
+++ for_post/js-math-pi/s1-basics/2pir.js
@@ -1,5 +1,6 @@
 var pi = Math.PI,
 r = 0.5,
+// circumference 
 c = 2 * pi * r;
 
 console.log(pi); // 3.141592653589793
---
+++ for_post/js-math-pi/s1-basics/pir2.js
@@ -0,0 +1,7 @@
+var pi = Math.PI,
+r = 0.5,
+// area
+a = Math.PI * Math.pow(r, 2);
+
+console.log(pi); // 3.141592653589793
+console.log(a); // 0.7853981633974483

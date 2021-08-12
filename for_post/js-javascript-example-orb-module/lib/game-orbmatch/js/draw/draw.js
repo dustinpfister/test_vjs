4302cbb5ab3dcb2da28669a961ceb4b8670d350f
dustinpfister <dustin.pfister@gmail.com>
2021-08-12 17:18:42 -0400

    removed old code

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/draw/draw.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/draw/draw.js
@@ -124,13 +124,3 @@ draw.gameState = function(sm, ctx, canvas){
         draw.button(b, ctx);
     });
 };
-/*
-// draw info of a single given orb
-draw.orbInfo = function (sm, ctx, canvas, orb) {
-    ctx.fillStyle = 'white';
-    ctx.textBaseline = 'top';
-    ctx.fillText('level: ' + orb.level, 10, 20);
-    ctx.fillText('points: ' + orb.points.join(','), 10, 30);
-    ctx.fillText('ratio: ' + orb.ratio.join(','), 10, 40);
-};
-*/

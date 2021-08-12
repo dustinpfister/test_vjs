5e2a89ae427c57f367de7a2b7b6b7017d8b3b28b
dustinpfister <dustin.pfister@gmail.com>
2021-08-12 10:47:06 -0400

    fixed mistake with use of getXp method

--- for_post/js-math-pow/s4-example-exp-system/utils.js
+++ for_post/js-math-pow/s4-example-exp-system/utils.js
@@ -1,16 +1,22 @@
 var utils = {};
 
 utils.XP = (function () {
+    // default values
+    var default_deltaNext = 50,
+    defualt_cap = 100;
     // get level with given xp
-    var getLevel = function (xp) {
-        return (1 + Math.sqrt(1 + 8 * xp / 50)) / 2;
+    var getLevel = function (xp, deltaNext) {
+        deltaNext = deltaNext === undefined ? default_deltaNext : deltaNext;
+        return (1 + Math.sqrt(1 + 8 * xp / deltaNext)) / 2;
     };
     // get exp to the given level with given current_level and xp
-    var getXP = function (level) {
-        return ((Math.pow(level, 2) - level) * 50) / 2;
+    var getXP = function (level, deltaNext) {
+        deltaNext = deltaNext === undefined ? default_deltaNext : deltaNext;
+        return ((Math.pow(level, 2) - level) * deltaNext) / 2;
     };
     // parse a levelObj by XP
-    var parseByXP = function (xp, cap) {
+    var parseByXP = function (xp, cap, deltaNext) {
+        //cap = cap === undefined ? default_cap : cap;
         var l = getLevel(xp);
         l = l > cap ? cap : l;
         var level = Math.floor(l),
@@ -27,7 +33,7 @@ utils.XP = (function () {
     return {
         // use getXP method and then pass that to parseXP for utils.XP.parseByLevel
         parseByLevel: function (l, cap) {
-            return parseByXP(getXP(l, cap), cap);
+            return parseByXP(getXP(l), cap);
         },
         // can just directly use parseByXP for utils.XP.parseByXP
         parseByXP: parseByXP
@@ -36,7 +42,7 @@ utils.XP = (function () {
     ());
 
 // seems to work okay
-var a = utils.XP.parseByLevel(120, 100);
-var b = utils.XP.parseByXP(a.xp + 0, 100);
+var a = utils.XP.parseByLevel(80, 100);
+var b = utils.XP.parseByXP(a.xp, 100);
 console.log(a); // { level: 10, levelFrac: 10, xp: 2250, forNext: 2750, toNext: 500 }
 console.log(b); // { level: 10, levelFrac: 10, xp: 2250, forNext: 2750, toNext: 500 }

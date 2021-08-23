13554b7c0d61665a1924dd6b763ab1d4bb563e92
dustinpfister <dustin.pfister@gmail.com>
2021-08-23 10:44:08 -0400

    pouch edit state started

--- for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/crafting/crafting.js
+++ for_post/js-javascript-example-orb-module/lib/game-orbmatch/js/crafting/crafting.js
@@ -1,9 +1,68 @@
 (function (api) {
 
+    // helpers
+
+    // buttons
+    var getButton = function (sm, x, y) {
+        var state = sm.states[sm.currentState];
+        var buttons = state.buttons;
+        var keys = Object.keys(buttons);
+        var i = 0,
+        buttonKey,
+        b,
+        len = keys.length;
+        while (i < len) {
+            buttonKey = keys[i];
+            b = buttons[buttonKey];
+            if (utils.boundingBox(b.x, b.y, b.w, b.h, x, y, 1, 1)) {
+                return b;
+            }
+            i += 1;
+        }
+        return null;
+    };
+    var buttonCheck = function (e, pos, sm) {
+        var b = getButton(sm, pos.x, pos.y);
+        if (b) {
+            b.onClick.call(sm, e, pos, sm, b);
+        }
+    };
+
     var states = {};
 
-    states.pouchEdit = {};
+    // pouch edit state
+    states.pouchEdit = {
+        buttons: {
+            createOrb: {
+                disp: 'Create Orb',
+                x: 220,
+                y: 200,
+                w: 200,
+                h: 64,
+                onClick: function (e, pos, game, button) {
+                    setState(sm, 'gameConfig');
+                }
+            }
+        },
+        start: function(craft){},
+        end: function(craft){},
+        update: function(craft, secs){},
+        draw: function(craft, ctx, canvas){
+            var state = craft.states.pouchEdit;
+            draw.background(sm, ctx, canvas);
+            draw.buttonCollection(state.buttons, ctx);
+        },
+        events : {
+            pointerStart: function (e, pos, sm) {
+                buttonCheck(e, pos, sm);
+            },
+            pointerMove: function (e, pos, sm) {},
+            pointerEnd: function (e, pos, sm) {}
+        }
+    };
+
     states.byRatio = {};
+
     states.deleteOrb = {};
 
     // create the main crafting object

fbf417503bc8a8bac995ff300081f218afd417af
dustinpfister <dustin.pfister@gmail.com>
2021-08-26 18:47:34 -0400

    clean up

--- for_post/js-string-replace/s5-altertaives/split-map-join-2.js
+++ for_post/js-string-replace/s5-altertaives/split-map-join-2.js
@@ -1,25 +1,41 @@
-var replace = function (opt) {
-    opt = opt || {};
-    opt.source = opt.source || '';
-    opt.sourceSep = opt.sourceSep === undefined ? ',' : opt.sourceSep;
-    opt.resultSep = opt.resultSep === undefined ? ',' : opt.resultSep;
-    opt.what = opt.what || '';
-    opt.replace = opt.replace || '';
-    return opt.source.split(opt.sourceSep).map(function (el, i, arr) {
-        if (typeof opt.what === 'string') {
-            if (el === opt.what) {
-                return opt.replace;
-            }
+var replace = (function () {
+    // create replacement method for an element
+    var createReplacement = function (el, i, arr, opt) {
+        // if replace is a string, or number
+        if (typeof opt.replace === 'string' || typeof opt.replace === 'number') {
+            return opt.replace;
         }
-        if (typeof opt.what === 'function') {
-            if (opt.what.call(opt, el, i, arr, opt)) {
-                return opt.replace;
-            }
+        if (typeof opt.replace === 'function') {
+            return opt.replace.call(opt, el, i, arr, opt);
         }
+        // fail at replacing el, and just return el
         return el;
-    }).join(opt.resultSep);
-};
-
+    };
+    // the returned function to the replace var
+    return function (opt) {
+        opt = opt || {};
+        opt.source = opt.source || '';
+        opt.sourceSep = opt.sourceSep === undefined ? ',' : opt.sourceSep;
+        opt.resultSep = opt.resultSep === undefined ? ',' : opt.resultSep;
+        opt.what = opt.what || '';
+        opt.replace = opt.replace || '';
+        return opt.source.split(opt.sourceSep).map(function (el, i, arr) {
+            if (typeof opt.what === 'string') {
+                if (el === opt.what) {
+                    return createReplacement(el, i, arr, opt);
+                }
+            }
+            if (typeof opt.what === 'function') {
+                if (opt.what.call(opt, el, i, arr, opt)) {
+                    return createReplacement(el, i, arr, opt);
+                }
+            }
+            return el;
+        }).join(opt.resultSep);
+    };
+}
+    ());
+ 
 // use example 1
 var a = 'foo,bar,bar,foo,bar';
 var b = replace({
@@ -29,15 +45,19 @@ var b = replace({
     });
 console.log(b);
 // barbarbar
-
+ 
 // use example 2 with function
-var c = 'foo,42,42,42,bar';
+var c = 'foo,1,2,3,bar';
 var d = replace({
-        source: a,
+        source: c,
         what: function (el) {
-            return parseFloat(el) != 'NaN';
+            return String(parseFloat(el)) != 'NaN';
         },
-        resultSep: ''
+        replace: function (el) {
+            var n = parseFloat(el);
+            return Math.pow(2, n);
+        },
+        resultSep: '-'
     });
 console.log(d);
-// barbarbar
+// foo-2-4-8-bar

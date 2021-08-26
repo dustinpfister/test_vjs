3281894ada1d8dcd68332be5cbd47be4a5a6ed52
dustinpfister <dustin.pfister@gmail.com>
2021-08-26 18:25:56 -0400

    example 2 looks good

--- for_post/js-string-replace/s5-altertaives/split-map-join-2.js
+++ for_post/js-string-replace/s5-altertaives/split-map-join-2.js
@@ -5,21 +5,26 @@ var replace = function (opt) {
     opt.resultSep = opt.resultSep === undefined ? ',' : opt.resultSep;
     opt.what = opt.what || '';
     opt.replace = opt.replace || '';
-    console.log();
-    return opt.source.split(opt.sourceSep).map(function (el) {
-        if (el === opt.what) {
-            return opt.replace;
+    return opt.source.split(opt.sourceSep).map(function (el, i, arr) {
+        if (typeof opt.what === 'string') {
+            if (el === opt.what) {
+                return opt.replace;
+            }
+        }
+        if (typeof opt.what === 'function') {
+            if (opt.what.call(opt, el, i, arr, opt)) {
+                return opt.replace;
+            }
         }
         return el;
     }).join(opt.resultSep);
 };
 
 var a = 'foo,bar,bar,foo,bar';
-
 var b = replace({
         source: a,
         what: 'foo',
         resultSep: ''
     });
 console.log(b);
-// bar,bar,bar,bar,bar
+// barbarbar

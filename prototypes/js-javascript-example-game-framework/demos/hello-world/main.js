77e39fcc37b49ebe87ab893b664469298cf15614
dustinpfister <dustin.pfister@gmail.com>
2021-08-29 11:50:03 -0400

    started a game object

--- prototypes/js-javascript-example-game-framework/demos/hello-world/main.js
+++ prototypes/js-javascript-example-game-framework/demos/hello-world/main.js
@@ -1,14 +1,18 @@
+// create an sm object
 var sm = gameFrame.smCreateMain({
     currentState: 'game',
+    game: {
+        text: 'Hello World',
+        x: 320,
+        y: 240
+    }
 });
-
+// add at least one state object
 gameFrame.smPushState(sm, {
     name: 'game',
     draw: function(sm, layers){
         canvasMod.draw(layers, 'background', 0);
     }
 });
-
+// start the state machine
 sm.loop();
-
-console.log(sm);

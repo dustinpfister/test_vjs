9232bf8ed0a8a34c477d7dca03d33cc603aea9b6
dustinpfister <dustin.pfister@gmail.com>
2021-08-29 11:47:01 -0400

    starting loop

--- prototypes/js-javascript-example-game-framework/demos/hello-world/main.js
+++ prototypes/js-javascript-example-game-framework/demos/hello-world/main.js
@@ -1,3 +1,14 @@
-var sm = gameFrame.smCreateMain();
+var sm = gameFrame.smCreateMain({
+    currentState: 'game',
+});
+
+gameFrame.smPushState(sm, {
+    name: 'game',
+    draw: function(sm, layers){
+        canvasMod.draw(layers, 'background', 0);
+    }
+});
+
+sm.loop();
 
 console.log(sm);

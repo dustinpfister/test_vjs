cfa7616f21ac017f3a4a80e1d7219fa34d45b1c6
dustinpfister <dustin.pfister@gmail.com>
2021-09-01 14:51:02 -0400

    private helper

--- for_post/js-javascript-module-object-literal/s4-iffe/append.js
+++ for_post/js-javascript-module-object-literal/s4-iffe/append.js
@@ -14,19 +14,33 @@ utils.boundingBox = function (x1, y1, w1, h1, x2, y2, w2, h2) {
         x1 > x2 + w2);
 };
 
+// append box methods
 (function (api) {
-    api.createBox = function () {
+
+    // private create helper
+    var createDefaultDataObject = function () {
+        return {
+            hp: {
+                current: 10,
+                max: 10
+            }
+        };
+    };
+
+    // create
+    api.createBox = function (opt) {
+        opt = opt || {};
         return {
             x: 0,
             y: 0,
             w: 32,
-            h: 32
+            h: 32,
+            data: opt.data || createDefaultDataObject()
         };
     };
 
 }
     (utils))
-
 
 var box = utils.createBox();
 

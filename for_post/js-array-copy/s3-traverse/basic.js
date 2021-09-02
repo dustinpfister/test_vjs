60af1a484897e196eacbb408eb595dc01ec2d4e3
dustinpfister <dustin.pfister@gmail.com>
2021-09-02 12:25:42 -0400

    type argument

--- for_post/js-array-copy/s3-traverse/basic.js
+++ for_post/js-array-copy/s3-traverse/basic.js
@@ -3,7 +3,7 @@ utils = {};
 utils.traverse = function (obj, forKey) {
     for (var i in obj) {
         // call forKey for every key found
-        forKey.call(obj[i], obj[i], i, obj);
+        forKey.call(obj[i], obj[i], i, typeof obj[i], obj);
         // call utils.traverse recursively if type is object and not null
         if (typeof obj[i] === 'object' && obj[i] != null) {
             var foundLabel = utils.traverse(obj[i], forKey);
@@ -14,8 +14,8 @@ utils.traverse = function (obj, forKey) {
 
 // basic example
 var arr = [1, 2, 3];
-utils.traverse(arr, function (val, key, arr) {
-    console.log(key, val);
+utils.traverse(arr, function (val, key, type, arr) {
+    console.log(key, val, type);
 });
 /*
 0 1
@@ -27,7 +27,7 @@ console.log('');
 
 // nested loop example
 var arr = [ {x: 1, y: 2}, 3, 4],
-var copy = [];
-utils.traverse(arr, function (val, key, arr) {
-    console.log(key, val);
+copy = [];
+utils.traverse(arr, function (val, key, type, arr) {
+    console.log(key, val, type);
 });

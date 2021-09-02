7ca721f04af1854b1c9f55992b56fda4de98aa42
dustinpfister <dustin.pfister@gmail.com>
2021-09-02 12:33:24 -0400

    level prop

--- for_post/js-array-copy/s3-traverse/basic.js
+++ for_post/js-array-copy/s3-traverse/basic.js
@@ -1,12 +1,14 @@
 utils = {};
 
-utils.traverse = function (obj, forKey) {
+utils.traverse = function (obj, forKey, level) {
+    level = level || 1;
     for (var i in obj) {
         // call forKey for every key found
-        forKey.call(obj[i], obj[i], i, typeof obj[i], obj);
+        forKey.call(obj[i], obj[i], i, typeof obj[i], level, obj);
         // call utils.traverse recursively if type is object and not null
         if (typeof obj[i] === 'object' && obj[i] != null) {
-            var foundLabel = utils.traverse(obj[i], forKey);
+            nextLevel = level + 1;
+            utils.traverse(obj[i], forKey, nextLevel);
         }
     }
     return null;
@@ -14,20 +16,23 @@ utils.traverse = function (obj, forKey) {
 
 // basic example
 var arr = [1, 2, 3];
-utils.traverse(arr, function (val, key, type, arr) {
-    console.log(key, val, type);
+utils.traverse(arr, function (val, key, type, level, arr) {
+    console.log(level, key, val, type);
 });
 /*
 0 1
 1 2,
 2 3
-*/
+ */
 
 console.log('');
 
 // nested loop example
-var arr = [ {x: 1, y: 2}, 3, 4],
+var arr = [{
+        x: 1,
+        y: 2
+    }, 3, 4],
 copy = [];
-utils.traverse(arr, function (val, key, type, arr) {
-    console.log(key, val, type);
+utils.traverse(arr, function (val, key, type, level, arr) {
+    console.log(level, key, val, type);
 });

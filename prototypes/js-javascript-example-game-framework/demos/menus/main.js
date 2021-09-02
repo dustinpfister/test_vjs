8c6a720116176130da012582f05b2b78fe86c52a
dustinpfister <dustin.pfister@gmail.com>
2021-09-02 14:30:08 -0400

    state objects set up the way I like it

--- prototypes/js-javascript-example-game-framework/demos/menus/main.js
+++ prototypes/js-javascript-example-game-framework/demos/menus/main.js
@@ -6,31 +6,44 @@ var sm = gameFrame.smCreateMain({
     game: {}
 });
 
-// add at least one state object
+// a main menu state
 gameFrame.smPushState(sm, {
     name: 'mainMenu',
-    // start hook will just fire once when the state object starts
+    buttons: {
+        newGame: { x: 128, y: 128, w: 128, h:64, disp: 'New Game', onClick: function(e, pos, sm, button){
+            gameFrame.smSetState(sm, 'game');
+        }}
+    },
     start: function(sm){
-        // draw background once
         canvasMod.draw(sm.layers, 'background', 0);
     },
-    // what to do on each update
+    draw: function(sm, layers){
+        canvasMod.draw(layers, 'clear', 1);
+        canvasMod.draw(layers, 'print', 1, sm.currentState, 10, 10);
+    }
+});
+// a game state
+gameFrame.smPushState(sm, {
+    name: 'game',
+    buttons: {
+        back: { x: 128, y: 128, w: 128, h:64, disp: 'New Game', onClick: function(e, pos, sm, button){
+            gameFrame.smSetState(sm, 'mainMenu');
+        }}
+    },
+    start: function(sm){
+        canvasMod.draw(sm.layers, 'background', 0);
+    },
     update: function(sm, secs){
 
     },
-    // draw will be called after each update
     draw: function(sm, layers){
         canvasMod.draw(layers, 'clear', 1);
         canvasMod.draw(layers, 'print', 1, sm.currentState, 10, 10);
     },
-    // events for this state
     events: {
-        pointerStart: function(e, pos, sm){
-        },
-        pointerMove: function(e, pos, sm){
-        },
-        pointerEnd: function(e, pos, sm){
-        }
+        pointerStart: function(e, pos, sm){},
+        pointerMove: function(e, pos, sm){},
+        pointerEnd: function(e, pos, sm){}
     }
 });
 // start the state machine

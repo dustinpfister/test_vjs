4e0463118a4bc13d417aeff758f5cc0a7899237a
dustinpfister <dustin.pfister@gmail.com>
2021-09-02 12:35:50 -0400

    basic traverse method

--- for_post/js-array-copy/s3-traverse/basic.js
+++ for_post/js-array-copy/s3-traverse/basic.js
@@ -31,8 +31,14 @@ console.log('');
 var arr = [{
         x: 1,
         y: 2
-    }, 3, 4],
-copy = [];
+    }, 3, 4];
 utils.traverse(arr, function (val, key, type, level, arr) {
     console.log(level, key, val, type);
 });
+/*
+1 '0' {x: 1,y: 2} 'object' 
+2 'x' 1 'number' 
+2 'y' 2 'number' 
+1 '1' 3 'number' 
+1 '2' 4 'number'
+*/

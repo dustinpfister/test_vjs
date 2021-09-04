9b008ea47798fc18a4b46d7227bff9a2c69487e0
dustinpfister <dustin.pfister@gmail.com>
2021-09-04 10:27:15 -0400

    s2 utils.js file

---
+++ for_post/js-xmlhttprequest/s2-custom-method/utils.js
@@ -0,0 +1,56 @@
+var utils = {};
+
+// no operation ref
+utils.noop = function () {};
+
+/********* ********** ********** *********/
+//  HTTP
+ /********* ********** ********** *********/
+
+// very simple http client
+utils.http = function(opt){
+    var opt = opt || {};
+    // default options
+    opt.url = opt.url || '';
+    opt.method = opt.method || 'GET';
+    opt.async = opt.async === undefined ? true: opt.async;
+    opt.body = opt.body === undefined ? null: opt.body;
+    opt.onDone = opt.onDone || utils.noop;
+    opt.onError = opt.onError || utils.noop;
+    opt.responseType = opt.responseType || '';  // set to 'blob' for png
+    // create and set up xhr
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = opt.responseType;
+    xhr.open(opt.method, opt.url, opt.async);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if(xhr.status >= 200 && xhr.status < 400){
+                opt.onDone.call(xhr, xhr.response, xhr);
+            }else{
+                opt.onError.call(xhr, xhr);
+            }
+        }
+    };
+    // send
+    xhr.send(opt.body);
+};
+
+// load just a png file, this calls utils.http with proper settings, and the response is an Image
+utils.httpPNG = function(opt){
+    opt = opt || {};
+    opt.onDone = opt.onDone || utils.noop;
+    opt.onError = opt.onError || utils.noop;
+    utils.http({
+        url: opt.url,
+        responseType: 'blob',
+        onDone : function(res, xhr){
+
+            var imageURL = window.URL.createObjectURL(res);
+            var image = new Image();
+            image.src = imageURL;
+
+            opt.onDone.call(xhr, image, xhr);
+        },
+        onError: opt.onError
+    });
+};
---
+++ for_post/js-xmlhttprequest/todo.md
@@ -0,0 +1,7 @@
+# js-xmlhttprequest
+
+## 2 - refine s2 section
+* refine s2 section based on what I worked out for utils.http
+
+## ( done 03/28/2018 ) - first set of examples
+* (done) first set of examples done

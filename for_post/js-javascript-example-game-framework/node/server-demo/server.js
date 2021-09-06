8afd49cf50e720004efc0e2e6335c66d02d811f7
dustinpfister <dustin.pfister@gmail.com>
2021-09-06 08:50:34 -0400

    using lstat promise method

--- for_post/js-javascript-example-game-framework/node/server-demo/server.js
+++ for_post/js-javascript-example-game-framework/node/server-demo/server.js
@@ -28,6 +28,51 @@ let port = process.argv[2] || 8080; // port 8888 for now
 let server = http.createServer(function (req, res) {
     // get the path
     let p = path.join(root, req.url);
+
+    lstat(p).then((stat)=>{
+
+// if it is not a file append index.html to path, and try that
+            if (!stat.isFile()) {
+                p = path.join(p, 'index.html');
+            }
+            // default encoding to utf-8, and get file extension
+            let encoding = 'utf-8';
+            let ext = path.extname(p).toLowerCase();
+            // binary encoding if...
+            encoding = ext === '.png' ? 'binary' : encoding;
+            // try to read the path
+            fs.readFile(p, encoding, function (e, file) {
+                // if error end
+                if (e) {
+                    res.writeHead(500);
+                    res.write(JSON.stringify(e));
+                    res.end();
+                }
+                // if file, send it out
+                if (file) {
+                    // default mime to text/plain
+                    let mime = 'text/plain';
+                    // text
+                    mime = ext === '.html' ? 'text/html' : mime;
+                    mime = ext === '.css' ? 'text/css' : mime;
+                    mime = ext === '.js' ? 'text/javascript' : mime;
+                    // images
+                    mime = ext === '.png' ? 'image/png' : mime;
+                    res.writeHead(200, {
+                        'Content-Type': mime
+                    });
+                    res.write(file, encoding);
+                    res.end();
+                }
+            });
+
+    }).catch((e)=>{
+        res.writeHead(500);
+        res.write(JSON.stringify(e));
+        res.end();
+    });
+
+/*
     // get stats of that path
     fs.lstat(p, function (e, stat) {
         // if error end
@@ -74,6 +119,9 @@ let server = http.createServer(function (req, res) {
             });
         }
     });
+*/
+
+
 });
  
 // start server

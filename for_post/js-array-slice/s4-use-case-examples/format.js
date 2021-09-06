53b7582a3d7aa950f3a39febad9d52a301d2292f
dustinpfister <dustin.pfister@gmail.com>
2021-09-06 14:44:25 -0400

    format example hit a wall

--- for_post/js-array-slice/s4-use-case-examples/format.js
+++ for_post/js-array-slice/s4-use-case-examples/format.js
@@ -1,7 +1,10 @@
 let formatNumber = function (n) {
-    let arr = String(n).split('');
-    let fract = arr.slice(-2).join('');
+    let integer = Math.floor(n);
+    let arr = String((n % 1).toFixed(2)).split('');
+    let fract = arr;
     return fract;
 };
 
 console.log(formatNumber(1000.25));
+
+console.log(formatNumber(1000.1));

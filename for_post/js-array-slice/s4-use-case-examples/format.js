fbf0cb81e25ce8d17fcf2bc5d78e66deac7b0c8c
dustinpfister <dustin.pfister@gmail.com>
2021-09-06 15:01:53 -0400

    format number example works

--- for_post/js-array-slice/s4-use-case-examples/format.js
+++ for_post/js-array-slice/s4-use-case-examples/format.js
@@ -1,7 +1,14 @@
 let formatCentValue = function (centValue) {
     var centsArray = String(centValue).split('').slice(-2);
-    var dollars = String(Math.floor(centValue / 100));
-    return dollars + '.' + centsArray.join('');
+    var dollars = String(Math.floor(centValue / 100)).split('');
+    var i = dollars.length - 3;
+    if (i > 0) {
+        while (i > 0) {
+            dollars.splice(i, 0, ',');
+            i -= 3;
+        }
+    }
+    return dollars.join('') + '.' + centsArray.join('');
 };
 
-console.log(formatCentValue(100025)); // 1000.25
+console.log(formatCentValue(100000025)); // 1000.25

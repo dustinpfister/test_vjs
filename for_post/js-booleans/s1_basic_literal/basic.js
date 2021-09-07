16be7ce258eb38a97a6986140419d57f2ae0d570
dustinpfister <dustin.pfister@gmail.com>
2021-09-07 09:43:08 -0400

    basic example looks good

--- for_post/js-booleans/s1_basic_literal/basic.js
+++ for_post/js-booleans/s1_basic_literal/basic.js
@@ -1,11 +1,12 @@
+// logging boolean
 var logging = true;
-
+// log function that will only log if logging is true
 var log = function (mess) {
     if (logging) {
         console.log(mess);
     }
 };
-
+// only foo will log
 log('foo');
 logging = false;
 log('bar');

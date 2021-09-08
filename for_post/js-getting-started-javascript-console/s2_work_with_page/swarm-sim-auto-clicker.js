aa9c9b70752e6ff23b165dbf51ce1d7397a11ff9
dustinpfister <dustin.pfister@gmail.com>
2021-09-08 11:13:54 -0400

    swarm sim object

--- for_post/js-getting-started-javascript-console/s2_work_with_page/swarm-sim-auto-clicker.js
+++ for_post/js-getting-started-javascript-console/s2_work_with_page/swarm-sim-auto-clicker.js
@@ -1,1 +1,12 @@
-document.querySelectorAll('.btn')[3].click();
+
+var sm = {};
+
+sm.getUpgrade = function(){
+    var ugl = document.querySelectorAll('tbody')[0].children,
+    ug = ugl[ugl.length - 2]
+
+};
+
+sm.getMaxButton = function(){
+    document.querySelectorAll('.btn')[3].click();
+};

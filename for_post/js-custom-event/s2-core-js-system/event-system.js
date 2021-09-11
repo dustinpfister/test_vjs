c6801f751239e62435f26d73f51220f75c424e21
dustinpfister <dustin.pfister@gmail.com>
2021-09-11 10:43:04 -0400

    comments

--- for_post/js-custom-event/s2-core-js-system/event-system.js
+++ for_post/js-custom-event/s2-core-js-system/event-system.js
@@ -1,14 +1,14 @@
 (function (api) {
 
-    //
+    // add an event for an object
     api.addEvent = function (obj, opt) {
         opt = opt || {};
         // user Event Object
         var userEvent = {};
         // MUST GIVE AN EVENT KEY
         userEvent.eventKey = opt.eventKey;
-        // need a forDispatch method
-        userEvent.forDispatch = opt.forDispatch || function () {};
+        // need a forDispatch method that will be called for each dispatch of an event
+        userEvent.forDispatch = opt.forDispatch || function (obj, dispatchOpt) {};
         // need an array of listeners
         userEvent.listeners = [];
         // attach to the objects own properties
@@ -32,6 +32,7 @@
     // dispatch an event for the given object, passing the event key, and options
     api.dispatch = function (obj, eventKey, dispatchOpt) {
         var eventObj = obj.ue[eventKey];
+        // loop listeners array
         eventObj.listeners.forEach(function (cb) {
             // call the listener
             cb.call(eventObj, eventObj.forDispatch.call(eventObj, obj, dispatchOpt));

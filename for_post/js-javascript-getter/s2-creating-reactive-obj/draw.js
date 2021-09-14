ec206c1392d44785b1e637245836bff011e463f6
dustinpfister <dustin.pfister@gmail.com>
2021-09-14 11:54:58 -0400

    todo

---
+++ for_post/js-javascript-getter/s2-creating-reactive-obj/draw.js
@@ -0,0 +1,30 @@
+
+var draw = function (obj) {
+    console.log(obj);
+};
+
+var createReactiveProp = function (obj, key, value) {
+    Object.defineProperty(obj, key, {
+        enumerable: false,
+        set: function (value) {
+            // call draw each time the value is set
+            draw(this);
+        },
+        get: function () {}
+    });
+    obj.key = value;
+    return obj;
+};
+
+var set = function (newValue, b) {
+    console.log(newValue, b);
+    draw(this);
+};
+
+var get = function () {};
+
+var obj = {};
+createReactiveProp(obj, 'count', 0, set, get);
+
+obj.count = 20;
+obj.count = 13;
--- for_post/js-javascript-getter/todo.md
+++ for_post/js-javascript-getter/todo.md
@@ -1,4 +1,7 @@
 # todo list for js-javascript-getter
 
+## () - new s2-creating-reactive-object example
+* start a new s2 example that involves a draw method being fired
+
 ## ( done 10/07/2020 ) - first set of examples
 * (done) first set of examples done for the post

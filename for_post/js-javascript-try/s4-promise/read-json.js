238405b03ec3b885d17eacde3c44c1356cefdf1b
dustinpfister <dustin.pfister@gmail.com>
2021-09-16 12:12:29 -0400

    what to do if the file is not found

--- for_post/js-javascript-try/s4-promise/read-json.js
+++ for_post/js-javascript-try/s4-promise/read-json.js
@@ -3,15 +3,15 @@ path = require('path'),
 promisify = require('util').promisify,
 readFile = promisify(fs.readFile);
 
-let uri_json = path.join(__dirname, 'foo.json');
+let uri_json = path.join(__dirname, 'conf.json');
 
 readFile(uri_json, 'utf8')
-.then((JSON_text) => {
-	
-	
-})
+.then((JSON_text) => {})
 .catch((e) => {
 
-    console.log(e);
+    // what to do if the file is not found
+    if (e.code === 'ENOENT') {
+        console.log('The conf.json file was not found');
+    }
 
 });

d594775ff63ceaf538e5f8c83f220bcd8f56efd5
dustinpfister <dustin.pfister@gmail.com>
2021-09-16 12:29:05 -0400

    spelling

--- for_post/js-javascript-try/s4-promise/read-json.js
+++ for_post/js-javascript-try/s4-promise/read-json.js
@@ -2,9 +2,9 @@ const fs = require('fs'),
 path = require('path'),
 promisify = require('util').promisify,
 readFile = promisify(fs.readFile);
-
+ 
 let uri_json = process.argv[2] || path.join(__dirname, 'conf.json');
-
+ 
 readFile(uri_json, 'utf8')
 .then((JSON_text) => {
     try {
@@ -14,19 +14,19 @@ readFile(uri_json, 'utf8')
     }
 })
 .then((obj) => {
-    console.log('JSON FIle parsed succesfly');
+    console.log('JSON FIle parsed successfully');
     console.log(obj);
 })
 .catch((e) => {
-
+ 
     // what to do if the file is not found
     if (e.code === 'ENOENT') {
         console.log('The JSON file at ' + uri_json + ' was not found');
     }
-
+ 
     if(e.name === 'SyntaxError') {
         console.log('looks like we have a syntax error');
         console.log('a good reason for this is that the JSON file is no good');
     }
-
+ 
 });

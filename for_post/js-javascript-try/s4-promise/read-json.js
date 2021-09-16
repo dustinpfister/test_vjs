a5a86ab0ad7393a1eb70fb2e77672203fbe153bf
dustinpfister <dustin.pfister@gmail.com>
2021-09-16 12:18:18 -0400

    better error message for not found

--- for_post/js-javascript-try/s4-promise/read-json.js
+++ for_post/js-javascript-try/s4-promise/read-json.js
@@ -3,7 +3,7 @@ path = require('path'),
 promisify = require('util').promisify,
 readFile = promisify(fs.readFile);
 
-let uri_json = path.join(__dirname, 'conf.json');
+let uri_json = process.argv[2] || path.join(__dirname, 'conf.json');
 
 readFile(uri_json, 'utf8')
 .then((JSON_text) => {
@@ -21,7 +21,7 @@ readFile(uri_json, 'utf8')
 
     // what to do if the file is not found
     if (e.code === 'ENOENT') {
-        console.log('The conf.json file was not found');
+        console.log('The JSON file at ' + uri_json + ' was not found');
     }
 
 });

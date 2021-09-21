32fde3210c9da79ff1367f112469cd68b2f0663f
dustinpfister <dustin.pfister@gmail.com>
2021-09-21 10:56:59 -0400

    basic node example

--- for_post/js-math-random/s1-basics/basic-node.js
+++ for_post/js-math-random/s1-basics/basic-node.js
@@ -1,2 +1,7 @@
+// the os module
+let os = require('os');
+// random number
 var n = Math.random();
-console.log(n); // random number between 0 and 1
+// using the write method of the stdout stream
+// of the process global
+process.stdout.write( String(n) + os.EOL )

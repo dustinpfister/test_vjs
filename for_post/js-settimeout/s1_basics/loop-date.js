08ce04c9168a8c6e467a12b938a3e8696f167b06
dustinpfister <dustin.pfister@gmail.com>
2021-09-22 12:47:58 -0400

    loop-date example looks good

--- for_post/js-settimeout/s1_basics/loop-date.js
+++ for_post/js-settimeout/s1_basics/loop-date.js
@@ -1,11 +1,22 @@
+// update method
 var update = function (state, secs) {
-    console.log('tick ' + secs.toFixed(2));
+    var obj = state.obj;
+    obj.x += Math.cos(obj.heading) * obj.pps * secs;
+    obj.y += Math.sin(obj.heading) * obj.pps * secs;
+    console.log(obj.x.toFixed(2), obj.y.toFixed(2));
 };
-
+// state object
 var state = {
-    lt : new Date(),
-    FPS : 2
+    lt: new Date(),
+    FPS: 2,
+    obj: {
+        x: 0,
+        y: 0,
+        pps: 32,
+        heading: Math.PI / 180 * 40
+    }
 };
+// loop
 var loop = function () {
     var now = new Date(),
     secs = (now - state.lt) / 1000;
@@ -16,5 +27,5 @@ var loop = function () {
         state.lt = now;
     }
 };
-
+// start loop
 loop();

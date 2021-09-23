f40b2e62b500a54ebb4432ae645e7ec27b955f6a
dustinpfister <dustin.pfister@gmail.com>
2021-09-23 14:51:56 -0400

    commnet

--- for_post/js-regex/s4_examples/wrap-text.js
+++ for_post/js-regex/s4_examples/wrap-text.js
@@ -1,3 +1,4 @@
+// based off of https://stackoverflow.com/questions/14484787/wrap-text-in-javascript
 var wrapText = function (str, width) {
     var patt = new RegExp('(?![^\\n]{1,' + width + ')([^\\n]{1,' + width + '})\\s', 'g');
     return str.replace(patt, '$1\n');

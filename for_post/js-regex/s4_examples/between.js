852c9f62c7183906cbe44173d62289bc7cad4393
dustinpfister <dustin.pfister@gmail.com>
2021-09-23 14:56:01 -0400

    spelling

--- for_post/js-regex/s4_examples/between.js
+++ for_post/js-regex/s4_examples/between.js
@@ -1,3 +1,3 @@
-let text = '--- title: foo --- bla bla beween --- other: stuff ---'
+let text = '--- title: foo --- bla bla between --- other: stuff ---'
 console.log(text.match(/---[\s|\S]*?---/g)[0]);
 // --- title: foo ---

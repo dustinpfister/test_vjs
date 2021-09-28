35e16b7700429b334146b4d31ea247c8f4a0923b
dustinpfister <dustin.pfister@gmail.com>
2021-09-28 11:13:24 -0400

    commnets

--- for_post/js-getting-started/s4-nodejs/file-shebang.js
+++ for_post/js-getting-started/s4-nodejs/file-shebang.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
 
-console.log('hello world');
+/*
+ *    $ node file-shebang
+ *    10.24.0
+ *
+ *    $ ./file-shebang.js
+ *     bash: ./file-shebang.js: Permission denied
+ *
+ *    $ sudo chmod 777 file-shebang.js
+ *    $ ./file-shebang.js
+ *    10.24.0
+ */
+
+console.log(process.version);

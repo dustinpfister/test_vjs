40419f47dc7a097daf1daf826314b7f424949c31
dustinpfister <dustin.pfister@gmail.com>
2021-09-29 13:07:41 -0400

    insert at method

--- for_post/js-array-push/s4-splice/splice-push-unshift.js
+++ for_post/js-array-push/s4-splice/splice-push-unshift.js
@@ -1,11 +1,18 @@
-var array = [1, 2, 3];
+var array = [4, 5, 6];
 
+var insertAt = function (array, index, what) {
+    [].splice.apply(array, [index, 0].concat(what));
+};
 // push
-var push = function(array, what){
-    [].splice.apply(array, [array.length, 0].concat(what));
-}
+var push = function (array, what) {
+    insertAt(array, array.length, what);
+};
+// unshift
+var unshift = function (array, what) {
+    insertAt(array, 0, what);
+};
 
-push(array, [4,5,6]);
-
+push(array, [7, 8, 9]);
+unshift(array, [1, 2, 3]);
 
 console.log(array); // 1-2-3

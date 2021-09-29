527de5e3228864ec748925d9d0a7d7c38492ec98
dustinpfister <dustin.pfister@gmail.com>
2021-09-29 13:33:54 -0400

    basic

---
+++ for_post/js-array-push/s3-concat/basic.js
@@ -0,0 +1,3 @@
+var array = [4, 5, 6];
+array = [1, 2, 3].concat(array, [7, 8, 9]);
+console.log(array); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
--- for_post/js-array-push/s3-concat/length_concat.js
+++
@@ -1,6 +0,0 @@
-var array = [4,5,6];
-
-array[array.length] = 7;
-array = [3].concat(array);
-
-console.log(array.join('-')); // 3-4-5-6-7

a61cdbfa9bc1dd3798c6fcdc1b42730e73dbdf3f
dustinpfister <dustin.pfister@gmail.com>
2021-09-30 07:32:37 -0400

    using a do while for the return example

--- for_post/js-array-push/s1-array-push/push_return.js
+++ for_post/js-array-push/s1-array-push/push_return.js
@@ -1,9 +1,7 @@
-var arr = [],
-obj;
-while (arr.push({}) < 10) {
-    obj = arr[arr.length - 1];
+var arr = [], obj;
+do {
+    obj = {};
     obj.n = 0;
-}
-arr.pop();
+} while (arr.push(obj) < 10)
 console.log(arr);
 //[{n:0},{n:0},{n:0},{n:0},{n:0},{n:0},{n:0},{n:0},{n:0}]

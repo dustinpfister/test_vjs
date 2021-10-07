bc68db33f958679b46a923fdb7d409f7d63e2ee5
dustinpfister <dustin.pfister@gmail.com>
2021-10-07 15:58:31 -0400

    todo

--- for_post/js-path-find/s3-canvas/lib/grid.js
+++ for_post/js-path-find/s3-canvas/lib/grid.js
@@ -1,4 +1,38 @@
 (function (api) {
+
+/*
+    api.getPath = function (grid, sx, sy, ex, ey) {
+        // copy the given grid
+        var grid = Grid.fromMatrix(givenGrid.nodes),
+        path = [],
+        opened = [],
+        node;
+        // set startNode and End Node to copy of grid
+        var startNode = grid.nodes[sy][sx];
+        endNode = grid.nodes[ey][ex];
+        // push start Node to open list
+        opened.push(startNode);
+        startNode.opened = true;
+        startNode.weight = 0;
+        // start walking
+        while (opened.length > 0) {
+            // pop out next Node from open list
+            node = opened.pop();
+            node.closed = true;
+            // if the node is the end node
+            if (node === endNode) {
+                return buildPath(node);
+            }
+            // loop current neighbors
+            forNeighbors(grid, node, endNode, open);
+            // sort the list of nodes be weight value to end node
+            sortOpen(open);
+        }
+        // return an empty array if we get here (can not get to end node)
+        return [];
+    };
+*/
+
     // create a grid object
     api.create = function (opt) {
         opt = opt || {};
@@ -19,7 +53,8 @@
                 i: i, // store index for this cell
                 cellX: i % grid.w, // grid index pos values as uppercase X, and Y
                 cellY: Math.floor(i / grid.w),
-                data: {} // user data object
+                data: {}
+                // user data object
             };
             // cell pixel pos values as lowercase x, and y
             cell.x = grid.xOffset + cell.cellX * grid.cellSize;
@@ -31,10 +66,10 @@
     };
     // get a cell by the given pixel position
     api.getCellByPixlePos = function (grid, x, y) {
-        var cellX = Math.floor( (x - grid.xOffset) / grid.cellSize ),
-        cellY = Math.floor( (y - grid.yOffset) / grid.cellSize ),
+        var cellX = Math.floor((x - grid.xOffset) / grid.cellSize),
+        cellY = Math.floor((y - grid.yOffset) / grid.cellSize),
         cell;
-        if(cellX >= 0 && cellY >= 0 && cellX < grid.w && cellY < grid.h){
+        if (cellX >= 0 && cellY >= 0 && cellX < grid.w && cellY < grid.h) {
             return grid.cells[cellY * grid.w + cellX];
         }
         return null;
--- for_post/js-path-find/todo.md
+++ for_post/js-path-find/todo.md
@@ -2,5 +2,6 @@
 
 ### () - new s3 section on canvas
 * (done) start a new client side javaScript section folder that uses canvas
+* I will need a gridMod.chunk method for the grid module
 * see about placing what I worked out in the nodejs version in the grid module
 * just get a path object for now

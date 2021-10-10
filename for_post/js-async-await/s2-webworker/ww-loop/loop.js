e7205db56a7dbdb8a06bf241d0e9e27e8062bc19
dustinpfister <dustin.pfister@gmail.com>
2021-10-10 13:50:36 -0400

    loop example updated

--- for_post/js-async-await/s2-webworker/ww-loop/loop.js
+++ for_post/js-async-await/s2-webworker/ww-loop/loop.js
@@ -1,18 +1,26 @@
 // async function that does something heavy
 let heavyAsync = async function () {
-    var i = Math.pow(10, 9);
+    var i = Math.floor(Math.pow(10, 9.25)),
+    st = new Date();
     while (i--) {}
-    console.log('bar');
+    var secs = (new Date() - st) / 1000;
+    console.log('');
+    console.log('heavy time: ', secs.toFixed(2));
+    console.log('');
 };
 // loop
-let i = 0, st;
+let i = 0,
+lt = new Date();
 let loop = function () {
     setTimeout(loop, 250);
-    st = new Date();
+    var now = new Date(),
+    secs = (now - lt) / 1000;
+    lt = now;
     if (i % 10 === 5) {
         heavyAsync();
     }
-    console.log('tick' + i, ' time: ' + (new Date() - st));
+    console.log('tick' + i, ' time: ' + secs.toFixed(2));
+
     i += 1;
 };
 

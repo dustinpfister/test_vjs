70c65da3e78013534538b1be24a2e303af31607c
dustinpfister <dustin.pfister@gmail.com>
2021-10-10 09:50:08 -0400

    server file added for s2 folder on web workers

---
+++ for_post/js-async-await/s2-webworker/server.js
@@ -0,0 +1,151 @@
+/*
+ *  server.js
+ *
+ *   This just provides a simple static server for the project.
+ *
+ *   ex: $ node server ./ 8080
+ *
+ */
+
+let http = require('http'),
+fs = require('fs'),
+path = require('path'),
+promisify = require('util').promisify,
+lstat = promisify(fs.lstat),
+readFile = promisify(fs.readFile),
+readdir = promisify(fs.readdir);
+
+
+// the root folder to serve
+let root = process.argv[2] || path.join(__dirname, './');
+
+// set port with argument or hard coded default
+let port = process.argv[3] || 8080; // port 8080 for now
+
+// create path info object
+let createPathInfoObject = (url) => {
+    // remove any extra / ( /foo/bar/  to /foo/bar )
+    let urlArr = url.split('');
+    if(urlArr[urlArr.length - 1] === '/'){
+        urlArr.pop();
+        url = urlArr.join('');
+    }  
+    // starting state
+    let pInfo = {
+        url : url,
+        uri : path.join(root, url),
+        encoding: 'utf-8',
+        mime: 'text/plain',
+        ext: '',
+        contents: [],
+        html: ''
+    };
+    //return pInfo;
+    return lstat(pInfo.uri)
+    .then((stat)=>{
+        pInfo.stat = stat;
+        if(pInfo.stat.isFile()){
+            pInfo.ext = path.extname(pInfo.uri).toLowerCase();
+            pInfo.ext = path.extname(pInfo.uri).toLowerCase();
+            pInfo.mime = pInfo.ext === '.html' ? 'text/html' : pInfo.mime;
+            pInfo.mime = pInfo.ext === '.css' ? 'text/css' : pInfo.mime;
+            pInfo.mime = pInfo.ext === '.js' ? 'text/javascript' : pInfo.mime;
+             // images
+            pInfo.mime = pInfo.ext === '.png' ? 'image/png' : pInfo.mime;
+            pInfo.mime = pInfo.ext === '.ico' ? 'image/x-icon' : pInfo.mime;
+            // binary encoding if...
+            pInfo.encoding = pInfo.ext === '.png' || pInfo.ext === '.ico' ? 'binary' : pInfo.encoding;
+            return pInfo;
+        }
+        if(pInfo.stat.isDirectory()){
+            pInfo.ext = '';
+            pInfo.mime = 'text/plain';
+            pInfo.encoding = 'utf-8';
+        }
+        return createDirInfo(pInfo);
+    });
+};
+
+// create an html index of a folder
+let createHTML = (pInfo) => {
+    var html = '<html><head><title>Index of - ' + pInfo.url + '</title>'+
+    '<style>body{padding:20px;background:#afafaf;font-family:arial;}div{display: inline-block;padding:10px;}</style>' +
+    '</head><body>';
+    html += '<h3>Contents of : ' + pInfo.url + '</h3>'
+    pInfo.contents.forEach((itemName)=>{
+        let itemURL = pInfo.url + '/' + itemName;
+        html += '<div> <a href=\"' + itemURL + '\" >' +  itemName + '</a> </div>'
+    });
+    html += '</body></html>';
+    return html;
+};
+
+// create dir info for a pInfo object
+let createDirInfo = (pInfo) => {
+    // first check for an index.html
+    let uriIndex = path.join( pInfo.uri, 'index.html' );
+    return readFile(uriIndex)
+    // if all goes file we have an indrex file call createPathInfoObject with new uri
+    .then((file)=>{
+        pInfo.uri = uriIndex;
+        pInfo.ext = '.html';
+        pInfo.mime = 'text/html';
+        return pInfo;
+    })
+    // else we do not get contents
+    .catch(()=>{
+        return readdir(pInfo.uri);
+    }).then((contents)=>{
+        if(contents && pInfo.ext === ''){
+            pInfo.contents = contents;
+            pInfo.mime = 'text/html';
+            pInfo.html = createHTML(pInfo);
+        }
+        return pInfo;
+    });
+};
+
+// create server object
+let server = http.createServer(function (req, res) {
+    // create path info object for req.url
+    createPathInfoObject(req.url)
+    // if we have a pinfo object without any problems
+    .then((pInfo)=>{
+        // send content
+        res.writeHead(200, {
+            'Content-Type': pInfo.mime
+        });
+        // if we have html send that
+        if(pInfo.html != ''){
+            res.write(pInfo.html, pInfo.encoding);
+            res.end();
+        }else{
+            // else we are sending a file
+            readFile(pInfo.uri, pInfo.encoding).then((file)=>{
+                res.write(file, pInfo.encoding);
+                res.end();
+            }).catch((e)=>{
+                // send content
+                res.writeHead(200, {
+                    'Content-Type': 'text/plain'
+                });
+                res.write(e.message, 'utf8');
+                res.end();
+            });
+        }
+    }).catch((e)=>{
+        // send content
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write(e.message, 'utf8');
+        res.end();
+    });
+});
+
+// start server
+server.listen(port, function () {
+    console.log('hosting a public folder at: ');
+    console.log('path: ' + root);
+    console.log('port: ' + port);
+});
--- for_post/js-async-await/todo.md
+++ for_post/js-async-await/todo.md
@@ -3,5 +3,7 @@
 ## () - split section to into a web worker and child process section
 * (done) have an s2 section just for web worker
 * (done) have an s3 section just for the child process module in nodejs
-* have an html file for a web worker example in s2 section
+* (done) have a ww-basic.html file for a web worker example in s2 section
+* (done) have a ww-basic.js file for the web worked file to be used for ww-basic
+* (done) looks like I will need a simple server script for the s2 folder
 * have an actual working web worker example

cbacd920c4893b5c21bec48bd1890e97a3ca0cc8
dustinpfister <dustin.pfister@gmail.com>
2021-10-12 11:35:05 -0400

    good test function

--- for_post/js-web-storage/s2-feature-test/web-storage.js
+++ for_post/js-web-storage/s2-feature-test/web-storage.js
@@ -1,10 +1,44 @@
 var ws = {};
+
+var test = function () {
+    console.log('web sorage test: ');
+    if (!localStorage) {
+        console.log('window.localStorage object not found, pass is false');
+        return false;
+    }
+    // save a test object for key ws-test
+    localStorage.setItem('ws-test', JSON.stringify({
+            value: 'foo'
+        }));
+    // try to now get what we just saved
+    var string = localStorage.getItem('ws-test');
+    if (string) {
+        // so we have a string parse to an object
+        try {
+            var result = JSON.parse(string);
+        } catch (e) {
+            console.log('got a dom string, but there was an error parsing JSON some how.');
+            return false;
+        }
+        // so we have an object, is are test value there?
+        var pass = result.value === 'foo';
+        console.log('Got an object and value test pass is: ' + pass);
+        // in any case remove the item
+        localStorage.removeItem('test');
+        // return result of pass boolean if all is well it should be true
+        return pass;
+    }
+    console.log('No result object pass is false');
+    return false;
+
+}
+
 // get an item with local storage
 ws.get = function (key, opt) {
     opt = opt || {};
     opt.onDisabled = opt.onDisabled || function () {};
     // feature test for local storage
-    if (localStorage) {
+    if (test()) {
         var mess = localStorage.getItem(key);
         if (mess) {
             return mess;
@@ -16,10 +50,10 @@ ws.get = function (key, opt) {
     }
 };
 // set an item with local storage
-ws.set = function (key, value) {
+ws.set = function (key, value, opt) {
     opt = opt || {};
     opt.onDisabled = opt.onDisabled || function () {};
-    if (localStorage) {
+    if (test()) {
         localStorage.setItem(key, value);
     } else {
         opt.onDisabled.call(opt, opt, key);

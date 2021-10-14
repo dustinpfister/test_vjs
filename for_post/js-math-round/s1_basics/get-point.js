5d0bb7f33f4f6aa4f4ae3347ef2df3d826864231
dustinpfister <dustin.pfister@gmail.com>
2021-10-14 08:50:35 -0400

    get-point example looks good

--- for_post/js-math-round/s1_basics/get-point.js
+++ for_post/js-math-round/s1_basics/get-point.js
@@ -1,14 +1,16 @@
+// get an point in an area with a width height, and 
+// values between 0 and 1.
 var getAreaPoint = function (w, h, wPer, hPer) {
     return {
         x: Math.floor(w * wPer),
         y: Math.floor(h * hPer)
     };
 };
-
+// random point
 var getRandomAreaPoint = function (w, h) {
     return getAreaPoint(w, h, Math.random(), Math.random());
 };
-
+// demo
 var i = 10;
 while(i--){
     console.log( getRandomAreaPoint(10, 10) );

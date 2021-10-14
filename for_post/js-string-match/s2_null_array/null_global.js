f3a0f600865edc7fe32ce98f4730285cec9ab7f2
dustinpfister <dustin.pfister@gmail.com>
2021-10-14 15:24:36 -0400

    null global example looking good

--- for_post/js-string-match/s2_null_array/null_global.js
+++ for_post/js-string-match/s2_null_array/null_global.js
@@ -1,22 +1,26 @@
 // getFooIndex method using String.match that will
 // return -1 when null is returned
-let getFooIndex = (str, patt) => {
+let getFooIndices = (str, patt) => {
     let m = str.match(patt);
     // if not null return index
     if (m) {
         if (m.index === undefined) {
             var result,
             indices = [];
+            // use exec to get all
             while (result = patt.exec(str)) {
                 indices.push(result.index);
             }
             return indices;
         }
-        //return m.index;
+        // just the one
+        return [m.index];
     }
-    // else return -1
-    return -1;
+    // none
+    return [];
 };
 
 let str1 = 'This foo example string is a string that has more than one foo';
-console.log(getFooIndex(str1, /foo/g)); // [5, 59]
+console.log(getFooIndices(str1, /foo/g)); // [5, 59]
+console.log(getFooIndices(str1, /foo/)); // [5]
+console.log(getFooIndices(str1, /bar/g)); // []

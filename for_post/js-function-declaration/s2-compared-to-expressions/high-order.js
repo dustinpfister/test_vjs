a07f4ab1926a27a0e8320731efc5072a6dff8a37
dustinpfister <dustin.pfister@gmail.com>
2021-10-15 11:11:31 -0400

    commnets to high order

--- for_post/js-function-declaration/s2-compared-to-expressions/high-order.js
+++ for_post/js-function-declaration/s2-compared-to-expressions/high-order.js
@@ -1,9 +1,9 @@
+// of course a function declatation can be
+// given as an argument to another function
 function foo(){
     return 'bar'
 }
-
 function foobar(bar){
     return 'foo' + bar();
 }
-
 console.log( foobar(foo) );

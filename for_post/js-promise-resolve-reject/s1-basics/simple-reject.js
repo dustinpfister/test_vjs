5aca644a5ea845dcf4a0bd38e3eff121099e0cfb
dustinpfister <dustin.pfister@gmail.com>
2021-10-16 09:10:51 -0400

    clean up

--- for_post/js-promise-resolve-reject/s1-basics/simple-reject.js
+++ for_post/js-promise-resolve-reject/s1-basics/simple-reject.js
@@ -1,7 +1,4 @@
 Promise.reject(new Error('No Good'))
-.then(function (str) {
-    console.log(str);
-})
 .catch(function (e) {
     console.warn(e.message);
 });
--- for_post/js-promise-resolve-reject/s1-basics/simple-resolve.js
+++ for_post/js-promise-resolve-reject/s1-basics/simple-resolve.js
@@ -1,8 +1,5 @@
 Promise.resolve('foo')
 .then(function (str) {
     console.log(str);
-})
-.catch(function (e) {
-    console.warn(e);
 });
 // 'foo'

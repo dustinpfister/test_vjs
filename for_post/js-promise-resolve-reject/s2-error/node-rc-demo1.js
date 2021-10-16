e91beecac54ccf1e628cecba8f6b4017a7b4641a
dustinpfister <dustin.pfister@gmail.com>
2021-10-16 12:50:53 -0400

    getting or creating a json file if given a folder

--- for_post/js-promise-resolve-reject/s2-error/node-rc-demo1.js
+++ for_post/js-promise-resolve-reject/s2-error/node-rc-demo1.js
@@ -16,11 +16,15 @@ let start = (uri) => {
         let newState = countApp.newState();
         // in the event of a DIR error I can create a conf.json there
         if (myCode === 'DIR') {
-            let newState = countApp.newState();
-            console.log('myCode error is DIR, writing new conf.json file there');
-            return countApp.writeConf(path.join(uri, 'conf.json'), newState)
+            console.log('myCode error is DIR, updating uri_conf, and reading again...');
+            // update uri_conf to this:
+            uri_conf = path.join(uri, 'conf.json');
+            return countApp.readConf(uri_conf)
+            .catch((e) => {
+                console.log('error trying to read file at updated uri_conf, trying to write one now...');
+                return countApp.writeConf(uri_conf, newState);
+            })
             .then(() => {
-                uri_conf = path.join(uri, 'conf.json');
                 return start(uri_conf);
             })
         }
@@ -39,7 +43,6 @@ start(uri_conf)
 .then((state) => {
     state.count += state.delta;
     console.log('new count: ' + state.count);
-	console.log(uri_conf);
     return countApp.writeConf(uri_conf, state)
 })
 .then(() => {

4f0b83e24f19d183ceea41f2467df4db5308e95e
dustinpfister <dustin.pfister@gmail.com>
2021-10-16 12:16:43 -0400

    new state method working

--- for_post/js-promise-resolve-reject/s2-error/node-rc-demo1.js
+++ for_post/js-promise-resolve-reject/s2-error/node-rc-demo1.js
@@ -4,12 +4,13 @@ countApp = require(path.join(__dirname, 'node-get-conf.js'));
 let uri_conf = countApp.parseURI();
 countApp.readConf(uri_conf)
 .then((obj) => {
-
     console.log(obj);
-
 })
 .catch((e) => {
+    let myCode = e.message.split(':')[0];
+    console.log(myCode);
 
-    console.log(e.message);
+    let newState = countApp.newState();
+    console.log(newState);
 
 });

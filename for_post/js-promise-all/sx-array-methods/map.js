2a8699dfdec4ed47e1bd558b46b53eecab37fd31
dustinpfister <dustin.pfister@gmail.com>
2021-10-17 16:35:15 -0400

    comments

--- for_post/js-promise-all/sx-array-methods/map.js
+++ for_post/js-promise-all/sx-array-methods/map.js
@@ -1,4 +1,3 @@
-// a delayed function that returns a promise
 let delayed = (ms) => {
     return new Promise((resolve, reject) => {
         let st = new Date();
@@ -8,8 +7,10 @@ let delayed = (ms) => {
     });
 }
 
+// an array of times
 let times = [10, 1500, 100, 75, 33, 150];
-
+// using array.map to create a new array of promises
+// my calling delayed method for each element in times array
 Promise.all(times.map((ms) => {
     return delayed(ms);
 }))

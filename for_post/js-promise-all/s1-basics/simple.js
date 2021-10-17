fe968f684654d223032afa9a22e6cf541e63277c
dustinpfister <dustin.pfister@gmail.com>
2021-10-17 13:38:13 -0400

    comments

--- for_post/js-promise-all/s1-basics/simple.js
+++ for_post/js-promise-all/s1-basics/simple.js
@@ -1,12 +1,14 @@
+// a delayed function that returns a promise
 let delayed = (ms) => {
     return new Promise((resolve, reject) => {
-		let st = new Date();
+        let st = new Date();
         setTimeout(() => {
             resolve(new Date() - st);
         }, ms || 1000)
     });
 }
-
+// calling the function twice for two elements in an array, and
+// passing that array to the promise all method
 Promise.all([delayed(10), delayed(1500)])
 .then((array) => {
     console.log(array);

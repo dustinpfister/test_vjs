38318d838873be517d08dd08924f41ed82479b71
dustinpfister <dustin.pfister@gmail.com>
2021-10-18 11:11:52 -0400

    better basic example

--- for_post/js-call-apply-and-bind/s3_apply/basic.js
+++ for_post/js-call-apply-and-bind/s3_apply/basic.js
@@ -1,21 +1,4 @@
-
-var obj = {
-    0: 1,
-    1: 2,
-    2: 3,
-    length: 3
-};
-
-[].push.apply(obj, ['foo', 'man', 'chew']);
-console.log( obj );
-/*
-{
-    '0': 1,
-    '1': 2,
-    '2': 3,
-    '3': 'foo',
-    '4': 'man',
-    '5': 'chew',
-    length: 6
-}
-*/
+var str = 'Hello Word'
+var a = [].slice.apply(str, [6, 10]);
+console.log(a);
+// [ 'W', 'o', 'r', 'd' ]

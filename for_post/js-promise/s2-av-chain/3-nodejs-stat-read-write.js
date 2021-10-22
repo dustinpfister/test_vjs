ecf4f1dcccb939464bac67d6bdfec711aa3f14de
dustinpfister <dustin.pfister@gmail.com>
2021-10-22 14:33:44 -0400

    getting a path

--- for_post/js-promise/s2-av-chain/3-nodejs-stat-read-write.js
+++ for_post/js-promise/s2-av-chain/3-nodejs-stat-read-write.js
@@ -1,5 +1,26 @@
 let fs = require('fs'),
+path = require('path'),
 promisify = require('util').promisify,
 stat = promisify(fs.stat),
 readFile = promisify(fs.readFile),
 writeFile = promisify(fs.writeFile);
+
+
+let startPath = process.argv[2] || process.cwd();
+
+stat(startPath)
+.catch((e)=>{
+    startPath = process.cwd();
+    console.warn('Error with the given path:');
+    console.warn(e.message); 
+    console.warn('defaulting to ' + startPath);
+    return stat(startPath);
+})
+.then((statObj)=>{
+    if(statObj.isDirectory()){
+        return Promise.resolve( path.join(startPath, 'conf.json') );
+    }
+})
+.then((filePath)=>{
+    console.log(filePath);
+});

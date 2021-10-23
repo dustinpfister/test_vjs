5316c1937a27b86ad12a68c4a35e003e6f31ecd1
dustinpfister <dustin.pfister@gmail.com>
2021-10-23 12:41:35 -0400

    commnets

--- for_post/js-javascript-throw/s5-global-handlers/global_2.js
+++ for_post/js-javascript-throw/s5-global-handlers/global_2.js
@@ -1,9 +1,9 @@
 
-
+// setting an new uncaughtException handler to which
+// I AM NOT calling process.exit
 process.on('uncaughtException', (e) => {
     console.log('An Error has happend: ' + e.message);
 });
-
 // loop using setInterval
 let count = 5;
 let t = setInterval(function () {
@@ -13,7 +13,7 @@ let t = setInterval(function () {
             clearInterval(t);
         }
     }, 1000);
-
-// Throwing an error
+// Throwing an error will cause the console.log below to not print
+// but the above loop will continue until it is done.
 throw new Error('Throwing an error outside of any try');
 console.log('This will not print');

2d4d90c7d4bf1aa62374f5db37dcabe939a497f7
dustinpfister <dustin.pfister@gmail.com>
2021-10-28 10:28:18 -0400

    comments

--- for_post/js-request-animation-frame/s2-ex-simple-animation-loop/main.js
+++ for_post/js-request-animation-frame/s2-ex-simple-animation-loop/main.js
@@ -1,12 +1,13 @@
-// a canvas view
+// a canvas element
 var canvas = document.createElement('canvas'),
 ctx = canvas.getContext('2d'),
 lt = new Date();
 canvas.width = 320;
 canvas.height = 240;
 document.body.appendChild(canvas);
-
+// create the state object
 var state = Model.create(canvas);
+// loop method using request animation frame
 var lt = new Date();
 var loop = function () {
     var now = new Date(),
@@ -17,3 +18,4 @@ var loop = function () {
     lt = now;
 };
 loop();
+

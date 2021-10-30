2a533bb7ed482102524f440d21100bdf90f3e9e3
dustinpfister <dustin.pfister@gmail.com>
2021-10-30 10:02:02 -0400

    for byte pow method

--- for_post/js-uint8array/s3-ex-hash-code-methods/2-sum-char-codes.js
+++ for_post/js-uint8array/s3-ex-hash-code-methods/2-sum-char-codes.js
@@ -18,6 +18,17 @@ console.log(sum); // 8443
 let a = Math.floor(sum / 256),
 b = sum % 256,
 c = a * 256 + b;
+console.log('\nBreak down values:');
 console.log(a); // 32
 console.log(b); // 251
 console.log(c); // 8443
+
+console.log('\nSome Additional values');
+let forBytePow = (n, exp) => {
+    return Math.log(n + 1) / Math.log( Math.pow(256, exp) );
+};
+console.log( forBytePow(sum, 1) );
+console.log( forBytePow(sum, 2) );
+console.log( forBytePow(sum, 3) );
+console.log( forBytePow(sum, 4) );
+console.log( forBytePow(sum, 5) );

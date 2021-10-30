2d07b326a53d2657fba04289fb6c22834d172d49
dustinpfister <dustin.pfister@gmail.com>
2021-10-30 08:00:58 -0400

    getting somewhere with hash code method example

--- for_post/js-uint8array/s3-ex-hash-code-methods/1-from-str.js
+++ for_post/js-uint8array/s3-ex-hash-code-methods/1-from-str.js
@@ -1,1 +1,28 @@
-let counts = Uint8Array.of(0, 0, 0, 0);
+let str = "AAAA";
+
+let counts = Uint8Array.of(0, 0);
+
+let stepCountsForChar = (counts, ci, ch) => {
+    var n = typeof ch === 'string' ? ch.charCodeAt(0) : ch;
+    var c = counts[ci],
+    d = c + n,
+    e = 0;
+    if (d > 255) {
+        e = d % 255;
+        counts[ci] = 0;
+    } else {
+        counts[ci] = d;
+    }
+    if (e > 0) {
+        ci += 1;
+        ci %= counts.length;
+        stepCountsForChar(counts, ci, e);
+    }
+};
+
+let ch, i = 0;
+while (ch = str[i]) {
+    var e = stepCountsForChar(counts, 0, ch);
+    i += 1;
+}
+console.log(counts)

0fcb5b922b0ae5e2954576d9542a7c40563421c5
dustinpfister <dustin.pfister@gmail.com>
2021-10-30 08:54:44 -0400

    more work on silly hash code thing

--- for_post/js-uint8array/s3-ex-hash-code-methods/1-from-str.js
+++ for_post/js-uint8array/s3-ex-hash-code-methods/1-from-str.js
@@ -1,13 +1,28 @@
-let str = "AAAA";
+let str = [255, 250, 32, 255].map((n) => {
+    return String.fromCharCode(n);
+}).join('');
+console.log(str);
 
-let counts = Uint8Array.of(0, 0);
+let counts = Uint8Array.of(0, 0, 0);
+
+/*
+let stepCounts = (counts, n, ci) => {
+    ci = ci === undefined ? 0 : ci;
+    var c = counts[ci], // the current count
+    s = c + n, // the sum of current count + n
+    oc = Math.floor(s / 256), // over count
+    r = s % 256; // remainder
+    console.log(c, s, oc, r);
+};
+stepCounts([0], 255)
+*/
 
 let stepCountsForChar = (counts, ci, ch) => {
     var n = typeof ch === 'string' ? ch.charCodeAt(0) : ch;
     var c = counts[ci],
     d = c + n,
     e = 0;
-    if (d > 255) {
+    if (d > 256) {
         e = d % 255;
         counts[ci] = 0;
     } else {
@@ -22,7 +37,7 @@ let stepCountsForChar = (counts, ci, ch) => {
 
 let ch, i = 0;
 while (ch = str[i]) {
-    var e = stepCountsForChar(counts, 0, ch);
+    stepCountsForChar(counts, 0, ch);
     i += 1;
 }
 console.log(counts)

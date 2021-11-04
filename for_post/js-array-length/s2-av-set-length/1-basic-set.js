802515ca289ed0cf37176828b58681fa36030dcb
dustinpfister <dustin.pfister@gmail.com>
2021-11-04 10:18:30 -0400

    set basic example

---
+++ for_post/js-array-length/s2-av-set-length/1-basic-set.js
@@ -0,0 +1,11 @@
+var a = [1, 2, 3];
+console.log(a);
+// [ 1, 2, 3 ]
+
+a.length = 1;
+console.log(a);
+// [ 1 ]
+
+a.length = 5;
+console.log(a);
+// [ 1, <4 empty items> ]
---
+++ for_post/js-array-length/todo.md
@@ -0,0 +1,4 @@
+# js-array-length todo list
+
+### () - expand av-set-length section
+* 1-basic-set : just a very simple example

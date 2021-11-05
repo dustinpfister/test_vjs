3e93a2f4d28db56479cd7d7b2555f29276e4ad5b
dustinpfister <dustin.pfister@gmail.com>
2021-11-05 07:48:59 -0400

    displaying secs

--- for_post/js-webworker/s1-basic/heavy.js
+++ for_post/js-webworker/s1-basic/heavy.js
@@ -1,6 +1,7 @@
 onmessage = function (e) {
     opt = e.data || { w: 10, h: 10 };
     var result={ sum: 0 },
+    st = new Date(),
     len = opt.w * opt.h,
     i = 0;
     while(i < len){
@@ -9,5 +10,6 @@ onmessage = function (e) {
         result.sum += Math.pow(x, y / 1000);
         i += 1;
     }
+    result.secs = parseFloat( ((new Date() - st) / 1000).toFixed(2) );
     postMessage(result);
 };

95192ada1340986bc159872579b52100c57e82b6
dustinpfister <dustin.pfister@gmail.com>
2021-11-07 10:11:42 -0500

    section folders

--- for_post/js-call-object/s1-basics/1-let.js
+++ for_post/js-call-object/s1-basics/1-let.js
@@ -1,15 +1,9 @@
 let func = function (a, b) {
- 
     let c = 5;
- 
     if (arguments.length >= 2) {
- 
         c += 5;
- 
     }
- 
     return a + b + c;
- 
 };
  
 console.log(func(3, 2)); // 15

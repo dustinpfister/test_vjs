9f19fc5ce926c52d432366fb25e2c4d346470997
dustinpfister <dustin.pfister@gmail.com>
2021-11-07 11:13:56 -0500

    appending child divs

--- for_post/js-javascript-style/s4-ex-moving-divs/main.js
+++ for_post/js-javascript-style/s4-ex-moving-divs/main.js
@@ -2,10 +2,13 @@ var createChildren = function (div) {
     var i = 10;
     while (i--) {
         var child = document.createElement('div');
-        child.className = '.div_child';
+        child.className = 'div_child';
         child.id = 'child_' + i;
         div.appendChild(child);
     }
 };
 
+// get ref to parent div
 var div_parent = document.querySelector('.div_parent');
+// create and append child divs to the parent div
+createChildren(div_parent);

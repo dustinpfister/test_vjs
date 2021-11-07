e1966a15d2464ce0589351f42b0f097d4633ff67
dustinpfister <dustin.pfister@gmail.com>
2021-11-07 11:18:37 -0500

    update children method started

--- for_post/js-javascript-style/s4-ex-moving-divs/main.js
+++ for_post/js-javascript-style/s4-ex-moving-divs/main.js
@@ -4,11 +4,37 @@ var createChildren = function (div) {
         var child = document.createElement('div');
         child.className = 'div_child';
         child.id = 'child_' + i;
+        child.dataset.x = 10;
+        child.dataset.y = 10;
         div.appendChild(child);
     }
+};
+
+var updateChildren = function (div) {
+    [].forEach.call(div.children, function (child) {
+        var x = child.dataset.x,
+        y = child.dataset.y;
+        child.style.left = x + 'px';
+        child.style.top = y + 'px';
+
+    });
 };
 
 // get ref to parent div
 var div_parent = document.querySelector('.div_parent');
 // create and append child divs to the parent div
 createChildren(div_parent);
+
+var lt = new Date();
+var loop = function () {
+    var now = new Date(),
+    secs = (now - lt) / 1000;
+    requestAnimationFrame(loop);
+
+    if (secs >= 1 / 30) {
+        updateChildren(div_parent);
+        lt = now;
+    }
+};
+
+loop();

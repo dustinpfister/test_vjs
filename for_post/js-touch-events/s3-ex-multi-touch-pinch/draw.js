12b90b12da4d51d464b95b45fe681ed04c43ed10
dustinpfister <dustin.pfister@gmail.com>
2021-11-08 09:11:53 -0500

    displaying delta

--- for_post/js-touch-events/s3-ex-multi-touch-pinch/draw.js
+++ for_post/js-touch-events/s3-ex-multi-touch-pinch/draw.js
@@ -3,17 +3,23 @@ var draw = {};
 
 draw.background = function (ctx, canvas) {
     // fill black
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'gray';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
 
 // debug pinch
 draw.debugPinch = function (ctx, canvas, pinch) {
+    ctx.fillStyle = 'white';
+    ctx.font = '15px arial';
+    ctx.textBaseline = 'top';
+    ctx.fillText('delta: ' + pinch.distanceDelta.toFixed(2), 10, 10);
+    // draw points
     ctx.strokeStyle = 'white';
     ctx.fillStyle = 'lime';
-    pinch.points.forEach(function (pt) {
+    Object.keys(pinch.points).forEach(function (ptKey) {
+        var pt = pinch.points[ptKey];
         ctx.beginPath()
-        ctx.arc(pt.x, pt.y, 16, 0, Math.PI * 2);
+        ctx.arc(pt.x, pt.y, 32, 0, Math.PI * 2);
         ctx.fill();
         ctx.stroke();
     });
--- for_post/js-touch-events/s3-ex-multi-touch-pinch/main.js
+++ for_post/js-touch-events/s3-ex-multi-touch-pinch/main.js
@@ -6,6 +6,7 @@ var pinch = {
     active: false,
     startDistance: 0,
     distance: 0,
+    distanceDelta: 0,
     points: {
         p1: {
             x: 0,
@@ -20,6 +21,7 @@ var pinch = {
 
 var touchStart = function (e) {
     if (e.touches.length >= 2) {
+        e.preventDefault();
         var pos = utils.getElementRelative(e, e.target, 0);
         var p1 = pinch.points.p1 = {
             x: pos.x,
@@ -35,14 +37,35 @@ var touchStart = function (e) {
 };
 
 var touchMove = function (e) {
-	
-	
-	
+    if (e.touches.length >= 2) {
+        var pos = utils.getElementRelative(e, e.target, 0);
+        var p1 = pinch.points.p1 = {
+            x: pos.x,
+            y: pos.y
+        }
+        var pos = utils.getElementRelative(e, e.target, 1);
+        var p2 = pinch.points.p2 = {
+            x: pos.x,
+            y: pos.y
+        }
+        pinch.distance = utils.distance(p1.x, p1.y, p2.x, p2.y);
+        pinch.distanceDelta = pinch.startDistance - pinch.distance;
+    }
 };
 
 var touchEnd = function (e) {};
 
 // attach a touch events
-canvas.addEventListener('touchstart', touchStart);
-canvas.addEventListener('touchend', touchEnd);
-canvas.addEventListener('touchmove', touchMove);
+canvas.addEventListener('touchstart', function (e) {
+    touchStart(e);
+});
+canvas.addEventListener('touchend', function (e) {
+    touchEnd(e);
+});
+canvas.addEventListener('touchmove', function (e) {
+    touchMove(e);
+    draw.background(ctx, canvas);
+    draw.debugPinch(ctx, canvas, pinch);
+});
+
+draw.background(ctx, canvas);

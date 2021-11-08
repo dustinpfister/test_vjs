3dc9b6240fdb8a0f910b33cb98de7387ad882451
dustinpfister <dustin.pfister@gmail.com>
2021-11-08 08:46:05 -0500

    start hander looks good

--- for_post/js-touch-events/s3-ex-multi-touch-pinch/main.js
+++ for_post/js-touch-events/s3-ex-multi-touch-pinch/main.js
@@ -4,6 +4,8 @@ ctx = canvas.getContext('2d');
 
 var pinch = {
     active: false,
+    startDistance: 0,
+    distance: 0,
     points: {
         p1: {
             x: 0,
@@ -19,21 +21,24 @@ var pinch = {
 var touchStart = function (e) {
     if (e.touches.length >= 2) {
         var pos = utils.getElementRelative(e, e.target, 0);
-        pinch.points.p1 = {
+        var p1 = pinch.points.p1 = {
             x: pos.x,
             y: pos.y
         }
         var pos = utils.getElementRelative(e, e.target, 1);
-        pinch.points.p2 = {
+        var p2 = pinch.points.p2 = {
             x: pos.x,
             y: pos.y
         }
-		console.log(e);
-        console.log(pinch);
+        pinch.startDistance = utils.distance(p1.x, p1.y, p2.x, p2.y);
     }
 };
 
-var touchMove = function (e) {};
+var touchMove = function (e) {
+	
+	
+	
+};
 
 var touchEnd = function (e) {};
 
@@ -41,4 +46,3 @@ var touchEnd = function (e) {};
 canvas.addEventListener('touchstart', touchStart);
 canvas.addEventListener('touchend', touchEnd);
 canvas.addEventListener('touchmove', touchMove);
-

965837a756ddf714d9b5805d4e31213e8b64b605
dustinpfister <dustin.pfister@gmail.com>
2021-11-08 10:10:24 -0500

    min size

--- for_post/js-touch-events/s3-ex-multi-touch-pinch/main.js
+++ for_post/js-touch-events/s3-ex-multi-touch-pinch/main.js
@@ -20,6 +20,7 @@ pinch.onPinchActive = function (pinch, multi, radian) {
     console.log(multi);
     draw.background(ctx, canvas);
     state.obj.size = 50 * (1 + (1 - multi));
+    state.obj.size = state.obj.size < 50 ? 50 : state.obj.size;
     state.obj.r = radian;
     draw.state(ctx, canvas, state);
     draw.debugPinch(ctx, canvas, pinch);

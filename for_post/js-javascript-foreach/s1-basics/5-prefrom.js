2693251433963c9fd5b6f76d02c1275b73789145
dustinpfister <dustin.pfister@gmail.com>
2021-11-11 15:28:52 -0500

    prefrom example is working great

--- for_post/js-javascript-foreach/s1-basics/5-prefrom.js
+++ for_post/js-javascript-foreach/s1-basics/5-prefrom.js
@@ -13,10 +13,14 @@ let createLargeArray = (count) => {
     return arr;
 };
 
+let getSecs = function (st) {
+    return (new Date() - st) / 1000;
+};
 
+// create a large array
 let st = new Date();
-let arr = createLargeArray(4000000);
-console.log((new Date() - st) / 1000);
+let arr = createLargeArray(2000000);
+console.log('It took ' + getSecs(st) + ' seconds to create an array of ' + arr.length + ' elements');
 
 // forEach
 st = new Date();
@@ -24,7 +28,7 @@ arr.forEach((obj) => {
     obj.s = obj.x + obj.y;
 });
 
-console.log((new Date() - st) / 1000);
+console.log('It took ' + getSecs(st) + ' seconds to loop over the array using Array.forEach');
 
 st = new Date();
 let i = arr.length, obj;
@@ -33,4 +37,8 @@ while (i--) {
     obj.s = obj.x + obj.y;
 }
 
-console.log((new Date() - st) / 1000);
+console.log('It took ' + getSecs(st) + ' seconds to loop over the array using a while loop');
+
+//It took 1.468 seconds to create an array of 2000000 elements
+//It took 1.875 seconds to loop over the array using Array.forEach
+//It took 0.062 seconds to loop over the array using a while loop

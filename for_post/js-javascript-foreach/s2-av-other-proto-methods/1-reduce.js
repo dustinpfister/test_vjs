01ab1038a4199900bbb8e8c4d307c8810981385b
dustinpfister <dustin.pfister@gmail.com>
2021-11-11 16:03:29 -0500

    better reduce example

--- for_post/js-javascript-foreach/s2-av-other-proto-methods/1-reduce.js
+++ for_post/js-javascript-foreach/s2-av-other-proto-methods/1-reduce.js
@@ -1,5 +1,17 @@
-let arr = [1, 2, 3],
-sum = arr.reduce((s, r) => {
-        return s + r;
-    });
-console.log(sum); // 6
+let a = [1, 2, 3].reduce((acc, n) => {
+    console.log(acc, n);
+    return acc + n;
+});
+console.log(a);
+// 1 2
+// 3 3
+// 6
+let b = [1, 2, 3].reduce((acc, n) => {
+    console.log(acc, n);
+    return acc + n;
+}, 0);
+console.log(b);
+// 0 1
+// 1 2
+// 3 3
+// 6

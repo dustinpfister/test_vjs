bba64ddffe8e3e5303d0d8d5c816481496e388a4
dustinpfister <dustin.pfister@gmail.com>
2021-11-12 10:14:24 -0500

    removed old code from example

--- for_post/js-javascript-if/s1-basics/2-no-brackets.js
+++ for_post/js-javascript-if/s1-basics/2-no-brackets.js
@@ -10,14 +10,3 @@ if (n === 42) {
 if (n >= 40)
     console.log('the answer');
 // > 'the answer'
-
-if (n === '42') {
-    console.log('the answer is a string');
-} else {
-    if (n === 42) {
-        console.log('the answer is a number.')
-    } else {
-        console.log('no answer');
-    }
-}
-// > 'the answer is a number'

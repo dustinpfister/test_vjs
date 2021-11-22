3785a484fdee4f406878a6f98107845cfffe9e3c
dustinpfister <dustin.pfister@gmail.com>
2021-11-22 09:27:54 -0500

    lables example

--- for_post/js-javascript-break/s1-basic/2-label.js
+++ for_post/js-javascript-break/s1-basic/2-label.js
@@ -6,17 +6,22 @@ let arr = [
     [3, 22, false],
     [8]
 ];
-
 let sum = 0,
 el,
 ia = 0,
 ib;
-
+// using break with labels
 outer: while (ia < arr.length) {
     el = arr[ia];
     if (el instanceof Array) {
         ib = 0;
         inner: while (ib < el.length) {
+            // break outer loop if we find a false boolean
+            if (el[ib] === false) {
+                break outer;
+            }
+            // add up numbers, but break inner at first instance
+            // of something other than a number
             if (typeof el[ib] === 'number') {
                 sum += el[ib];
             } else {

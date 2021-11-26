f7db8b3d6ea3c67a04eee05e73deac973b8a9b50
dustinpfister <dustin.pfister@gmail.com>
2021-11-26 12:14:10 -0500

    comments

--- for_post/js-array-fill/s3-av-while/5-while-higher-order.js
+++ for_post/js-array-fill/s3-av-while/5-while-higher-order.js
@@ -1,11 +1,14 @@
-// range method
-
+// fill module that returns a higher order function
+// that will take a filler method, or a string value that is used
+// to get a built in option
 var fill = (function () {
     // hard coded options for filler methods
     var FILLERS = {
+        // simple primitive function example
         prim: function (i, arr, prim) {
             return prim === undefined ? 0 : prim;
         },
+        // range
         range: function (i, arr, nStart, delta) {
             nStart = nStart === undefined ? 0 : nStart;
             delta = delta === undefined ? 1 : delta;
@@ -37,9 +40,13 @@ var fill = (function () {
     };
 }
     ());
-
-console.log(fill(5));                // [ 0, 0, 0, 0, 0 ]
-console.log(fill(5, 'prim', 1));     // [ 1, 1, 1, 1, 1 ]
-console.log(fill(5, 'range'));       // [ 0, 1, 2, 3, 4 ]
+// DEMOS
+console.log(fill(5)); // [ 0, 0, 0, 0, 0 ]
+console.log(fill(5, 'prim', 1)); // [ 1, 1, 1, 1, 1 ]
+console.log(fill(5, 'range')); // [ 0, 1, 2, 3, 4 ]
 console.log(fill(5, 'range', 8, 2)); // [ 8, 10, 12, 14, 16 ]
-
+// custom filler method example
+var pows = function (i, arr, base, startE, deltaE, startN) {
+    return startN + Math.pow(base, startE + deltaE * i);
+};
+console.log( fill(5, pows, 2, 2, 4, -4) ); // [ 0, 60, 1020, 16380, 262140 ]

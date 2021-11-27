f981c1319f163206defe0aca20522b69bbcee6ef
dustinpfister <dustin.pfister@gmail.com>
2021-11-27 09:44:20 -0500

    basic join example looking good

---
+++ for_post/js-array-to-string/s2-join/1-join.js
@@ -0,0 +1,6 @@
+var a = [1, 2, 3, 4];
+// there is calling to string directory
+console.log(a.toString()); // '1,2,3,4'
+console.log(a.join());     // '1,2,3,4'
+console.log(a.join(' '));  // '1 2 3 4'
+console.log(a.join(''));   // '1234'

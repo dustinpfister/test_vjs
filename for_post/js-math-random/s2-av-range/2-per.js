013d7b856aa383f07e620b45e1dc62e8e5ad9361
dustinpfister <dustin.pfister@gmail.com>
2021-11-30 12:08:48 -0500

    changed name of function

--- for_post/js-math-random/s2-av-range/2-per.js
+++ for_post/js-math-random/s2-av-range/2-per.js
@@ -1,14 +1,14 @@
 // per range method
-var perRange = function (per, low, high) {
+var rangePer = function (per, low, high) {
     per = per === undefined ? 0 : per;
     low = low === undefined ? 0 : low;
     high = high === undefined ? 1 : high;
     return low + (high - low) * per;
 };
 // so I can plug math.random in for per
-console.log( perRange(Math.random(), 5, 15) );
+console.log(rangePer(Math.random(), 5, 15) );
 // put I can also pass a number literal
-console.log( perRange(0.0, 5, 15) ); // 5;
-console.log( perRange(0.5, 5, 15) ); // 10;
-console.log( perRange(1.0, 5, 15) ); // 15;
+console.log( rangePer(0.0, 5, 15) ); // 5;
+console.log( rangePer(0.5, 5, 15) ); // 10;
+console.log( rangePer(1.0, 5, 15) ); // 15;
 

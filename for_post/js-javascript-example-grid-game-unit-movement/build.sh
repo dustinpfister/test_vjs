7a7675960220d7109b5ef2fb18aec653a4df8d09
dustinpfister <dustin.pfister@gmail.com>
2021-12-05 08:52:37 -0500

    new js and lib folders

--- for_post/js-javascript-example-grid-game-unit-movement/build.sh
+++ for_post/js-javascript-example-grid-game-unit-movement/build.sh
@@ -2,7 +2,7 @@
 
 top=$(cat ./pkg/top.txt)
 bottom=$(cat ./pkg/bottom.txt)
-js=$(cat ./lib/utils.js ./lib/map.js ./lib/game.js ./lib/draw.js ./main.js)
+js=$(cat ./js/lib/utils.js ./js/lib/map.js ./js/lib/game.js ./js/lib/draw.js ./js/main.js)
 min=$(echo "$js" | uglifyjs)
 
 echo "${top}${min}${bottom}";

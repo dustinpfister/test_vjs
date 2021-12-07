b88ae86bd3a9b3e9b206bfbcc24351906c65ec9c
dustinpfister <dustin.pfister@gmail.com>
2021-12-07 14:02:52 -0500

    removed old create unit code from game.js

--- prototypes/js-javascript-example-turn-based-rpg/js/game.js
+++ prototypes/js-javascript-example-turn-based-rpg/js/game.js
@@ -116,50 +116,6 @@ var gameMod = (function () {
 /********** **********
      UNITS
 *********** *********/
-    // create a base unit
-    var createBaseUnit = function () {
-        return {
-            // current unit stats
-            maxHP: 1,           // max number of hit points for the unit
-            maxCellsPerTurn: 0,   // the max number of cells a unit can move
-            // current values
-            HP: 1,
-            weaponIndex: 0,
-            sheetIndex: 0,
-            type: null,
-            meleeTarget: null, // cell index to attack in 'melee' processTurn state
-            moveCells: [], // array of cells to move in 'move' processTurn state
-            currentCellIndex: null,
-            active: false
-        }
-    };
-    // create a player unit
-    var createPlayerUnit = function () {
-        var player = createBaseUnit();
-        player.type = 'player';
-        player.active = true;
-        player.maxCellsPerTurn = 3;
-        player.sheetIndex = 2; // player sheet
-        player.maxHP = 30;
-        return player;
-    };    // create a player unit
-    var createEnemyUnit = function () {
-        var enemy = createBaseUnit();
-        enemy.type = 'enemy';
-        enemy.active = true;
-        enemy.maxCellsPerTurn = 2;
-        enemy.sheetIndex = 3;
-        enemy.maxHP = 5;
-        return enemy;
-    };
-    // create a player unit
-    var createWallUnit = function () {
-        var wall = createBaseUnit();
-        wall.type = 'wall';
-        wall.active = true;
-        wall.sheetIndex = 1;
-        return wall;
-    };
     // place a unit at the given location in the current map
     var placeUnit = function (game, unit, x, y) {
         var map = game.maps[game.mapIndex];
@@ -270,7 +226,7 @@ var getCellsByUnitType = function(map, type){
                 }
                 // wall blocks set for new games and not
                 if(cellIndex === 1){
-                    var wall = unitMod.createUnit('wall'); //createWallUnit();
+                    var wall = unitMod.createUnit('wall');
                     placeUnit(game, wall, x, y);
                 }
                 // player always set
@@ -282,7 +238,7 @@ var getCellsByUnitType = function(map, type){
                 // enemy
                 if(cellIndex === 3 && newGame){
                     game.remainingEnemies += 1;
-                    var enemy = unitMod.createUnit('enemy'); //createEnemyUnit();
+                    var enemy = unitMod.createUnit('enemy');
                     enemy.HP = enemy.maxHP;
                     placeUnit(game, enemy, x, y);
                 }
@@ -319,7 +275,7 @@ var getCellsByUnitType = function(map, type){
                 y: null
             },
             mapStrings: opt.maps || ['2'],
-            player: unitMod.createUnit('player'), //createPlayerUnit(),
+            player: unitMod.createUnit('player'),
             remainingEnemies: 0
         };
         game.mapStrings.forEach(function(){
--- prototypes/js-javascript-example-turn-based-rpg/js/lib/units.js
+++ prototypes/js-javascript-example-turn-based-rpg/js/lib/units.js
@@ -1,5 +1,12 @@
 var unitMod = (function () {
     
+    // PUBLIC API
+    var api = {};
+
+/********** **********
+     CREATE A UNIT
+*********** *********/
+
     // create a base unit object
     var createBaseUnit = function () {
         return {
@@ -41,41 +48,6 @@ var unitMod = (function () {
             wall.sheetIndex = 1;
         }
     };
-
-
-/*
-    // create a player unit
-    var createPlayerUnit = function () {
-        var player = createBaseUnit();
-        player.type = 'player';
-        player.active = true;
-        player.maxCellsPerTurn = 3;
-        player.sheetIndex = 2; // player sheet
-        player.maxHP = 30;
-        return player;
-    };    // create a player unit
-    var createEnemyUnit = function () {
-        var enemy = createBaseUnit();
-        enemy.type = 'enemy';
-        enemy.active = true;
-        enemy.maxCellsPerTurn = 2;
-        enemy.sheetIndex = 3;
-        enemy.maxHP = 5;
-        return enemy;
-    };
-    // create a player unit
-    var createWallUnit = function () {
-        var wall = createBaseUnit();
-        wall.type = 'wall';
-        wall.active = true;
-        wall.sheetIndex = 1;
-        return wall;
-    };
-*/
-
-
-    // PUBLIC API
-    var api = {};
 
     // Public unitMod.create method
     api.createUnit = function(type){

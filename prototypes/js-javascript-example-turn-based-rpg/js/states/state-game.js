68a8ad63f71062317b46e894713d1312b48269fd
dustinpfister <dustin.pfister@gmail.com>
2021-12-08 10:26:42 -0500

    states folder

---
+++ prototypes/js-javascript-example-turn-based-rpg/js/states/state-game.js
@@ -0,0 +1,15 @@
+sm.states.push({
+    update: function(sm, secs){
+        gameMod.update(sm.game);
+    },
+    draw: function(sm, layers){
+        draw.back(sm);
+        draw.map(sm);
+        draw.info(sm);
+    },
+    events: {
+        pointerStart: function(e, pos, sm){
+            gameMod.playerPointer(sm.game, pos.x, pos.y);
+        }
+    }
+});

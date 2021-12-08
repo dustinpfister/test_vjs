a7e65dd53b455177b4745357dfcac5e78adb18b7
dustinpfister <dustin.pfister@gmail.com>
2021-12-08 10:46:55 -0500

    added bouning box

--- prototypes/js-javascript-example-turn-based-rpg/js/lib/utils.js
+++ prototypes/js-javascript-example-turn-based-rpg/js/lib/utils.js
@@ -7,6 +7,14 @@ utils.valueByRange = function(per, nMin, nMax){
     nMax = nMax === undefined ? 1 : nMax;
     return nMin + Math.round(per * (nMax - nMin));
 };
+// bounding box
+utils.boundingBox = function (x1, y1, w1, h1, x2, y2, w2, h2) {
+    return !(
+        y1 + h1 < y2 ||
+        y1 > y2 + h2 ||
+        x1 + w1 < x2 ||
+        x1 > x2 + w2);
+};
 // distance
 utils.distance = function (x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));

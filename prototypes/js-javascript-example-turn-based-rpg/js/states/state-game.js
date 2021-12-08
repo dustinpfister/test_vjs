4de268eee04d69593fb4cf7cd49d885c778f1690
dustinpfister <dustin.pfister@gmail.com>
2021-12-08 13:28:19 -0500

    game state clean up

--- prototypes/js-javascript-example-turn-based-rpg/js/states/state-game.js
+++ prototypes/js-javascript-example-turn-based-rpg/js/states/state-game.js
@@ -6,21 +6,17 @@ sm.states.game = {
     draw: function(sm, layers){
         draw.back(sm);
         draw.map(sm);
-  
         if(sm.game.mode === 'map'){
             draw.info(sm);
         }
-
         if(sm.game.mode === 'menu'){
             draw.back(sm, 'rgba(0,0,0,0.5)');
             draw.options(sm);
         }
-
         draw.ver(sm);
     },
     events: {
         pointerStart: function(e, pos, sm){
-            //gameMod.playerPointer(sm.game, pos.x, pos.y);
             gameMod.pointerStart(sm, pos.x, pos.y);
         },
         pointerEnd: function(e, pos, sm){

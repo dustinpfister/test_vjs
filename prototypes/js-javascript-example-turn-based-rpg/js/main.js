6d5d35bfabf60edf02bb1cf05016e2770f85bcaa
dustinpfister <dustin.pfister@gmail.com>
2021-12-09 15:02:23 -0500

    game still works by way of file protocol but with hard coded world map

--- prototypes/js-javascript-example-turn-based-rpg/js/main.js
+++ prototypes/js-javascript-example-turn-based-rpg/js/main.js
@@ -2,30 +2,52 @@
 // set the current state
 sm.setState('game');
 
+// start loop helper
+var startLoop = function(){
+       // loop with frame capping set by sm.fps value
+    var loop = function () {
+        var now = new Date(),
+        secs = (now - sm.lt) / 1000;
+        requestAnimationFrame(loop);
+        if(secs >= 1 / sm.fps){
+            sm.stateObj.update.call(sm, sm, secs);
+            sm.stateObj.draw.call(sm, sm, {}); // empty object for 'layers' at least for now
+            sm.lt = now;
+        }
+    };
+    loop();
+};
 
+// load world map
 utils.http({
     url: './json/world-home.json',
     onDone: function(res, xhr){
+        // parse world map
         try{
             var map = JSON.parse(res);
         }catch(e){
             var map = {};
         }
-        console.log(map);
+        // Crude yet effective way of loading json world map
+        sm.game = gameMod.create({
+            marginX : 14,
+            marginY : 7,
+            w: 9,
+            h: 7,
+            mapStrings : map.mapStrings
+        });
+        // now start the loop
+        startLoop();
     },
+    onError: function(){
+        console.log('error loading home world map!');
+        console.log('are you using this by way of the file protocol?');
+        // now start the loop
+        startLoop();
+
+    }
 });
 
-// loop with frame capping set by sm.fps value
-var loop = function () {
-    var now = new Date(),
-    secs = (now - sm.lt) / 1000;
-    requestAnimationFrame(loop);
-    if(secs >= 1 / sm.fps){
-        sm.stateObj.update.call(sm, sm, secs);
-        sm.stateObj.draw.call(sm, sm, {}); // empty object for 'layers' at least for now
-        sm.lt = now;
-    }
-};
-loop();
+
 
 

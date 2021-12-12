a3be4743b8be4afe8cfb76ae0d38d5e6975dbfbd
dustinpfister <dustin.pfister@gmail.com>
2021-12-12 16:05:26 -0500

    loader state just goes to title state in start hook

--- for_post/js-javascript-example-turn-based-rpg/js/states/state-loader.js
+++ for_post/js-javascript-example-turn-based-rpg/js/states/state-loader.js
@@ -2,11 +2,16 @@ sm.states.loader = {
     key: 'loader',
     start: function(sm){
 
+        sm.setState('title');
+
     },
     end: function(sm){
 
     },
     update: function(sm, secs){
+
+    
+
     },
     draw: function(sm, layers){
         draw.back(sm);

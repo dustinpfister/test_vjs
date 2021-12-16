0f03b832177029f3c9b3cbf15e5cd3e159503c27
dustinpfister <dustin.pfister@gmail.com>
2021-12-16 10:36:45 -0500

    1-object-keys example looking good

--- for_post/js-javascript-foreach/s6-av-vjs-foreach/1-object-keys.js
+++ for_post/js-javascript-foreach/s6-av-vjs-foreach/1-object-keys.js
@@ -7,3 +7,32 @@ let forEach = function (obj, func) {
         i += 1;
     }
 };
+ 
+// works with solid arrays
+let a = [1,2,3,4];
+forEach(a, (n) => {
+    console.log(n);
+});
+// 1 2 3 4
+ 
+// works just like array for each when it comes to sparse arrays
+let obj = {0: 1, 2: 2, 5: 3, 15: 4};
+Object.defineProperty(obj, 'length', {value: 16});
+let b = Array.prototype.map.call(obj, function(n){ return n; });
+forEach(b, (n) => {
+    console.log(n);
+});
+// 1 2 3 4
+ 
+// I can also directly pass an array like object
+forEach(obj, (n) => {
+    console.log(n);
+});
+// 1 2 3 4
+ 
+// Works with named collections also
+let obj2 = {foo: 1, bar: 2, baz: 3, taz: 4};
+forEach(obj2, (n) => {
+    console.log(n);
+});
+// 1 2 3 4

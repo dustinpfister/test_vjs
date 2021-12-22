eaca817faec115f60c73727318c83292109ee9a5
dustinpfister <dustin.pfister@gmail.com>
2021-12-22 13:18:32 -0500

    item-class.js

---
+++ for_post/js-math-random/s7-ex-item-prob/item-class.js
@@ -0,0 +1,41 @@
+var itemClass = (function(){
+    // creating a item classes object
+    var DEFAULT_POOL = [
+        { desc: 'Junk', points: 1000 },
+        { desc: 'Common', points: 250 },
+        { desc: 'Fair', points: 160 },
+        { desc: 'Rare', points: 80 },
+        { desc: 'Epic', points: 15}
+    ]
+ 
+    var api = {};
+ 
+    // create ITEM Classes object
+    api.create = function(opt){
+        opt = opt || {};
+        opt.pool = opt.pool || DEFAULT_POOL;
+        // get total points
+        opt.totalPoints = opt.pool.reduce( function(acc, obj){ return acc + obj.points;}, 0);
+        // set 0-1 numbs for each itemClasses object
+        opt.pool = opt.pool.map( function(obj, i){ obj.per = obj.points / opt.totalPoints; obj.i = i; return obj; } );
+        return opt;
+    };
+    // GET a random ITEM classes object
+    api.getRandomItemClass = function(classes){
+        var i = 0,
+        len = classes.pool.length
+        roll = Math.random(),
+        n = 1
+        while(i < len){
+            var item = classes.pool[i];
+            n -= item.per;
+            if(roll > n){
+                return item;
+            }
+            i += 1;
+        }
+        return item;
+    };
+ 
+    return api;
+}());

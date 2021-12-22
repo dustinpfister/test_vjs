011315887e8bea959715319eaf40b0b535acc337
dustinpfister <dustin.pfister@gmail.com>
2021-12-22 14:49:59 -0500

    cleaner default pool

--- for_post/js-math-random/s7-ex-item-prob/item-class.js
+++ for_post/js-math-random/s7-ex-item-prob/item-class.js
@@ -1,19 +1,32 @@
 var itemClass = (function(){
+    var DEFAULT_COLORS = ['#efefef', 'lime', 'blue', 'orange', 'purple'];
     // default pool of objects for each item class
     var DEFAULT_POOL = [
-        { desc: 'Junk', color: '#efefef', points: 1000 },
-        { desc: 'Common', color: 'lime', points: 250 },
-        { desc: 'Fair', color: 'blue', points: 160 },
-        { desc: 'Rare', color: 'orange', points: 80 },
-        { desc: 'Epic', color: 'purple', points: 15}
-    ]
+        { desc: 'Junk', range: [1000, 250] },
+        { desc: 'Common', range: [300, 800] },
+        { desc: 'Fair', range: [150, 500] },
+        { desc: 'Rare', range: [50, 250] },
+        { desc: 'Epic', range: [10, 100]}
+    ];
+    DEFAULT_POOL = DEFAULT_POOL.map(function(obj, i){ obj.color = DEFAULT_COLORS[i] || 'white'; return obj;  })
     // public api
     var api = {};
     // create ITEM Classes object
     api.create = function(opt){
         opt = opt || {};
-        var classes = {};
+        var classes = {
+            levelPer: opt.levelPer || 0
+        };
+        // use given pool array or default starting pool
         classes.pool = opt.pool || DEFAULT_POOL;
+        // parse given pool collection
+        classes.pool = classes.pool.map(function(obj, i){
+            obj.range = obj.range || [1,1];
+            obj.points = obj.range[0] + (obj.range[1] - obj.range[0]) * classes.levelPer;
+            obj.desc = obj.desc || '';
+            obj.color = obj.color || DEFAULT_COLORS[i] || 'white';
+            return obj;
+        });
         // get total points
         classes.totalPoints = classes.pool.reduce( function(acc, obj){ return acc + obj.points;}, 0);
         // set 0-1 numbs for each itemClasses object

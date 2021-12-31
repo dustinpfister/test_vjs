15b85f15ecbf62f87b73e92615964567b9c37394
dustinpfister <dustin.pfister@gmail.com>
2021-12-31 12:35:06 -0500

    mass gets added

--- prototypes/js-javascript-example-digital-art-reduce-pool/lib/game.js
+++ prototypes/js-javascript-example-digital-art-reduce-pool/lib/game.js
@@ -3,17 +3,29 @@ var gameMod = (function () {
 
     var api = {};
 
+    UNIT_SIZE_RANGE = [32, 128]
+
     var UNIT_OPT = {
-        count: 15,
+        count: 25,
         disableLifespan: true
     };
 
-    UNIT_OPT.spawn = function (obj, pool, game, opt) {
+    // set the size of the given unit object
+    var getSize = function (unit) {
+        var totalMass = 25 * 50,
+        sizePer = unit.data.mass / totalMass,
+        size = UNIT_SIZE_RANGE[0] + (UNIT_SIZE_RANGE[1] - UNIT_SIZE_RANGE[0]) * sizePer;
+        return size;
+    };
 
+    UNIT_OPT.spawn = function (obj, pool, game, opt) {
         var canvas = game.sm.canvas;
+        // start mass
+        obj.data.mass = 50;
         // size and position
-        obj.w = 32;
-        obj.h = 32;
+        var size = getSize(obj);
+        obj.w = size;
+        obj.h = size;
         var r = canvas.height * 0.4,
         a = Math.PI * 2 * Math.random();
         obj.x = canvas.width / 2 - obj.w / 2 + Math.cos(a) * r;
@@ -29,12 +41,15 @@ var gameMod = (function () {
         obj.x = utils.wrapNumber(obj.x, -32, game.sm.canvas.width + 32);
         obj.y = utils.wrapNumber(obj.y, -32, game.sm.canvas.height + 32);
 
-        var over = poolMod.getOverlaping(obj, pool);
-        if (over.length > 0) {
-            console.log(over.length);
-            over.forEach(function () {
-                poolMod.purge(pool, obj, game)
-            })
+        var under = poolMod.getOverlaping(obj, pool);
+        if (under.length > 0) {
+            under.forEach(function (underUnit) {
+                obj.data.mass += underUnit.data.mass;
+                poolMod.purge(pool, underUnit, game)
+            });
+            var size = getSize(obj);
+            obj.w = size;
+            obj.h = size;
         }
 
     };
--- prototypes/js-javascript-example-digital-art-reduce-pool/todo.md
+++ prototypes/js-javascript-example-digital-art-reduce-pool/todo.md
@@ -1,12 +1,11 @@
 # js-javascript-example-digital-art-reduce-pool
 
 ### () - r0 - first state of example
-* start out with pool.js file from turn based RPG example
-* start out with a utils.js with the cate canvas method and various other tools such as wrapNumber
-* new draw.js module started from the ground up
-* new game.js file that will crate and return the game state of the art project
-* the gameMod will contain an full object pool as game.units
-* the game state will contain an array called game.activeUnits that will be what is used in draw.js
-* game.activeUnits will start out as all objects in game.units pool
-* when two units overlap one will remain and the other will not
+* (done) start out with pool.js file from turn based RPG example
+* (done) start out with a utils.js with the canvas method and various other tools such as wrapNumber
+* (done) new draw.js module started from the ground up
+* (done) new game.js file that will crate and return the game state of the art project
+* (done) the gameMod will contain an full object pool as game.units
+* (done) when two units overlap one will remain and the other will not
 
+* for each purged unit a mass value will transfer to the unit that remains

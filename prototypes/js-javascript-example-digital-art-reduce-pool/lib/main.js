e7b067655a31c4ef954b065a3de296d38698e8fa
dustinpfister <dustin.pfister@gmail.com>
2021-12-31 11:28:15 -0500

    started a draw module in main

--- prototypes/js-javascript-example-digital-art-reduce-pool/lib/main.js
+++ prototypes/js-javascript-example-digital-art-reduce-pool/lib/main.js
@@ -1,21 +1,33 @@
-var LIFESPAN = 7;
+
+var draw = {};
+
+draw.background = function (ctx, canvas) {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(-1, -1, canvas.width + 2, canvas.height + 2);
+};
 
 // state object
-var canvasObj = utils.createCanvas();
-
+var canvasObj = utils.createCanvas({
+    width: 640,
+    height: 480
+});
 var sm = {
-    lt : new Date(),
-    fps: 30
+    lt: new Date(),
+    fps: 30,
+    ctx: canvasObj.ctx,
+    canvas: canvasObj.canvas
 };
 
 // basic app loop
-var loop = function(){
+var loop = function () {
     var now = new Date(),
     secs = (now - sm.lt) / 1000;
     requestAnimationFrame(loop);
-    if(secs >= 1 / sm.fps){
-        
-    }
+    if (secs >= 1 / sm.fps) {
+		
+		draw.background(sm.ctx, sm.canvas);
+		
+	}
     sm.lt = now;
 };
 loop();

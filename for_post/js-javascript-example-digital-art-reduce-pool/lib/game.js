6b9606df002b50c6755f247f35c04dcea04985b3
dustinpfister <dustin.pfister@gmail.com>
2022-01-02 12:01:38 -0500

    clean up of game.js

--- for_post/js-javascript-example-digital-art-reduce-pool/lib/game.js
+++ for_post/js-javascript-example-digital-art-reduce-pool/lib/game.js
@@ -58,20 +58,14 @@ var gameMod = (function () {
                     if(uud.mode === 'move'){
                         uud.mode = 'transfer';
                         uud.transferTarget = obj;
-
-                
-
                         uud.a = Math.atan2(obj.y - underUnit.y, obj.x - underUnit.x) + Math.PI;
                         uud.d = utils.distance(underUnit.x, underUnit.y, obj.x, obj.y);
-
-
                         uud.m = uud.mass;
                     }
                 });
             }
             // if active count is 1, set this last move mode unit to splitup mode
             if(game.activeCount === 1){
-game.splitDelay = 3;
                 obj.data.mode = 'splitup';
             }
         }
@@ -97,43 +91,22 @@ game.splitDelay = 3;
             obj.data.alpha = obj.data.mass / 50;
             obj.data.alpha = obj.data.alpha > 1 ? 1 : obj.data.alpha;
             // update size on unit and target unit
-
-
             updateByMass(obj);
             updateByMass(target);
-
             // how to update heading and speed
-
             var x1 = obj.x + obj.w / 2,
             y1 = obj.y + obj.h / 2,
             x2 = target.x + target.w / 2,
             y2 = target.y + target.h / 2;
             var d = utils.distance(x1, y1, x2, y2);
             var a = Math.atan2(y2 - y1, x2 - x1);
-
             obj.heading = a;
+            // pps
             var per = d / UNIT_TRANSFER_MODE_MAX_DIST,
             per = per > 1 ? 1 : per;
             obj.pps = UNIT_TRANSFER_MODE_MAX_PPS * per;
-
+            // moce the unit
             moveUnit(game, obj, secs);
-
-
-/*
-            var d = obj.data.d,
-            a = obj.data.a,
-            per = obj.data.mass / obj.data.m;
-            obj.x = target.x + Math.cos(a) * (d * per);
-            obj.y = target.y + Math.sin(a) * (d * per);
-*/
-
-/*
-            var d = obj.data.d,
-            a = obj.data.a,
-            per = obj.data.mass / obj.data.m;
-            obj.x = (target.x + target.w / 2) + Math.cos(a) * (d * per);
-            obj.y = (target.y + target.h / 2) + Math.sin(a) * (d * per);
-*/
         }
     };
 
@@ -154,19 +127,6 @@ game.splitDelay = 3;
                     heading: 'random'
                 });
             }
-            // all active? then set all back to move mode
-//            if(game.activeCount === UNIT_COUNT){
-
-//game.splitDelay -= secs;
-
-//if(game.splitDelay <= 0){
-
-                //pool.objects.forEach(function(obj){
-                //    obj.data.mode = 'move';
-                //});
-//game.splitDelay = 3;
-//}
-//            }
             // update size and positon by mass
             updateByMass(obj);
         }
@@ -182,28 +142,18 @@ game.splitDelay = 3;
         obj.data.alpha = 1;
         // start mass
         obj.data.mass = spawnOpt.mass === undefined ? 50 : spawnOpt.mass;	
-
         // size and position
         var size = getSize(obj);
         obj.w = size;
         obj.h = size;
         // random pos from center
-        //var r = canvas.height * 0.4,
-        //a = Math.PI * 2 * Math.random();
-        //obj.x = canvas.width / 2 - obj.w / 2 + Math.cos(a) * r;
-        //obj.y = canvas.height / 2 - obj.h / 2 + Math.sin(a) * r;
-
         var r = canvas.height * 0.4,
         a = Math.PI * 2 * Math.random();
         obj.x = canvas.width / 2 - obj.w / 2 + Math.cos(a) * r;
         obj.y = canvas.height / 2 - obj.h / 2 + Math.sin(a) * r;
-        
-        
         // speed and heading
         obj.pps = 32 + Math.floor(64 * Math.random());
-        
         obj.heading = spawnOpt.heading || 'center';
-
         // heading to center
         if(typeof obj.heading === 'string'){
             if(obj.heading === 'center'){
@@ -213,8 +163,6 @@ game.splitDelay = 3;
                 obj.heading = Math.PI * 2 * Math.random();
             }
         }
-        
-        
     };
     // update a unit
     UNIT_OPT.update = function (obj, pool, game, secs) {
@@ -263,7 +211,9 @@ if(game.activeCount === UNIT_COUNT && game.units.objects[0].data.mode === 'split
         });
         game.splitDelay = 3;
     }
-} 
+}else{
+    game.splitDelay = 3;
+}
 
 
     };

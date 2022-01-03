517f65a28b759ca38907821a222cc99e4752937c
dustinpfister <dustin.pfister@gmail.com>
2022-01-03 15:07:56 -0500

    clean up

--- for_post/js-javascript-example-digital-art-reduce-pool/lib/game.js
+++ for_post/js-javascript-example-digital-art-reduce-pool/lib/game.js
@@ -186,29 +186,14 @@ var gameMod = (function () {
         update: function(obj, pool, game, secs){
             // move the unit
             moveUnit(game, obj, secs);
-            // if active count is below UNIT COUNT then spawn a new unit
+            // if active count is below UNIT COUNT then spawn new units
             if(game.activeCount < UNIT_COUNT){
-
                 poolMod.getActiveObjects(pool).forEach(function(aObj){
                    aObj.data.mass = UNIT_MASS_PER;
                 });
-
-
-
-// purge all first
-//poolMod.purgeAll(pool, game, {});
-        //poolMod.update(pool, secs, game);
-// then respawn all
                 var len = UNIT_COUNT - game.activeCount,
                 i = 0;
-
-                
                 while(i < len){
-
-                    //var hMass = Math.floor(obj.data.mass / 2),
-                    //r = obj.data.mass - hMass * 2;
-                    // this cuttent unit should be halfMass plus any remainder
-                    //obj.data.mass = hMass + r;
                     poolMod.spawn(game.units, game, {
                         mode: 'splitup',
                         mass: UNIT_MASS_PER,
@@ -218,7 +203,6 @@ var gameMod = (function () {
                     });
                     i += 1;
                 }
-
             }
             // update size and positon by mass
             updateByMass(obj);

227b971c34dbeb4c24970219c8d023e26e890703
dustinpfister <dustin.pfister@gmail.com>
2022-01-06 10:16:12 -0500

    todo

--- prototypes/js-javascript-example-digital-art-unsteady-stars/lib/draw.js
+++ prototypes/js-javascript-example-digital-art-unsteady-stars/lib/draw.js
@@ -26,6 +26,30 @@ var draw = (function(){
         // return gradiant
         return gradient;
     };
+    // draw direction helper
+    var strokeDirHelper = function(ctx, obj, dir, radiusBegin, radiusEnd){
+        radiusBegin = radiusBegin === undefined ? obj.r2 : radiusBegin;
+        radiusEnd = radiusEnd === undefined ? obj.r1 : radiusEnd;
+        ctx.beginPath();
+        ctx.moveTo(
+            obj.x + Math.cos(dir) * radiusBegin, 
+            obj.y + Math.sin(dir) * radiusBegin);
+        ctx.lineTo(
+            obj.x + Math.cos(dir) * radiusEnd,
+            obj.y + Math.sin(dir) * radiusEnd);
+        ctx.stroke();
+    };
+    // draw star info
+    var drawStarInfo = function(ctx, obj){
+        ctx.fillStyle = 'rgba(0,0,0,0.5)';
+        ctx.font = '10px arial';
+        ctx.textBaseline = 'top';
+        ctx.textAlign = 'left';
+        ctx.fillText('pos: ' + Math.floor(obj.x) + ', ' + Math.floor(obj.y), obj.x + 10, obj.y + 10);
+        ctx.fillText('pps: ' + Math.floor(obj.pps), obj.x + 10, obj.y + 20);
+        ctx.fillText('heading: ' + utils.radianToDegree(obj.heading), obj.x + 10, obj.y + 30);
+        ctx.fillText('facing: ' + utils.radianToDegree(obj.facing), obj.x + 10, obj.y + 40);
+    };
     // PUBLIC API METHODS
     var api = {};
     // draw the background
@@ -52,6 +76,78 @@ var draw = (function(){
             }
         });
         ctx.globalAlpha = 1;
+    };
+    // draw a star
+    api.star = function(ctx, obj, state){
+        ctx.lineWidth = 6;
+        ctx.globalAlpha = obj.alpha;
+        ctx.save();
+        ctx.translate(obj.x, obj.y);
+        ctx.rotate(obj.facing);
+        // new draw points method works greate with new star.create1 and star.create2 (0.3.0+)
+        api.points(ctx, obj.points, 0, 0);
+        ctx.restore();
+        // draw dir lines for heading and facing
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'rgba(255,255,255,0.4)';
+        strokeDirHelper(ctx, obj, obj.heading, obj.r1 * 0.5, obj.r1);
+        strokeDirHelper(ctx, obj, obj.facing, 0, obj.r1 * 0.5);
+        ctx.globalAlpha = 1;
+        if(state.selected){
+            drawStarInfo(ctx, state.selected);
+        }
+    };
+    // new draw points
+    api.points = function (ctx, points, cx, cy, opt) {
+        opt = opt || {};
+        ctx.save();
+        ctx.translate(cx, cy);
+        points.forEach(function (pointArray) {
+            var len = pointArray.length,
+            close = opt.close === undefined ? true : opt.close,
+            fill = opt.fill === undefined ? 'black' : opt.fill,
+            stroke = opt.stroke === undefined ? 'white' : opt.stroke,
+            lineWidth = opt.lineWidth === undefined ? 3 : opt.lineWidth,
+            el,
+            i = 2;
+            ctx.beginPath();
+            ctx.moveTo(pointArray[0], pointArray[1]);
+            while (i < len) {
+                el = pointArray[i];
+                if (typeof el === 'number') {
+                    ctx.lineTo(el, pointArray[i + 1]);
+                    i += 2;
+                } else {
+                    var parts = el.split(':');
+                    if (parts[0] === 'close') {
+                        close = parts[1] === 'true' ? true : false;
+                    }
+                    if (parts[0] === 'stroke') {
+                        stroke = parts[1] || false;
+                    }
+                    if (parts[0] === 'fill') {
+                        fill = parts[1] || false;
+                    }
+                    if (parts[0] === 'lineWidth') {
+                        lineWidth = parts[1] || 1;
+                    }
+                    i += 1;
+                }
+            }
+            ctx.lineWidth = lineWidth;
+            if (close) {
+                ctx.closePath();
+            }
+            if (fill) {
+                ctx.fillStyle = fill;
+                ctx.fill();
+            }
+            if (stroke) {
+                ctx.strokeStyle = stroke;
+                ctx.stroke();
+            }
+        });
+        ctx.restore();
     };
     // draw version number
     api.info = function (sm, ctx, canvas) {
--- prototypes/js-javascript-example-digital-art-unsteady-stars/todo.md
+++ prototypes/js-javascript-example-digital-art-unsteady-stars/todo.md
@@ -4,9 +4,20 @@
 
 <!-- Additional Features -->
 
+### () - r2 - Background Gird
+
 <!-- General Idea or MVP of Project done -->
 
-### () - r0 - First revision
-* start with the source code from r5 of reduce pool digital art example
-* I will need a whole new game.js file 
-* I will need to revise any and all other files
+### () - r1 - expand star.js with 'unsteady stars'
+* Add methods to create and return an 'unsteady stars' object
+* The methods can still return a object that will work with draw.points buy with addital data attached by way of named keys
+* Add update methods to star.js that can be used to mutate, reset, ect one of these 'unsteady stars'
+
+### () - r0 - Start with source code from various other projects
+* (done) start with the source code from r5 of reduce pool digital art example
+* (done) start with the star.js file from my canvas-example-star project
+* (done) expand draw.js with methods from the draw.js file in canvas-example-star
+* new game.js file where the over all state for now is just an object pool with the objects just moving around randomly
+* use star.js to create an array of points for each display object in the main units pool
+* update the draw.pool method to draw and points array, star or otherwise
+* update main.js with any needed changes

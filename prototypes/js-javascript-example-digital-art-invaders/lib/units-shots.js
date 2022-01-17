9c40aefecd785a41bf9bf6b65f33b9874647b7dd
dustinpfister <dustin.pfister@gmail.com>
2022-01-17 14:14:48 -0500

    shell blast

--- prototypes/js-javascript-example-digital-art-invaders/lib/units-shots.js
+++ prototypes/js-javascript-example-digital-art-invaders/lib/units-shots.js
@@ -53,6 +53,20 @@ unitsMod.load( (function () {
        },
        onHit : function(unit, pool, game, secs, hitObjects){},
        onEndPoint: function(unit, pool, game, secs){
+		   var uDat = unit.data;
+		   // blast!
+		    unit.w = 128
+			unit.h = 128;
+            var hitObjects = poolMod.getOverlaping(unit, uDat.hitPool);
+			
+			hitObjects.forEach(function(target){
+                target.data.hp -= uDat.attack;
+                target.data.hp = target.data.hp < 0 ? 0 : target.data.hp;
+                if(target.data.hp === 0){
+                    poolMod.purge(target, game);
+                }
+           });
+		   
            poolMod.purge(unit, game);
        }
    };
@@ -108,12 +122,12 @@ unitsMod.load( (function () {
             if(uDat.subTypeObj.endPointCheck){
                // is the distance from the start point greater than that distance to the end point?
                var de = utils.distance(uDat.sx, uDat.sy, uDat.ex, uDat.ey);
-			   if(ds >= de){
-				   
+               if(ds >= de){
+                   
                    unit.x = uDat.sx + Math.cos(unit.heading) * de;
                    unit.y = uDat.sy + Math.sin(unit.heading) * de;
-				   uDat.subTypeObj.onEndPoint(unit, pool, game, secs);
-			   }
+                   uDat.subTypeObj.onEndPoint(unit, pool, game, secs);
+               }
             }
             // check hit pool and switch to hitMode if one or more objects where hit
             if(uDat.hitPool && uDat.subTypeObj.hitCheck){

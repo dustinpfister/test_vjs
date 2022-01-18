937cf1aaaf0956a06ca1d7f081b4c7fd71276b18
dustinpfister <dustin.pfister@gmail.com>
2022-01-18 13:54:52 -0500

    draw.ver

--- for_post/js-javascript-example-draw-points/lib/draw.js
+++ for_post/js-javascript-example-draw-points/lib/draw.js
@@ -1,6 +1,22 @@
-var draw = {};
+var draw = (function(){
 
-draw.background = function(ctx, canvas, style){
-    ctx.fillStyle = style || 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-};
+    var api = {};
+
+    // draw a background
+    api.background = function(ctx, canvas, style){
+        ctx.fillStyle = style || 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    };
+
+    // draw version number
+    api.ver = function (sm, ctx, canvas) {
+        ctx.fillStyle = DEFAULT_TEXT_COLOR;
+        ctx.textBaseline = 'top';
+        ctx.textAlign = 'left';
+        ctx.font = '14px arial';
+        ctx.fillText('version: ' + sm.ver, 5, canvas.height - 15);
+    };
+
+    return api;
+
+}());

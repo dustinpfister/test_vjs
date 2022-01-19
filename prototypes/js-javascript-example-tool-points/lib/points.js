3ad4099bb6d25b0f00fb5f34a23bd24f7586c905
dustinpfister <dustin.pfister@gmail.com>
2022-01-19 12:41:34 -0500

    create box method working great

--- prototypes/js-javascript-example-tool-points/lib/points.js
+++ prototypes/js-javascript-example-tool-points/lib/points.js
@@ -2,29 +2,30 @@ var pointMod = (function(){
 
     var api = {};
 
-// create box method
-api.createBox = function(opt){
-    // parse options
-    opt = utils.defaults(opt, {
-        x: 0, y: 0, w: 50, h: 50
-    });
-    var points = [[]],
-    i = 0,
-    len = 4,
-    hw = opt.w / 2,
-    hh = opt.h / 2;
-    // push points
-    while(i < len){
-        var pi = Math.floor(i / 2),
-        x = (opt.x - hw) + pi % 2 * opt.w,
-        y = (opt.y - hh) + Math.floor(pi / 2) * opt.h;
-        points[0].push(x, y);
-        i += 2;
-    }
-    // return box points
-    return points;
-};
-
+    // create box method
+    api.createBox = function(opt){
+        // parse options
+        opt = opt || {};
+        opt = utils.defaults(opt, {
+            x: 0, y: 0, w: 50, h: 50
+        });
+        // push points
+        var points = [[]],
+        i = 0,
+        len = 2 * 4,
+        hw = opt.w / 2,
+        hh = opt.h / 2;
+        while(i < len){
+            var pi = Math.floor(i / 2),
+            yi = Math.floor(pi / 2),
+            y = (opt.y - hh) + yi * opt.h,
+            x = (opt.x - hw) + ( yi === 0 ? pi % 2 : 1 - pi % 2 ) * opt.w;
+            points[0].push(x, y);
+            i += 2;
+        }
+        // return box points
+        return points;
+    };
 
     // return the public api
     return api;

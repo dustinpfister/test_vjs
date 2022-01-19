890dc75a9be17818e43fab6d15f62a633eff75c7
dustinpfister <dustin.pfister@gmail.com>
2022-01-19 12:30:48 -0500

    points.js and createBox

--- prototypes/js-javascript-example-tool-points/lib/points.js
+++ prototypes/js-javascript-example-tool-points/lib/points.js
@@ -2,10 +2,27 @@ var pointMod = (function(){
 
     var api = {};
 
+// create box method
 api.createBox = function(opt){
-
-
-
+    // parse options
+    opt = utils.defaults(opt, {
+        x: 0, y: 0, w: 50, h: 50
+    });
+    var points = [[]],
+    i = 0,
+    len = 4,
+    hw = opt.w / 2,
+    hh = opt.h / 2;
+    // push points
+    while(i < len){
+        var pi = Math.floor(i / 2),
+        x = (opt.x - hw) + pi % 2 * opt.w,
+        y = (opt.y - hh) + Math.floor(pi / 2) * opt.h;
+        points[0].push(x, y);
+        i += 2;
+    }
+    // return box points
+    return points;
 };
 
 

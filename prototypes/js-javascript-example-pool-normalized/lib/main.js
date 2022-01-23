7323a33bdf10f8f1e0549ecda71eb54fc5d9f71b
dustinpfister <dustin.pfister@gmail.com>
2022-01-23 15:35:04 -0500

    new simple pool normalized example

--- prototypes/js-javascript-example-pool-normalized/lib/main.js
+++ prototypes/js-javascript-example-pool-normalized/lib/main.js
@@ -3,12 +3,42 @@ var canvasObj = utils.createCanvas({
         width: 640,
         height: 480
     });
+// pool options object
+var POOL_OPT = {
+    count: 10,
+    disableLifespan: true
+};
+// on spawn
+POOL_OPT.spawn = function (unit, pool, sm, spawnOpt) {
+        spawnOpt = spawnOpt || {};
+        var canvas = sm.canvas;
+        // usere defined data for this example
+        var uDat = unit.data;
+        uDat.fillStyle = '#aa0000';
+        uDat.alpha = 1;
+        uDat.size = 32;
+        // size and pos
+        unit.w = uDat.size;
+        unit.h = uDat.size;
+        unit.x = canvas.width / 2;
+        unit.y = canvas.height / 2;
+        // heading
+        unit.heading = Math.PI * 2 * Math.random();
+        // speed
+        unit.pps = 32 + 128 * Math.random();
+};
+// update a unit
+POOL_OPT.update = function (unit, pool, sm, secs) {
+    poolMod.moveByPPS(unit, secs);
+    poolMod.wrap(unit, sm.canvas, unit.data.size)
+};
+
 // main sm object
 var sm = {
     ver: 'r0',
     lt: new Date(),
     fps: 30,
-    game: null,
+    pool: poolMod.create(POOL_OPT),
     ctx: canvasObj.ctx,
     canvas: canvasObj.canvas,
     background: {
@@ -17,24 +47,23 @@ var sm = {
        radius: 200
     }
 };
-// set up game object
-sm.game = gameMod.create({
-        sm: sm
-    });
+
+// spawn all
+poolMod.spawnAll(sm.pool, sm, {})
+
 // basic app loop
 var loop = function () {
     var now = new Date(),
     secs = (now - sm.lt) / 1000;
     requestAnimationFrame(loop);
     if (secs >= 1 / sm.fps) {
-        // update game
-        gameMod.update(sm.game, secs);
+        // update pool
+        poolMod.update(sm.pool, secs, sm);
         // draw
         draw.background(sm, sm.ctx, sm.canvas);
-        draw.pool(sm, sm.game.buildings, sm.ctx);
-        draw.pool(sm, sm.game.attackers, sm.ctx);
-        draw.pool(sm, sm.game.shots, sm.ctx);
+        draw.pool(sm, sm.pool, sm.ctx);
         draw.ver(sm, sm.ctx, sm.canvas);
+        // set sm.lt to now
         sm.lt = now;
     }
 };

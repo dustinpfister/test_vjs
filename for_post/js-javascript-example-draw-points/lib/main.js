f06ff87881081d8d89c9ac10cc78fcb177138cf7
dustinpfister <dustin.pfister@gmail.com>
2022-01-23 13:14:01 -0500

    draw points

--- for_post/js-javascript-example-draw-points/lib/main.js
+++ for_post/js-javascript-example-draw-points/lib/main.js
@@ -6,22 +6,30 @@ canvas.width = 640;
 canvas.height = 480;
 
 var sm = {
-   ver: 'r3'
+   ver: 'r3',
+   objects: []
 };
 // using the pointMod.createEllipse method
-var ellipse = pointMod.createEllipse({
+sm.objects.push(pointMod.createEllipse({
     points: 40,
     r1: 300,
-    r2: 75
-});
+    r2: 75,
+    x: canvas.width / 2,
+    y: canvas.height / 2
+}));
 // creating a points object manually
-var arr = [
+sm.objects.push([
     [25, 75, 175, 50, 17, 110, 'fill:red', 'stroke:lime'],
     [100, 20, 165, 25, 22, 130, 300, 130, 300, 20, 'fill:false', 'close:false']
-];
+]);
 // drawing
 draw.background(ctx, canvas, 'blue');
+
+sm.objects.forEach(function(points){
+    draw.points(ctx, points, 0, 0);
+});
+
 // using the draw points method
-draw.points(ctx, ellipse, canvas.width / 2, canvas.height / 2);
-draw.points(ctx, arr, 100, 10);
+//draw.points(ctx, ellipse, canvas.width / 2, canvas.height / 2);
+//draw.points(ctx, arr, 100, 10);
 draw.ver(sm, ctx, canvas);

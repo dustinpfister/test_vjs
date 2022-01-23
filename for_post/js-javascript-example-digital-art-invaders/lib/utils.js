7e18abbd2e87a6728d9590512aa14e2633c827ec
dustinpfister <dustin.pfister@gmail.com>
2022-01-23 08:11:45 -0500

    removed unused methods from utils.js

--- for_post/js-javascript-example-digital-art-invaders/lib/utils.js
+++ for_post/js-javascript-example-digital-art-invaders/lib/utils.js
@@ -71,27 +71,12 @@ utils.boundingBox = function (x1, y1, w1, h1, x2, y2, w2, h2) {
         x1 + w1 < x2 ||
         x1 > x2 + w2);
 };
-// chunk and array
-/*
-utils.chunk = function (arr, size) {
-    var chunkedArr = [];
-    arr = arr || [];
-    size = size === undefined ? 1 : size;
-    for (var i = 0; i < arr.length; i += size) {
-        chunkedArr.push(arr.slice(i, i + size));
-    }
-    return chunkedArr;
-};
-*/
 //******** **********
 //  ANGLES
 //******** **********
 // PI * 2
 utils.PI2 = Math.PI * 2;
 // unit conversion
-utils.radToDeg = function(n){
-    return utils.mod(n, utils.PI2) / utils.PI2 * 360;
-};
 utils.degToRad = function(n){
    return utils.mod(n, 360) / 360 * utils.PI2;
 };
@@ -100,32 +85,4 @@ utils.normalizeHalf = function (n, scale) {
     var c = scale || utils.PI2,
     h = c / 2;
     return utils.mod(n + h, c) - h;
-};
-// the angular distance between two angles
-utils.angleDistance = function (a, b, scale) {
-    var m = scale || utils.PI2,
-    h = m / 2,
-    diff = utils.normalizeHalf(a - b);
-    if (diff > h) {
-        diff = diff - m;
-    }
-    return utils.mod( Math.abs(diff), m);
-};
-// get the angle from one point to another
-utils.getAngleToPoint = function (pt1, pt2, scale) {
-    var a = Math.atan2(pt1.y - pt2.y, pt1.x - pt2.x);
-    return utils.normalizeHalf(a, scale || utils.PI2);
-};
-// get -1, 1, or 0 depending on the the state of two angles
-utils.shortestAngleDirection = function (a1, a2, scale) {
-    var z = a1 - a2,
-    x = utils.normalizeHalf(z, scale || utils.PI2);
-    if (x < 0) {
-        return -1; // Left
-    }
-    if (x > 0) {
-        return 1; // Right
-    }
-    // if a1 === a2 or any other case
-    return 0;
 };

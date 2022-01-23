42b54cb68615bb84e56ab59e6f74a87af523fe3c
dustinpfister <dustin.pfister@gmail.com>
2022-01-23 13:11:04 -0500

    deleted old code

--- for_post/js-javascript-example-draw-points/lib/main.js
+++ for_post/js-javascript-example-draw-points/lib/main.js
@@ -25,37 +25,3 @@ draw.background(ctx, canvas, 'blue');
 draw.points(ctx, ellipse, canvas.width / 2, canvas.height / 2);
 draw.points(ctx, arr, 100, 10);
 draw.ver(sm, ctx, canvas);
-
-
-/*
-// literal of a points array
-var points = [
-    [25, 75, 175, 50, 17, 210, 'fill:red', 'stroke:lime'],
-    [100, 20, 165, 55, 22, 200, 'fill:false']
-];
-
-// method that will create a points array
-var demoMethod = function () {
-    var i = 0,
-    radius = 100,
-    radian,
-    arr,
-    x,
-    y,
-    count = 8,
-    points = [];
-    while (i < count) {
-        radian = Math.PI * 2 / count * i;
-        x = Math.cos(radian) * radius;
-        y = Math.sin(radian) * radius;
-        arr = [Math.round(x), Math.round(y), 0, 0, 'stroke:white', 'close:false', 'lineWidth:' + (2 + i * 2)];
-        points.push(arr);
-        i += 1;
-    }
-    return points
-};
-*/
-//draw.background(ctx, canvas, 'blue');
-//draw.points(ctx, points, 80, 5);
-//draw.ver(sm, ctx, canvas);
-//draw.points(ctx, demoMethod(), 300, 150);

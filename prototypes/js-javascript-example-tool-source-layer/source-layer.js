9be8544c336a6330de1f7cca14971f6d9e10d0dc
dustinpfister <dustin.pfister@gmail.com>
2022-01-24 14:32:20 -0500

    drawing to the canvas

--- prototypes/js-javascript-example-tool-source-layer/source-layer.js
+++ prototypes/js-javascript-example-tool-source-layer/source-layer.js
@@ -10,6 +10,19 @@ var sourceLayer = (function(){
         return null
     };
 
+    var draw = function(source){
+        var canvas = source.canvas,
+        ctx = source.ctx;
+        // clear source layer
+        ctx.clearRect(-1, -1 , canvas.width + 2, canvas.height + 2);
+        // draw source image to layer with current settings
+        //ctx.drawImage(source.image, source.sx, source.sy, source.sw, source.sh, source.dx, source.dy, source.dw, source.dh);
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0,0, 100, 100);
+
+    };
+
     var api = {};
 
     api.create = function(opt){
@@ -20,11 +33,13 @@ var sourceLayer = (function(){
             zoom: 1,
             radian: 0,
             image: null,
-            sx: 0, sy: 0, sw: 32, sh: 32, sx: 0, dx: 0, dw: 32, dh: 32,
+            sx: 0, sy: 0, sw: 100, sh: 100, sx: 0, dx: 0, dw: 100, dh: 100,
             onUpdate: opt.onUpdate || function(source){}
         };
         source.canvas = resolveElRef(opt.canvas);
         if(source.canvas){
+            source.canvas.width = opt.width;
+            source.canvas.height = opt.height;
             source.ctx = source.canvas.getContext('2d');
         }
         return source;
@@ -42,6 +57,7 @@ var sourceLayer = (function(){
                 var img = source.image = new Image();
                 img.src = reader.result;
                 img.addEventListener('load', function(){
+                    draw(source);
                     source.onUpdate.call(source, source);
                 });
             }, false);

bdb25b34ad1f9b4754bff8418a4d1f4bab561588
dustinpfister <dustin.pfister@gmail.com>
2022-01-24 14:12:54 -0500

    resloveElRef helper

--- prototypes/js-javascript-example-tool-source-layer/source-layer.js
+++ prototypes/js-javascript-example-tool-source-layer/source-layer.js
@@ -1,5 +1,15 @@
 var sourceLayer = (function(){
 
+    var resolveElRef = function(elRef){
+        if(typeof elRef === 'object' && elRef != null){
+            return elRef
+        }
+        if(typeof elRef === 'string'){
+            return document.querySelector(elRef);
+        }
+        return null
+    };
+
     var api = {};
 
     api.create = function(opt){
@@ -12,6 +22,8 @@ var sourceLayer = (function(){
             image: null,
             sx: 0, sy: 0, sw: 32, sh: 32, sx: 0, dx: 0, dw: 32, dh: 32
         };
+        source.canvas = resolveElRef(opt.canvas)
+/*
         if(opt.canvas){
             if(typeof opt.canvas === 'object' && opt.canvas != null){
                 source.canvas = opt.canvas;
@@ -20,10 +32,39 @@ var sourceLayer = (function(){
                 source.canvas = document.querySelector(opt.canvas);
             }
         }
+*/
         if(source.canvas){
             source.ctx = source.canvas.getContext('2d');
         }
         return source;
+
+    };
+
+    api.appendImageHandler = function(source, fileEl){
+
+
+
+bgImageInput.addEventListener('change', function(e){
+   var files = e.target.files,
+   file = files[0];
+   var reader = new FileReader();
+   reader.addEventListener('load', function () {
+
+      var img = sm.background.image = new Image();
+      img.src = reader.result;
+      img.addEventListener('load', function(){
+          // parse background with image and canvas
+          sm.background = draw.BGParseOpt(sm.background, canvas);
+          // set current state
+          setState(sm, sm.currentState);
+      });
+
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+});
 
     };
 

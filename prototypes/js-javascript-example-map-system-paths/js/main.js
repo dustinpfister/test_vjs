f902d82edb6bdcdec8711bf2ccf7dcf44c266fbf
dustinpfister <dustin.pfister@gmail.com>
2022-01-28 15:31:36 -0500

    setting current index

--- prototypes/js-javascript-example-map-system-paths/js/main.js
+++ prototypes/js-javascript-example-map-system-paths/js/main.js
@@ -59,20 +59,18 @@ sm.canvas.addEventListener('click', function(e){
     // if build menu clicked
     var bm = sm.buildMenu,
     w = bm.cellSize * bm.w,
-    h = bm.cellSize * bm.buttons.length / bm.w,
-    i;
+    h = bm.cellSize * bm.buttons.length / bm.w;
     if(utils.boundingBox( bm.x, bm.y, w, h, pos.x, pos.y, 1, 1 )){
-   
         var x = Math.floor((pos.x - bm.x) / bm.cellSize);
         var y = Math.floor((pos.y - bm.y) / bm.cellSize);
-        i = y * bm.w + x;
-
-console.log(i);
-
+        var i = y * bm.w + x;
+        var button = bm.buttons[i];
+        if(button){
+            bm.currentIndex = i;
+            console.log(button);
+        }
     }
     
-
-
     render(sm);
 
 });

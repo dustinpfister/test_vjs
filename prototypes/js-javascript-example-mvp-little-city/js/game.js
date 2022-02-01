056f60efa143e9637401e6113298e6b2230f7637
dustinpfister <dustin.pfister@gmail.com>
2022-02-01 13:19:10 -0500

    new start city

--- prototypes/js-javascript-example-mvp-little-city/js/game.js
+++ prototypes/js-javascript-example-mvp-little-city/js/game.js
@@ -31,7 +31,7 @@ var gameMod = (function(){
         opt.cellData = opt.cellData || [];
         var game = {
             hardSet: hardSet,
-            money: 300,
+            money: 3000,
             population: 0,
             year: 1900,
             secsPerYear: 10,
--- prototypes/js-javascript-example-mvp-little-city/states/init.js
+++ prototypes/js-javascript-example-mvp-little-city/states/init.js
@@ -48,12 +48,12 @@ smMod.load({
 
         // create game object
         sm.game = gameMod.create({
+            /*
             cellData: [
                 { x: 3, y: 2, unitKey: 'road'},
                 { x: 3, y: 3, unitKey: 'road'},
                 { x: 3, y: 4, unitKey: 'road'},
                 { x: 3, y: 5, unitKey: 'road'},
-
                 { x: 4, y: 1, unitKey: 'road'},
                 { x: 5, y: 1, unitKey: 'road'},
                 { x: 6, y: 1, unitKey: 'road'},
@@ -61,19 +61,48 @@ smMod.load({
                 { x: 2, y: 1, unitKey: 'road'},
                 { x: 1, y: 1, unitKey: 'road'},
                 { x: 0, y: 1, unitKey: 'road'},
-
-
                 { x: 2, y: 3, unitKey: 'res'},
                 { x: 2, y: 5, unitKey: 'res'},
-
                 { x: 0, y: 3, unitKey: 'res'},
                 { x: 6, y: 3, unitKey: 'res'},
                 { x: 7, y: 3, unitKey: 'res'},
-
-
                 { x: 4, y: 3, unitKey: 'com'},
                 { x: 1, y: 0, unitKey: 'com'},
                 { x: 0, y: 0, unitKey: 'com'}
+            ]*/
+            cellData: [
+                { x: 0, y: 1, unitKey: 'road'},
+                { x: 1, y: 1, unitKey: 'road'},
+                { x: 2, y: 1, unitKey: 'road'},
+                { x: 3, y: 1, unitKey: 'road'},
+                { x: 3, y: 2, unitKey: 'road'},
+                { x: 3, y: 3, unitKey: 'road'},
+                { x: 3, y: 4, unitKey: 'road'},
+                { x: 4, y: 4, unitKey: 'road'},
+                { x: 5, y: 4, unitKey: 'road'},
+                { x: 6, y: 4, unitKey: 'road'},
+                { x: 7, y: 4, unitKey: 'road'},
+                { x: 7, y: 3, unitKey: 'road'},
+                { x: 7, y: 2, unitKey: 'road'},
+                { x: 7, y: 1, unitKey: 'road'},
+                { x: 7, y: 0, unitKey: 'road'},
+
+                { x: 2, y: 2, unitKey: 'com'},
+                { x: 2, y: 3, unitKey: 'com'},
+                { x: 2, y: 4, unitKey: 'com'},
+                { x: 2, y: 5, unitKey: 'com'},
+                { x: 2, y: 6, unitKey: 'com'},
+
+                { x: 4, y: 3, unitKey: 'res'},
+                { x: 5, y: 3, unitKey: 'res'},
+                { x: 6, y: 3, unitKey: 'res'},
+                { x: 6, y: 2, unitKey: 'res'},
+
+                { x: 8, y: 0, unitKey: 'res'},
+                { x: 9, y: 0, unitKey: 'res'},
+                { x: 8, y: 1, unitKey: 'res'},
+                { x: 9, y: 1, unitKey: 'res'}
+
             ]
         });
 

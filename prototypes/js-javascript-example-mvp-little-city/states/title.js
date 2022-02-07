6b72924941185e1bddf59a6e6870f677b08caeae
dustinpfister <dustin.pfister@gmail.com>
2022-02-07 11:08:28 -0500

    place holder title text

--- prototypes/js-javascript-example-mvp-little-city/states/title.js
+++ prototypes/js-javascript-example-mvp-little-city/states/title.js
@@ -2,11 +2,23 @@ smMod.load({
     stateKey: 'title',
     update: function(sm, secs){
 
-console.log('title state...')
+        //console.log('title state...')
 
         // switch to gameBuild state
-        smMod.setState(sm, 'createNew');
+        //smMod.setState(sm, 'createNew');
     },
-    draw: function(sm, ctx, canvas){},
+    draw: function(sm, ctx, canvas){
+        var sDat = sm.stateObj.data;
+        draw.back(sm);
+
+        ctx.fillStyle = 'white';
+        ctx.font = '25px arial';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText('Little City MVP', sm.canvas.width * 0.5, sm.canvas.height * 0.25);
+
+
+        draw.ver(sm);
+    },
     events: { click: function(e, pos, sm){}}
 });

5ab61d9a2fe0467abf04b99c2d8106be3e9c82a7
dustinpfister <dustin.pfister@gmail.com>
2022-02-07 11:31:57 -0500

    clean up of title state

--- prototypes/js-javascript-example-mvp-little-city/states/title.js
+++ prototypes/js-javascript-example-mvp-little-city/states/title.js
@@ -1,41 +1,26 @@
 smMod.load({
     stateKey: 'title',
-    update: function(sm, secs){
-
-        //console.log('title state...')
-
-        // switch to gameBuild state
-        //smMod.setState(sm, 'createNew');
-    },
+    update: function(sm, secs){},
     draw: function(sm, ctx, canvas){
         var sDat = sm.stateObj.data;
         draw.back(sm);
 
         ctx.fillStyle = 'white';
-        ctx.font = '25px arial';
+        ctx.font = '60px arial';
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
         ctx.fillText('Little City MVP', sm.canvas.width * 0.5, sm.canvas.height * 0.25);
 
-
         draw.menu(sm, sm.titleMenu);
-
         draw.ver(sm);
     },
     events: { 
-
          click: function(e, pos, sm){
-
-                // if title menu clicked
-                var button = smMod.gridMenu.click(sm.titleMenu, pos);
-
-console.log(button)
-
-                if(button){
-                    smMod.setState(sm, button.action)
-                }
-
-
+            // if title menu clicked
+            var button = smMod.gridMenu.click(sm.titleMenu, pos);
+            if(button){
+                smMod.setState(sm, button.action)
+            }
         }
     }
 });

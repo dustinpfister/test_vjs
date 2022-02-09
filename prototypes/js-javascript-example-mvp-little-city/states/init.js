43afb1c7fccb6b4890e9ea7bd274258fd60efdce
dustinpfister <dustin.pfister@gmail.com>
2022-02-09 13:08:06 -0500

    clean start of cells in init state

--- prototypes/js-javascript-example-mvp-little-city/states/init.js
+++ prototypes/js-javascript-example-mvp-little-city/states/init.js
@@ -117,41 +117,7 @@ smMod.load({
 
         // create game object
         sm.game = gameMod.create({
-            cellData: [
-                { x: 0, y: 1, unitKey: 'road'},
-                { x: 1, y: 1, unitKey: 'road'},
-                { x: 2, y: 1, unitKey: 'road'},
-                { x: 3, y: 1, unitKey: 'road'},
-                { x: 3, y: 2, unitKey: 'road'},
-                { x: 3, y: 3, unitKey: 'road'},
-                { x: 3, y: 4, unitKey: 'road'},
-                { x: 4, y: 4, unitKey: 'road'},
-                { x: 5, y: 4, unitKey: 'road'},
-                { x: 6, y: 4, unitKey: 'road'},
-                { x: 7, y: 4, unitKey: 'road'},
-                { x: 7, y: 3, unitKey: 'road'},
-                { x: 7, y: 2, unitKey: 'road'},
-                { x: 7, y: 1, unitKey: 'road'},
-                { x: 7, y: 0, unitKey: 'road'},
-
-                { x: 2, y: 2, unitKey: 'com'},
-                { x: 2, y: 3, unitKey: 'com'},
-                { x: 2, y: 4, unitKey: 'com'},
-                { x: 2, y: 5, unitKey: 'com'},
-                { x: 2, y: 6, unitKey: 'com'},
-
-                { x: 4, y: 3, unitKey: 'res'},
-                { x: 5, y: 3, unitKey: 'res'},
-                { x: 6, y: 3, unitKey: 'res'},
-                { x: 6, y: 2, unitKey: 'res'},
-
-                { x: 8, y: 0, unitKey: 'res'},
-                { x: 9, y: 0, unitKey: 'res'},
-                { x: 8, y: 1, unitKey: 'res'},
-                { x: 9, y: 1, unitKey: 'res'}
-
-            ]
-
+            cellData: []
         });
 
         // switch to gameBuild state

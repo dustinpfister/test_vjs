2ffde646d8314f6f9d00810da6d353093ffeb72e
dustinpfister <dustin.pfister@gmail.com>
2022-02-16 16:03:13 -0500

    setting save index

--- prototypes/js-javascript-example-mvp-little-city/states/create-from-save.js
+++ prototypes/js-javascript-example-mvp-little-city/states/create-from-save.js
@@ -1,31 +1,28 @@
 smMod.load({
     stateKey: 'createFromSave',
-    data: {
-    },
+    data: {},
     update: function(sm, secs){},
     draw: function(sm, ctx, canvas){
         var sDat = sm.stateObj.data;
         draw.back(sm);
-
         draw.menu(sm, sm.slotMenu);
-
         draw.ver(sm);
-
     },
     events: { 
         click: function(e, pos, sm){
             var sDat = sm.stateObj.data;
-
             var button = smMod.gridMenu.click(sm.slotMenu, pos);
             if(button){
                 var gameOpt = sm.saves.slots[button.index];
                 if(gameOpt){
-                    sm.game =  gameMod.create(gameOpt);
-                            smMod.setState(sm, 'gameBuild');
+                    // make sure the current index is set to this slot
+                    sm.saves.currentIndex = parseInt(button.index);
+                    // create new game object with the loaded gameOpt object and switch
+                    // to the gameBuilt state
+                    sm.game = gameMod.create(gameOpt);
+                    smMod.setState(sm, 'gameBuild');
                 }
             }
-
-
         }
     }
 });

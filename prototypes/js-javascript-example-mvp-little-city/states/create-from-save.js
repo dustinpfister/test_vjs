029fb4af666c157aee87db069dffc3d941873bd7
dustinpfister <dustin.pfister@gmail.com>
2022-02-16 15:30:01 -0500

    drawing and clicking slot menu

--- prototypes/js-javascript-example-mvp-little-city/states/create-from-save.js
+++ prototypes/js-javascript-example-mvp-little-city/states/create-from-save.js
@@ -6,12 +6,20 @@ smMod.load({
     draw: function(sm, ctx, canvas){
         var sDat = sm.stateObj.data;
         draw.back(sm);
+
+        draw.menu(sm, sm.slotMenu);
+
         draw.ver(sm);
 
     },
     events: { 
         click: function(e, pos, sm){
             var sDat = sm.stateObj.data;
+
+            var button = smMod.gridMenu.click(sm.slotMenu, pos);
+            if(button){
+console.log(button)
+            }
 
 
         }
